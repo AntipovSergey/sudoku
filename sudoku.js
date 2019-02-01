483e8ee8b180f7029bacce60e286391b0a0bac15
beshunmo <v.khvan@outlook.com>
2019-02-01 16:16:37 +0300

    r1 vladimir

--- sudoku.js
+++ sudoku.js
@@ -1,46 +1,30 @@
-// Takes a board as a string in the format
-// you see in the puzzle file. Returns
-// something representing a board after
-// your solver has tried to solve it.
-// How you represent your board is up to you!
+//Принимает строку. Возвращает исходный массив
 function solve(boardString) {
 	let count = 0;
 	let str = [];
 	let boardFirst = [];
-	for(let i = 0; i <= boardString.length; i++){
-		if(count == 9) {
+	for (let i = 0; i <= boardString.length; i++) {
+		if (count === 9) {
+			for (let j = 0; j < str.length; j++) {
+				if (str[j] !== '-') {
+					str[j] = parseInt(str[j]);
+				}
+			}
 			boardFirst.push(str.slice(0, 9))
 			count = 0;
 			str.splice(0, 81);
+
+			
 		}
 		str.push(boardString[i]);
 		count++;
-	}	
+	}
 	return boardFirst
-}
-
-
-// Returns a boolean indicating whether
-// or not the provided board is solved.
-// The input board will be in whatever
-// form `solve` returns.
-function isSolved(board) {
-
-}
-
-
-// Takes in a board in some form and
-// returns a String that's well formatted
-// for output to the screen.
-// The input board will be in whatever
-// form `solve` returns.
-function prettyBoard(board) {
-
 }
 
 //Переворачивает матрицу
 function transpose(arr) {
-    return a[0].map(function (_, c) { return a.map(function (r) { return r[c]; }); });
+	return arr[0].map(function (_, c) { return arr.map(function (r) { return r[c]; }); });
 }
 
 //Принимает i-й массив. Определяет есть ли число в строке.
@@ -60,40 +44,54 @@ function includeInSquare(arrSquare, num) {
 	return arrSquare.includes(num);
 }
 
-//Принимает исходный массив. Расставляет числа по ячейкам.
-function setNumbers(arr) {
-	for (let i = 0; i < arr.length; i++) {
-		
-	}
-
-	return newArr;
-}
-
-function parseBoard(arr) {
-
-	return arr.split('');
-}
-
-
-module.exports = {
-	solve: solve,
-	isSolved: isSolved,
-	prettyBoard: prettyBoard
-}
-
+//Принимает исходный массив. Возвращает массив со значениями в квадрате
 function getSquare(arr, row, col) {
 	let newArr = [];
 	xSquare = Math.floor(row / 3)
 	ySquare = Math.floor(col / 3)
-  
+
 	for (let r = 0; r < 9; r++) {
-	  if (Math.floor(r / 3) === xSquare) {
-		for (let c = 0; c < 9; c++) {
-		  if (Math.floor(c / 3) === ySquare) {
-			newArr.push(arr[r][c])
-		  }
+		if (Math.floor(r / 3) === xSquare) {
+			for (let c = 0; c < 9; c++) {
+				if (Math.floor(c / 3) === ySquare) {
+					newArr.push(arr[r][c])
+				}
+			}
 		}
-	  }
 	}
 	return newArr;
-  }
+}
+
+//Принимает исходный массив. Расставляет числа по ячейкам.
+function setNumbers(arr) {
+	for (let i = 0; i < arr.length; i++) {
+		for (let j = 0; j < arr[i].length; j++)	{
+			let elem = arr[i][j];
+			if (elem === '-') {
+				for (let n = 1; n <= 9; n++) {
+					let arrSquare = getSquare(arr, i, j);
+					if (includeInSquare(arrSquare, n)) {
+						continue;
+					} else if (includeInRow(arr[i], n)) {
+						continue;
+					} else if (includeInCol(arr, j, n)) {
+						continue;
+					} else {
+						arr[i][j] = n;
+					}
+				}
+			}
+		}
+	}
+
+	return arr;
+}
+let a = solve('1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--');
+
+console.log(setNumbers(a));
+// module.exports = {
+// 	solve: solve,
+// 	isSolved: isSolved,
+// 	prettyBoard: prettyBoard
+// }
+
