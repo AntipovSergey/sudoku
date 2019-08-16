1848dd7b32ddbf49d7bd0782c99774f5ba3b0020
Petrucho <petrucho@post.cz>
2019-08-16 12:33:57 +0300

    UPD - correct files

--- searchInColumn.js
+++ searchInColumn.js
@@ -1,7 +1,6 @@
 function searchInCol(obj = {}, i = 0, j = 0) {
   for (k = 0; k < objILength; k++) {
     if (obj[k][j] != "") {
-      let something3 = obj[k][j];
       incorrectNumbers[obj[k][j]] = true;
     }
   };
