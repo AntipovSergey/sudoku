7d016cf550923afad56bbb6196d070ce412f8668
squb1lly <squb1lly@gmail.com>
2019-08-16 14:16:35 +0300

    added findersAll

--- searchInRowOrColumn.js
+++ searchInRowOrColumn.js
@@ -1,7 +1,22 @@
-function searchInRowOrCol(obj = {}, isCol = false, j = 0) {
-  for (k = 0; k < objILength; k++) {
-    if (obj[i][k] != "") {
-      incorrectNumbers[obj[i][k]] = true;
+let incorrectNumbers = {};
+
+function searchInRowOrCol(arr, isCol = false, i = 0, j = 0) {
+  if (isCol) {
+    for (i = 0; i < arr.length; i++) {
+      if (arr[i][j] != "") {
+        incorrectNumbers[arr[i][j]] = true;
+      };
     };
-  };
-};
+  } else {
+    for (j = 0; j < arr.length; j++) {
+      if (arr[i][j] != "") {
+        incorrectNumbers[arr[i][j]] = true;
+      };
+    };
+  }
+}
+
+
+module.exports = {
+  searchInRowOrCol: searchInRowOrCol,
+}
