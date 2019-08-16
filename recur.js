94089211376d613db09213876a857fef51067c7c
Maklay <okunev.js@gmail.com>
2019-08-16 14:16:53 +0300

    it's worked

--- recur.js
+++ recur.js
@@ -32,19 +32,21 @@ let mass = [
         [1, 2, 3, 4, 5, 6, 7, 8, 9], 9, [1, 2, 3, 4, 5, 6, 7, 8, 9], 3, [1, 2, 3, 4, 5, 6, 7, 8, 9]
     ]
 ]
-
+let gameSolved = false;
 function solveSudocu(sudocu) {
-    let gameSolved = false;
+    iterationCorect = true
+    
     for (let i = 0; i < sudocu.length; i++) {
-        for (let j = 0; j < sudocu.length; j++) {
+        for (let j = 0; j < sudocu[i].length; j++) {
             if (typeof sudocu[i][j] === 'object') {
                 for (let k = 0; k < sudocu[i][j].length; k++) {
                     let newSudocu = JSON.parse(JSON.stringify(sudocu))
-                    newSudocu[i][j][k] = newSudocu[i][j][k]
+                    newSudocu[i][j] = newSudocu[i][j][k]
                     recurentSolve(newSudocu, i, j);
                     if (gameSolved) {
                         return
                     }
+                    iterationCorect = true
                 }
             }
         }
@@ -52,22 +54,29 @@ function solveSudocu(sudocu) {
 }
 
 function recurentSolve(sudocu, row, col) {
+    renderSudocu(sudocu)
     if (!isSudocuCorrect(sudocu, row, col)) {
+        iterationCorect = false
         return
-    }
-    if (isSudocuCorrect(sudocu, row, col) && isSudocuSolved(sudocu)) {
-        return
+    } else if (isSudocuSolved(row, col)) {
         gameSolved = true;
+        return
     }
-
     for (let i = 0; i < sudocu.length; i++) {
         for (let j = 0; j < sudocu.length; j++) {
             if (typeof sudocu[i][j] === 'object') {
                 for (let k = 0; k < sudocu[i][j].length; k++) {
                     let newSudocu = JSON.parse(JSON.stringify(sudocu))
-                    newSudocu[i][j][k] = newSudocu[i][j][k]
+                    newSudocu[i][j] = newSudocu[i][j][k]
                     recurentSolve(newSudocu, i, j);
+/*                     if (!iterationCorect) {
+                        return
+                    } */
+                    if (gameSolved) {
+                        return
+                    }
                 }
+                return;
             }
         }
     }
@@ -75,24 +84,31 @@ function recurentSolve(sudocu, row, col) {
 
 function isSudocuCorrect(sudocu, row, col) {
 
+    let corr = {}
+
     function isRowCorrect(sudocu, row) {
-        rowCorr = {}
+        corr = {}
         for (let j = 0; j < sudocu[row].length; j++) {
-            if (rowCorr[sudocu[row][j]]) {
-                return false;
-            } else {
-                rowCorr[sudocu[row][j]] = true;
+            if (typeof sudocu[row][j] !== 'object') {
+                if (corr[sudocu[row][j]]) {
+                    return false;
+                } else {
+                    corr[sudocu[row][j]] = true;
+                }
             }
         }
         return true;
     }
 
     function isColCorrect(sudocu, col) {
+        corr = {}
         for (let i = 0; i < sudocu.length; i++) {
-            if (rowCorr[sudocu[i][col]]) {
-                return false;
-            } else {
-                rowCorr[sudocu[i][col]] = true;
+            if (typeof sudocu[i][col] !== 'object') {
+                if (corr[sudocu[i][col]]) {
+                    return false;
+                } else {
+                    corr[sudocu[i][col]] = true;
+                }
             }
         }
         return true;
@@ -100,7 +116,45 @@ function isSudocuCorrect(sudocu, row, col) {
     }
 
     function isSquareCorrect(sudocu, row, col) {
-
+        corr = {}
+        let borders = defineBoarders(row, col);
+        for (let i = borders.top; i <= borders.bottom; i++) {
+            for (let j = borders.left; j <= borders.right; j++) {
+                if (typeof sudocu[i][j] !== 'object') {
+                    if (corr[sudocu[i][j]]) {
+                        return false;
+                    } else {
+                        corr[sudocu[i][j]] = true;
+                    }
+                }
+            }
+        }
+        return true
     }
+    return isSquareCorrect(sudocu, row, col) && isColCorrect(sudocu, col) && isRowCorrect(sudocu, row)
 
 }
+
+function isSudocuSolved(i, j) {
+    return i == 8 && j == 8;
+}
+
+function renderSudocu(sudocu) {
+    let render = '';
+    for (let i = 0; i < sudocu.length; i++) {
+        if (i !== 0) {
+            render += '|\n'
+        }
+        for (let j = 0; j < sudocu[i].length; j++) {
+            if (typeof sudocu[i][j] === 'object') {
+                render += '| ';
+            } else {
+                render += '|' + sudocu[i][j]
+            }
+        }
+    }
+    render += '|'
+    console.log(render);
+}
+renderSudocu(mass);
+solveSudocu(mass)
