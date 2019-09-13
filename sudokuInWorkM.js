e87d42b6e19c1091d0c6e052ed63c6919f9be80a
MikhailKolichko <mikhailkolichko@gmail.com>
2019-09-13 13:56:44 +0300

    горизонталь и доска

--- sudokuInWorkM.js
+++ sudokuInWorkM.js
@@ -5,32 +5,20 @@ const incomeString = '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76
 // создаем доску 
 const parse = (string) => {
   let arr = string.split('');
-  let board = [
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-  ];
-  for (let i = 0; i < 9; i += 1) {
-    for (let j = 0; j < 9; j += 1) {
-      board[i][j] = arr[i + j]
+  let board = []
+    for (let i = 0; i < arr.length; i += 9) {
+      board.push(arr.slice(i, i + 9))
     }
-  }
   return board
 }
 //------------------------------------
 
-// первая координата недостающего числа по горизонтали
+// первая координата недостающего числа
 const findCoordinateH = () => {
   let board = parse(incomeString)
   let coordinate = [];
-  for (i = 0; i < 9; i += 1) {
-    for (j = 0; j < 9; j += 1) {
+  for (let i = 0; i < 9; i += 1) {
+    for (let j = 0; j < 9; j += 1) {
       if (board[i][j] == '-') {
         coordinate.push(i, j)
         return coordinate;
@@ -40,13 +28,24 @@ const findCoordinateH = () => {
 }
 //-----------------------------------------
 
-// поиск недостоющих цифр по горизонтали
+// поиск недостоющих цифр по
 
-const possibleNumH = () => {
+const horizon = () => {
   let numbers = ['1','2','3','4','5','6','7','8','9'] 
   let arrForSearch = parse(incomeString)[findCoordinateH()[0]]
-  return arrForSearch
+  let possibleNum = []
+  for (let i = 0; i < 9; i += 1) {
+    if(arrForSearch.includes(numbers[i])){
+      continue
+    }
+    else{
+      possibleNum.push(numbers[i])
+    }
+  }
+  return possibleNum
 }
+
+
 
 //-----------------------------------------
 
