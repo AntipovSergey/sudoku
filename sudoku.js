4a81dd705ffee8ad322c5be5b3526806c605e234
Julia <macbook@MacBook-Air-MacBook.local>
2019-10-25 11:25:36 +0300

    PrettyBoard

--- sudoku.js
+++ sudoku.js
@@ -3,19 +3,13 @@
 // something representing a board after
 // your solver has tried to solve it.
 // How you represent your board is up to you!
-function solve(boardString) {
-
-}
-
+function solve(boardString) {}
 
 // Returns a boolean indicating whether
 // or not the provided board is solved.
 // The input board will be in whatever
 // form `solve` returns.
-function isSolved(board) {
-
-}
-
+function isSolved(board) {}
 
 // Takes in a board in some form and
 // returns a String that's well formatted
@@ -23,12 +17,23 @@ function isSolved(board) {
 // The input board will be in whatever
 // form `solve` returns.
 function prettyBoard(board) {
-
+  let stringBoard =
+    "1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--";
+  board = stringBoard.split("");
+  let mass = [];
+  for (let i = 0; i < 9; i++) {
+    mass[i] = board.splice(0, 9);
+  }
+  return mass;
 }
-
+console.log(
+  prettyBoard(
+    "1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--"
+  )
+);
 
 module.exports = {
-	solve: solve,
-	isSolved: isSolved,
-	prettyBoard: prettyBoard
-}
+  solve: solve,
+  isSolved: isSolved,
+  prettyBoard: prettyBoard
+};
