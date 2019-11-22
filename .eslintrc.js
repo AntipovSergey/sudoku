2da5ff0ff7c4ab81e63501dc1fe6a97745f3a116
Artem Geroyan <53540917+artemmartov@users.noreply.github.com>
2019-11-22 15:22:22 +0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,10 @@
 module.exports={
-<<<<<<< HEAD
+
 	"extends": "airbnb-base",
 	"env": {
    	 "browser": true
   }
  }
-=======
     "extends" : "airbnb-base"
 }
->>>>>>> 8f25202cbe3d10d855fe58923c29f049502cb933
+
