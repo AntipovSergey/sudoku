ddae8f333fd126941c7bada96a28c7fea3007e94
artem geroyan <geroyan.artem@mail.ru>
2019-11-22 14:55:20 +0300

    js checkLines add

--- sudoku.js
+++ sudoku.js
@@ -32,3 +32,34 @@ module.exports = {
 	isSolved: isSolved,
 	prettyBoard: prettyBoard
 }
+
+
+// Проверяет горизонталь и вертикаль на возможность добавления числа
+
+function checklLines(array, row, column, element) {
+	let result = 0;
+	for (let x = 0; x < array[row].length; x++) {
+		console.log(array[row][x]);
+		if (element === array[row][x]){
+			result++;
+		}
+	}
+
+	for (let y = 0; y < array.length; y++) {
+		console.log(array[y][column]);
+		if (array[y][column] === element){
+			result++;
+		}
+	}
+    return result === 0;
+}
+
+console.log(checklLines(arr, 0, 0, 3));
+
+
+
+
+
+
+
+
