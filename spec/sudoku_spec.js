eeeef566db99f78c43fdedc8233662e7a5330b83
Sveta Fo <svetafo@gmail.com>
2019-11-22 17:11:47 +0300

    test

--- spec/sudoku_spec.js
+++ spec/sudoku_spec.js
@@ -1,2 +1,23 @@
-const sudoku = require('../sudoku.js')
 
+describe ("sudokuParse", function () {
+    it("should return the correct lenght of string",
+        function () {expect(board.length).toEqual(8);
+            for (let i = 0; i < board.length; i++) {
+                expect(board[i].length).toEqual(8);
+            }
+        });
+});
+
+describe("solve", function () {
+    it("should generate an 8x8 array", function () {
+        expect(board.length).toEqual(8);
+        for (let i = 0; i < board.length; i++) {
+            expect(board[i].length).toEqual(8);
+        }
+    });
+});
+
+describe("isSolved", function () {
+    it("should return a string", function () {
+        expect(typeof isSolved (board)).toEqual('string');
+    });
