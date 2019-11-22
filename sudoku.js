4217ecc417111fa8fe71713a98e2a1638995508c
Анатолий Злотников <Zlotnikov@MacBook-Air-Anatolij.local>
2019-11-22 16:20:44 +0300

    Logic

--- sudoku.js
+++ sudoku.js
@@ -5,8 +5,42 @@
 // How you represent your board is up to you!
 function solve(boardString) {
 
-}
+	sudokuSolver(board);
 
+	function isValid(board, row, col, k) {
+		for (let i = 0; i < 9; i++) {
+			const m = 3 * Math.floor(row / 3) + Math.floor(i / 3);
+			const n = 3 * Math.floor(col / 3) + i % 3;
+			if (board[row][i] == k || board[i][col] == k || board[m][n] == k) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+
+	function sudokuSolver(data) {
+		for (let i = 0; i < 9; i++) {
+			for (let j = 0; j < 9; j++) {
+				if (data[i][j] == '-') {
+					for (let k = 1; k <= 9; k++) {
+						if (isValid(data, i, j, k)) {
+							data[i][j] = '${k}';
+							if (sudokuSolver(data)) {
+								return true;
+							} else {
+								data[i][j] = '-';
+							}
+						}
+					}
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	return board;
+}
 
 // Returns a boolean indicating whether
 // or not the provided board is solved.
@@ -23,8 +57,6 @@ function isSolved(board) {
 // The input board will be in whatever
 // form `solve` returns.
 function prettyBoard(board) {
-
-}
 
 
 module.exports = {
