f421652d23a7eb8ea9ecaa68dfe3aa5b67d98f27
Ilya Mikhaylov <doxxmsk@gmail.com>
2019-11-22 13:48:04 +0300

    searchString function

--- runner.js
+++ runner.js
@@ -33,7 +33,6 @@ fs.readFile('./sudoku_puzzles.txt', 'utf-8', function read(err, data) {
 // This function searches the string where index is located.
   function stringSearch(index) {
     let subArr = [];
-    let testingstring = '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--'
     for (let i = 0; i < 81; i += 9) {
       subArr.push(boardString.substring(i, i + 9));
     }
