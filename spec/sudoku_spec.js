04050a1773a0787aa423dbe7634de7c3502a8158
Vadim S <vadim.stebakov@gmail.com>
2019-11-22 14:55:05 +0300

    add new functions

--- spec/sudoku_spec.js
+++ spec/sudoku_spec.js
@@ -1,2 +1,41 @@
-const sudoku = require('../sudoku.js')
+// const sudoku = require('../sudoku.js');
 
+describe('checkSquare()', () => {
+  it('check if the value in the 3x3 square', () => {
+    const board = [
+      [1, 0, 5, 8, 0, 2, 0, 0, 0],
+      [0, 9, 0, 0, 7, 6, 4, 0, 5],
+      [0, 0, 4, 0, 0, 8, 1, 9, 0],
+      [1, 9, 0, 0, 7, 3, 0, 6, 7],
+      [6, 2, 0, 8, 3, 0, 9, 0, 0],
+      [0, 0, 0, 6, 1, 0, 5, 0, 0],
+      [0, 0, 7, 6, 0, 0, 0, 3, 0],
+      [4, 3, 0, 0, 2, 0, 5, 0, 1],
+      [6, 0, 0, 3, 0, 8, 9, 0, 0],
+    ];
+    const row = 7;
+    const column = 5;
+    const value = 4;
+    expect(checkSquare(board, row, column, value)).toBeTruthy();
+  });
+});
+
+describe('checkSquare()', () => {
+  it('check if the value in the 3x3 square', () => {
+    const board = [
+      [1, 0, 5, 8, 0, 2, 0, 0, 0],
+      [0, 9, 0, 0, 7, 6, 4, 0, 5],
+      [0, 0, 4, 0, 0, 8, 1, 9, 0],
+      [1, 9, 0, 0, 7, 3, 0, 6, 7],
+      [6, 2, 0, 8, 3, 0, 9, 0, 0],
+      [0, 0, 0, 6, 1, 0, 5, 0, 0],
+      [0, 0, 7, 6, 0, 0, 0, 3, 0],
+      [4, 3, 0, 0, 2, 0, 5, 0, 1],
+      [6, 0, 0, 3, 0, 8, 9, 0, 0],
+    ];
+    const row = 1;
+    const column = 2;
+    const value = 9;
+    expect(checkSquare(board, row, column, value)).toBeFalsy();
+  });
+});
--- sudoku.js
+++ sudoku.js
@@ -7,15 +7,63 @@ function solve(boardString) {
 
 }
 
-check3x3Square
+const testBoard = [
+  [1, 0, 5, 8, 0, 2, 0, 0, 0],
+  [0, 9, 0, 0, 7, 6, 4, 0, 5],
+  [0, 0, 4, 0, 0, 8, 1, 9, 0],
+  [1, 9, 0, 0, 7, 3, 0, 6, 7],
+  [6, 2, 0, 8, 3, 0, 9, 0, 0],
+  [0, 0, 0, 6, 1, 0, 5, 0, 0],
+  [0, 0, 7, 6, 0, 0, 0, 3, 0],
+  [4, 3, 0, 0, 2, 0, 5, 0, 1],
+  [6, 0, 0, 3, 0, 8, 9, 0, 0],
+];
 
+// возвращает массив координат пустых ячеек в виде  [[ 4, 8 ], [ 5, 0 ], ...
+function findEmptyCells(testBoard) {
+  const emptyCells = [];
 
+  for (let i = 0; i < testBoard.length; i += 1) {
+    for (let j = 0; j < testBoard[i].length; j += 1) {
+      console.log(testBoard[i][j]);
+      if (testBoard[i][j] === 0) {
+        emptyCells.push([i, j]);
+      }
+    }
+  }
+  return emptyCells;
+}
+
+// проверяет возможность вставки по блоку 3х3
+function checkSquare(testBoard, row, column, value) {
+  let columnCorner = 0;
+  let rowCorner = 0;
+  const squareSize = 3;
+
+  while (column >= columnCorner + squareSize) {
+    columnCorner += squareSize;
+  }
+
+  while (row >= rowCorner + squareSize) {
+    rowCorner += squareSize;
+  }
+
+  for (let i = rowCorner; i < rowCorner + squareSize; i += 1) {
+    for (let j = columnCorner; j < columnCorner + squareSize; j += 1) {
+      if (testBoard[i][j] === value) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
 
 // Returns a boolean indicating whether
 // or not the provided board is solved.
 // The input board will be in whatever
 // form `solve` returns.
-function isSolved(board) {
+function isSolved() {
 
 }
 
@@ -31,7 +79,7 @@ function prettyBoard(board) {
 
 
 module.exports = {
-	solve: solve,
-	isSolved: isSolved,
-	prettyBoard: prettyBoard
-}
+  solve,
+  isSolved,
+  prettyBoard,
+};
