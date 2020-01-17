f6b4c16374d84e48a6e742d228ff79a1d1cddd05
Kiba Olga <olgadamakibovna@gmail.com>
2020-01-17 12:18:15 +0300

    func str to arr done

--- sudoku.js
+++ sudoku.js
@@ -4,31 +4,61 @@
 // your solver has tried to solve it.
 // How you represent your board is up to you!
 function solve(boardString) {
+  let arr = boardString.split("");
+  let arrOne = arr.slice(0, 9);
+  let lineOne = [...arrOne];
+  let arrTwo = arr.slice(9, 18);
+  let lineTwo = [...arrTwo];
+  let arrThree = arr.slice(18, 27);
+  let lineThree = [...arrThree];
+  let arrFour = arr.slice(27, 36);
+  let lineFour = [...arrFour];
+  let arrFive = arr.slice(36, 45);
+  let lineFive = [...arrFive];
+  let arrSix = arr.slice(45, 54);
+  let lineSix = [...arrSix];
+  let arrSeven = arr.slice(54, 63);
+  let lineSeven = [...arrSeven];
+  let arrEight = arr.slice(63, 72);
+  let lineEight = [...arrEight];
+  let arrNine = arr.slice(72, 81);
+  let lineNine = [...arrNine];
+  let board = [];
+  board.push(
+    lineOne,
+    lineTwo,
+    lineThree,
+    lineFour,
+    lineFive,
+    lineSix,
+    lineSeven,
+    lineEight,
+    lineNine
+  );
 
+  return board;
 }
-
+console.log(
+  solve(
+    "1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--"
+  )
+);
 
 // Returns a boolean indicating whether
 // or not the provided board is solved.
 // The input board will be in whatever
 // form `solve` returns.
-function isSolved(board) {
-
-}
-
+function isSolved(board) {}
 
 // Takes in a board in some form and
 // returns a String that's well formatted
 // for output to the screen.
 // The input board will be in whatever
 // form `solve` returns.
-function prettyBoard(board) {
-
-}
-
+function prettyBoard(board) {}
 
 module.exports = {
-	solve: solve,
-	isSolved: isSolved,
-	prettyBoard: prettyBoard
-}
+  solve: solve,
+  isSolved: isSolved,
+  prettyBoard: prettyBoard
+};
