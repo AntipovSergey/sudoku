79a1a48a8d5937a95d8b2daf2d3ea9423a0b7c4e
Michail <pmihail734@gmail.com>
2020-02-14 14:37:08 +0300

    kub

--- sudoku.js
+++ sudoku.js
@@ -3,32 +3,57 @@
 // something representing a board after
 // your solver has tried to solve it.
 // How you represent your board is up to you!
+
+let string =
+  "1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--";
+
 function solve(boardString) {
+  let board = [];
 
+  let newArray = boardString.split("");
+  for (let i = 0; i < boardString.length; i += 9) {
+    board.push(newArray.slice(i, i + 9));
+  }
+  return board;
 }
 
+function kub(arr) {
+  let generalArray = [];
+  let columnKub = [];
+  let arrayCubes = [];
 
-// Returns a boolean indicating whether
-// or not the provided board is solved.
-// The input board will be in whatever
-// form `solve` returns.
-function isSolved(board) {
+  let assistantArray1 = [];
+  let assistantArray2 = [];
+  let assistantArray3 = [];
 
+  for (let i = 0; i < arr.length; i++) {
+    let firstThreeColumns = arr[i].slice(0, 3);
+    let secondThreeColumns = arr[i].slice(3, 6);
+    let thirdThreeColumns = arr[i].slice(6, 9);
+    assistantArray1.push(firstThreeColumns);
+    assistantArray2.push(secondThreeColumns);
+    assistantArray3.push(thirdThreeColumns);
+  }
+  generalArray.push(assistantArray1);
+  generalArray.push(assistantArray2);
+  generalArray.push(assistantArray3);
+
+  for (let i = 0; i < generalArray.length; i++) {
+    for (let j = 0; j < generalArray[i].length; j++) {
+      columnKub.push(generalArray[i][j].slice(0, 3));
+    }
+  }
+
+  for (let i = 0; i < columnKub.length; i += 3) {
+    let swap = columnKub[i].concat(columnKub[i + 1], columnKub[i + 2]);
+    arrayCubes.push(swap);
+  }
+
+  console.log(arrayCubes);
 }
 
+kub(solve(string));
 
-// Takes in a board in some form and
-// returns a String that's well formatted
-// for output to the screen.
-// The input board will be in whatever
-// form `solve` returns.
-function prettyBoard(board) {
-
-}
-
-// Exports all the functions to use them in another file.
 module.exports = {
-	solve: solve,
-	isSolved: isSolved,
-	prettyBoard: prettyBoard
-}
+  solve: solve
+};
