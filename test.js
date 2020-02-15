76e0c056f8e5646698c47ca6da00e655198a7d18
Max_Gabrielov <maxgabrielov@gmail.com>
2020-02-15 21:59:27 +0300

    Release 0

--- test.js
+++ test.js
@@ -1,47 +1,3 @@
-
-
-// // Use filesystem.
-// const fs = require('fs');
-// // Use functions from sudoku.js file.
-// const sudoku = require('./sudoku');
-
-// // The sudoku puzzles that your program will solve can be found
-// // in the sudoku-puzzles.txt file.
-// //
-// // Remember, the file has newline characters at the end of each line,
-// // so you should remove them.
-
-// // Gets one puzzle from the text file.
-// function sudokuParse(content, puzzleNumber = 0) {
-//   let puzzle = content.split('\n')[puzzleNumber];
-//   console.log(puzzle);
-//   return puzzle;
-// }
-
-// function readAndSolve(err, data) {
-//   if (err) {
-//     throw err;
-//   }
-//   let puzzle = sudokuParse(data);
-
-//   let solvedPuzzle = sudoku.solve(puzzle);
-//   if (sudoku.isSolved(solvedPuzzle)) {
-//     console.log("The board was solved!");
-//     console.log(sudoku.prettyBoard(solvedPuzzle));
-//   }
-//   else {
-//     console.log("The board wasn't solved :(");
-//   }
-// }
-
-// // Reads file and sends data from it to the readAndSolve function.
-// fs.readFile(
-//   './sudoku-puzzles.txt',
-//   'utf-8',
-//   readAndSolve
-// );
-
-
 function solve(boardString) {
   let newArr = [];
   let arr = boardString.split('');
@@ -52,8 +8,6 @@ function solve(boardString) {
 }
 
 let test = solve('1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--');
-
-
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
@@ -184,4 +138,4 @@ function cube9(test) {
   }
   return test
 }
-console.table(cube9(test));
+// console.table(cube9(test));
