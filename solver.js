2d005ebfdc1d16b1980456b3e32f0229543a30db
Andrey Semenov <semnov.andrey@gmail.com>
2020-03-20 13:44:17 +0300

    getNums ready

--- solver.js
+++ solver.js
@@ -20,19 +20,58 @@ function isEmpty(cell) {
   return // true?false
 }
 
-function addNumInField(field, cell, num){
+function addNumInField(field, cell, num) {
   // todo
   return // nothing;
 }
 
-function nextCell(cell){
+function nextCell(cell) {
   // todo
   return // cell
 }
 
-function clearCell(cell){
+function clearCell(cell) {
   // todo
   return // nothing
+}
+
+function getNums(field, cell) {
+  function checkVertical(cell, num) {
+    for (let i = 0; i < 9; i++) {
+      if (field[i][cell[1]] === num) return false;
+    }
+    return true;
+  }
+
+  function checkHorisontal(cell, num) {
+    for (let i = 0; i < 9; i++) {
+      if (field[cell[0]][i] === num) return false;
+    }
+    return true;
+  }
+
+  function checkSquare(cell, num) {
+    const xShift = Math.floor(cell[0] / 3)*3;
+    const yShift = Math.floor(cell[1] / 3)*3;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (field[xShift + i][yShift + j] === num && !(xShift + i === cell[0] && yShift + j === cell[1])) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  function checkNum(cell, num) {
+    return checkVertical(cell, num) && checkHorisontal(cell, num) && checkSquare(cell, num);
+  }
+
+  let correct = [];
+  for (let i = 1; i <= 9; i++) {
+    if (checkNum(cell, num)) correct.push(i);
+  }
+  return correct; //array of correct nums to fill in cell
 }
 
 
