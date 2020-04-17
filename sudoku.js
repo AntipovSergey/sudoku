623f8d1d62cb9391dbd77135e478ec54084571ac
Yuriy.Evseev <lestnecedel@gmail.com>
2020-04-17 14:07:53 +0300

    push mybranch2

--- sudoku.js
+++ sudoku.js
@@ -1,4 +1,4 @@
-// Takes a board as a string in the format
+/* // Takes a board as a string in the format
 // you see in the puzzle file. Returns
 // something representing a board after
 // your solver has tried to solve it.
@@ -28,12 +28,42 @@ function solve(board) {
         return true;
     }
   }
+  return true;
 }
+
+function findEmpty (board){
+  for (let i of board){
+    for (le j = 0; j < board.length; j += 1){
+      if (board[i][j] === 0)
+      return (i,j)
+    }
+  }
+  return false;
+}
+
 
 function isValid(board, number, position) {
   // Check row
-  for (let i of board[i])
+  for (let i of board[0].length)
+  if ((board[position][0][i]) === num && position[1] != i)
+  return false
+  // Check column
+  for (let i of board.length){
+    if (board[i][position[1]] == number && position[0] != i)
+    return false;
+  }
+// Check box 
+
+let box_x = postiion[1] / 3;
+let box_y = position[0] / 3;
+
+for (let i of (box_y * 3, box * 3 + 3){
+  for (let j of (box_x * 3, box_x * 3 + 3){
+    if (board[i][j] === num && (i,j) != poistion) return false;
+  }
 }
+retrun 
+  }
 // Takes in a board in some form and
 // returns a String that's well formatted
 // for output to the screen.
@@ -49,3 +79,19 @@ module.exports = {
   isSolved: isSolved,
   prettyBoard: prettyBoard
 }
+ */
+
+
+function sudokuSolver(board) {
+  for (let vertical = 0; vart < board.length; vertical += 1) {
+    for (let horizontal = 0; horizontal < board.length; horizontal += 1) {
+      console.log
+    }
+  }
+}
+
+let board = [
+  [
+
+  ]
+]
