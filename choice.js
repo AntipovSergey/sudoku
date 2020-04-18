fb17a02a43a823b52a8a9628c956b5cfb6709005
Alex K <smthng@yandex.ru>
2020-04-18 13:04:34 +0300

    Release 0.3

--- choice.js
+++ choice.js
@@ -2,7 +2,7 @@
 /* eslint-disable node/no-unsupported-features/node-builtins */
 
 const string1 =
-  '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--';
+  '---------------------------------------------------------------------------------';
 
 let pool = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
@@ -13,20 +13,20 @@ for (let i = 0; i < 81; i += 9) {
   raws.push(splited.slice(i, i + 9));
 }
 
-let log = [[raws], [pool], 0, 0];
+let log = [[raws].slice(), [pool].slice(), 0, 0];
 let nextH = 0;
 let nextV = 0;
 
-function choice(coordH, coordV) {
-  if (nextH < 8) {
-    nextH += 1;
-  }
+function choice(coordV, coordH) {
   if (nextH === 8) {
     nextH = 0;
     nextV += 1;
   }
-  if (raws[coordH][coordV] !== '-') {
-    choice(nextH, nextV);
+  if (nextH < 8) {
+    nextH += 1;
+  }
+  if (raws[coordV][coordH] !== '-') {
+    choice(nextV, nextH);
   }
 
   // Переменные для хранения содержимого строки, столбца и квадрата, соответствующих текущей ячейке
@@ -70,31 +70,37 @@ function choice(coordH, coordV) {
   // Получение пула свободных чисел
   // poolCheck(horiz, vert, sqr).split('');
 
+  if (pool.length === 1) {
+    raws[coordV][coordH] = pool[0];
+    pool = [];
+    pool = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  }
   if (pool.length > 1) {
-    log.push([raws.slice(''), pool, coordH, coordV]);
-    raws[coordH][coordV] = pool[-1];
-    choice(nextH, nextV);
+    log.push([raws.slice(), pool.slice(), coordV, coordH]);
+    raws[coordV][coordH] = pool[pool.length - 1];
   }
   if (pool === -1) {
     raws = [];
-    raws.push(log[-1].slice(''));
+    raws.push(log[log.length - 1][0].slice());
+    pool = [];
+    pool.push(log[log.length - 1][1].slice());
     pool.pop();
     if (pool.length === 0) {
       log.pop();
-      choice(log[-1][2], log[-1][3]);
+      raws = [];
+      raws.push(log[log.length - 1][0].slice());
+      pool = [];
+      pool.push(log[log.length - 1][1].slice());
+      pool.pop();
+      choice(log[log.length - 1][2], log[log.length - 1][3]);
     }
-    choice(log[-1][2], log[-1][3]);
+    choice(log[log.length - 1][2], log[log.length - 1][3]);
   }
-  if (pool.length === 1) {
-    raws[coordH][coordV] = pool[0];
-    if (coordH === 8 && coordV === 8) {
-      console.table(raws);
-      return raws;
-    }
-    choice(nextH, nextV);
+  if (coordV === 8 && coordH === 8) {
+    console.table(raws);
+    return raws;
   }
-
-  // console.log(log[0]);
+  choice(nextV, nextH);
 }
 
 console.table(raws);
