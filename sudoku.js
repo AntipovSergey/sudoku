9117729b8bc699ec4381ba72f969e7116699976c
Dmitry Bochok <BochDim38@mail.ru>
2020-06-01 17:26:42 +0800

    Release 0.1

--- sudoku.js
+++ sudoku.js
@@ -4,9 +4,24 @@
 // your solver has tried to solve it.
 // How you represent your board is up to you!
 function solve(boardString) {
-
+  for (let i = 0; i < boardString.length; i++) {
+    for (let o = 0; o < boardString[i.length]; o++) {
+      const element = boardString[i[o];
+  ]    
+    }  
+  }
 }
 
+let arr = 
+[[1, 0, 5, 8, 0, 2, 0, 0, 0],
+  [0, 9, 0, 0, 7, 6, 4, 0, 5],
+  [2, 0, 0, 4, 0, 0, 8, 1, 9],
+  [0, 1, 9, 0, 0, 7, 3, 0, 6],
+  [7, 6, 2, 0, 8, 3, 0, 9, 0],    
+  [0, 0, 0, 0, 6, 1, 0, 5, 0],
+  [0, 0, 7, 6, 0, 0, 0, 3, 0],
+  [4, 3, 0, 0, 2, 0, 5, 0, 1],
+  [6, 0, 0, 3, 0, 8, 9, 0, 0]]
 
 // Returns a boolean indicating whether
 // or not the provided board is solved.
