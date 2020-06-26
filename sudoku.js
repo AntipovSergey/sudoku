fe8108c0fdc6684a825c4add64cbc2b4a2761005
Tembot <maremshaov.t@yandex.ru>
2020-06-26 10:39:18 +0300

    FindEmptyDone

--- sudoku.js
+++ sudoku.js
@@ -3,32 +3,38 @@
 // something representing a board after
 // your solver has tried to solve it.
 // How you represent your board is up to you!
-function solve(boardString) {
-
-}
-
+function solve(boardString) {}
 
 // Returns a boolean indicating whether
 // or not the provided board is solved.
 // The input board will be in whatever
 // form `solve` returns.
-function isSolved(board) {
+function isSolved(board) {}
 
+function FindEmpty(puzzle) {
+  let position = [];
+  for (let i = 0; i < puzzle.length; i++) {
+    for (let j = 0; j < puzzle[i].length; j++) {
+      if (puzzle[i][j] == "-") {
+        position[0] = i;
+        position[1] = j;
+        return position;
+      }
+    }
+  }
+  return false;
 }
-
 
 // Takes in a board in some form and
 // returns a String that's well formatted
 // for output to the screen.
 // The input board will be in whatever
 // form `solve` returns.
-function prettyBoard(board) {
-
-}
+function prettyBoard(board) {}
 
 // Exports all the functions to use them in another file.
 module.exports = {
-	solve: solve,
-	isSolved: isSolved,
-	prettyBoard: prettyBoard
-}
+  solve: solve,
+  isSolved: isSolved,
+  prettyBoard: prettyBoard,
+};
