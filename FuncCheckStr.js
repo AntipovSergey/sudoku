db9c7db451b5a851809fb0d26dbd3539a51704ae
Igor Ribozavr <Igo-nikolaenk@yandex.ru>
2020-06-26 15:28:42 +0300

    connector added

--- FuncCheckStr.js
+++ FuncCheckStr.js
@@ -1,17 +1,28 @@
+const createBoard = require('./sudoku.js');
+let inputSudokuPuzzle2 = '--8------49-157--2--3--419-185-6--2-----2--6-96-4-53---3--72--4-49-3--57827--9-13';
+let boardArr = createBoard.createBoard(inputSudokuPuzzle2);
+
+console.table(boardArr);
 
 
-function FuncCheckStr(newArr) {
 
-  for (let i = 0; i < Arr.length; i+=1) {
-    for(let j = 0; g < Arr[i].length; j+=1){
-      if (Arr[i][j] "h"){
 
+
+
+function FuncCheckStr(boardArr) {
+
+  for (let i = 0; i < boardArr.length; i+=1) {
+    for(let j = 0; j < boardArr[i].length; j+=1){
+      if (Arr[i][j] === "string"){
+
+        
       } 
     }
   }
 }
 
 
+ console.log(FuncCheckStr(boardArr));
  
 
 
@@ -20,8 +31,8 @@ function FuncCheckStr(newArr) {
 
 
 
-modules.export = {
-  importSudokuFromStr,
+module.exports = {
+  FuncCheckStr,
 }
-let Arr = 
-console.log(typeof Arr[i][j]);
+// let Arr = 
+// console.log(typeof Arr[i][j]);
