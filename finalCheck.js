3d86680324a04c0703425067d193161c4887fb8a
Bezz <bezobazov1999@gmail.com>
2020-06-26 16:42:57 +0300

    Release Finish

--- finalCheck.js
+++ finalCheck.js
@@ -1,11 +1,19 @@
+let counter = 100;
 function finalCheck(arr) {
   for (i = 0; i < arr.length; i++) {
     if (arr[i].includes("-")) {
-      return checkArr();
-  }
+      if (counter == 0) {
+        return console.log("сорян, в следующий раз");
+      } else {
+        counter--;
+        return goingThrough(array);
+      }
     }
-      return (
-        console.log("\t     (ツ) М О Л О Д Ц Ы! (ツ) \n\t\t★★★Это ПОБЕДА!★★★"),
-        console.table(arr)
-        );
+  }
+  return (
+    console.log("\t     (ツ) М О Л О Д Ц Ы! (ツ) \n\t\t★★★Это ПОБЕДА!★★★"),
+    console.table(arr)
+  );
 }
+
+module.exports = finalCheck;
