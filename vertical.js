f47cf8e59f7c6cc6dc184dc622b79e4a9b8f6b26
Amir <yangaev.0000@gmail.com>
2020-06-26 13:59:04 +0300

    vertical

---
+++ vertical.js
@@ -0,0 +1,18 @@
+
+
+
+
+function vertical(a, b) {
+
+  const result = ""
+  for (let i = 0; i < puzzleAr.length; i++) {
+    for (let p = 0; p < puzzleAr[i]; p++) {
+      result = result + puzzleAr[i][p];
+    }
+  }
+return 
+
+}d
+//console.log(result.includes())
+
+
