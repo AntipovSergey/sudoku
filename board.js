3ec6fbc9942195754ec4bd012219eb850b46126b
Amir <yangaev.0000@gmail.com>
2020-07-31 15:04:26 +0300

    board

---
+++ board.js
@@ -0,0 +1,15 @@
+let newArr1 =
+  '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--'
+
+
+const superBoard = function solve(boardString) {
+  let newArr = boardString.split("");
+  let BordSize = 9;
+  let subArr = [];
+  for (let i = 0; i < Math.ceil(newArr.length / BordSize); i++) {
+    subArr[i] = newArr.slice(i * BordSize, i * BordSize + BordSize);
+  }
+  return subArr;
+};
+
+console.table(superBoard(newArr1));
