4ddf58362e1a485297aefc58e134b65e11b6989f
Евгений Швецов <67862313+EvgenyShvetcov@users.noreply.github.com>
2020-07-31 16:10:23 +0300

    findInRow

--- findInSquare.js
+++ findInSquare.js
@@ -1,4 +1,4 @@
-let board = [['1','','3','4','5','6','7','8','9']];
+let board = ['1','','3','4','5','6','7','8','9'];
 let variants = ['1','2','3','4','5','6','7','8','9'];
 
 function findSquare(board,variants){
@@ -6,6 +6,7 @@ function findSquare(board,variants){
                     variants = variants.filter(el => el !== board[i]);
         }
     return variants;
+    
 }
 
 console.log(findSquare(board, variants));
