77b79b9942839e92dc203d2c771089620f0dd056
Boris <mesh.boris@mail.ru>
2020-07-31 16:33:14 +0300

    making vertical massive

--- solver.js
+++ solver.js
@@ -9,52 +9,49 @@
 
 // }
 
-module.exports = {
 
-};
-const { finalArr } = require('./test2');
-/*
-let squareArr1 = [];
-let squareArr2 = [];
-let squareArr3 = [];
-let squareArr4 = [];
-let squareArr5 = [];
-let squareArr6 = [];
-let squareArr7 = [];
-let squareArr8 = [];
-let squareArr9 = [];
-*/
-
-// function createArrayOfSquares(finalArr) {
-//   let squareArr = [];
-//   for (let i = 0; i < 2; i++) {
-//     for (let j = 0; j < 2; j++) {
-//       squareArr.push(finalArr[i][j]);
-//     }
-//   }
-//   return squareArr;
-// };
-
-//let sq = createArrayOfSquares(finalArr);
-//console.log(sq);
-
+const { finalArr } = require('./getSudoku');
+let arrColumn = [];
 function createArrayOfColumns(finalArr) {
-  let arrColumn = [];
-  for (let i = 0; i < 8; i++) {
+  for (let i = 0; i < 9; i++) {
     for (let j = 0; j < 9; j++) {
       arrColumn.push(finalArr[j][i]);
     }
   }
   return arrColumn;
 }
-console.log(createArrayOfColumns(finalArr));
 
-const x = arrFirst.slice(0, 9);
-const row2 = arrFirst.slice(9, 18);
-const row3 = arrFirst.slice(18, 27);
-const row4 = arrFirst.slice(27, 36);
-const row5 = arrFirst.slice(36, 45);
-const row6 = arrFirst.slice(45, 54);
-const row7 = arrFirst.slice(54, 63);
-const row8 = arrFirst.slice(63, 72);
-const row9 = arrFirst.slice(72, 81);
+createArrayOfColumns(finalArr);
+
+const column1 = arrColumn.slice(0, 9);
+const column2 = arrColumn.slice(9, 18);
+const column3 = arrColumn.slice(18, 27);
+const column4 = arrColumn.slice(27, 36);
+const column5 = arrColumn.slice(36, 45);
+const column6 = arrColumn.slice(45, 54);
+const column7 = arrColumn.slice(54, 63);
+const column8 = arrColumn.slice(63, 72);
+const column9 = arrColumn.slice(72, 81);
+
+let massiveArrColumn = [];
+// eslint-disable-next-line max-len
+massiveArrColumn.push(column1, column2, column3, column4, column5, column6, column7, column8, column9);
+console.table(massiveArrColumn);
+
+// for (let i = 0; i < 9; i++) {
+//     for (let j = 0; j < 9; j++) {
+//       for (let k = 0; k < 9; k++) {
+        
+        
+//       }
+// }
+
+// }
+
+
+
+
+module.exports = {
+  massiveArrColumn,
+
+};
