6228301943f71dc294f5af14a8764260c3cb187c
RostislavBartenev <rostislav.bartenev@gmail.com>
2020-08-04 14:33:12 +0600

    solve

--- sudoku.js
+++ sudoku.js
@@ -21,155 +21,96 @@ function solve(boardString) {
     rowArr = [];
   }
 
-  console.log(mainArr);
-
-  return mainArr;
-}
-
-// let testArr = [
-
-//   [
-//     [1], ['-'],
-//     [5], [8],
-//     ['-'], [2],
-//     ['-'], ['-'],
-//     ['-']
-//   ],
-//   [
-//     ['-'], [9],
-//     ['-'], ['-'],
-//     [7], [6],
-//     [4], ['-'],
-//     [5]
-//   ],
-//   [
-//     [2], ['-'],
-//     ['-'], [4],
-//     ['-'], ['-'],
-//     [8], [1],
-//     [9]
-//   ],
-//   [
-//     ['-'], [1],
-//     [9], ['-'],
-//     ['-'], [7],
-//     [3], ['-'],
-//     [6]
-//   ],
-//   [
-//     [7], [6],
-//     [2], ['-'],
-//     [8], [3],
-//     ['-'], [9],
-//     ['-']
-//   ],
-//   [
-//     ['-'], ['-'],
-//     ['-'], ['-'],
-//     [6], [1],
-//     ['-'], [5],
-//     ['-']
-//   ],
-//   [
-//     ['-'], ['-'],
-//     [7], [6],
-//     ['-'], ['-'],
-//     ['-'], [3],
-//     ['-']
-//   ],
-//   [
-//     [4], [3],
-//     ['-'], ['-'],
-//     [2], ['-'],
-//     [5], ['-'],
-//     [1]
-//   ],
-//   [
-//     [6], ['-'],
-//     ['-'], [3],
-//     ['-'], [8],
-//     [9], ['-'],
-//     ['-']
-//   ]
-// ]
-// console.log(typeof (testArr[0][1]));
-
-function salavat(arr) {
-
-
-  arr.forEach((element) => {
-    let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    let numsInRow = [];
-    let newNums = [];
-    element.forEach((el1) => {
-      if (el1 != '-') {
-        numsInRow.push(el1);
-      }
-    })
-
-    nums.forEach((el2) => {
-      for (let i = 0; i < numsInRow.length; i++) {
-        if (el2 == numsInRow[i]) {
-          nums.splice(el2 - 1, 1, 0);
+  function firstFilling(someArr) {
+    for (let y = 0; y < someArr.length; y++) {
+      for (let x = 0; x < someArr[0].length; x++) {
+        if (someArr[y][x][0] === '-') {
+          someArr[y].splice(x, 1, ['1', '2', '3', '4', '5', '6', '7', '8', '9']);
         }
-        // console.log(nums);
       }
     }
-    )
-    nums.forEach((el) => {
-      if (el !== 0) {
-        newNums.push(el);
+    return someArr;
+  }
+
+  firstFilling(mainArr);
+
+  function searchCol(arr) {
+    for (let m = 0; m < arr.length; m += 1) {
+      const values = []; // [ [ '1' ], [ '2' ], [ '7' ], [ '4' ], [ '6' ] ]
+      const proverka = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+      for (let j = 0; j < arr.length; j += 1) {
+        if (arr[j][m].length === 1) {
+          values.push(arr[j][m]);
+        }
       }
-    })
-    element.forEach((el3) => {
-      if (el3 == '-') {
-        el3.splice(0, 1, newNums);
+
+      const filters = [];
+
+      proverka.forEach((el2) => {
+        for (let i = 0; i < values.length; i += 1) {
+          if (el2 === values[i][0]) {
+            proverka.splice(el2 - 1, 1, 0);
+          }
+        }
+      });
+
+      for (let i = 0; i < proverka.length; i += 1) {
+        if (proverka[i] !== 0) {
+          filters.push(proverka[i]);
+        }
       }
-    })
 
-  });
-  // console.table(arr);
-  // console.log(arr[0][1]);
-
-}
-
-// nums.forEach((el2) => {
-//   for (let i = 0; i < numsInRow.length; i++) {
-//     if (el2 == numsInRow[i]) {
-//       nums.slice(el2, 1)
-//     })
-
-salavat(testArr);
-
-function chekAndRemoveInSubArrays(board) {
-  function checkInEachSubArray(y, x) {
-    let tempArr = [];
-    for (let i = x; i < x + 3; i++) {
-      for (let j = y; j < y + 3; j++) {
-        if (board[i][j].length === 1) { tempArr.push(board[i][j][0]); }
+      for (let j = 0; j < arr.length; j += 1) {
+        if (arr[j][m].length !== 1) {
+          arr[j][m] = filters;
+        }
       }
     }
+    return (arr);
+  }
 
-    for (let f = x; f < x + 3; f++) {
-      for (let g = y; g < y + 3; g++) {
-        if (board[f][g].length > 1) {
-          for (let h = board[f][g].length - 1; h >= 0; h--) {
-            if (board[f][g].length === 1) break;
-            if (tempArr.includes(board[f][g][h])) { board[f][g].splice(h, 1) }
+  searchCol(mainArr);
+
+  console.table(mainArr);
+
+  function chekAndRemoveInSubArrays(board) {
+    function checkInEachSubArray(y, x) {
+      const tempArr = [];
+      for (let i = x; i < x + 3; i++) {
+        for (let j = y; j < y + 3; j++) {
+          if (board[i][j].length === 1) { tempArr.push(board[i][j][0]); }
+        }
+      }
+
+      for (let f = x; f < x + 3; f++) {
+        for (let g = y; g < y + 3; g++) {
+          if (board[f][g].length > 1) {
+            for (let h = board[f][g].length - 1; h >= 0; h--) {
+              if (board[f][g].length === 1) break;
+              if (tempArr.includes(board[f][g][h])) { board[f][g].splice(h, 1); }
+            }
           }
         }
       }
     }
-    //console.table(board);
+
+    for (let n = 0; n < board[0].length; n += 3) {
+      for (let p = 0; p < board.length; p += 3) {
+        checkInEachSubArray(n, p);
+      }
+    }
+    return board;
   }
 
-  for (let n = 0; n < board[0].length; n += 3) {
-    for (let p = 0; p < board.length; p += 3) {
-      checkInEachSubArray(n, p)
-    }
-  }
-  return board;
+  chekAndRemoveInSubArrays(mainArr);
+
+  console.table(mainArr);
+  console.log(mainArr[0][1]);
+
+  return mainArr;
 }
+
 // Returns a boolean indicating whether
 // or not the provided board is solveds.
 // The input board will be in whatever
