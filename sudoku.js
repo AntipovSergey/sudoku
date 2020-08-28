93b4ddffacef05b9b7781cac355a0281124ceff8
Alexander <glazyrin-ai@yandex.ru>
2020-08-28 13:43:53 +0300

    com1

--- sudoku.js
+++ sudoku.js
@@ -4,10 +4,74 @@
 // something representing a board after
 // your solver has tried to solve it.
 // How you represent your board is up to you!
-function solve(boardString) {
 
+function solve(boardString) {
+  // Создаем двухуровневый массив из строки
+  function createSudArr() {
+    const arr = boardString.replace('\n', '').split('');
+    const sudokuArr = [];
+    for (let i = 0; i < 9; i += 1) {
+      const stringArr = [];
+      for (let j = 0; j < 9; j += 1) {
+        stringArr.push(arr[j + (i * 9)]);
+      }
+      sudokuArr.push(stringArr);
+    }
+    return sudokuArr;
+  }
+
+  const sudArr = createSudArr();
+
+  // Получаем группу, в которой находится незаполненная ячейка
+  function getGroup(arr, height, width) {
+    function getNums(num) {
+      if (num <= 3) return [0, 1, 2];
+      if (num > 3 && num <= 6) return [3, 4, 5];
+      return [6, 7, 8];
+    }
+
+    const x = getNums(width);
+    const y = getNums(height);
+
+    return [arr[x[0]][y[0]], arr[x[0]][y[1]], arr[x[0]][y[2]], arr[x[1]][y[0]], arr[x[1]][y[1]],
+      arr[x[1]][y[2]], arr[x[2]][y[0]], arr[x[2]][y[1]], arr[x[2]][y[2]]];
+  }
+
+  // Получаем незаполненный элемент
+  function getElem(arr1, arr2, arr3) {
+    const allNums = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+    const leftNums = [];
+    for (let i = 0; i < allNums.length; i += 1) {
+      if (!arr1.includes(allNums[i]) && !arr2.includes(allNums[i]) && !arr3.includes(allNums[i])) {
+        leftNums.push(allNums[i]);
+      }
+    }
+
+    // если не хватает одного элемента, то возвращаем его
+    if (leftNums.length === 1) return leftNums[0];
+    return 0;
+  }
+
+  // проходим по массиву судоку
+  for (let i = 0; i < sudArr.length; i += 1) {
+    for (let j = 0; j < sudArr[i].length; j += 1) {
+      if (sudArr[i][j] === '-') { // Если ячейка не заполнена
+        const strArr = [sudArr[0][j], sudArr[1][j], sudArr[2][j], sudArr[3][j], sudArr[4][j],
+          sudArr[5][j], sudArr[6][j], sudArr[7][j], sudArr[8][j]];
+        const colArr = [sudArr[i][0], sudArr[i][1], sudArr[i][2], sudArr[i][3], sudArr[i][4],
+          sudArr[i][5], sudArr[i][6], sudArr[i][7], sudArr[i][8]];
+        const groupArr = getGroup(sudArr, i, j);
+        const num = getElem(strArr, colArr, groupArr);
+        if (num) {
+          sudArr[i][j] = num;
+        }
+      }
+    }
+  }
 }
 
+solve('1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--');
 
 // Returns a boolean indicating whether
 // or not the provided board is solved.
@@ -16,7 +80,6 @@ function solve(boardString) {
 function isSolved(board) {
 
 }
-
 
 // Takes in a board in some form and
 // returns a String that's well formatted
@@ -29,7 +92,7 @@ function prettyBoard(board) {
 
 // Exports all the functions to use them in another file.
 module.exports = {
-	solve: solve,
-	isSolved: isSolved,
-	prettyBoard: prettyBoard
-}
+  solve,
+  isSolved,
+  prettyBoard,
+};
