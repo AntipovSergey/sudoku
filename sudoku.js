8aefc9a5889b875f60e19d38702a6222ad0a44f5
HoneyMS <Honeyms@mail.ru>
2020-08-28 12:56:50 +0300

    функция prettyBoard

--- sudoku.js
+++ sudoku.js
@@ -22,13 +22,25 @@ function isSolved(board) {
 // for output to the screen.
 // The input board will be in whatever
 // form `solve` returns.
+
+
 function prettyBoard(board) {
+    let arr2 = [...[...board]];
+    let arr1 = [];
+    let string = '';
+    for (let i = 0; i < arr2.length; i++) {
+        arr1.push(arr2.splice(0, 9));
+        string = arr1[i].join(' ');
+        console.log(string);
+    }
+
+
 
 }
-
+prettyBoard('1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--');
 // Exports all the functions to use them in another file.
 module.exports = {
-	solve: solve,
-	isSolved: isSolved,
-	prettyBoard: prettyBoard
+    solve: solve,
+    isSolved: isSolved,
+    prettyBoard: prettyBoard
 }
