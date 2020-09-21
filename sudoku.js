c18f1b22d1e8cde75c96f382287dbec7b1fc56b6
anastasiiasok <anastasiiasok@gmail.com>
2020-09-21 13:00:53 +0300

    created first function

--- sudoku.js
+++ sudoku.js
@@ -7,7 +7,6 @@ function solve(boardString) {
 
 }
 
-
 // Returns a boolean indicating whether
 // or not the provided board is solved.
 // The input board will be in whatever
@@ -15,7 +14,6 @@ function solve(boardString) {
 function isSolved(board) {
 
 }
-
 
 // Takes in a board in some form and
 // returns a String that's well formatted
@@ -26,9 +24,24 @@ function prettyBoard(board) {
 
 }
 
+function arraytoString(sudoku) {
+  const result = [];
+  const arr1 = sudoku.split('');
+  let arr = [];
+  for (let i = 0; i < arr1.length; i += 1) {
+    arr.push(arr1[i]);
+    if ((i + 1) % 9 === 0) {
+      result.push(arr);
+      arr = [];
+    }
+  }
+  return result;
+}
+arraytoString('1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89---');
+
 // Exports all the functions to use them in another file.
 module.exports = {
-	solve: solve,
-	isSolved: isSolved,
-	prettyBoard: prettyBoard
-}
+  solve,
+  isSolved,
+  prettyBoard,
+};
