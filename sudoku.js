d1cfc6035673c554044e61f6662440d242e350f0
Aleksei Vesselko <aleksei.vesselko@gmail.com>
2020-09-21 12:01:14 +0100

    solve & isSolved functions

--- sudoku.js
+++ sudoku.js
@@ -4,31 +4,60 @@
 // your solver has tried to solve it.
 // How you represent your board is up to you!
 function solve(boardString) {
-
+  let result = [...board];
+  while (!isSolved(result)) {
+    for (let i = 0; i < result.length; i++) {
+      for (let y = 0; y < result[i].length; y++) {
+        if (result[i][y] === '-') {
+          for (let x = 1; x <= 9; x++) {
+            let possibleResults = [];
+            if (
+              func1(x, result[i][y]) &&
+              func2(x, result[i][y]) &&
+              func3(x, result[i][y])
+            ) {
+              possibleResults.push(x);
+              if (possibleResults.length === 1) {
+                result[i][y] == x;
+              } else {
+                continue;
+              }
+            } else {
+              continue;
+            }
+          }
+        }
+        continue;
+      }
+    }
+  }
+  return result;
 }
-
 
 // Returns a boolean indicating whether
 // or not the provided board is solved.
 // The input board will be in whatever
 // form `solve` returns.
 function isSolved(board) {
-
+  for (let i = 0; i < board.length; i++) {
+    for (let y = 0; y < board[i].length; y++) {
+      if (board[i][y] === '-') return false;
+      continue;
+    }
+  }
+  return true;
 }
-
 
 // Takes in a board in some form and
 // returns a String that's well formatted
 // for output to the screen.
 // The input board will be in whatever
 // form `solve` returns.
-function prettyBoard(board) {
-
-}
+function prettyBoard(board) {}
 
 // Exports all the functions to use them in another file.
 module.exports = {
-	solve: solve,
-	isSolved: isSolved,
-	prettyBoard: prettyBoard
-}
+  solve: solve,
+  isSolved: isSolved,
+  prettyBoard: prettyBoard,
+};
