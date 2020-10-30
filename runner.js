a8c143d058b30bca748120638eac0f4e13506157
Victor <vzabrovskiu@gmail.com>
2020-10-30 16:54:12 +0300

    First 5 tests

--- runner.js
+++ runner.js
@@ -20,7 +20,7 @@ function readAndSolve(err, data) {
   if (err) {
     throw err;
   }
-  let puzzle = sudokuParse(data);
+  let puzzle = sudokuParse(data, 0); // какой тест
 
   let solvedPuzzle = sudoku.solve(puzzle);
   if (sudoku.isSolved(solvedPuzzle)) {
