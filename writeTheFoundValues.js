cb600de923266b62c7cc3099329cd8270c389b2c
Valeria <valeria.vol@gmail.com>
2020-10-30 15:06:02 +0300

    Release writeTheFoundValues

---
+++ writeTheFoundValues.js
@@ -0,0 +1,20 @@
+//Написать функцию, которая в изначальном массиве по индексу заменять значение элемента на значение по ключу (если значени единственное) / writeTheFoundValues
+
+
+function writeTheFoundValues (objectContainingValues, arrWithSolution){
+  const coordKeep = [];
+  for (key in objectContainingValues){
+    if (objectContainingValues[key].length === 1){
+      coordKeep.push(key);
+    }
+  } 
+  console.log(coordKeep);
+
+  coordKeep.forEach((el)=>{
+    arrWithSolution[el[0]][el[1]] = objectContainingValues[el]
+  })
+  return arrWithSolution;
+
+} 
+
+writeTheFoundValues(objectContainingValues, arrWithSolution)
