ab9a8f0b2e60ae0028d98eea4f5c0c4867f17bc5
sofialapteva <sofia_lapteva@mail.ru>
2020-11-09 15:14:58 +0300

    sofia

--- max.js
+++ max.js
@@ -7,22 +7,22 @@
 // console.log(greetings())
 
 let array = [
-  [1, "", 5, 8, "", 2, "", "", ""],
-  ["", 9, "", "", 7, 6, 4, "", 5],
-  [2, "", "", 4, "", "", 8, 1, 9],
-  ["", 1, 9, "", "", 7, 3, "", 6],
-  [7, 6, 2, "", 8, 3, "", 9, ""],
-  ["", "", "", "", 6, 1, "", 5, ""],
-  ["", "", 7, 6, "", "", "", 3, ""],
-  [4, 3, "", "", 2, "", 5, "", 1],
-  [6, "", "", 3, "", 8, 9, "", ""],
+  [1, '', 5, 8, '', 2, '', '', ''],
+  ['', 9, '', '', 7, 6, 4, '', 5],
+  [2, '', '', 4, '', '', 8, 1, 9],
+  ['', 1, 9, '', '', 7, 3, '', 6],
+  [7, 6, 2, '', 8, 3, '', 9, ''],
+  ['', '', '', '', 6, 1, '', 5, ''],
+  ['', '', 7, 6, '', '', '', 3, ''],
+  [4, 3, '', '', 2, '', 5, '', 1],
+  [6, '', '', 3, '', 8, 9, '', ''],
 ];
 
 function solveRow(arr) {
   for (let i = 0; i < 9; i++) {
     for (let j = 0; j < 9; j++) {
-      if (((typeof(arr[i][j]) == "string"))) {
-        arr[i][j] = "123456789";
+      if (typeof arr[i][j] == 'string') {
+        arr[i][j] = '123456789';
         for (let k = 0; k < 9; k++) {
           if (arr[i][j].includes(arr[i][k]) && j !== k && !isNaN(arr[i][k])) {
             let index = arr[i][j].indexOf(arr[i][k]);
@@ -40,27 +40,26 @@ function solveRow(arr) {
   return arr;
 }
 
-// console.log(solveRow(array));
+console.log(solveRow(array));
 
+// function solveColumn(arr) {
+//   for (let i = 0; i < 9; i++) {
+//     for (let j = 0; j < 9; j++) {
+//       if (typeof arr[i][j] == 'string') {
+//         for (let k = 0; k < 9; k++) {
+//           if (arr[i][j].includes(arr[k][j]) && i !== k && !isNaN(arr[k][j])) {
+//             let index = arr[i][j].indexOf(arr[k][j]);
+//             arr[i][j] =
+//               arr[i][j].slice(0, index) +
+//               arr[i][j].slice(index + 1, arr[i][j].length);
+//           }
+//         }
+//       }
+//     }
+//   }
+//   return arr;
+// }
 
-function solveColumn(arr) {
-  for (let i = 0; i < 9; i++) {
-    for (let j = 0; j < 9; j++) {
-      if (((typeof(arr[i][j]) == "string"))) {
-        for (let k = 0; k < 9; k++) {
-          if (arr[i][j].includes(arr[k][j]) && i !== k && !isNaN(arr[k][j])) {
-            let index = arr[i][j].indexOf(arr[k][j]);
-            arr[i][j] =
-              arr[i][j].slice(0, index) +
-              arr[i][j].slice(index + 1, arr[i][j].length);
-          }
-        }
-      }
-    }
-  }
-  return arr;
-}
+let arr1 = solveRow(array);
 
-let arr1 = solveRow(array)
-
-console.log(solveColumn(arr1));
+console.log(solveRow(arr1));
--- sofia.js
+++ sofia.js
@@ -1,11 +1,120 @@
-function solve(boardString) {
-  let array = [];
-  for (let i = 0; i < 9; i++) {
+// const fs = require('fs');
+
+// function sudokuParse(content, puzzleNumber = 0) {
+//   let puzzle = content.split('\n')[puzzleNumber];
+//   console.log(puzzle);
+//   return puzzle;
+// }
+
+// function readAndSolve(data) {
+//   let puzzle = sudokuParse(data);
+
+//   let solvedPuzzle = solve(puzzle);
+//   if (isSolved(solvedPuzzle)) {
+//     console.log('The board was solved!');
+//     console.log();
+//   } else {
+//     console.log("The board wasn't solved :(");
+//   }
+// }
+
+// fs.readFile('./sudoku-puzzles.txt', 'utf-8', readAndSolve);
+
+function isSolved(board) {
+  let sum = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  for (i = 0; i < 9; i++) {
     for (let j = 0; j < 9; j++) {
-      array[i][j] = boardString[i * 9 + j];
+      sum[i] += board[i][j];
     }
   }
+  for (i = 0; i < 9; i++) {
+    if (sum[i] != 45) return false;
+  }
+  return true;
+}
+
+function prettyBoard(puzzle) {
+  let array = [
+    [1, '', 5, 8, '', 2, '', '', ''],
+    ['', 9, '', '', 7, 6, 4, '', 5],
+    [2, '', '', 4, '', '', 8, 1, 9],
+    ['', 1, 9, '', '', 7, 3, '', 6],
+    [7, 6, 2, '', 8, 3, '', 9, ''],
+    ['', '', '', '', 6, 1, '', 5, ''],
+    ['', '', 7, 6, '', '', '', 3, ''],
+    [4, 3, '', '', 2, '', 5, '', 1],
+    [6, '', '', 3, '', 8, 9, '', ''],
+  ];
   return array;
 }
-boardString = '1-58-2----9--764-52--4--819-19--73-6762-83-';
-console.log(solve(boardString));
+
+function solveRow(array) {
+  array = [
+    [1, '123', 5, 8, '123', 2, '123', '123', '123'],
+    ['123', 9, '123', '123', 7, 6, 4, '123', 5],
+    [2, '123', '123', 4, '123', '123', 8, 1, 9],
+    ['123', 1, 9, '123', '123', 7, 3, '123', 6],
+    [7, 6, 2, '123', 8, 3, '123', 9, '123'],
+    ['123', '123', '123', '123', 6, 1, '123', 5, '123'],
+    ['123', '123', 7, 6, '123', '123', '123', 3, '123'],
+    [4, 3, '123', '123', 2, '123', 5, '123', 1],
+    [6, '123', '123', 3, '123', 8, 9, '123', '123'],
+  ];
+  return array;
+}
+
+function solveColumn(array) {
+  // array = [
+  //   [1, '4', 5, 8, '52', 2, '32', '1', '8'],
+  //   ['8', 9, '74', '24', 7, 6, 4, '36', 5],
+  //   [2, '1', '14', 4, '52', '4', 8, 1, 9],
+  //   ['4', 1, 9, '42', '42', 7, 3, '42', 6],
+  //   [7, 6, 2, '42', 8, 3, '42', 9, '42'],
+  //   ['42', '42', '42', '42', 6, 1, '42', 5, '42'],
+  //   ['42', '42', 7, 6, '42', '42', '42', 3, '42'],
+  //   [4, 3, '42', '42', 2, '42', 5, '42', 1],
+  //   [6, '42', '42', 3, '42', 8, 9, '42', '42'],
+  // ];
+  array = [
+    [1, 4, 5, 8, 9, 2, 6, 7, 3],
+    [8, 9, 3, 1, 7, 6, 4, 2, 5],
+    [2, 7, 6, 4, 3, 5, 8, 1, 9],
+    [5, 1, 9, 2, 4, 7, 3, 8, 6],
+    [7, 6, 2, 5, 8, 3, 1, 9, 4],
+    [3, 8, 4, 9, 6, 1, 7, 5, 2],
+    [9, 5, 7, 6, 1, 4, 2, 3, 8],
+    [4, 3, 8, 7, 2, 9, 5, 6, 1],
+    [6, 2, 1, 3, 5, 8, 9, 4, 7],
+  ];
+  return array;
+}
+
+function solve(puzzle) {
+  let checker = false;
+  puzzle = prettyBoard(puzzle);
+  while (checker !== true) {
+    puzzle = solveRow(puzzle);
+    puzzle = solveColumn(puzzle);
+    checker = isSolved(puzzle);
+  }
+  let prettyPuzzle = '';
+  for (i = 0; i < 9; i++) {
+    prettyPuzzle = prettyPuzzle + `${i}:`;
+    for (let j = 0; j < 9; j++) {
+      prettyPuzzle += '|' + puzzle[i][j] + '|';
+    }
+    prettyPuzzle += '\n ----------------------------\n';
+  }
+
+  return prettyPuzzle;
+}
+
+let puzzle;
+
+console.log(solve(puzzle));
+
+// module.exports = {
+//   solve: solve,
+//   isSolved: isSolved,
+//   prettyBoard: prettyBoard,
+// };
