382d3ecab738cd76f63b6c336b16d512a69fadba
sofialapteva <sofia_lapteva@mail.ru>
2020-11-09 12:34:47 +0300

    plan

--- sofia.js
+++ sofia.js
@@ -1,5 +1,11 @@
-function greetings() {
-  console.log('Hello from Sofia');
+function solve(boardString) {
+  let array = [];
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      array[i][j] = boardString[i * 9 + j];
+    }
+  }
+  return array;
 }
-
-module.exports = { greetings };
+boardString = '1-58-2----9--764-52--4--819-19--73-6762-83-';
+console.log(solve(boardString));
