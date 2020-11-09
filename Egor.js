cbc0abcc3506e5e9923de8edc03c8709bbfdca26
Egor <egor.semenov.94@bk.ru>
2020-11-09 14:39:05 +0300

    Egor stringToArr

--- Egor.js
+++ Egor.js
@@ -1,1 +1,15 @@
-let a = 'Egor'
+
+
+
+function stringToArr(string) {
+
+  let b = string.split("")
+  const arr = [];
+  for (i = 0; i < b.length; i++) {
+    arr.push(b.slice(i, i + 9))
+  }
+  return arr;
+}
+
+
+module.exports = {stringToArr}
