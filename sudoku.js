be985bce051af7f912150ef6316140466c7a6240
Alla Yefremova <yefremova.alla@gmail.com>
2020-12-04 16:33:10 +0300

    Function strToArr

--- sudoku.js
+++ sudoku.js
@@ -1,12 +1,26 @@
 // Takes a board as a string in the format
-// you see in the puzzle file. Returns
+// you see in the puzzle file. Returns)
 // something representing a board after
 // your solver has tried to solve it.
 // How you represent your board is up to you!
-function solve(boardString) {
 
+let str = '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--';
+
+function strToArr(boardString) {
+	let newStr = boardString.replace(/-/g, '0');
+	let newArr = newStr.split('');
+	subArr = [];
+
+	for (let i = 0; i < newArr.length/9; i++) {
+		subArr[i] = newArr.slice(i*9, i*9 + 9);
+	}
+
+	return subArr;
 }
 
+let arrSudoku = strToArr(str);
+
+// console.log(arrSudoku);
 
 // Returns a boolean indicating whether
 // or not the provided board is solved.
@@ -15,7 +29,6 @@ function solve(boardString) {
 function isSolved(board) {
 
 }
-
 
 // Takes in a board in some form and
 // returns a String that's well formatted
