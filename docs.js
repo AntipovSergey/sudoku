e4a26a1ee44d861b0aa229f2bf4a439220edc005
Sofya <shesofya@gmail.com>
2020-12-04 12:08:12 +0300

    Add file structure

--- docs.js
+++ docs.js
@@ -1,9 +1,13 @@
 const bigSquares = getBigSquares(sudoku);
 // { sq0: [], sq1: [], ...}
 
+const boardGuesses = {
+
+};
+
 const fillIteration = sudoku => {
-	for (let row = 0; row < sudoku.length; row++) {
-		for (let col = 0; col < sudoku[row].length; col++) {
+	for (let row = 0; row < sudoku.length; row += 3) {
+		for (let col = 0; col < sudoku[row].length; col += 3) {
 			if(sudoku[row][col]) {
 				continue;
 			} else if (!sudoku[row][col]) {
@@ -13,7 +17,7 @@ const fillIteration = sudoku => {
 				sq = getSquare(sudoku[row][col])
 				c = getexceptionsFromSq(sq)	// Set
 				exceptions.push(a, b, c);
-				if (exceptions contain 8 digits) {
+				if (exceptions.length === 8) {
 					sudoku[row][col] = getTheOne();
 				}
 			}
@@ -39,3 +43,22 @@ const sudokuToArray = () => {}
 const sudokuToString = () => {}
 
 sudokuArray = sudokuToArray(string)
+
+
+[
+	{
+		init: true,
+		value: 1,
+		row: 1,
+		col: 3,
+		sq: 2
+	},
+	{
+		init: false,
+		value: 0,
+		guesses: [1, 3, 4, 5],
+		row: 4,
+		col: 3,
+		sq: 4
+	}
+]
---
+++ helpers.js
@@ -0,0 +1,26 @@
+
+// Саша
+const sudokuToArray = () => {}
+
+// Саша
+const sudokuToString = () => {}
+
+// Соня
+const sudokuToMap = (sudokuArray) => {} // return sudokuDB [{}]
+//Большая функция, потребуются мельче внутри
+
+// Влад
+const getexceptionsFromRow = (sudoku, row) => {} // []
+
+//Влад
+const getexceptionsFromCol = (sudoku, row) => {} // []
+
+// Влад
+const getExceptionsFromSquare = (sudoku, sq) => {} // []
+
+// Влад
+const getAllExceptions = (setRow, setCol, setSq) => {} // Set 
+
+// Саша
+const getGuesses = (exceptions) => {} // guesses []
+
