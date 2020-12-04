63a04b1047100cb66300e94a28648919649b53ea
Alexander Blinkov <tingol88@yandex.ru>
2020-12-04 13:53:37 +0300

    impliment sudokuToArray

--- helpers.js
+++ helpers.js
@@ -1,14 +1,22 @@
 // Саша
+const sudokuToArray = (str = '') => {
+  let newArr = str.split('').map(item => Number(item) || 0)
+  let res = [];
+  for (let i = 0; i < 9; i++) {
+    res.push(newArr.splice(0, 9));
+  }
+  return res;
+}
 
-const sudokuToArray = () => {}
- // someth form Alex - test
+//console.log(sudokuToArrayX('1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--'))
+
 
 // Саша
-const sudokuToString = () => {};
+const sudokuToString = () => { };
 
 // Соня
 const sudokuToMap = (sudokuArray) => {
-	return {};
+  return {};
 } // return sudokuDB [{}]
 
 //Большая функция, потребуются мельче внутри
@@ -24,10 +32,10 @@ const getexceptionsFromCol = (sudoku, row) => {
 }; // []
 
 // Влад
-const getExceptionsFromSquare = (sudoku, sq) => {}; // []
+const getExceptionsFromSquare = (sudoku, sq) => { }; // []
 
 // Влад
-const getAllExceptions = (setRow, setCol, setSq) => {}; // Set
+const getAllExceptions = (setRow, setCol, setSq) => { }; // Set
 
 // Саша
-const getGuesses = (exceptions) => {}; // guesses []
+const getGuesses = (exceptions) => { }; // guesses []
