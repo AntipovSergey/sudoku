d0c7ff54e5d963fac0aee8d5c21910429ce415e5
Sofya <shesofya@gmail.com>
2020-12-04 14:53:36 +0300

    Initialize solve and checkGuess, not ready

--- solver.js
+++ solver.js
@@ -1,2 +1,32 @@
-// solve() - Главная функция
-// checkGuess() - рекурсивная функция
+const {
+	sudokuToArray,
+	sudokuToString,
+	sudokuToMap,
+	getSquare,
+	getexceptionsFromRow,
+	getexceptionsFromCol,
+	getExceptionsFromSquare,
+	getAllExceptions,
+	getGuesses
+} = require('./helpers');
+
+const checkGuess = () => {
+
+};
+
+const solve = (sudoku) => {
+	const sudokuDB = sudokuToMap(sudokuToArray(sudoku));
+
+	for (let i = 0; i < sudokuDB.length; i++) {
+		const cell = sudokuDB[i];
+
+		if (cell.value) { // Если значение клетки не 0 - идем на следующую итерацию
+			continue;
+		}
+
+		const exceptions = getAllExceptions(i, sudokuDB);
+		cell.guesses = getGuesses(cell, exceptions);
+	}
+};
+
+module.exports = { solve };
