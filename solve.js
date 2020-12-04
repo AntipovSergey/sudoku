433658259539558fc0cd848a952776c0f4c4859f
Yuriy <uradjachkov@gmail.com>
2020-12-04 14:57:04 +0300

    Тест

--- solve.js
+++ solve.js
@@ -1,9 +1,18 @@
-// Takes a board as a string in the format
-// you see in the puzzle file. Returns
-// something representing a board after
-// your solver has tried to solve it.
-// How you represent your board is up to you!
 function solve(boardString) {
-
+  let arrOfNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  for (let i = 0; i < boardString.length; i++) {
+    let newArr = []
+    for (let j = 0; j < boardString.length; j++)
+      if (boardString[i][j] > 0) {
+        newArr.push(boardString[i][j])
+      } else {
+        newArr.push()
+        console.log(newArr);
+      }
+    return newArr
+  }
 }
-
+console.log(solve([
+  [4, 2, 0, 0, 0],
+  [2, 3, 4, 0, 1]
+]))
