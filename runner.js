375bebd43a934c8c3b12385d754463ee1f651264
chalexan <cherednychenko.alexandr@gmail.com>
2020-12-14 19:11:17 +0300

    fix bugs_final

--- runner.js
+++ runner.js
@@ -20,24 +20,12 @@ function readAndSolve(err, data) {
   if (err) {
     throw err;
   }
-  const rightBoard =
-  [
-    [1, 4, 5, 8, 9, 2, 6, 7, 3],
-    [8, 9, 3, 1, 7, 6, 4, 2, 5],
-    [2, 7, 6, 4, 3, 5, 8, 1, 9],
-    [5, 1, 9, 2, 4, 7, 3, 8, 6],
-    [7, 6, 2, 5, 8, 3, 1, 9, 4],
-    [3, 8, 4, 9, 6, 1, 7, 5, 2],
-    [9, 5, 7, 6, 1, 4, 2, 3, 8],
-    [4, 3, 8, 7, 2, 9, 5, 6, 1],
-    [6, 2, 1, 3, 5, 8, 9, 4, 7]
-  ];
   let puzzle = sudokuParse(data, 0);
   let solvedPuzzle = sudoku.solve(puzzle);
 
   if (sudoku.isSolved(solvedPuzzle)) {
     console.log(sudoku.prettyBoard(solvedPuzzle));
-    if (sudoku.globalCheck(rightBoard)) {
+    if (sudoku.globalCheck(solvedPuzzle)) {
       console.log("The board was solved!");
     } else {
     console.log("The board wasn't solved :(");
