972fd8113eb166780313a606995752e9bc0703cc
alubochkin <alubochkin@gmail.com>
2020-12-15 18:04:54 +0300

    merge

--- sudokuTab.js
+++ sudokuTab.js
@@ -1,3 +1,8 @@
+// let boardString = ''---------------------------------------------------------------------------------''
+// let boardString = '--7--8------2---6-65--79----7----3-5-83---67-2-1----8----71--38-2---5------4--2--'
+// let boardString =  '----------2-65-------18--4--9----6-4-3---57-------------------73------9----------'
+
+
 let boardString = '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--';
 let data = [];
 function solve(boardString) {
@@ -38,7 +43,7 @@ let btn = document.getElementById('btnTab');
 
 btn.addEventListener('click', () => { 
   isSolved(data); 
-  render(data, inputColection, 50)
+  render(data, inputColection, 200)
 
 });
 
