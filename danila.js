beb2364eaac8164b76ee2de6ad3159a64117c217
Artnagornyi <artnagornyi@gmail.com>
2021-01-15 16:35:24 +0300

    Release 2: Create function renderSudoku

--- danila.js
+++ danila.js
@@ -1,15 +1,3 @@
-const place = [
-  ['1', null, '5', '8', null, '2', null, null, null],
-  [null, '9', null, null, '7', '6', '4', null, '5'],
-  ['2', null, null, '4', null, null, '8', '1', '9'],
-  ['2', null, null, '4', null, null, '8', '1', '9'],
-  ['2', null, null, '4', null, null, '8', '1', '9'],
-  ['2', null, null, '4', null, null, '8', '1', '9'],
-  ['2', null, null, '4', null, null, '8', '1', '9'],
-  ['2', null, null, '4', null, null, '8', '1', '9'],
-  ['2', null, null, '4', null, null, '8', '1', '9'],
-];
-
 function setStringInTwoArr(inputArray, coordinates) {
   const coordArray = coordinates.split(',');
   let y = coordArray[0];
@@ -31,11 +19,19 @@ function setStringInTwoArr(inputArray, coordinates) {
   return str;
 }
 
-console.log(setStringInTwoArr(place, '0,3'));
-console.log(typeof setStringInTwoArr(place, '0,3'));
+function renderSudoku(nestedArray, objectSquare) {
+  let exportArray = [];
 
-function renderSudoku(nestedArray, ) {
-
+  for (let key in objectSquare) {
+    let y = +key[0];
+    let x = +key[2];
+    nestedArray[y][x] = objectSquare[key];
+  }
+  return exportArray;
 }
 
-// module.exports = {setStringInTwoArr}
+
+module.exports = {
+  setStringInTwoArr,
+  renderSudoku,
+};
