3aa059d1c6485da3e3a57871cf0b5d0e61594a9a
Ayuna <aunmiku@gmail.com>
2021-01-15 15:10:33 +0300

    Функция перевода строки в массив

--- sudoku.js
+++ sudoku.js
@@ -11,11 +11,38 @@ const arrTest = [
 console.log(arrTest);
 
 
+function strToArray (boardStr){
+  let board = [];
+  let nBoard = 9; //размерность судоку
+  let k = 0; 
+  let subStr;
+  for (let i=0; i<8; i++){
+    if (k<boardStr.length-9){
+      subStr = boardStr.substring(k, k+9).split('');
+      arr.push(subStr);
+      // arr.push(boardString.substring(k, k+9).split['']);
+      
+    } else {
+      
+      arr.push(boardStr.substring(k, k+8).split(''));
+      arr.push(boardStr[boardStr.length-1]);
+    }
 
-function solve(boardString) {
+    k=k+9;
+    
+  }
+
+  console.log(arr);
 
 }
 
+let board1 = "1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--";
+strToArray(board1);
+
+function solve(boardString) {
+  
+  
+}
 
 // Returns a boolean indicating whether
 // or not the provided board is solved.
