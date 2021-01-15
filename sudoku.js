a10a3bdaca076e97fa8e889340c192d6b313ccbb
PAV <popov.pav.aleksei@gmail.com>
2021-01-15 17:01:46 +0300

    Function Solve

--- sudoku.js
+++ sudoku.js
@@ -16,24 +16,34 @@ function strToArray(boardStr) {
   let nBoard = 9; // размерность судоку
   let k = 0;
   let subStr;
-  for (let i = 0; i < 8; i++) {
+  for (let i = 0; i < nBoard; i++) {
     if (k < boardStr.length - 9) {
       subStr = boardStr.substring(k, k + 9).split('');
-      arr.push(subStr);
+      board.push(subStr);
       // arr.push(boardString.substring(k, k+9).split['']);
     } else {
-      arr.push(boardStr.substring(k, k + 8).split(''));
-      arr.push(boardStr[boardStr.length - 1]);
+      board.push(boardStr.substring(k, k + 8).split(''));
+      board.push(boardStr[boardStr.length - 1]);
     }
-
     k += 9;
   }
+  return board
 
-  console.log(arr);
+  // console.log(arr);
 }
 
 function search(board, emptyPoints, emptyPointIndex) {
-
+  for (let i = 0; i < emptyPoints.length; i++) {
+    q = [1,2,3,4,5,6,7,8,9];
+    for (let j = 0; j< q.length; j++) {
+      if (isPossibleColumn(abc)) {
+        let a =  isPossibleRow(abc)
+        if (a) {
+          board[i] = q[j]
+        }
+      }
+    }
+  }
 }
 
 function findEmpty(board) {
@@ -52,7 +62,9 @@ let board1 = "1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43-
 strToArray(board1);
 
 function solve(boardString) {
-
+  const board = strToArray(boardString);
+  const emptySpaces = findEmpty(board);
+  const solvedSudoku = search(board, emptySpaces, 0);
 }
 
 // Returns a boolean indicating whether
@@ -63,6 +75,25 @@ function isSolved(board) {
 
 }
 
+function isPossibleRow(board, numRow, target){
+  for (let i = 0; i < board.length; i++) {
+    if(board[numRow][i] === target){
+      return false
+    }
+  }
+  return true
+}
+
+function isPossibleColumn(board, numCol, target){
+  for (let i = 0; i < board.length; i++){
+    if (board[i][numCol] === target){
+      return false
+    }
+  }
+  return true
+}
+
+
 
 // Takes in a board in some form and
 // returns a String that's well formatted
@@ -70,9 +101,13 @@ function isSolved(board) {
 // The input board will be in whatever
 // form `solve` returns.
 function prettyBoard(board) {
-
-
+  let newArr = board.split(“”).map((item) => Number(item) || 0);
+  for (let i = 0; i < 9; i++) {
+    newArr.push(newArr.splice(0, 9));
+  }
+  return newArr;
 }
+
 
 // Exports all the functions to use them in another file.
 module.exports = {
