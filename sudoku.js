d45eb4287e77adc28a9a106e9a5c94add7264b6e
Roman Borodulin <bubinbei@yandex.ru>
2021-01-15 13:44:02 +0300

    sting to arr

--- sudoku.js
+++ sudoku.js
@@ -4,9 +4,25 @@
 // your solver has tried to solve it.
 // How you represent your board is up to you!
 function solve(boardString) {
+  let sud = "1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--"
 
+  let s = 0
+  sud.split('').join("")
+  let arrResult = [] //
+  for (let i = 0 ; i < 9 ; i++){
+    let arr = []
+    for(let m = 0;m < 9; m++) {
+      arr.push(sud.replace(/\-/g,'0').split('')[m+s])  
+    }
+    s+=9
+    arrResult.push(arr)
+  }
+  console.log(arrResult);
+
+
+ 
 }
-
+solve()
 
 // Returns a boolean indicating whether
 // or not the provided board is solved.
