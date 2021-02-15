e302e67ca1d3e361c7a6061ba97e17a6130bd414
komisdari <darijana701@gmail.com>
2021-02-15 16:12:19 +0300

    Verticalnii

--- sudoku.js
+++ sudoku.js
@@ -6,7 +6,20 @@
 function solve(boardString) {
 
 }
+const checkVertical = (board, y, x) => {
+  let allvariants = ['1','2','3','4','5','6','7','8','9'];
+  let news = [];
+  for(let i = 0; i < board.length; i += 1){
+    // allvariants = allvariants.filter(el => el !== board[i][x]);
+    if (board[i][x].push(news)) {
+      return false;
+    }
+  }
+  return true;
+  // return allvariants;
+} 
 
+console.log(checkVertical(board, 0,2))
 
 // Returns a boolean indicating whether
 // or not the provided board is solved.
