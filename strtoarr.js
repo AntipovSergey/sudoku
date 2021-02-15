6a03ea016ea653af360871b5b0f1dc27b5ba654d
Andrei <ad933@mail.ru>
2021-02-15 13:36:45 +0400

    module exports Array

--- strtoarr.js
+++ strtoarr.js
@@ -1,9 +1,9 @@
-let sudoku =  '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--'
-
-const strToArr = () => {
+const strToArr = (sudoku) => {
   const sudokuArr = [];
   let result = sudoku.split('')
   for (let i = 0;i<result.length;i++) {
     sudokuArr.push(result.splice(0,9));
   }
 }
+
+module.exports = strToArr;
