eb59ede7d904267515fd975c416035e792ceece2
Oksana Klymenok <kapris15@yahoo.com>
2021-02-14 23:44:20 -1000

    Changed

--- isInVertical.js
+++ isInVertical.js
@@ -1,9 +1,8 @@
-function isInVertical(value, y, x, board){
-  for (let i=0; i<board.lenght; i++){
-      if (board[i][x]===value)
-      return false;
+function isInVertical(value, y, x, board) {
+  for (let i = 0; i < board.lenght; i++) {
+    if (board[i][x] === value) return false
   }
-  return true;
+  return true
 }
 
-module.exports=isInVertical;
+module.exports = isInVertical
