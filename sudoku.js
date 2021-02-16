0f54f0d00c52a626c0bf87a300947bd8e0e2c532
Dima <shetkodv@gmail.com>
2021-02-16 11:25:56 +0300

    Sudoku

--- sudoku.js
+++ sudoku.js
@@ -12,73 +12,80 @@ function createBoard(str) {
 }
 
 function findEmpty(matrix) {
-  let result = null;
   for (let i = 0; i < matrix.length; i += 1) {
     for (let j = 0; j < matrix.length; j++) {
-      if(matrix[i][j] === '-'){
+      if (matrix[i][j] === '-') {
         return [i, j];
       }
     }
   }
+  return null;
 }
 
 const checkVertical = (board, x) => {
-  let allvariants = ['1','2','3','4','5','6','7','8','9'];
-  for(let i = 0; i < board.length; i += 1){
-     allvariants = allvariants.filter(el => el !== board[i][x]);
+  let allvariants = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  for (let i = 0; i < board.length; i += 1) {
+    allvariants = allvariants.filter(el => el !== board[i][x]);
   }
   return allvariants;
-} 
+}
 
 const checkGorizontal = (board, y, arr) => {
   let allvariants = arr;
-  for(let i = 0; i < board.length; i += 1){
-     allvariants = allvariants.filter(el => el !== board[y][i]);
+  for (let i = 0; i < board.length; i += 1) {
+    allvariants = allvariants.filter(el => el !== board[y][i]);
   }
   return allvariants;
-} 
+}
 
-const board = [
-  ['1','-','5','8','-','2','-','-','-'],
-  ['-','9','-','-','7','6','4','-','5'],
-  ['2','-','-','4','-','-','8','1','9'],
-  ['-','1','9','-','-','7','3','-','6'],
-  ['7','6','2','-','8','3','-','9','-'],
-  ['-','-','-','-','6','1','-','5','-'],
-  ['-','-','7','6','-','-','-','3','-'],
-  ['4','3','-','-','2','-','5','-','1'],
-  ['6','-','-','3','-','8','9','-','-']
-  ] 
+function blockCheckout(board, arrValue, x, y) {
+  const newArr = [];
+  const boxRow = Math.floor(x / 3) * 3;
+  const boxCol = Math.floor(y / 3) * 3;
+
+
+  for (let i = boxRow; i < boxRow + 3; i++) {
+    for (let j = boxCol; j < boxCol + 3; j++) {
+      if(board[i][j] !== '-'){
+        newArr.push(board[i][j])
+      }
+    }
+  }
+
+  const result = arrValue.filter(el => !el.includes(newArr));
+  // console.log(result);
+  // console.log(board[x][y]);
+
+  board[x][y] = result[0];
+  // console.log(board);
+  return board;
+
+}
+
+
+
 
 
 // How you represent your board is up to you!
 function solve(boardString) {
   const board = createBoard(boardString);
+  
   const coord = findEmpty(board);
+  if(!findEmpty(board)) return board; 
 
   const [x, y] = coord;
 
   const vertical = checkVertical(board, y);
   const gorizontal = checkGorizontal(board, x, vertical);
 
-  console.log(gorizontal);
+  const value =  blockCheckout(board, gorizontal, x, y);
+  console.log(value);
 
 }
 let text = '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--'
-
 solve(text)
 
-function blockCheckout(board) {
-  let newArr = [];
-  for (let j = 0; j < board.length; j++) {
-    let arr = [];
-    for (let i = 0; i < board[j].length; i++) {
-      arr.push(board[j][i]);
-    }
-    newArr.push(arr);
-  }
-  return newArr;
-}
+
 
 // Returns a boolean indicating whether
 // or not the provided board is solved.
@@ -100,7 +107,24 @@ function prettyBoard(board) {
 
 // Exports all the functions to use them in another file.
 module.exports = {
-	solve: solve,
-	isSolved: isSolved,
-	prettyBoard: prettyBoard
+  solve: solve,
+  isSolved: isSolved,
+  prettyBoard: prettyBoard
 }
+
+
+// const board = [
+//   ['-', '-', '5', '-', '3', '-', '-', '8', '1'],
+//   ['9', '-', '2', '8', '5', '-', '-', '6', '-'],
+//   ['6', '-', '-', '-', '-', '4', '-', '5', '-'],
+//   ['-', '-', '7', '4', '-', '2', '8', '3', '-'],
+//   ['3', '4', '9', '7', '6', '-', '-', '-', '5'],
+//   ['-', '-', '8', '3', '-', '-', '4', '9', '-'],
+//   ['1', '5', '-', '-', '8', '7', '-', '-', '2'],
+//   ['-', '9', '-', '-', '-', '-', '6', '-', '-'],
+//   ['-', '2', '6', '-', '4', '9', '5', '-', '3']
+// ]
+
+
+
+
