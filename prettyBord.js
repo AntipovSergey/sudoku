ef6f3151c965bbb5fc491d3a0a784ae6b05a6501
Eugene <ondrinevg@gmail.com>
2021-02-19 16:30:22 +0300

    Prodaction cod

--- prettyBord.js
+++ prettyBord.js
@@ -2,18 +2,22 @@
 
 function stringToArray(sudoku) {
 
-   const lengthStr = Math.sqrt(sudoku.length);
-   const result = [];
-   let arr = [];
+  const lengthStr = Math.sqrt(sudoku.length);
+  const result = [];
+  let arr = [];
 
-   for (let i = 1; i < sudoku.length + 1; i++) {
-      arr.push(sudoku[i - 1]);
+  for (let i = 1; i < sudoku.length + 1; i++) {
+    arr.push(sudoku[i - 1]);
 
-         if (i % lengthStr === 0) {
-            result.push(arr);
-            arr = [];
-         }
-      }
-      return result;
+    if (i % lengthStr === 0) {
+      result.push(arr);
+      arr = [];
+    }
+  }
+  return result;
 };
+
+module.exports = {
+  stringToArray
+}
 
--- spec/sudoku.spec.js
+++ spec/sudoku.spec.js
@@ -1,2 +1,19 @@
-const sudoku = require('../sudoku.js')
+const sudoku = require('../sudoku.js');
+const { validation } = require('../validation');
+
+describe ('Вызов функции и тест', () => {
+  let board;
+  const str = '----------2-65-------18--4--9----6-4-3---57-------------------73------9----------';
+  const strLength = Math.sqrt(str.length);
+
+  const tempArr = str.replace(new RegExp(`(.{` + strLength + `})`, 'g'), function (p1) { return p1 + '\n' }).split('\n');
+  tempArr.length = strLength;
+  const newArr = tempArr.map(element => element.split(''));
+  beforeEach(() => {
+  board = newArr;
+  });
+  it('Проверка валидации', () => {  
+    expect(validation(board, [0, 0], '-')).toBe(false);
+  });
+})
 
