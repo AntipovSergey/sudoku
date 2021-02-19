86914e24e777188f6595168f88a79ab00e15440f
Yushachkov01 <a_yushachkov@mail.ru>
2021-02-19 14:27:08 +0300

    Added comment

--- fillBlock.js
+++ fillBlock.js
@@ -1,19 +1,8 @@
 function fillBlock(array,num,i,j){
   if(isHorizontal() && isVertucal() && isBoxGood()) {
-      let myResult = "";
-    
-      array.forEach((element) => {
-        if (Array.isArray(element)) {
-          myResult += crawlTreeFirst(element);
-        } else {
-          myResult += element;
-          // console.log(myResult);
-        }
-      });
-      return myResult;
-    }
   }
-
+}
+//wkdfl,ce;sdlf
 
     module.exports = {
       fillBlock,
