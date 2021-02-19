3ee7554cc942b84706ee01dfec45d8e33c42cf5b
Zhurkin_Alex <lexus1812141@gmail.com>
2021-02-19 13:02:04 +0300

    Function-Search

--- sudoku.js
+++ sudoku.js
@@ -1,34 +1,27 @@
-// Takes a board as a string in the format
-// you see in the puzzle file. Returns
-// something representing a board after
-// your solver has tried to solve it.
-// How you represent your board is up to you!
-function solve(boardString) {
 
+let array = [
+ [ '1', '2', '5', '8', '7', '2', '8', '9', '4'],
+ [ '7', '1', '-', '-', '5', '4', '-', '6', '-']
+]
+
+let search = function placeNumber(board) {
+  for(let str=0; str<board.length; str+=1){
+    for(let col=0; col<board[str].length; col+=1 ){
+      // console.log(board[str][col]);
+      if(board[str][col] == '-'){
+        return [str, col]
+      }
+    }
+  }
 }
 
-
-// Returns a boolean indicating whether
-// or not the provided board is solved.
-// The input board will be in whatever
-// form `solve` returns.
-function isSolved(board) {
-
-}
+console.log(search(array))
 
 
-// Takes in a board in some form and
-// returns a String that's well formatted
-// for output to the screen.
-// The input board will be in whatever
-// form `solve` returns.
-function prettyBoard(board) {
 
-}
 
-// Exports all the functions to use them in another file.
-module.exports = {
-	solve: solve,
-	isSolved: isSolved,
-	prettyBoard: prettyBoard
-}
+// module.exports = {
+// 	solve: solve,
+// 	isSolved: isSolved,
+// 	prettyBoard: prettyBoard
+// }
