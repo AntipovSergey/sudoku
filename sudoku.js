6652be31e4bd58a7dc0f4bbbece1acb933d69689
IvanShchadov <shchadov.ivan@gmail.com>
2021-02-19 12:38:40 +0300

    Function createArray created

--- sudoku.js
+++ sudoku.js
@@ -1,10 +1,26 @@
+string = '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--'
+
+function createArray(string) {
+  let array = string.split('');
+  let newArray = [];
+  for (let i=0; i<9; i++) {
+    newArray.push(array.splice(0, 9));
+  }
+  
+  return newArray
+}
+
+console.table(createArray(string))
+
+
 // Takes a board as a string in the format
 // you see in the puzzle file. Returns
 // something representing a board after
 // your solver has tried to solve it.
 // How you represent your board is up to you!
-function solve(boardString) {
 
+function solve(boardString) {
+  
 }
 
 
@@ -13,7 +29,7 @@ function solve(boardString) {
 // The input board will be in whatever
 // form `solve` returns.
 function isSolved(board) {
-
+  
 }
 
 
