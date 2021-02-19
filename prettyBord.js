b0c31d5f05b41c0e180015563b8103f5ca24e3f2
IluhaByrm81 <iluhaburm@yandex.ru>
2021-02-19 15:41:30 +0300

    stringToArray

---
+++ prettyBord.js
@@ -0,0 +1,19 @@
+//  преобразование исходных данных в массив
+
+function stringToArray(sudoku) {
+
+   const lengthStr = Math.sqrt(sudoku.length);
+   const result = [];
+   let arr = [];
+
+   for (let i = 1; i < sudoku.length + 1; i++) {
+      arr.push(sudoku[i - 1]);
+
+         if (i % lengthStr === 0) {
+            result.push(arr);
+            arr = [];
+         }
+      }
+      return result;
+};
+
