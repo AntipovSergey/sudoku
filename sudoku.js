1aa4ccd1098449616648d1fe55fdc64d00dc7ad0
Daniil <a336nm@gmail.com>
2021-03-19 17:03:35 +0300

    rel n+1

--- sudoku.js
+++ sudoku.js
@@ -7,7 +7,7 @@ function solve(boardString) {
 
 }
 
-let puzzle = '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--'
+// let puzzle = '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--'
 
 function sudokuData (puzzle) {
   let puzzleArr = puzzle.match(/.{1,9}/g)
@@ -37,7 +37,7 @@ function sudokuData (puzzle) {
   return sudoku
 }
   
-// console.table(sudokuData(puzzle))
+// console.table(sudokuData(puzzle))git 
 
 
 
