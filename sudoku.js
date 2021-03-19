9475112419f2a53b6ddc565ac223f0064b7439d4
Daniil <a336nm@gmail.com>
2021-03-19 16:26:07 +0300

    func sudokuData

--- sudoku.js
+++ sudoku.js
@@ -7,16 +7,38 @@ function solve(boardString) {
 
 }
 
+let puzzle = '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--'
+
 function sudokuData (puzzle) {
   let puzzleArr = puzzle.match(/.{1,9}/g)
   let puzzleArrData = []
   for (let i=0; i<puzzleArr.length; i++) {
     puzzleArrData.push(puzzleArr[i].split(''))
   }
-  
-  
-  return puzzleArrData
+  let sudoku = []
+  let nums = [1,2,3,4,5,6,7,8,9]
+  for (let i=0; i<puzzleArrData.length; i++) {
+    for (let j=0; j<puzzleArrData.length; j++) {
+      let obj = {
+        row: i,
+        col: j,
+        boxNum: [(Math.floor(i/Math.sqrt(puzzleArrData.length))), (Math.floor(j/Math.sqrt(puzzleArrData.length)))]
+      }
+      if (puzzleArrData[i][j]=== '-') {
+        obj.numbers = nums
+        obj.isInitNum = false
+      } else {
+        obj.numbers = puzzleArrData[i][j]
+        obj.isInitNum = true
+      } 
+      sudoku.push(obj)
+    } 
+  }
+  return sudoku
 }
+  
+console.table(sudokuData(puzzle))
+
 
 
 // Returns a boolean indicating whether
