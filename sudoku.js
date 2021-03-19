4f394308702f2b52e635859a33519fa1fc6b9726
Alex Frankenstein <aleksej@MacBook-Pro-Alex.local>
2021-03-19 12:01:18 +0300

    solve

--- sudoku.js
+++ sudoku.js
@@ -1,34 +1,50 @@
-// Takes a board as a string in the format
-// you see in the puzzle file. Returns
-// something representing a board after
-// your solver has tried to solve it.
-// How you represent your board is up to you!
-function solve(boardString) {
+let a = prettyBoard(
+  "----------2-65-------18--4--9----6-4-3---57-------------------73------9----------"
+);
+solve(a);
+console.table(a);
 
+function solve(board) {
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      if (board[i][j] == "-") {
+        for (let s = 1; s <= 9; s++) {
+          if (isSolved(board, i, j, s)) {
+            board[i][j] = `${s}`;
+            if (solve(board)) {
+              return true;
+            } else {
+              board[i][j] = "-";
+            }
+          }
+        }
+        return false;
+      }
+    }
+  }
+  return true;
+}
+function isSolved(board, row, col, s) {
 }
 
-
-// Returns a boolean indicating whether
-// or not the provided board is solved.
-// The input board will be in whatever
-// form `solve` returns.
-function isSolved(board) {
-
-}
-
-
-// Takes in a board in some form and
-// returns a String that's well formatted
-// for output to the screen.
-// The input board will be in whatever
-// form `solve` returns.
 function prettyBoard(board) {
+  let side = Math.sqrt(board.length);
+  let res = [];
 
+  for (let i = 0; i < board.length; i += side) {
+    let a = board.slice(-board.length + i, -board.length + side + i);
+    res.push(a.split(""));
+  }
+
+  res.pop();
+  let a = board.slice(-side);
+  res.push(a.split(""));
+
+  return res;
 }
 
-// Exports all the functions to use them in another file.
 module.exports = {
-	solve: solve,
-	isSolved: isSolved,
-	prettyBoard: prettyBoard
-}
+  solve: solve,
+  isSolved: isSolved,
+  prettyBoard: prettyBoard,
+};
