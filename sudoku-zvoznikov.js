a1106935c2391ab847f17b7a16442fe5120d9dc7
AndreyZvoznikov <ag@elbrusboot.camp>
2021-03-19 14:12:12 +0300

    Release-check-strings-and-columns

--- sudoku-zvoznikov.js
+++ sudoku-zvoznikov.js
@@ -1,26 +1,49 @@
 let boardString = '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--';
 
-function solve (boardString) {
+function searchStringsWithDigits (boardString) {
   const predSud = boardString.match(/.{1,9}/g);
   const digitsToFill = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
-  const checkObject = {};
-  let sum = '';
+  const checkStringsObject = {};
   for (let i = 0; i < predSud.length; i += 1) {
-    let newArr = [];
+    let arrOfStrings = [];
     for (let j = 0; j < predSud.length; j += 1) {
       if (predSud[i][j] !== '-') {
-        newArr.push(predSud[i][j])
+        arrOfStrings.push(predSud[i][j]);
       }
     }
-    let oldArr = [];
+    let newArrOfStrings = [];
     for (let k = 0; k < digitsToFill.length; k += 1) {
-      if (!newArr.includes(digitsToFill[k])) {
-        oldArr.push(digitsToFill[k]);
+      if (!arrOfStrings.includes(digitsToFill[k])) {
+        newArrOfStrings.push(digitsToFill[k]);
       }
     }
-    checkObject[i] = oldArr;
+    checkStringsObject[i] = newArrOfStrings;
   }
-  return checkObject;
+  return checkStringsObject;
 }
 
-console.log(solve(boardString))
+function searchColumnsWithDigits (boardString) {
+  const predSud = boardString.match(/.{1,9}/g);
+  const digitsToFill = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  const checkColumnsObject = {};
+  for (let i = 0; i < predSud.length; i += 1) {
+    let arrOfColumns = [];
+    for (let j = 0; j < predSud.length; j += 1) {
+      if (predSud[j][i] !== '-') {
+        arrOfColumns.push(predSud[j][i]);
+      }
+    }
+    let newArrOfColumns = [];
+    for (let k = 0; k < digitsToFill.length; k += 1) {
+      if (!arrOfColumns.includes(digitsToFill[k])) {
+        newArrOfColumns.push(digitsToFill[k]);
+      }
+    }
+    checkColumnsObject[i] = newArrOfColumns;
+  }
+  return checkColumnsObject;
+}
+
+function searchSquaresWithDigits (boardString) {
+  
+}
