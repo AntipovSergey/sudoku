5bb796cd162a20280e426480b6be677349fec1be
Oksana Klymenok <kapris15@yahoo.com>
2021-03-21 22:16:54 -1000

    Brainstorming

--- sudoku.js
+++ sudoku.js
@@ -4,15 +4,35 @@
 // your solver has tried to solve it.
 // How you represent your board is up to you!
 function solve(boardString) {
-
-}
-
+ 
 
 // Returns a boolean indicating whether
 // or not the provided board is solved.
 // The input board will be in whatever
 // form `solve` returns.
-function isSolved(board) {
+function isSolved(board) { 
+  if (solve()){
+    return true;
+  } else {
+  
+  }
+  [[],
+[],[]]
+}
+//                         [5, -, 4, 6, 7, 8, 9, 1, 2],
+//                         [6, 7, 2, 1, 9, 5, 3, 4, 8],
+//                         [1, 9, 8, 3, 4, 2, 5, 6, 7],
+//                         [8, 5, 9, 7, 6, 1, 4, 2, 3],
+//                         [4, 2, 6, 8, 5, 3, 7, 9, 1],
+//                         [7, 1, 3, 9, 2, 4, 8, 5, 6],
+//                         [9, 6, 1, 5, 3, 7, 2, 8, 4],
+//                         [2, 8, 7, 4, 1, 9, 6, 3, 5],
+//                         [3, 4, 5, 2, 8, 6, 1, 7, 9]
+//  boxSize=3;
+arr[r][c]
+const boxRow=Math.floor(r/boxSize)*boxSize;
+const boxCol=Math.floor(c/boxSize)*boxSize;
+for (let i=boxRow; i<boxRow+boxSize; )
 
 }
 
