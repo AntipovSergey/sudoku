7ae191378910cfbc166922ab7a7039d98d8b2316
Elina <khisamutdinova.elina@gmail.com>
2021-03-22 09:10:11 +0100

    привели к виду массива массивов

--- sudoku.js
+++ sudoku.js
@@ -3,10 +3,37 @@
 // something representing a board after
 // your solver has tried to solve it.
 // How you represent your board is up to you!
-function solve(boardString) {
+const line = '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--';
 
+// 1-5 8-2 ---
+// -9- -76 4-5
+// 2-- 4-- 819
+// -19 --7 3-6
+// 762 -83 -9-
+// --- -61 -5-
+// --7 6-- -3-
+// 43- -2- 5-1
+// 6-- 3-8 9--
+
+function solve(boardString) {
+  const table = [];
+  const splittedLine = line.split('');
+  for (let i = 0; i < 9; i++) {
+    const array = [];
+    for (let j = 0; j < 9; j++) {
+      array.push(splittedLine.shift());
+    }
+    table.push(array);
+    // console.log(splittedLine)
+  }
+  // const innerArray = [];
+  // for (let j = 0; j < 9; j++) {
+  //   if (j !== '-') {
+
+  console.log(table);
 }
 
+solve(line);
 
 // Returns a boolean indicating whether
 // or not the provided board is solved.
@@ -15,7 +42,6 @@ function solve(boardString) {
 function isSolved(board) {
 
 }
-
 
 // Takes in a board in some form and
 // returns a String that's well formatted
@@ -28,7 +54,7 @@ function prettyBoard(board) {
 
 // Exports all the functions to use them in another file.
 module.exports = {
-	solve: solve,
-	isSolved: isSolved,
-	prettyBoard: prettyBoard
-}
+  solve,
+  isSolved,
+  prettyBoard,
+};
