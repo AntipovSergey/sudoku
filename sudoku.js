056ddac5fca44b722e985023d69c4e724f8b4c92
Denis <denis.obraztsov.92@mail.ru>
2021-03-22 19:13:32 +0300

    prettyBoard совмещает входящий массив с массивами в строку по пробелу

--- sudoku.js
+++ sudoku.js
@@ -1,42 +1,37 @@
 // Функция создания массива судоку
 
-
 function arrBoard(puzzle) {
+  const board = [];
 
-  let board =[]
-  
-  let end = 9
-  for (i=0;i<81;i+=9){
-    let arr = puzzle.slice(i,end).split('')
-    board.push(arr)
-    end+=9
+  let end = 9;
+  for (i = 0; i < 81; i += 9) {
+    const arr = puzzle.slice(i, end).split('');
+    board.push(arr);
+    end += 9;
   }
-  return board
-  
+  return board;
 }
-
-
 
 // Алгоритм решения судоку
 function solve(boardString) {
-
+  
 }
-
 
 // Проверка результата работы функции solve
 function isSolved(board) {
 
 }
 
-
 // Функция вывода строки в стандартный вид(не массив)
 function prettyBoard(board) {
-
+  return board.forEach((element) => {
+    console.log(element.join(' '));
+  });
 }
 
 // Exports all the functions to use them in another file.
 module.exports = {
-	solve: solve,
-	isSolved: isSolved,
-	prettyBoard: prettyBoard
-}
+  solve,
+  isSolved,
+  prettyBoard,
+};
