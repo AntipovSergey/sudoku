27cce0d3627f7fa981eea3f354e04933766853f9
Oksana Klymenok <kapris15@yahoo.com>
2021-03-22 20:38:17 -1000

    Added sudoku2

--- sudoku.js
+++ sudoku.js
@@ -1,21 +1,42 @@
-// Takes a board as a string in the format
-// you see in the puzzle file. Returns
-// something representing a board after
-// your solver has tried to solve it.
-
 const { parse } = require("path");
 
-// How you represent your board is up to you!
 function solve(boardString) {
-  const resultArr = [];
-  const boardArr = boardString.split('');
-  while (boardArr.length > 0) {
-    resultArr.push(boardArr.splice(0, 9));
+  const arr=stringToArray(boardString);
+  const newArr=solution(arr);
+  while (newArr.flat().indexOf('-')!==-1){
+    const copyArr=JSON.parse(JSON.stringify(newArr));
+    solution(newArr);
+    if (copyArr.flat().join('')===newArr.flat().join('')){
+      console.log('Doesnt work');
+      nextLevelCheck(arr);
+    }
   }
-  return resultArr;
+  return newArr;
+  
+
 }
 
-const checkCell=(num, array, i, j)=>{
+function isSolved(board) {
+  if (board.flat().includes('-')){
+    return false;
+  } else {
+    return true;
+  }
+ }
+
+function prettyBoard(board) {
+  return board.join('\n');
+}
+
+function stringToArray(string){
+  const resultArr = [];
+  const boardArr = string.split('');
+  while (boardArr.length > 0) {
+    resultArr.push(boardArr.splice(0, 9));
+}
+return resultArr;
+}
+function checkCell(num, array, i, j){
   if (array[i].includes(num.toString())){
     return false;
   }
@@ -31,12 +52,12 @@ const checkCell=(num, array, i, j)=>{
        if (array[n][m] === num.toString()) {
          return false;
        }
-  }
+  }}
   return true;
  }
- }
-const solution =(arr)=>{
-  let possibleArr=[];
+
+ function solution(arr){
+let possibleArr=[];
   for (let i=0; i<arr.length; i++){
     for (let j=0; j<arr.length; j++){
       if (arr[i][j]==='-'){
@@ -54,40 +75,46 @@ const solution =(arr)=>{
     }
   }
   return arr;
-}
-// Returns a boolean indicating whether
-// or not the provided board is solved.
-// The input board will be in whatever
-// form `solve` returns.
-function isSolved(board) { 
-  const arr=solve(board);
-  solution(arr);
-  console.log(solution(arr));
-  while (arr.flat().indexOf('-')!==-1){
-    const copyArr=JSON.parse(JSON.stringify(arr));
-    solution(arr);
-    if (copyArr.flat().join('')===arr.flat().join('')){
-      console.log('Doesnt work');
-      nextLevelCheck(arr);
-      break;
+ }
+
+function nextLevelCheck(arr){
+  let copyArr=JSON.parse(JSON.stringify(arr));
+  let possibleArr=[];
+  for (let i=0; i<arr.length; i++){
+    for (let j=0; j<arr.length; j++){
+      if (arr[i][j]==='-'){
+        for (let k=1; k<10; k++){
+        if (checkCell(k, arr, i, j)){
+          possibleArr.push(k);
+      } }
+    if (possibleArr.length === 2) {
+      copyArr[i][j] = possibleArr[0].toString();
+      // const maybe1Arr=solution(copyArr);
+
+      while (copyArr.flat().indexOf('-')!==-1){
+        let copyOfCopyArr=JSON.parse(JSON.stringify(copyArr));
+        let resultArr=solution(copyArr);
+        if (copyOfCopyArr.flat().join('')===resultArr.flat().join('')){
+          console.log('Doesnt work');
+          arr[i][j]=possibleArr[1].toString;
+          console.log(arr[i][j]);
+copyArr=JSON.parse(JSON.stringify(arr));
+console.log(copyArr);
+break;
+        }
+      }
+        } else {
+      possibleArr = [];
+    }
+    } 
     }
   }
-  // console.log(prettyBoard(arr))
-  return console.log(prettyBoard(arr));
-}
-console.log(isSolved('---6891--8------2915------84-3----5-2----5----9-24-8-1-847--91-5------6--6-41----'))
-
-function nextLevelCheck(array){
-  
+  return copyArr;
 }
 
-function prettyBoard(board) {
-  return board.join('\n');
-}
-
-// Exports all the functions to use them in another file.
 module.exports = {
 	solve: solve,
 	isSolved: isSolved,
 	prettyBoard: prettyBoard
 }
+
