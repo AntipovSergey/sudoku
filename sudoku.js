5d52958cce76f3c022b54b729c62616820b7ec30
Artem <bamblbookb@gmail.com>
2021-04-09 11:39:02 +0300

    Release-0

--- sudoku.js
+++ sudoku.js
@@ -3,9 +3,24 @@
 // something representing a board after
 // your solver has tried to solve it.
 // How you represent your board is up to you!
-function solve(boardString) {
 
-}
+
+let boardString = [
+  ["1", "-", "5", "8", "-", "2", "-", "-", "-"],
+  ["-", "9", "-", "-", "7", "6", "4", "-", "5"],
+  ["2", "-", "-", "4", "-", "-", "8", "1", "9"],
+  ["-", "1", "9", "-", "-", "7", "3", "-", "6"],
+  ["7", "6", "2", "-", "8", "3", "-", "9", "-"],
+  ["-", "-", "-", "-", "6", "1", "-", "5", "-"],
+  ["-", "-", "7", "6", "-", "-", "-", "3", "-"],
+  ["4", "3", "-", "-", "2", "-", "5", "-", "1"],
+  ["6", "-", "-", "3", "-", "8", "9", "-", "-"],
+];
+
+function solve(numberB, boardString) {
+  
+  }
+
 
 
 // Returns a boolean indicating whether
