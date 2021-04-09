ff1abcf216fa500f18c4a4fbf5a389f87deb6e9f
Egor Bezmen <egor1911@gmail.com>
2021-04-09 16:41:11 +0300

    release-0 horizontal check

--- sudoku.js
+++ sudoku.js
@@ -17,33 +17,47 @@ let boardString = [
   ["6", "-", "-", "3", "-", "8", "9", "-", "-"],
 ];
 
-function solve(numberB, boardString) {
-  
+//поиск по горизонтали 
+function checkRow(val, arr, number = 1) {
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      if (arr[i][j] === val) {
+        if(number > 9) {
+          number = 1;
+        }
+        if (arr[i].includes(String(number)) || arr[i].includes(number)) {
+          return checkRow(val, arr, number + 1)
+        } else {
+          arr[i][j] = number //1
+        }
+      } 
+    }
   }
-
-
-
-// Returns a boolean indicating whether
-// or not the provided board is solved.
-// The input board will be in whatever
-// form `solve` returns.
-function isSolved(board) {
-
+  return arr
 }
+  console.table(checkRow("-", boardString))
+
+  // Returns a boolean indicating whether
+  // or not the provided board is solved.
+  // The input board will be in whatever
+  // form `solve` returns.
+  // function isSolved(board) {
+
+  // }
 
 
-// Takes in a board in some form and
-// returns a String that's well formatted
-// for output to the screen.
-// The input board will be in whatever
-// form `solve` returns.
-function prettyBoard(board) {
+  // Takes in a board in some form and
+  // returns a String that's well formatted
+  // for output to the screen.
+  // The input board will be in whatever
+  // form `solve` returns.
+  // function prettyBoard(board) {
 
-}
+  // }
 
-// Exports all the functions to use them in another file.
-module.exports = {
-	solve: solve,
-	isSolved: isSolved,
-	prettyBoard: prettyBoard
-}
+  // Exports all the functions to use them in another file.
+  // module.exports = {
+  //   solve: solve,
+  //   isSolved: isSolved,
+  //   prettyBoard: prettyBoard
+  // }
