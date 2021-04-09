7845a7f172a59055ab06e130ac7a7e7d695a38a6
Yaroslav <serafim90@mail.ru>
2021-04-09 14:15:21 +0300

    added fn copyBoard + fn checkHorizontal + fn checkVertical

--- sudoku.js
+++ sudoku.js
@@ -5,7 +5,39 @@
 // How you represent your board is up to you!
 function solve(boardString) {
 
+// const string = "1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89---" 
+const board = boardString.match(/.{9}/g).map((el) => el.split(''))  // временный
+
+function copyBoard(board) {
+  const copyBoard = JSON.parse(JSON.stringify(board))
+  return copyBoard
 }
+
+const arr = ['1','2','3','4','5','6','7','8','9'] // временный
+function checkHorizontal(arr, board, loc) {
+  for (let i = 0; i < board[loc.row].length; i += 1) {
+    let indexRow = arr.indexOf(board[loc.row][i]) 
+    if (indexRow !== -1) {
+      arr.splice(indexRow, 1)
+    } 
+  }
+}
+
+function checkVertical(arr, board, loc) {
+  let verticalLine = []
+  for (let i = 0; i < board[loc.col].length; i += 1) {
+    verticalLine.push(board[i][loc.col])
+  }
+  for (let i = 0; i < verticalLine.length; i += 1) {
+    let indexCol = arr.indexOf(verticalLine[i]) 
+    if (indexCol !== -1) {
+      arr.splice(indexCol, 1)
+    } 
+  }
+}
+
+}
+console.log(solve("1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89---"))
 
 
 // Returns a boolean indicating whether
