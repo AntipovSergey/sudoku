a3c138c07f99f239e41abd4234beca0c62eb20c6
Aizana <ayzana2010@gmail.com>
2021-04-23 16:26:58 +0300

    Release 0

--- sudoku.js
+++ sudoku.js
@@ -3,9 +3,60 @@
 // something representing a board after
 // your solver has tried to solve it.
 // How you represent your board is up to you!
-function solve(boardString) {
-
+const arr1 = '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--'
+//Преобразовали строку в многомерный массив
+let arr=[]
+for (let i=0;i<9; i++){
+  arr[i]=arr1.substr(i*9,9);
 }
+   for (let j=0;j<arr.length;j++){
+   arr[j]=arr[j].split('')
+   } 
+// console.log(arr)
+
+//Решение самой функции
+function solve(arr) {
+let strH = '';
+  let strV = '';
+  const arrH = [];
+  const arrV = [];
+//Горизонтальные массивы
+  for (let i = 0; i < arr.length; i++) {
+    strH = arr[i].join('');
+    arrH.push(strH);
+//Вертикальные массивы
+    const columnArray = [];
+
+    for (let j = 0; j < 9; j++) {
+      columnArray.push(arr[j][i]);
+    }
+
+    const columnString = columnArray.join('');
+    arrV.push(columnString);
+  }
+
+  console.log(arrV)
+
+  strH;
+  strV;
+
+  // console.log('arrH:', arrH);
+  // console.log('arrV:', arrV);
+  //Выполняем проверку
+
+  // const CONST = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  // const x2 = [];
+
+  // for (let o = 0; o < 9; o++) {
+  //   if (!arr.includes(CONST[o])) {
+  //     x2.push(CONST[o]);
+  //   }
+  // }
+  // console.log(x2); // результат от строки
+}
+
+console.log(solve(arr))
+
 
 
 // Returns a boolean indicating whether
