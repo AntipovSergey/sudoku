92eec0d656d816055908911cae7f60421ed8fc84
Kris <kristina.eldzarova@gmail.com>
2021-04-30 12:22:30 +0300

    Sudoku Array Done

--- sudoku.js
+++ sudoku.js
@@ -3,9 +3,21 @@
 // something representing a board after
 // your solver has tried to solve it.
 // How you represent your board is up to you!
-function solve(boardString) {
 
+let sudoku = "1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--"
+
+function solve(boardString) {
+  let newArr = [];
+  let sudokuArr = boardString.split("")
+  let size = 9
+
+  for (let i = 0; i < size; i++) {
+    newArr.push(sudokuArr.splice(0, size))
+  }
+return newArr
 }
+
+
 
 
 // Returns a boolean indicating whether
