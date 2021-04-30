4e3dc8b7945a66b0e248ee15a48c7cb913e4c5de
Victor Yanochkin <v65988@gmail.com>
2021-04-30 16:31:04 +0300

    Step Function 2

--- stepFunction.js
+++ stepFunction.js
@@ -3,7 +3,7 @@ function makeStep(array) {
     for (let j = 0; j < array[i].length; j++){
       if (array[i][j] === '-') {
         for (let k = 1; k < 10; k++){
-          if (!(funStr(k, arr[i])/*&&funColumn(k, arr, i)&&funSquare(k)*/)) {
+          if (!(stringSearch(k, arr[i])&&columnSearch(k, arr, i)/*&&funSquare(k)*/)) {
             array[i][j] = k
             break
           }
@@ -13,3 +13,16 @@ function makeStep(array) {
   }
   return array
 }
+
+let string = '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--'
+
+let array = createArray(string)
+
+function createPrettyBoard(string) {
+  string = string.replace(/([\d-]{9})([\d-]{9})([\d-]{9})([\d-]{9})([\d-]{9})([\d-]{9})([\d-]{9})([\d-]{9})([\d-]{9})/g, `$1\n$2\n$3\n$4\n$5\n$6\n$7\n$8\n$9`)
+  string = string.replace(/(\S)/g, `$1 `)
+  console.log (string)
+  return string
+}
+
+createPrettyBoard(string);
