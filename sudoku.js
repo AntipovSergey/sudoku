689ffd7da58023b48336f339cdff390148b9cc9a
MariaR <8insouciance@gmail.com>
2021-04-30 13:52:27 +0300

    Sudoku horizontal process work

--- sudoku.js
+++ sudoku.js
@@ -4,9 +4,9 @@
 // your solver has tried to solve it.
 // How you represent your board is up to you!
 
-let sudoku = "1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--"
+const sudoku = "1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--"
 
-function solve(boardString) {
+function createArr(boardString) {
   let newArr = [];
   let sudokuArr = boardString.split("")
   let size = 9
@@ -14,7 +14,28 @@ function solve(boardString) {
   for (let i = 0; i < size; i++) {
     newArr.push(sudokuArr.splice(0, size))
   }
-return newArr
+  return newArr
+}
+
+
+function makeHor(boardString) {
+let sudokuArray = createArr(sudoku);
+
+for (let i = 0; i < sudokuArray.length; i++) {
+  for (let j = 0; j < sudokuArray[i].length; j++) {
+    if (sudokuArray[i][j] !== '-') {
+      sudokuArray[i][j] = Number(sudokuArray[i][j])
+    }
+  }
+}
+return sudokuArray
+}
+console.table(makeHor(sudoku))
+
+
+
+function solve(boardString) {
+
 }
 
 
