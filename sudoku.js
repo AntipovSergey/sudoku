4e7d8b0c24038cfa8da2b28a40e20dac5338c3ee
Andrew Bochka <a.g.bochka@gmail.com>
2021-05-17 17:54:31 +0300

    bugFix

--- sudoku.js
+++ sudoku.js
@@ -2,10 +2,10 @@ let boardString =
   '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--';
 
 // находим 0 и вычисляем его координаты
-function searchZeroInRowAndColumn(board) {
+function searchZero(board) {
   for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board[i].length; j++) {
-      if (board[i][j] === '0') {
+      if (board[i][j] === 0) {
         console.log([i, j]);
         return [i, j];
       }
@@ -31,37 +31,51 @@ function solve(boardString) {
     // создаем строку
     let row = [];
     for (let j = position; j < position + 9; j += 1) {
-      row.push(rawArr[j]);
+      row.push(Number(rawArr[j]));
     }
     return row;
   }
-  console.table(boardArr);
   return boardArr;
+  // if (!isSolved()) {
+  // }
 }
 
-solve(boardString);
+let boardArr = solve(boardString);
 
 function isSolved() {
-  if (searchZeroInRowAndColumn(boardString) === null) {
+  if (searchZero(boardString) === null) {
     return true;
-  } return false;
+  }
+  return false;
 }
 
-function checkZero(coordZero, board, num) {
-  const row = coordZero[0];
-  const column = coordZero[1];
-  for (let i = 0; i < board[row].length; i++) {
-    if (num === board[row][i] && i !== column) {
-      return false;
-    }
+let coordZero = searchZero(boardArr);
+
+console.log(checkZero(coordZero, boardArr));
+console.table(boardArr);
+
+function checkZero(coordZero, board, num = 1) {
+  const r = coordZero[0];
+  const c = coordZero[1];
+  for (let i = 0; i < 9; i++) {
+    if (num === board[r][i] && i !== c) return false;
+    if (num === board[i][c] && i !== r) return false;
   }
 
+  // проверка на нахождение NUM в BOX
+  const boxRow = Math.floor(r / 3) * 3;
+  const boxCol = Math.floor(c / 3) * 3;
+  for (let i = boxRow; i < boxRow + 3; i++) {
+    for (let j = boxCol; j < boxCol + 3; j++) {
+      if (board[i][j] === num && i !== r && j !== c) {
+        return false;
+      }
+    }
+  }
+  return true;
 }
 
-
-function prettyBoard(board) {
-
-}
+function prettyBoard(board) {}
 
 // Exports all the functions to use them in another file.
 module.exports = {
