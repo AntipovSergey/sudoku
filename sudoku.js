aa1f15992338989e8e06819eec7b42e9a4d7ac96
Insaev <insaevstudio@gmail.com>
2021-05-17 14:50:38 +0300

    searchZeroInRowAndColumn

--- sudoku.js
+++ sudoku.js
@@ -1,28 +1,29 @@
-// Takes a board as a string in the format
-// you see in the puzzle file. Returns
-// something representing a board after
-// your solver has tried to solve it.
-// How you represent your board is up to you!
+function solve(boardString) {
+  // находим 0 и вычисляем его координаты
+  function searchZeroInRowAndColumn(board) {
+    for (let i = 0; i < board.length; i++) {
+      for (let j = 0; j < board[i].length; j++) {
+        if (board[i][j] === '0') {
+          console.log([i, j]);
+          return [i, j];
+        }
+      }
+    }
+    return null;
+  }
+  searchZeroInRowAndColumn(boardString);
+}
 
-// Returns a boolean indicating whether
-// or not the provided board is solved.
-// The input board will be in whatever
-// form `solve` returns.
-function isSolved(board) {}
 
-// Takes in a board in some form and
-// returns a String that's well formatted
-// for output to the screen.
-// The input board will be in whatever
-// form `solve` returns.
-function prettyBoard(board) {}
+function isSolved(board) {
 
-// Exports all the functions to use them in another file.
-// module.exports = {
-//   solve: solve,
-//   isSolved: isSolved,
-//   prettyBoard: prettyBoard,
-// };
+}
+
+
+function prettyBoard(board) {
+
+}
+
 
 function changeOnZero(string) {
   const newString = string.replace(/-/gi, 0);
@@ -50,7 +51,16 @@ function makeStructure(string) {
     }
     return row;
   }
+  console.log(boardArr);
   return boardArr;
 }
 
-console.table(makeStructure(changeOnZero(str)));
+const board = makeStructure(changeOnZero(str));
+solve(board);
+
+// Exports all the functions to use them in another file.
+module.exports = {
+  solve: solve,
+  isSolved: isSolved,
+  prettyBoard: prettyBoard,
+};
