64b269ffa4f4ab7c8235cfe315937fbf031510fe
Alex <Karunas353@gmail.com>
2021-05-28 16:20:55 +0300

    Release2

--- sudoku_Kirillov.js
+++ sudoku_Kirillov.js
@@ -1,30 +1,9 @@
-let board = [[1, 3, 5, 6, 7, 0, 9, 2, 9],
-           [1, 3, 5, 6, 7, 8, 9, 2, 9],
-           [1, 3, 0, 6, 0, 8, 9, 2, 9],
-           [1, 3, 0, 6, 0, 8, 9, 2, 9],
-           [1, 3, 5, 6, 0, 8, 9, 2, 9],
-           [1, 3, 5, 0, 7, 8, 0, 2, 9],
-           [1, 3, 5, 6, 7, 8, 9, 2, 9],
-           [1, 3, 5, 6, 7, 8, 9, 2, 9]];  
-
 function searchInRow(board) {
-  
- for (let i = 0; i < board.length; i++) {
-   for (let j = 0; j < board.length; j++) {
-     let firstNumber = board[i][j];
-      for (let value of board) {
-        if(firstNumber === value) {
-          return false
-        } else {
-          return true
-        }
-   }
-  }
+  let sorted = board.sort((x, y) => x - y);
+  for (let i = 0; i < sorted.length; i++)
+    if (sorted[i] == sorted[i + 1]) {
+      return false;
+    } else {
+      return true;
+    }
 }
-  return newArr;
-}
-
-console.log(searchInRow(board));
-
-
-
--- sudoku_Kirillov2.js
+++ sudoku_Kirillov2.js
@@ -1,41 +1,19 @@
-let board = [[1, 3, 5, 6, 7, 0, 9, 2, 9],
-           [1, 3, 5, 6, 7, 8, 9, 2, 9],
-           [1, 3, 0, 6, 0, 8, 9, 2, 9],
-           [1, 3, 0, 6, 0, 8, 9, 2, 9],
-           [1, 3, 5, 6, 0, 8, 9, 2, 9],
-           [1, 3, 5, 0, 7, 8, 0, 2, 9],
-           [1, 3, 5, 6, 7, 8, 9, 2, 9],
-           [1, 3, 5, 6, 7, 8, 9, 2, 9]]; 
-             
-
 function searchInColumn(board, ArrLength) {
-
   let newArr = [];
-  for(let i = 0; i < board.length; i++){
-     newArr.push([]);
-  };
-  for(let i = 0; i < board.length; i++){
-    for(let j = 0; j < ArrLength; j++){
-     newArr[j].push(board[i][j]);
-     };
-  };
-        
-
-  for (let i = 0; i < newArr.length; i++) {
-   for (let j = 0; j < newArr.length; j++) {
-     let firstNumber = newArr[i][j];
-      for (let value of newArr) {
-        if(firstNumber === value) {
-          return false
-        } else {
-          return true
-        }
-   }
+  for (let i = 0; i < board.length; i++) {
+    newArr.push([]);
   }
-}
-  return newArr;
-}
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < ArrLength; j++) {
+      newArr[j].push(board[i][j]);
+    }
+  }
 
-
-// console.log(searchInColumn(board));
-console.log(searchInColumn(board, 3));
+  let sorted = newArr.sort((x, y) => x - y);
+  for (let i = 0; i < sorted.length; i++)
+    if (sorted[i] == sorted[i + 1]) {
+      return false;
+    } else {
+      return true;
+    }
+}
