1d875ea72ad0f9393881c2414076ec4b852d6fad
Dmitriev-NN <brianbaitano@mail.ru>
2021-05-28 11:08:07 +0300

    Start

---
+++ stringToArray.js
@@ -0,0 +1,16 @@
+let str = '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--';
+let array = [];
+
+for (let i = 0; i < array.length; i++) {
+    
+    
+}
+// const stringFromArr = '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--1';
+// const transitArr = Array.from(str);
+// // console.log(Array.from(stringFromArr));
+// let result = [];
+// for (let i = 0; i < stringFromArr.length; i = i + 10) {
+//   result.push(transitArr.slice(i, i + 9));
+// }
+// console.log(str.length);
+// console.table(result);
