612b1f755b6f6e7bfba09ca8a2d9a273fb20c8d3
rus.lan.r <ruslan.rodionow98@gmail.com>
2021-05-28 14:42:39 +0300

    test2

--- blockCheck.js
+++ blockCheck.js
@@ -1,12 +1,3 @@
-// 1 - 5 8 - 2 - - -
-// - 9 - - 7 6 4 - 5
-// 2 - - 4 - - 8 1 9
-// - 1 9 - - 7 3 - 6
-// 7 6 2 - 8 3 - 9 -
-// - - - - 6 1 - 5 -
-// - - 7 6 - - - 3 -
-// 4 3 - - 2 - 5 - 1
-// 6 - - 3 - 8 9 - -
 
 var sudoku = [
   [0, 0, 3, 0, 0, 8, 2, 0, 4],
@@ -19,52 +10,21 @@ var sudoku = [
   [8, 0, 0, 0, 0, 1, 0, 0, 0],
   [0, 7, 0, 0, 5, 0, 4, 0, 0],
 ];
-// //
-// let x = 0;
-// let y = 0;
-// for (let indeXSquare = 0; indeXSquare < 9; indeXSquare++) {
-//   let checkMask = [[1, 2, 3, 4, 5, 6, 7, 8, 9], [indeXSquare]];
-//   // switch (indeXSquare) {
-//   //   case 0:
-//   //     x = 0;
-//   //     y = 0;
-//   //     console.log("case 0");
-//   //     break;
-//   //   case 3:
-//   //     x = 3;
-//   //     y = 0;
-//   //     console.log("case 3");
-//   //     break;
-//   //   case 6:
-//   //     x = 6;
-//   //     y = 0;
-//   //     console.log("case 6");
-//   //     break;
-//   // }
 
-//   for (let i = 0 + x; i < 3 + x; i += 1) {
-//     for (let j = 0 + y; j < 3 + y; j += 1) {
-//       for (let indexChech = 0; indexChech < checkMask.length; indexChech++) {
-//         if (sudoku[i][j] === checkMask[0][indexChech]) {
-//           checkMask[0].splice(indexChech, 1);
-//         }
-//       }
+function checkMatchInBlock(sudoku, i, j, arrayPossible) {
+  let startSquareCoordX = Math.floor(i / 3) * 3;
+  let startSquareCoordY = Math.floor(j / 3) * 3;
 
-//       console.log(i, j);
-//     } // конец для for j
- 
-//     // console.log("x -> ", x);
-//     // console.log("y -> ", y);
-//   } // конец для for i
-//   y += 3;
-//   x = 0;
-
-//   //console.log(checkMask);
-//   console.log();
-// }
-
-
-function checkMatchInBlock(){
-
+  for (let i = startSquareCoordX; i < 3 + startSquareCoordX; i += 1) {
+    for (let j = startSquareCoordY; j < 3 + startSquareCoordY; j += 1) {
+      if (sudoku[i][j] !== 0) {
+        const index = arrayPossible.findIndex((val) => val === sudoku[i][j]);
+        if (index != -1) {
+          arrayPossible.splice(index, 1);
+        }
+      }
+    }
+  }
+  return arrayPossible;
 }
 
