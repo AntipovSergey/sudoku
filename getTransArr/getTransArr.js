4608b2872740e877232c8a2eb58e3851344ef522
Roman <mr.baskovr@gmail.com>
2021-06-18 14:52:56 +0300

    Solve() and prettyBoard()

--- getTransArr/getTransArr.js
+++ getTransArr/getTransArr.js
@@ -1,4 +1,11 @@
+const fs = require('fs');
+const sudokuPuzzles = fs.readFileSync('../sudoku-puzzles.txt', 'utf-8');
 
+// Takes a board as a string in the format
+// you see in the puzzle file. Returns
+// something representing a board after
+// your solver has tried to solve it.
+// How you represent your board is up to you!
 function solve(boardString) {
   let board = []; 
   let newStr = boardString.replace(/\-/g, 0);
@@ -8,6 +15,49 @@ function solve(boardString) {
   }
   return board;
 }
-//проверка:
-let str = '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--'
-console.table(solve(str));
+//Проверка:
+// let str = '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--'
+// console.table(solve(str));
+// let firstPuzzle = sudokuPuzzles.split('\n')[0];
+// console.table(solboard
+
+
+// // Returns a boolean indicating whether
+// // or not the provided board is solved.
+// // The input board will be in whatever
+// // form `solve` returns.
+function isSolved(board) {
+
+}
+
+
+// // Takes in a board in some form and
+// // returns a String that's well formatted
+// // for output to the screen.
+// // The input board will be in whatever
+// // form `solve` returns.
+function prettyBoard(board) {
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      if (board[i][j] === '0') {
+        board[i][j] = Math.floor(Math.random() * 9) + 1;
+      } else {
+        board[i][j] = +board[i][j];
+      }
+    }
+  } 
+  return board;
+}
+//Проверка: 
+// let firstPuzzle = sudokuPuzzles.split('\n')[0];
+// console.table(prettyBoard(solve(firstPuzzle)));
+
+
+
+// Exports all the functions to use them in another file.
+module.exports = {
+	solve: solve,
+	isSolved: isSolved,
+	prettyBoard: prettyBoard
+}
+
--- spec/sudoku.spec.js
+++ spec/sudoku.spec.js
@@ -1,2 +1,3 @@
 const sudoku = require('../sudoku.js')
-
+//const sudoku = require('../getTransArr/getTransArr.js')
+//вариант выше если основной js-файл getTransArr.js
