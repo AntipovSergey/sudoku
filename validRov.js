18fa7baea2712df28f18d633f36489fdd67660f9
007eroxin <007eroxin00@gmail.com>
2021-06-18 12:03:16 +0300

    validRow

--- validRov.js
+++ validRov.js
@@ -1,1 +1,9 @@
 let tex = '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--'
+
+function solve(boardString) {
+    let arr = []
+    for (let i = 0; i <= boardString.length; i++) {
+                
+    }
+    }
+    console.log(solve(tex));
