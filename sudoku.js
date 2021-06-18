5fdfaecd126eb6dd228d5e648471e0021b088553
Papakhin <papakhin.egor@gmail.com>
2021-06-18 16:56:00 +0300

    sudocu

--- sudoku.js
+++ sudoku.js
@@ -1,4 +1,4 @@
-const data = [
+const input = [
   ['1', '-', '5', '8', '-', '2', '-', '-', '-'],
   ['-', '9', '-', '-', '7', '6', '4', '-', '5'],
   ['2', '-', '-', '4', '-', '-', '8', '1', '9'],
@@ -10,71 +10,94 @@ const data = [
   ['6', '-', '-', '3', '-', '8', '9', '-', '-'],
 ];
 
+// Takes a board as a string in the format
+// you see in the puzzle file. Returns
+// something representing a board after
+// your solver has tried to solve it.
+// How you represent your board is up to you!
+function solve(boardString) {
+}
 
-function solve(board) {
+// Returns a boolean indicating whether
+// or not the provided board is solved.
+// The input board will be in whatever
+// form `solve` returns.
+function isSolved(board) {
   const size = 9;
-  function searchEmpty(board) {
-    const size = 9;
-    for (let i = 0; i < size; i++) {
-      for (let j = 0; j < size; j++) {
-        if (board[i][j] === '-') {
-          return [i, j];
+
+  function findEmpty(board) {
+    for (let r = 0; r < size; r++) {
+      for (let c = 0; c < size; c++) {
+        if (board[r][c] === '-') {
+          return [r, c];
         }
       }
     }
     return null;
   }
-  console.log(searchEmpty(data));
-  function valid(num, pos, board) {
 
-    const [i, j] = pos;
+  function val(num, pos, board) {
+    const [r, c] = pos;
 
-    for (let c = 0; c < size; c++) {
-      if (board[i][c] === num) return false;
-    }
-    for (let c = 0; c < size; c++) {
-      if (board[j][c] === num) return false;
+    for (let i = 0; i < size; i++) {
+      if (board[i][c] === num) {
+        return false;
+      }
     }
 
+    for (let i = 0; i < size; i++) {
+      if (board[r][i] === num) {
+        return false;
+      }
+    }
+
+    const boxRow = Math.floor(r / 3) * 3;
+    const boxCol = Math.floor(c / 3) * 3;
+    for (let i = boxRow; i < boxRow + 3; i++) {
+      for (let j = boxCol; j < boxCol + 3; j++) {
+        if (board[i][j] === num) {
+          return false;
+        }
+      }
+    }
+
+    return true;
   }
+
   const solved = () => {
-    const currPos = searchEmpty(board)
-    if (currPose === null) return true
-
-    for (let i = 1; i < size + 1; i++) {
-      currentNum = i.toString()
-      isValue = validate(currentNum, currPos, board)
-
-
+    const currPos = findEmpty(board);
+    if (currPos === null) {
+      return true;
     }
+    for (let i = 1; i <= size; i++) {
+      const currNum = i.toString();
 
-  }
-
-
-
-
-  console.log(solve(data));
-
-  // Returns a boolean indicating whether
-  // or not the provided board is solved.
-  // The input board will be in whatever
-  // form `solve` returns.
-  function isSolved(board) {
-
-  }
-
-  // Takes in a board in some form and
-  // returns a String that's well formatted
-  // for output to the screen.
-  // The input board will be in whatever
-  // form `solve` returns.
-  function prettyBoard(board) {
-
-  }
-
-  // Exports all the functions to use them in another file.
-  module.exports = {
-    solve,
-    isSolved,
-    prettyBoard,
+      const isValid = val(currNum, currPos, board);
+      if (isValid) {
+        const [a, b] = currPos;
+        board[a][b] = currNum;
+      }
+    }
   };
+
+  solved();
+  return board;
+}
+
+console.table(isSolved(input));
+
+// Takes in a board in some form and
+// returns a String that's well formatted
+// for output to the screen.
+// The input board will be in whatever
+// form `solve` returns.
+function prettyBoard(board) {
+
+}
+
+// Exports all the functions to use them in another file.
+module.exports = {
+  solve,
+  isSolved,
+  prettyBoard,
+};
