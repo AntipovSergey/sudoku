5f766c1e7c2dd2c693d1d235056c226e925cf6f9
Artem <gabidov05@gmail.com>
2021-06-18 16:23:56 +0300

    Release Finish

--- sudoku.js
+++ sudoku.js
@@ -12,24 +12,47 @@ function solve(boardString) {
   for (let elem of arrArrs) {
     arr.push(elem.split(''));
   }
-  // for (let i = 0; i < arr.length; i++) {
-  //   for (let j = 0; j < arr[i].length; j++) {
-  //     if (arr[i][j] )
-  //   }
-  // }
-  console.log(arr);
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      if (arr[i][j] === '-') {
+        arr[i][j] = 0;
+      } else {
+        arr[i][j] = +arr[i][j];
+      }
+    }
+  }
   return arr;
 }
-
 
 // Returns a boolean indicating whether
 // or not the provided board is solved.
 // The input board will be in whatever
 // form `solve` returns.
-function isSolved(board) {
-
+function isSolved(arrArrs) {
+  for (let i = 0; i < arrArrs.length; i++) {
+    for (let j = 0; j < arrArrs[i].length; j++) {
+      if (arrArrs[i][j] === 0) {
+        arrArrs[i][j]++;
+        for (let k = 0; k <= 9; k++) {
+          if (!arrArrs[i].includes(k) && !vertical(arrArrs, j).includes(k)) {
+            arrArrs[i][j] = k;
+            break;
+          }
+        }
+      } 
+    }
+  }
+  console.table(arrArrs)
+  return arrArrs;
 }
 
+function vertical(arrArrs, j) {
+  let arr = [];
+  for (let i = 0; i < arrArrs.length; i++) {
+    arr.push(arrArrs[i][j]);
+  }
+  return arr;
+}
 
 // Takes in a board in some form and
 // returns a String that's well formatted
