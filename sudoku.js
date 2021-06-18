53babf77ccea623f78b9f21c8467db06a44c89b2
Nikita <timandyrak1488@yandex.ru>
2021-06-18 11:43:45 +0300

    solve

--- sudoku.js
+++ sudoku.js
@@ -3,8 +3,70 @@
 // something representing a board after
 // your solver has tried to solve it.
 // How you represent your board is up to you!
-function solve(boardString) {
+const sudoku = [
+[1,[1,2,3,4,5,6,7,8,9],5,8,0,2,0,0,0]
+[0,9,0,0,7,6,4,0,5]
+[2,0,0,4,0,0,8,1,9]
+[0,1,9,0,0,7,3,0,6]
+[7,6,2,0,8,3,0,9,0]
+[0,0,0,0,6,1,0,5,0]
+[0,0,7,6,0,0,0,3,0]
+[4,3,0,0,2,0,5,0,1]
+[6,0,0,3,0,8,9,0,0]
+]
 
+const candidates = [1,2,3,4,5,6,7,8,9]
+
+
+function solve(boardString) {
+  for (let x = 0; x < sudoku.length; x++){
+    for (let y = 0; y < sudoku[0].length; y++){
+      function rowCol (){
+        if ( sudoku[x][y] != 0){
+          
+        }
+      }
+     
+     
+     
+     
+
+     
+     
+     
+     
+     
+     
+     
+     
+      // function row (){
+      //   if (sudoku[x][y] === 0){
+      //     sudoku[x][y] = candidates;
+
+      //     for (let z = 0; z <= sudoku[x][y]; z++){
+      //       for(let x = 0; x < sudoku.length; x++){
+      //         if (sudoku[x][y] != sudoku[x][y][z])
+      //       let currentCell = candidates.
+      //       }
+      //     if (sudoku[x][y] != sudoku[x][y][z])
+      //       let currentCell = candidates.
+      //     }
+      //   }
+      // }
+
+
+
+      function col (){
+
+      }
+
+
+
+      function sqr (){
+
+      }
+    }
+  }
 }
 
 
@@ -25,7 +87,6 @@ function isSolved(board) {
 function prettyBoard(board) {
 
 }
-
 // Exports all the functions to use them in another file.
 module.exports = {
 	solve: solve,
