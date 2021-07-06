a3ed2a5ae7bc69e8411807b844866e211c243e42
Roman <adalaf@yandex.ru>
2021-07-06 19:13:53 +0300

    Release(final style done)

--- runner.js
+++ runner.js
@@ -1,7 +1,10 @@
+/* eslint-disable max-len */
+/* eslint-disable no-multi-spaces */
 // Use filesystem.
 const fs = require('fs');
 // Use functions from sudoku.js file.
 const sudoku = require('./sudoku');
+const CFonts = require('cfonts');
 
 // The sudoku puzzles that your program will solve can be found
 // in the sudoku-puzzles.txt file.
@@ -10,9 +13,13 @@ const sudoku = require('./sudoku');
 // so you should remove them.
 
 // Gets one puzzle from the text file.
-function sudokuParse(content, puzzleNumber = 0) {
+function sudokuParse(content, puzzleNumber = 14) {
   let puzzle = content.split('\n')[puzzleNumber];
-  console.log(puzzle);
+  console.log(CFonts.say(puzzle, {
+    font: 'console',              // define the font face
+    align: 'center',              // define text alignment
+    colors: ['yellowBright'],         // define all colors
+  }));
   return puzzle;
 }
 
@@ -24,13 +31,49 @@ function readAndSolve(err, data) {
 
   let solvedPuzzle = sudoku.solve(puzzle);
   if (sudoku.isSolved(solvedPuzzle)) {
-    console.log("The board was solved!");
-    console.log(sudoku.prettyBoard(solvedPuzzle));
+    console.log(CFonts.say("The board was solved!", {
+      font: 'slick',              // define the font face
+      align: 'center',              // define text alignment
+      colors: ['red'],         // define all colors
+    }));
+    console.log(CFonts.say(sudoku.prettyBoard(solvedPuzzle) , {
+      font: 'console',              // define the font face
+      align: 'center',              // define text alignment
+      colors: ['white'],         // define all colors
+      background: 'transparent',  // define the background color, you can also use `backgroundColor` here as key
+      letterSpacing: 1,           // define letter spacing
+      lineHeight: 1,              // define the line height
+      space: true,               // define if the output text should have empty lines on top and on the bottom
+      maxLength: '40',             // define how many character can be on one line
+      gradient: false,            // define your two gradient colors
+      independentGradient: false, // define if you want to recalculate the gradient for each new line
+      transitionGradient: false,  // define if this is a transition between colors directly
+      env: 'node',                 // define the environment CFonts is being executed in
+    }));
   }
   else {
-    console.log("The board wasn't solved :(");
+    console.log(CFonts.say("The board wasn't solved :(", {
+      font: 'chrome',              // define the font face
+      align: 'center',              // define text alignment
+      colors: ['red', 'yellow','red'],         // define all colors
+    }));
   }
 }
+
+CFonts.say('SUDOKU SOLVE!', {
+  font: 'block',              // define the font face
+  align: 'center',              // define text alignment
+  colors: ['whiteBright'],         // define all colors
+  background: 'transparent',  // define the background color, you can also use `backgroundColor` here as key
+  letterSpacing: 3,           // define letter spacing
+  lineHeight: 2,              // define the line height
+  space: true,               // define if the output text should have empty lines on top and on the bottom
+  maxLength: '13',             // define how many character can be on one line
+  gradient: ['red','blue']            // define your two gradient colors
+  // independentGradient: false, // define if you want to recalculate the gradient for each new line
+  // transitionGradient: false,  // define if this is a transition between colors directly
+  // env: 'node',                // define the environment CFonts is being executed in
+});
 
 // Reads file and sends data from it to the readAndSolve function.
 fs.readFile(
@@ -38,4 +81,3 @@ fs.readFile(
   'utf-8',
   readAndSolve
 );
-
