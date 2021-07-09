619ec9f47fee0743ad09127e6f069caf7bf0575e
Arty <a.tirabyan@yandex.ru>
2021-07-09 11:03:04 +0300

    Transponding string to Array of Arrays

--- runner.js
+++ runner.js
@@ -3,16 +3,17 @@ const fs = require('fs');
 // Use functions from sudoku.js file.
 const sudoku = require('./sudoku');
 
+// Reads file and sends data from it to the readAndSolve function.
+fs.readFile('./sudoku-puzzles.txt', 'utf-8', readAndSolve);
+
 // The sudoku puzzles that your program will solve can be found
 // in the sudoku-puzzles.txt file.
-//
 // Remember, the file has newline characters at the end of each line,
 // so you should remove them.
 
 // Gets one puzzle from the text file.
 function sudokuParse(content, puzzleNumber = 0) {
   let puzzle = content.split('\n')[puzzleNumber];
-  console.log(puzzle);
   return puzzle;
 }
 
@@ -23,6 +24,7 @@ function readAndSolve(err, data) {
   let puzzle = sudokuParse(data);
 
   let solvedPuzzle = sudoku.solve(puzzle);
+
   if (sudoku.isSolved(solvedPuzzle)) {
     console.log("The board was solved!");
     console.log(sudoku.prettyBoard(solvedPuzzle));
@@ -31,11 +33,4 @@ function readAndSolve(err, data) {
     console.log("The board wasn't solved :(");
   }
 }
-
-// Reads file and sends data from it to the readAndSolve function.
-fs.readFile(
-  './sudoku-puzzles.txt',
-  'utf-8',
-  readAndSolve
-);
 
--- sudoku.js
+++ sudoku.js
@@ -3,10 +3,23 @@
 // something representing a board after
 // your solver has tried to solve it.
 // How you represent your board is up to you!
+let a = '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--';
 function solve(boardString) {
-
+  let arr = [];
+  let x = 0;
+  let z = [];
+  for (let i = 0; i <= 8; i++) {
+    for (let j = 0; j <= 8; j++) {
+      arr.push(boardString[x]);
+      x += 1;
+    }
+    z.push(arr);
+    arr = [];
+  }
+  return z;
 }
 
+console.table(solve(a));
 
 // Returns a boolean indicating whether
 // or not the provided board is solved.
@@ -28,7 +41,7 @@ function prettyBoard(board) {
 
 // Exports all the functions to use them in another file.
 module.exports = {
-	solve: solve,
-	isSolved: isSolved,
-	prettyBoard: prettyBoard
+  solve: solve,
+  isSolved: isSolved,
+  prettyBoard: prettyBoard
 }
