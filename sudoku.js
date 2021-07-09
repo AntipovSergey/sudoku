d098b2d5e464c9792db74a67024f32243af4fb60
Alexandra <sasha.lebedeva30@gmail.com>
2021-07-09 13:53:43 +0300

    made function that collects all empty strings indexes

--- sudoku.js
+++ sudoku.js
@@ -6,11 +6,33 @@
 function arrayBoardFromStringBoard(boardString) {
   const arrOfStr = boardString.match(/.{9}/g)
   const result = [];
-  for (let i = 0; i < arrOfStr.length; i ++) {
+  for (let i = 0; i < arrOfStr.length; i++) {
     result.push(arrOfStr[i].split(''))
   }
   return result
 }
+
+function solvingSudoku(string) {
+  // making array board 
+  const board = arrayBoardFromStringBoard(string);
+  // making array of indexes of empty string in format[indexOfArray, indexOfElement]
+  function findAllEmptyCells(board) {
+    const arrayOfIndexesOfEmptyStrings = [];
+    for (let i = 0; i < board.length; i++) {
+      for (let j = 0; j < board[i].length; j++) {
+        if (board[i][j] === '-') {
+          // const indexOfArray = i;
+          // const indexOfElement = j;
+          arrayOfIndexesOfEmptyStrings.push([i,j]);
+        }
+      }
+    }
+    return arrayOfIndexesOfEmptyStrings
+  };
+  // go through emptycell ine by one
+  
+}
+
 
 
 // Returns a boolean indicating whether
@@ -33,7 +55,9 @@ function prettyBoard(board) {
 
 // Exports all the functions to use them in another file.
 module.exports = {
-	solve: solve,
-	isSolved: isSolved,
-	prettyBoard: prettyBoard
+  solve: solve,
+  isSolved: isSolved,
+  prettyBoard: prettyBoard
 }
+
+
