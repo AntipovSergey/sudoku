5ab60d22ae23f1cc7c1641ab7542b61f3b4e0c79
Ksenia <ks.nikolaychuk@gmail.com>
2021-07-30 13:12:35 +0300

    Search in rows and columns

--- sudoku.js
+++ sudoku.js
@@ -1,3 +1,5 @@
+
+
 // Takes a board as a string in the format
 // you see in the puzzle file. Returns
 // something representing a board after
@@ -24,6 +26,31 @@ function isSolved(board) {
 // form `solve` returns.
 function prettyBoard(board) {
 
+}
+//Поиск числа в горизонтальной строке
+function searchWordHorizontal (num,arr) {
+  let ArrElemToString = arr.map(el => el.join(''))
+  for (let i = 0; i < ArrElemToString.length; i++) {
+    if(ArrElemToString[i].includes(num)) {
+      return true;
+    }
+    
+  }
+  return false;
+}
+
+// Поиск числа в вертикальной строке
+function searchWordVertical (num,arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let verticalWord = '';
+    for (let j = 0; j < arr.length; j++) {
+      verticalWord += arr[j][i]      
+    }
+    if(verticalWord.includes(num)) {
+      return true
+    }
+  }
+  return false
 }
 
 // Exports all the functions to use them in another file.
