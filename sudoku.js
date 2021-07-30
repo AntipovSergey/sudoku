d18377d9685d317c1f1bc495bcdab4f6cf93f970
Alex Syro <alsyro@gmail.com>
2021-07-30 14:25:34 +0300

    Some fixing coordinate searching

--- sudoku.js
+++ sudoku.js
@@ -1,6 +1,5 @@
-const fs = require('fs')
-const RAW_SUDOKU_FILE= fs.readFileSync('./sudoku-puzzles.txt', 'utf-8')
-
+const fs = require("fs");
+const RAW_SUDOKU_FILE = fs.readFileSync("./sudoku-puzzles.txt", "utf-8");
 
 // Takes a board as a string in the format
 // you see in the puzzle file. Returns
@@ -15,17 +14,30 @@ function solve(boardString) {}
 // form `solve` returns.
 function isSolved(board) {}
 
+function getCellCoordinates(board) {
+  const coordinateArray = [];
+  for (let row = 0; row < board.length; row++) {
+		for (let col = 0; col < board[row].length; col++) {
+			const colIndex = board[row][col];
+			if (colIndex != -1) {
+				coordinateArray.push([row, col]);
+			}
+		}
+  }
+  return coordinateArray;
+}
+
 // Takes in a board in some form and
 // returns a String that's well formatted
 // for output to the screen.
 // The input board will be in whatever
 // form `solve` returns.
 function prettyBoard(file) {
-	const LINE_BREAKER_REGEXP = /\n/gm
-	let board = file.slice().replace(LINE_BREAKER_REGEXP, '');
+  const LINE_BREAKER_REGEXP = /\n/gm;
+  let board = file.slice().replace(LINE_BREAKER_REGEXP, "");
 
   let boardsContainer = {};
-  let inputString = board.split('');
+  let inputString = board.split("");
   const ROW_LENGTH = 9;
   let sudokuNumberInContainer = 0;
   while (inputString.length !== 0) {
@@ -35,47 +47,45 @@ function prettyBoard(file) {
       sudokuArray.push(stringSlice);
       inputString = inputString.slice(ROW_LENGTH, -1);
     }
-		boardsContainer[sudokuNumberInContainer] = sudokuArray
-		sudokuNumberInContainer += 1;
+    boardsContainer[sudokuNumberInContainer] = sudokuArray;
+    sudokuNumberInContainer += 1;
   }
-	return boardsContainer;
+  return boardsContainer;
 }
 //Поиск числа в горизонтальной строке
-function searchWordHorizontal (num,arr) {
-  let ArrElemToString = arr.map(el => el.join(''))
+function searchWordHorizontal(num, arr) {
+  let ArrElemToString = arr.map((el) => el.join(""));
   for (let i = 0; i < ArrElemToString.length; i++) {
-    if(ArrElemToString[i].includes(num+'')) {
+    if (ArrElemToString[i].includes(num + "")) {
       return true;
     }
-    
   }
   return false;
 }
 
 // Поиск числа в вертикальной строке
-function searchWordVertical (num,arr) {
+function searchWordVertical(num, arr) {
   for (let i = 0; i < arr.length; i++) {
-    let verticalWord = '';
+    let verticalWord = "";
     for (let j = 0; j < arr.length; j++) {
-      verticalWord += arr[j][i]      
+      verticalWord += arr[j][i];
     }
-    if(verticalWord.includes(num+'')) {
-      return true
+    if (verticalWord.includes(num + "")) {
+      return true;
     }
   }
-  return false
+  return false;
 }
 
-const sudokuZero = prettyBoard(RAW_SUDOKU_FILE)['0']
-let isInVertical = searchWordVertical(6, sudokuZero)
-let isInHorizontal = searchWordHorizontal(9, sudokuZero)
+const sudokuZero = prettyBoard(RAW_SUDOKU_FILE)["0"];
+let isInVertical = searchWordVertical(6, sudokuZero);
+let isInHorizontal = searchWordHorizontal(9, sudokuZero);
 
-console.log(isInVertical, isInHorizontal)
+console.log(getCellCoordinates(sudokuZero));
 
 // Exports all the functions to use them in another file.
 module.exports = {
-	solve: solve,
-	isSolved: isSolved,
-	prettyBoard: prettyBoard
-}
-
+  solve: solve,
+  isSolved: isSolved,
+  prettyBoard: prettyBoard,
+};
