b10dee3b0bd5822f71438f13bda849223316ab50
Dima <saykind93@mail.ru>
2021-07-30 15:10:49 +0300

    squats

--- sudoku.js
+++ sudoku.js
@@ -3,18 +3,64 @@
 // something representing a board after
 // your solver has tried to solve it.
 // How you represent your board is up to you!
-function solve(boardString) {
 
+let boardString = 
+[["1","-","5","8","-","2","-","-","-"],
+["-","9","-","-","7","6","4","-","2"],
+["-","8","-","-","6","5","3","-","7"],
+["-","6","-","-","5","2","1","-","3"],
+["-","7","-","-","4","3","2","-","4"],
+["-","-","-","-","3","1","6","-","1"],
+["-","3","-","-","2","4","1","-","9"],
+["-","2","-","-","1","8","9","-","3"],
+["-","1","-","-","0","2","4","-","1"]]
+
+//разделение общего массива на квадраты
+function solve(boardString) {
+let subString =  boardString.join('').replace(/,/g, "")
+let subarray = []
+let subsubsubarray = []
+let finalArray = []
+for(let i = 0; i<subString.length; i++){
+  subarray.push(subString[i])
 }
+
+//let subsubarray = subString[i].split('')
+subsubsubarray = subString.match(/(.{1,3})/g)
+return subsubsubarray
+}
+
+
+let b = 0
+let test = solve(boardString)
+
+console.log(test)
+let finalArray = []
+function newFunc(){
+if (finalArray.length > 24) {return finalArray}
+for (let i = b; i< test.length; i = i + 3){
+ finalArray.push(test[i])
+} 
+b = b + 1
+return newFunc()
+}
+
+test2 = newFunc().join("").match(/(.{1,9})/g)
+console.log(test2)
+let finalArrayF = []
+for (let i = 0; i < test2.length; i++)
+finalArrayF.push(test2[i].split(''))
+console.log(finalArrayF)
+
 
 
 // Returns a boolean indicating whether
 // or not the provided board is solved.
 // The input board will be in whatever
 // form `solve` returns.
-function isSolved(board) {
+//function isSolved(board) {
 
-}
+//}
 
 
 // Takes in a board in some form and
@@ -22,13 +68,13 @@ function isSolved(board) {
 // for output to the screen.
 // The input board will be in whatever
 // form `solve` returns.
-function prettyBoard(board) {
+//function prettyBoard(board) {
 
-}
+//}
 
 // Exports all the functions to use them in another file.
-module.exports = {
-	solve: solve,
-	isSolved: isSolved,
-	prettyBoard: prettyBoard
-}
+//module.exports = {
+//	solve: solve,
+//	isSolved: isSolved,
+//	prettyBoard: prettyBoard
+//}
