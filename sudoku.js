7884206f976b2990c3ce0c22806318208c62c574
Vladimir <khlebnikovvladimir19@gmail.com>
2021-07-30 16:42:36 +0300

    release 148

--- sudoku.js
+++ sudoku.js
@@ -4,32 +4,146 @@
 // your solver has tried to solve it.
 // How you represent your board is up to you!
 function transformPuzzle(puzzleString) {
-let arr = puzzleString.split('');
-let wholePizza = [];
+  let arr = puzzleString.split('');
+  let wholePizza = [];
 
-  for(let i = 0; i < 9; i++) {
-let pizzaSlice = [];
-    for(let j = 0; j < 9; j++) {
-pizzaSlice.push(arr.shift());
+  for (let i = 0; i < 9; i++) {
+    let pizzaSlice = [];
+    for (let j = 0; j < 9; j++) {
+      pizzaSlice.push(arr.shift());
     }
-wholePizza.push(pizzaSlice);
+    wholePizza.push(pizzaSlice);
   }
-
   return wholePizza;
 }
 
+function replaceEmpty(board) {
+  for(let i = 0; i < 9; i++) {
+    for(let j = 0; j < 9; j++) {
+      if (board[i][j] === '-') board[i][j] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    }
+  }
+}
 
-function solve(boardString) {
+let board = [[5, '-' , 0, 0, 0, 0, 0, 1, 2],
+[0, [1, 2, 3, 4, 5, 6, 7, 8, 9], 0, 0, 0, 0, 0, 0, 0],
+[0, 6, 0, 3, 4, 2, 5, 6, 7],
+[8, 5, 9, 7, 6, 1, 4, 2, 3],
+[4, 2, 6, 8, 5, 3, 7, 9, 1],
+[7, 1, 3, 9, 2, 4, 8, 5, 6],
+[9, 6, 1, 5, 3, 7, 2, 8, 4],
+[2, 8, 7, 4, 1, 9, 6, 3, 5],
+[3, 4, 5, 2, 8, 6, 1, 7, 9]];
 
-  return [[5, 3, 4, 6, 7, 8, 9, 1, 2], 
-  [6, 7, 2, 1, 9, 5, 3, 4, 8],
-  [1, 9, 8, 3, 4, 2, 5, 6, 7],
-  [8, 5, 9, 7, 6, 1, 4, 2, 3],
-  [4, 2, 6, 8, 5, 3, 7, 9, 1],
-  [7, 1, 3, 9, 2, 4, 8, 5, 6],
-  [9, 6, 1, 5, 3, 7, 2, 8, 4],
-  [2, 8, 7, 4, 1, 9, 6, 3, 5],
-  [3, 4, 5, 2, 8, 6, 1, 7, 9]]
+replaceEmpty(board);
+console.log(board); 
+
+
+function testSquare(x, y, el, board) {
+  el = board[x][y];
+  if (Array.isArray(el)) {
+    let square = [];
+    if (x < 3 && y < 3) {
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+          square.push(board[i][j]);
+        }
+      }
+    }
+
+    if (x < 3 && y < 6 && y >= 3) {
+      for (let i = 0; i < 3; i++) {
+        for (let j = 3; j < 6; j++) {
+          square.push(board[i][j]);
+        }
+      }
+    }
+
+    if (x < 3 && y < 9 && y >= 6) {
+      for (let i = 0; i < 3; i++) {
+        for (let j = 6; j < 9; j++) {
+          square.push(board[i][j]);
+        }
+      }
+    }
+
+
+    if (x < 6 && x >= 3 && y < 3) {
+      for (let i = 3; i < 6; i++) {
+        for (let j = 0; j < 3; j++) {
+          square.push(board[i][j]);
+        }
+      }
+    }
+
+    if (x < 6 && x >= 3 && y < 6 && y >= 3) {
+      for (let i = 3; i < 6; i++) {
+        for (let j = 3; j < 6; j++) {
+          square.push(board[i][j]);
+        }
+      }
+    }
+
+    if (x < 6 && x >= 3 && y < 9 && y >= 6) {
+      for (let i = 3; i < 6; i++) {
+        for (let j = 6; j < 9; j++) {
+          square.push(board[i][j]);
+        }
+      }
+    }
+
+    if (x < 9 && x >= 6 && y < 3) {
+      for (let i = 6; i < 9; i++) {
+        for (let j = 0; j < 3; j++) {
+          square.push(board[i][j]);
+        }
+      }
+    }
+
+    if (x < 9 && x >= 6 && y < 6 && y >= 3) {
+      for (let i = 6; i < 9; i++) {
+        for (let j = 3; j < 6; j++) {
+          square.push(board[i][j]);
+        }
+      }
+    }
+
+    if (x < 9 && x >= 6 && y < 9 && y >= 6) {
+      for (let i = 6; i < 9; i++) {
+        for (let j = 6; j < 9; j++) {
+          square.push(board[i][j]);
+        }
+      }
+    }
+    for (let i = 0; i < el.length; i++) {
+      
+      if (square.includes(el[i])) {  el[i] = "d";}
+    }
+    board[x][y] = el.filter((elem, i) => elem !== 'd')
+  }
+
+
+}
+
+
+function solve(board) {
+
+
+  for (let i = 0; i < 9; i++) {
+
+    for (let j = 0; j < 9; j++) {
+
+      testVertical(i, j, board);
+
+      testHorizontal(i, j, board);
+      testSquare(i, j, board);
+
+
+    }
+
+  }
+
+
 }
 
 
@@ -37,68 +151,71 @@ function solve(boardString) {
 // or not the provided board is solved.
 // The input board will be in whatever
 // form `solve` returns.
+
+
+
 function isSolved(board) {
   let etalon = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   let copy1 = [];
   for (let i = 0; i < board.length; i++) {
-    copy1.push(board[i].slice()); 
+    copy1.push(board[i].slice());
   }
-  
-  let column = [];  
-  let square = [];  
+
+  let column = [];
+  let square = [];
   let k = 0;
   square[k] = [];
-   
-    for(i = 0; i < 9; i+=3) {
-      for (let j = 0; j < 9; j++){ 
-      square[k].push(board[j][i], board[j][i+1], board[j][i+2]);  
-                if (square[k].length >= 9) { k++; square[k] = []; } 
-      }
+
+  for (i = 0; i < 9; i += 3) {
+    for (let j = 0; j < 9; j++) {
+      square[k].push(board[j][i], board[j][i + 1], board[j][i + 2]);
+      if (square[k].length >= 9) { k++; square[k] = []; }
     }
-    square.pop();
-    for (let prop of square) {
-      prop.sort();
-      for(let i = 0; i < 9; i++) {
-        if (prop[i] != etalon[i]) {
-          return 'Try again!';
-        }
-      
-    }
-      }
-  
-    
-    
-    for(let propp of copy1) {
-      propp.sort();
-      for(let i = 0; i < 9; i++) {
-        if (propp[i] != etalon[i]) {
-          return 'Try again!';
-        }
-        
-      }
-      
-    }
-  
-  
-    for(let i = 0; i < 9; i++) {
-      column[i] = [];
-    board.forEach(a => column[i].push(a[i]));
-    column[i].sort();
-  
-      }
-    
-  
-    for(let prop of column) {
+  }
+  square.pop();
+  for (let prop of square) {
+    prop.sort();
     for (let i = 0; i < 9; i++) {
       if (prop[i] != etalon[i]) {
-          return 'Try again!';
+        return 'Try again!';
       }
-      
-      
+
     }
+  }
+
+
+
+  for (let propp of copy1) {
+    propp.sort();
+    for (let i = 0; i < 9; i++) {
+      if (propp[i] != etalon[i]) {
+        return 'Try again!';
       }
-  
-  return 'Finished!'; 
+
+    }
+
+  }
+
+
+  for (let i = 0; i < 9; i++) {
+    column[i] = [];
+    board.forEach(a => column[i].push(a[i]));
+    column[i].sort();
+
+  }
+
+
+  for (let prop of column) {
+    for (let i = 0; i < 9; i++) {
+      if (prop[i] != etalon[i]) {
+        return 'Try again!';
+      }
+
+
+    }
+  }
+
+  return 'Finished!';
 }
 
 
@@ -114,7 +231,10 @@ function prettyBoard(board) {
 // Exports all the functions to use them in another file.
 module.exports = {
   transformPuzzle: transformPuzzle,
-	solve: solve,
-	isSolved: isSolved,
-	prettyBoard: prettyBoard
+  solve: solve,
+  isSolved: isSolved,
+  prettyBoard: prettyBoard
 }
+
+
+
