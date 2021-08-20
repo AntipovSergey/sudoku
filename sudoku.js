dbeb21ff424eca6aef231ebe3df494bda1f44fab
Nikolay <nelidovn@gmail.com>
2021-08-20 16:45:01 +0300

    проверяет горизонтально и квадратом

--- sudoku.js
+++ sudoku.js
@@ -11,10 +11,62 @@ function boardToArray(string) {
   );
   return nestedArrNum;
 }
+function horizCheck(arr, i, j, n) {
+  let answer = true;
+  for (el of arr[i]) {
+    if (el == n) answer = false;
+  }
+  return answer;
+}
+function verticalCheck(arr, i, j, n) {
+  let answer = true;
+  for (let f = 0; f < arr.length; f++) {
+    if (arr[f][j] === n) answer == false;
+    verArr.push(arr[f][j]);
+    console.log(verArr);
+  }
+  return answer;
+}
+
+function squareCheck(boardSudoku, row_I, col_J, numberValue) {
+  row_I = Math.floor(row_I / 3) * 3;
+  col_J = Math.floor(col_J / 3) * 3;
+  let isFound = true;
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      if (boardSudoku[row_I + i][col_J + j] === numberValue) isFound = false;
+    }
+  }
+  return isFound;
+}
+
+function solution(arr) {
+  let newArr = [...arr];
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[i][j] == null) {
+        for (let n = 1; n <= 9; n++) {
+          if (
+            horizCheck(newArr, i, j, n) &&
+            verticalCheck(newArr, i, j, n) &&
+            squareCheck(newArr, i, j, n)
+          ) {
+            newArr[i][j] = n;
+            if (Array.isArray(solution(newArr))) {
+              return newArr;
+            }
+          }
+        }
+      }
+    }
+  }
+  return newArr;
+}
 
 function solve(boardString) {
   let boardArray = boardToArray(boardString);
-  function solution(boardArray) {}
+  let solutedArr = solution(boardArray);
+  return solutedArr;
 }
 
 // Returns a boolean indicating whether
@@ -37,7 +89,7 @@ module.exports = {
   prettyBoard: prettyBoard,
 };
 console.log(
-  boardToArray(
+  solve(
     '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--'
   )
 );
