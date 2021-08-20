8f475421f4c3a5318bbff566dfd7b1f2615af9b5
Ahmad <ahmad@hojikov.ru>
2021-08-20 14:15:26 +0300

    Массив столбцов и массив квадратов

--- sudoku.js
+++ sudoku.js
@@ -6,6 +6,18 @@
 function solve(boardString) {
 
 }
+let board = [[1, 4, 5, 3, 5, 3, 6, 7, 2],
+[1, 4, 5, 3, 5, 3, 6, 7, 2],
+[1, 4, 5, 3, 5, 3, 6, 7, 2],
+[1, 4, 5, 3, 5, 3, 6, 7, 2],
+[1, 4, 5, 3, 5, 3, 6, 7, 2],
+[1, 4, 5, 3, 5, 3, 6, 7, 2],
+[1, 4, 5, 3, 5, 3, 6, 7, 2],
+[1, 4, 5, 3, 5, 3, 6, 7, 2],
+[1, 4, 5, 3, 5, 3, 6, 7, 2]
+]
+
+
 
 
 // Returns a boolean indicating whether
@@ -14,8 +26,53 @@ function solve(boardString) {
 // form `solve` returns.
 function isSolved(board) {
 }
+function sliceArrayCol(arr, col) {
+  let column = [];
+  for (let i = 0; i < 8; i++) {
+    column.push(arr[col][i])
 
+  }
+  return column
 
+}
+function sliceArraySquare(arr, sq) {
+  let square = [];
+  let obji ={
+    1:0,
+    2:0,
+    3:0,
+    4:3,
+    5:3,
+    6:3,
+    7:6,
+    8:6,
+    9:6,
+  };
+  let objn ={
+    1:0,
+    2:3,
+    3:6,
+    4:0,
+    5:3,
+    6:6,
+    7:0,
+    8:3,
+    9:6,
+    
+  };
+ 
+  let i = obji[sq]
+     for(let a = 0;a < 3; a++){
+      let n = objn[sq]
+       for(let b = 0;b < 3;b++){
+       
+        square.push(arr[i][n])
+        n++
+     }
+     i++
+     }
+  return square
+  }
 // Takes in a board in some form and
 // returns a String that's well formatted
 // for output to the screen.
@@ -27,7 +84,7 @@ function prettyBoard(board) {
 
 // Exports all the functions to use them in another file.
 module.exports = {
-	solve: solve,
-	isSolved: isSolved,
-	prettyBoard: prettyBoard
+  solve: solve,
+  isSolved: isSolved,
+  prettyBoard: prettyBoard
 }
