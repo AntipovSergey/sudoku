2cc1ccc3c36e7b68c885707357b81bb559445b4b
Eduard <eduard.gurdjian@gmail.com>
2021-08-20 14:43:00 +0300

    PrettyBoard rel.1

--- sudoku.js
+++ sudoku.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-plusplus */
 // Takes a board as a string in the format
 // you see in the puzzle file. Returns
 // something representing a board after
@@ -6,7 +7,6 @@
 function solve(boardString) {
 
 }
-
 
 // Returns a boolean indicating whether
 // or not the provided board is solved.
@@ -26,13 +26,17 @@ function prettyBoard(board) {
   const prettyBoardStr = board.map((elem) => elem.join(' ')).join('\n');
   console.log(prettyBoardStr);
 }
+
 const sudokuStr = '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--';
 const sudokuArr = [];
-for (let i = 0; i < 81; )
-prettyBoard()
+for (let i = 0; i < 81; i+=9) {
+  sudokuArr.push(sudokuStr.slice(i, i + 9).split(''));
+}
+console.table(sudokuArr);
+prettyBoard(sudokuArr);
 // Exports all the functions to use them in another file.
 module.exports = {
-	solve: solve,
-	isSolved: isSolved,
-	prettyBoard: prettyBoard
-}
+  solve,
+  isSolved,
+  prettyBoard,
+};
