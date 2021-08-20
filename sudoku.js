fd328424201485b87ee6fa4f00bbd589cfe1ce92
Eduard <eduard.gurdjian@gmail.com>
2021-08-20 11:54:19 +0300

    Релиз 0 Преттиборд

--- sudoku.js
+++ sudoku.js
@@ -23,9 +23,13 @@ function isSolved(board) {
 // The input board will be in whatever
 // form `solve` returns.
 function prettyBoard(board) {
-
+  const prettyBoardStr = board.map((elem) => elem.join(' ')).join('\n');
+  console.log(prettyBoardStr);
 }
-
+const sudokuStr = '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--';
+const sudokuArr = [];
+for (let i = 0; i < 81; )
+prettyBoard()
 // Exports all the functions to use them in another file.
 module.exports = {
 	solve: solve,
