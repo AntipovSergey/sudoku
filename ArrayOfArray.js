d61548cd21b39f94e17817698a499158cc48f2c4
Denis <adadenis1982@yandex.ru>
2021-09-10 14:18:55 +0300

    CreateFunctionArrayOfArray

--- ArrayOfArray.js
+++ ArrayOfArray.js
@@ -1,1 +1,16 @@
-console.log('Hello');
+function sudokuParse(content, puzzleNumber = 0) {
+  const puzzle = content
+    .replaceAll('-', '0')
+    .split('\n')
+    [puzzleNumber].split('');
+
+  let tempArray = [];
+  let num = 9;
+
+  for (let i = 0; i < 81; i += 9) {
+    tempArray.push(puzzle.slice(i, num));
+    num += 9;
+  }
+
+  return tempArray.map((el) => el.map((el) => Number(el)));
+}
