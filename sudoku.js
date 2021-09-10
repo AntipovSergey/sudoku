6695970d9534a83cbcae692a311cc6b8a3da73a9
Kirill <belokopytovks2014@yandex.ru>
2021-09-10 13:40:57 +0300

    prettyBoardForSudoku and parsingSudokuFromText - done

--- sudoku.js
+++ sudoku.js
@@ -4,18 +4,16 @@
 // your solver has tried to solve it.
 // How you represent your board is up to you!
 
-const string = `1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--`;
 function boardToArray(sudokuString) {
   const arrayOfString = sudokuString.match(/.{9}/g);
   const nestedArrayOfString = arrayOfString.map((el) => el.split(""));
   const nestedArrayNumbers = nestedArrayOfString.map((el) =>
     el.map((element) =>
-      element === "-" ? (element = null) : (element = +element)
+      element === "-" ? (element = "-") : (element = +element)
     )
   );
   return nestedArrayNumbers;
 }
-console.table(boardToArray(string));
 
 function solve(boardString) {}
 
@@ -30,13 +28,15 @@ function isSolved(board) {}
 // for output to the screen.
 // The input board will be in whatever
 // form `solve` returns.
-function prettyBoard(board) {
-  const str = "";
-  for (let i = 0; i < board.length; i++) {
-    str += `${board[i].join("  ")}\n` + `\n`;
+
+function prettyBoard(nestedArrayNumbers) {
+  let prettySudoku = "\n";
+  for (let i = 0; i < nestedArrayNumbers.length; i++) {
+    prettySudoku += `${nestedArrayNumbers[i].join("  ")}\n` + `\n`;
   }
-  return str;
+  return prettySudoku;
 }
+console.log(prettyBoard(sudoku));
 
 // Exports all the functions to use them in another file.
 module.exports = {
