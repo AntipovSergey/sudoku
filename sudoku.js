878188468b51a41c67779b0cba24d190a0c609ce
Kirill <belokopytovks2014@yandex.ru>
2021-09-10 11:44:33 +0300

    nestedArrayOfNumbers - done

--- sudoku.js
+++ sudoku.js
@@ -3,8 +3,21 @@
 // something representing a board after
 // your solver has tried to solve it.
 // How you represent your board is up to you!
-function solve(boardString) {
+
+const string = `1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--`;
+function boardToArray(sudokuString) {
+  const arrayOfString = sudokuString.match(/.{9}/g);
+  const nestedArrayOfString = arrayOfString.map((el) => el.split(""));
+  const nestedArrayNumbers = nestedArrayOfString.map((el) =>
+    el.map((element) =>
+      element === "-" ? (element = null) : (element = +element)
+    )
+  );
+  return nestedArrayNumbers;
 }
+console.table(boardToArray(string));
+
+function solve(boardString) {}
 
 // Returns a boolean indicating whether
 // or not the provided board is solved.
