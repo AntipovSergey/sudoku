90042a98a0eafe99ea7d1a8bbad694d4e88cde95
Kirill <belokopytovks2014@yandex.ru>
2021-09-10 11:14:38 +0300

    prettyBoardFirst - done

--- sudoku.js
+++ sudoku.js
@@ -4,18 +4,13 @@
 // your solver has tried to solve it.
 // How you represent your board is up to you!
 function solve(boardString) {
-
 }
-
 
 // Returns a boolean indicating whether
 // or not the provided board is solved.
 // The input board will be in whatever
 // form `solve` returns.
-function isSolved(board) {
-
-}
-
+function isSolved(board) {}
 
 // Takes in a board in some form and
 // returns a String that's well formatted
@@ -23,12 +18,16 @@ function isSolved(board) {
 // The input board will be in whatever
 // form `solve` returns.
 function prettyBoard(board) {
-
+  const str = "";
+  for (let i = 0; i < board.length; i++) {
+    str += `${board[i].join("  ")}\n` + `\n`;
+  }
+  return str;
 }
 
 // Exports all the functions to use them in another file.
 module.exports = {
-	solve: solve,
-	isSolved: isSolved,
-	prettyBoard: prettyBoard
-}
+  solve: solve,
+  isSolved: isSolved,
+  prettyBoard: prettyBoard,
+};
