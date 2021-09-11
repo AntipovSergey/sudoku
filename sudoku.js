e3a02969e1a3a6356b63233ed64758d92d602edc
buralex-git <buralex.gm@gmail.com>
2021-09-12 00:50:21 +0300

    tests implemented

--- sudoku.js
+++ sudoku.js
@@ -243,11 +243,6 @@ function isSolved(board) {
     return everyCheckSuccessful;
 }
 
-const board = solve("1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--");
-console.table(board)
-console.log(isSolved(board));
-
-
 // Takes in a board in some form and
 // returns a String that's well formatted
 // for output to the screen.
