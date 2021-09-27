d8880177711a322e07858945f1e597b493632377
Vlad <woodygarryson@gmail.com>
2021-09-27 13:15:54 +0300

    upd

--- runner.js
+++ runner.js
@@ -10,54 +10,56 @@ const sudoku = require('./sudoku');
 // so you should remove them.
 
 // Gets one puzzle from the text file.
-function sudokuParse(content, puzzleNumber = 0) {
-  let puzzle = content.split('\n')[puzzleNumber].split('');
+let str = '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--'
+
+function sudokuParse(str) {
+  let puzzle = str.split('');
+  console.log(puzzle);
   let arrayOfArrays = []
   let num = 9;
-  for (let i = 0; i < content.length; i += 9) {
+  for (let i = 0; i < str.length; i += 9) {
     arrayOfArrays.push(puzzle.slice(i, num));
-    num = num + i
+    num = num + 9
   } return arrayOfArrays;
-  // console.log(puzzle);
   // return puzzle;
 }
+console.log(sudokuParse(str));
+// let checkResult = sudokuParse()
 
-let checkResult = sudokuParse()
+// function check(checkResult) {
+//   for (let i = 0; i < checkResult.length; i++) {
+//     for (let j = 0; j < checkResult.length; j++) {
+//       if (checkResult === '-') {
+//         for (let k = 1; k <= 9; k++) {
+//           if (!checkResult[i].includes(k)) {
+//             checkResult[i][j] = k;
+//           } continue;
+//         }
+//       }
+//     }
+//   } return checkResult
+// }
 
-function check(checkResult) {
-  for (let i = 0; i < checkResult.length; i++) {
-    for (let j = 0; j < checkResult.length; j++) {
-      if (checkResult === '-') {
-        for (let k = 1; k <= 9; k++) {
-          if (!checkResult[i].includes(k)) {
-            checkResult[i][j] = k;
-          } continue;
-        }
-      }
-    }
-  } return checkResult
-}
+// function readAndSolve(err, data) {
+//   if (err) {
+//     throw err;
+//   }
+//   let puzzle = sudokuParse(data);
 
-function readAndSolve(err, data) {
-  if (err) {
-    throw err;
-  }
-  let puzzle = sudokuParse(data);
+//   let solvedPuzzle = sudoku.solve(puzzle);
+//   if (sudoku.isSolved(solvedPuzzle)) {
+//     console.log("The board was solved!");
+//     console.log(sudoku.prettyBoard(solvedPuzzle));
+//   }
+//   else {
+//     console.log("The board wasn't solved :(");
+//   }
+// }
 
-  let solvedPuzzle = sudoku.solve(puzzle);
-  if (sudoku.isSolved(solvedPuzzle)) {
-    console.log("The board was solved!");
-    console.log(sudoku.prettyBoard(solvedPuzzle));
-  }
-  else {
-    console.log("The board wasn't solved :(");
-  }
-}
-
-// Reads file and sends data from it to the readAndSolve function.
-fs.readFile(
-  './sudoku-puzzles.txt',
-  'utf-8',
-  readAndSolve
-);
+// // Reads file and sends data from it to the readAndSolve function.
+// fs.readFile(
+//   './sudoku-puzzles.txt',
+//   'utf-8',
+//   readAndSolve
+// );
 
