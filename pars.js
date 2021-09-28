174a355eb839fefdca0a43612715cf68559671cd
Djambulat <buvi008@mail.ru>
2021-09-28 12:29:34 +0300

    Pars:ReChanged2.0

--- pars.js
+++ pars.js
@@ -1,22 +1,26 @@
 const fs = require('fs');
 
+// const array = [];
 let pars = fs.readFileSync('./sudoku-puzzles.txt', 'utf-8');
-pars = pars.split('\n');
 
-
-function parsing(str) {
-  const arr = [];
-  for (let i = 0; i < str.length; i += 9) {
-    arr.push(str.slice(i, i+9).split(''))
-  }
-
+function getArrayOfStrings(arr) {
+  arr = pars.split('\n');
+  arr.pop();
   return arr;
 }
 
-function getRightString(num) {
-  return pars[num - 1];
+// console.log(getArrayOfStrings(pars));
+const arrayOfPuzzles = getArrayOfStrings(pars)
+
+function parsing(indexPuzzle) {
+  const puzzle = arrayOfPuzzles[indexPuzzle-1]
+  const arr = [];
+  for (let i = 0; i < puzzle.length; i += 9) {
+    arr.push(puzzle.slice(i, i+9).split(''))
+  }
+  console.log(arr);
+  return arr
 }
 
-
-parsing(getRightString(13))
+// parsing(13)
 module.exports = {parsing}
