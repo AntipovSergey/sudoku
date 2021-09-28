9ea1cdc0f14d1ba159870e2e6ffebac9e19fad57
Egor <egor.bartuli@gmail.com>
2021-09-28 17:12:42 +0300

    border right and bottom

--- script.js
+++ script.js
@@ -16,10 +16,19 @@ function buttons (func1=1, func2=1) {
     div.appendChild(table);
     const arrSove = pars(uIwD.flevel.value);
       for (let i = 0; i < arrSove.length; i++) {
-        let tr = document.createElement('tr')
+        let tr = document.createElement('tr');
         for (let j = 0; j < arrSove[i].length; j++) {
           let td = document.createElement('td');
           td.innerHTML = arrSove[i][j];
+          if (j === 2 || j === 5) {
+            td.className = 'borderRight'
+          }
+          if (i === 2 || i === 5) {
+            td.className = 'borderBottom'
+          }
+          if ((j === 2 || j === 5) && (i === 2 || i === 5)) {
+            td.className = 'toBorder'
+          }
           tr.appendChild(td);
         }
         table.appendChild(tr);
