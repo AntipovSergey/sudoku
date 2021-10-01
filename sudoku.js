ba8f4298fb384651f7b2178f222abb2745118a74
d4rsen <kostoprav.darsen@gmail.com>
2021-10-01 11:55:20 +0300

    второй коммит

--- sudoku.js
+++ sudoku.js
@@ -3,6 +3,8 @@
 // something representing a board after
 // your solver has tried to solve it.
 // How you represent your board is up to you!
+let test = 'test'
+
 function solve(boardString) {
 
 }
