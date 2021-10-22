a35f835514ec558a2990c680c607454e30830e33
Michael <alsm16.wm@gmail.com>
2021-10-22 14:43:39 +0300

    ADD: squareCheck

--- sudoku.js
+++ sudoku.js
@@ -3,10 +3,7 @@
 // something representing a board after
 // your solver has tried to solve it.
 // How you represent your board is up to you!
-function solve(boardString) {
-
-}
-
+function solve(boardString) {}
 
 const findEmpty = (board) => {
   // find '.'
@@ -18,22 +15,56 @@ const findEmpty = (board) => {
   // Найти пустую клетку
   // На входе полная таблица
   // Итерируемся через строки
-    for (let row = 0; row < size; row++) {
-      //  Итерируемся по строке выбирая c  значения 
-        for (let column = 0; column < size; column++) {
-           // Если элемент в строке r и на её позиции column  
-           // === '.'
-            if(board[row][column] === '.') {
-                return [row,column];
-            }
-        }
+  for (let row = 0; row < size; row++) {
+    //  Итерируемся по строке выбирая c  значения
+    for (let column = 0; column < size; column++) {
+      // Если элемент в строке r и на её позиции column
+      // === '.'
+      if (board[row][column] === ".") {
+        return [row, column];
+      }
     }
-    return 'ready';
+  }
+  return "ready";
+};
+
+
+function squareCheck(arr, pos, target) {
+  let tests = {0:/[0-2]/, 3:/[3-5]/, 6:/[6-8]/}
+  const [row,column] = pos;
+  let startRow;
+  let startColumn;
+  let element;
+  
+  // Приведение к нормальной точке отсчёта)
+  for (i in tests){
+    regex = tests[i]
+    if(regex.test(row)){
+      startRow = i
+    }
+    if(regex.test(column)){
+      startColumn = i
+    } 
+  }
+  console.log(startRow, startColumn);
+  
+  for (let i = startRow; i < startRow + 3; i++){
+    for (let j = startColumn; j < startColumn + 3; j++){
+      element = arr[i][j]
+      if (element !== '.'){
+        if(Number(element) === target){
+          return false
+        } 
+        
+      }
+    }
+  }
+  // Совпадение
+  // На входе полная таблица
+  
+  return true;
 }
 
-
-
-containerForSquareCheck(board)
 let board = [
   ["5", "3", "5", "3", "7", ".", ".", ".", "."],
   ["6", ".", ".", "1", "9", "5", ".", ".", "."],
@@ -43,33 +74,28 @@ let board = [
   ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
   [".", "6", ".", ".", ".", ".", "2", "8", "."],
   [".", ".", ".", "4", "1", "9", ".", ".", "5"],
-  [".", ".", ".", ".", "8", ".", ".", "7", "9"]
-]; 
+  [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+];
 console.log(
-  findEmpty(board)
+  squareCheck(board, [1,1],3)
 );
 
 // Returns a boolean indicating whether
 // or not the provided board is solved.
 // The input board will be in whatever
 // form `solve` returns.
-function isSolved(board) {
-
-}
-
+function isSolved(board) {}
 
 // Takes in a board in some form and
 // returns a String that's well formatted
 // for output to the screen.
 // The input board will be in whatever
 // form `solve` returns.
-function prettyBoard(board) {
-
-}
+function prettyBoard(board) {}
 
 // Exports all the functions to use them in another file.
 module.exports = {
-	solve: solve,
-	isSolved: isSolved,
-	prettyBoard: prettyBoard
-}
+  solve: solve,
+  isSolved: isSolved,
+  prettyBoard: prettyBoard,
+};
