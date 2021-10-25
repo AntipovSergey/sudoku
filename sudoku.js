3deab090b214c3de5c785c7db5df9a757f5a688f
Александр Масалов <89600779+mslsash@users.noreply.github.com>
2021-10-25 15:52:49 +0300

    done check-funktions

--- sudoku.js
+++ sudoku.js
@@ -1,8 +1,37 @@
+// Функции проверки на наличие значений
+// по горизнотали
+function horizontalСheck(board, number, i) {
+    for(let j = 0; j < board.length; j++) {
+        if(number === board[i][j]) return false;
+    }
+}
+// по вертикали
+function verticalСheck(board, number, j) {
+    for(let i = 0; i < board.length; i++) {
+        if(number === board[i][j]) return false;
+    }
+}
+// по блоку
+function blockCheck(board, i, j, number) {
+    for (let k = 0; k < 9; k++) {
+      const ai = 3 * Math.floor(i / 3) + Math.floor(k / 3);
+      const bj = 3 * Math.floor(j / 3) + (k % 3);
+      if (number === board[ai][bj]) return false;
+    }  
+    return true;
+  }
+
+
+
+
 // Takes a board as a string in the format
 // you see in the puzzle file. Returns
 // something representing a board after
 // your solver has tried to solve it.
 // How you represent your board is up to you!
+// Принимает доску в виде строки в формате, который вы видите в файле головоломки. 
+// Возвращает что-то, представляющее доску, после того, как решатель попытался ее решить. 
+// Как вы представляете свою доску, зависит от вас!
 function solve(boardString) {
 
 }
@@ -12,6 +41,8 @@ function solve(boardString) {
 // or not the provided board is solved.
 // The input board will be in whatever
 // form `solve` returns.
+// Возвращает логическое значение, указывающее, решена ли заданная доска. 
+// Поле ввода будет в той форме, в которой возвращается «решение».
 function isSolved(board) {
 
 }
@@ -22,11 +53,14 @@ function isSolved(board) {
 // for output to the screen.
 // The input board will be in whatever
 // form `solve` returns.
+// Принимает доску в той или иной форме и возвращает строку, хорошо отформатированную для вывода на экран. 
+// Поле ввода будет в той форме, в которой возвращается «решение».
 function prettyBoard(board) {
 
 }
 
 // Exports all the functions to use them in another file.
+// Экспортирует все функции, чтобы использовать их в другом файле.
 module.exports = {
 	solve: solve,
 	isSolved: isSolved,
