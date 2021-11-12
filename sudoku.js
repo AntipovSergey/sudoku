f51744713f6de247024ba2166303a89935082497
Marsel33 <marsel.ibatullin1993@gail.com>
2021-11-12 15:09:48 +0300

    dsnckj

--- sudoku.js
+++ sudoku.js
@@ -3,9 +3,21 @@
 // something representing a board after
 // your solver has tried to solve it.
 // How you represent your board is up to you!
-function solve(boardString) {
+function isSolved(board) {
 
-}
+	const xBoard = board.Math.floor(x / 3) * 3
+	const yBoard = board.Math.floor(y / 3) * 3
+	
+	  for (let y = 0; y <= yBoard; y += 1) {
+		for (let x = 0; x <= xBoard; x += 1) {
+		  if (xBoard[y][x] === n || yBoard[x][y] === n){
+			return false
+		  } else {
+			return true 
+		  }
+		}
+	  }
+	}
 
 
 // Returns a boolean indicating whether
