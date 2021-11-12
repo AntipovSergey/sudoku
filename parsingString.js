178f8cc9a47af2543eecd3f151456fbcb92299cd
egor <egornick98@gmail.com>
2021-11-12 14:36:53 +0300

    parsingString

---
+++ parsingString.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const sudocuString = fs.readFileSync('./sudoku-puzzles.txt', 'utf-8')
+
+function parsingString(str)  {
+const reg = /-/g
+const replaseString = str.replace(reg, '0');
+let sudokuArr = replaseString.split('');
+const resultArr = [];
+for (let i = 1; i < 10; i++) {
+  resultArr.push(sudokuArr.splice(0, 9))
+};
+const swapStrNum = resultArr.map((el) => el.map((el) => Number(el)))
+return swapStrNum
+}
+
+console.log(parsingString(sudocuString))
+
+
