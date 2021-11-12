b8851a2b7a96c797a02e1c19a8e4fa3d4a8a3930
Aleksey Mityukov <imondoro@gmail.com>
2021-11-12 11:46:11 +0300

    fs

--- sudoku.js
+++ sudoku.js
@@ -1,20 +1,31 @@
+const fs = require('fs');
+const { findSourceMap } = require('module');
+function getBoards(path) {
+  let input = fs.readFileSync(`${__dirname}${path}.txt`, 'utf-8')
+  return input.trim().split('\n');
+}
+
+function getSudokuTask(num) {
+
+}
+console.log(getSudokuTask(0))
 // Takes a board as a string in the format
-// you see in the puzzle file. Returns
+// you see in the board file. Returns
 // something representing a board after
 // your solver has tried to solve it.
 // How you represent your board is up to you!
-function solve(boardString) {
+// function solve(boardString) {
 
-}
+// }
 
 
 // Returns a boolean indicating whether
 // or not the provided board is solved.
 // The input board will be in whatever
 // form `solve` returns.
-function isSolved(board) {
+// function isSolved(board) {
 
-}
+// }
 
 
 // Takes in a board in some form and
@@ -22,13 +33,14 @@ function isSolved(board) {
 // for output to the screen.
 // The input board will be in whatever
 // form `solve` returns.
-function prettyBoard(board) {
+// function prettyBoard(board) {
 
-}
+// }
 
 // Exports all the functions to use them in another file.
-module.exports = {
-	solve: solve,
-	isSolved: isSolved,
-	prettyBoard: prettyBoard
-}
+// module.exports = {
+// 	solve: solve,
+// 	isSolved: isSolved,
+// 	prettyBoard: prettyBoard
+
+// }
