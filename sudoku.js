46f3429c1630db08aa7ee361fdb462b54966b90d
Aleksey Mityukov <imondoro@gmail.com>
2021-11-12 12:10:51 +0300

    get board

--- sudoku.js
+++ sudoku.js
@@ -1,14 +1,28 @@
 const fs = require('fs');
-const { findSourceMap } = require('module');
+
 function getBoards(path) {
   let input = fs.readFileSync(`${__dirname}${path}.txt`, 'utf-8')
   return input.trim().split('\n');
 }
+function taskToArr(num){
+  let formattedStr = '';
+  let arr = [];
+  let unformattedStr = getSudokuTask(num)
+  for (let i = 0; i < unformattedStr.length -8; i = i + 9) {
+    formattedStr += unformattedStr.slice(i, i + 9) + '\n'
+  }
+  arr = formattedStr.trim().split('\n');
+  return arr;
+}
+console.log(taskToArr(0))
 
 function getSudokuTask(num) {
-
+  let allTasks = getBoards('/sudoku-puzzles')
+  return allTasks[num];
 }
-console.log(getSudokuTask(0))
+
+const board = taskToArr()
+
 // Takes a board as a string in the format
 // you see in the board file. Returns
 // something representing a board after
