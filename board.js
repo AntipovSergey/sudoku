7692eae5208f35ac16c7c5cbe78c1cf0ef51e026
Anatoly Karpov <karpovplace@gmail.com>
2021-11-12 15:45:26 +0300

    level1

--- board.js
+++ board.js
@@ -16,18 +16,160 @@ function makeBoard(str) {
 /*Возвращает массив оставшихся кандидатов*/
 // coordinates = [x,y], где x строка, y столбец
 
+function lineSearch(board, candidates, coordinates) {
+  let survivCandidates = [];
 
+  for (let i = coordinates[0]; i < board.length; i++) {
+    let boardOne = board[i];
 
-
-
-function squareSearch (board, candidates) {
-
-function squareSearch(board, candidates) {
-
-  if (board.length === 1) {
-    return board;
+    for (let j = 0; j < candidates.length; j++) {
+      if (!boardOne.includes(candidates[j])) {
+        survivCandidates.push(candidates[j]);
+      }
+    }
+    return survivCandidates;
   }
 }
+function columnSearch (board, candidates, coordinates) {
+  let result = []
+  let arr = []
+  let coor = coordinates[1]
+
+  if (candidates.length === 1) {
+    return candidates
+  }
+
+  for(let i = 0; i < board.length; i++) {
+    arr.push(board[i][coor])
+  }
+  
+  for (let i = 0; i < candidates.length; i++ ) {
+  if (!arr.includes(candidates[i])) {
+    result.push(candidates[i])
+  }
+  }
+  return result
+}
+
+function squareSearch (board, candidates, coordinates) {
+  if (candidates.length === 1) {
+    return candidates
+  }
+  let x = coordinates[0] // это внешний массив
+  let y = coordinates[1] // это внутренний
+  let square = []
+  switch (x){
+    case 0 : // -------------------------------------- вот здесь первая треть
+    case 1 :
+    case 2 :
+      switch (y){
+        case 0 :
+        case 1 :
+        case 2 : {
+          for (let i = 0; i < 3; i++){
+            square.push(board[i][0])
+            square.push(board[i][1])
+            square.push(board[i][2])
+          }          
+        } break;
+        case 3 :
+        case 4 :
+        case 5 : {
+          for (let i = 0; i < 3; i++){
+            square.push(board[i][3])
+            square.push(board[i][4])
+            square.push(board[i][5])
+          }          
+        } break;
+        case 6 :
+        case 7 :
+        case 8 : {
+          for (let i = 0; i < 3; i++){
+            square.push(board[i][6])
+            square.push(board[i][7])
+            square.push(board[i][8])
+          }          
+        } break;        
+      } break;
+    case 3 : // -------------------------------------- вот здесь вторая треть
+    case 4 :
+    case 5 :
+      switch (y){
+        case 0 :
+        case 1 :
+        case 2 : {
+          for (let i = 3; i < 6; i++){
+            square.push(board[i][0])
+            square.push(board[i][1])
+            square.push(board[i][2])
+          }          
+        } break;
+        case 3 :
+        case 4 :
+        case 5 : {
+          for (let i = 3; i < 6; i++){
+            square.push(board[i][3])
+            square.push(board[i][4])
+            square.push(board[i][5])
+          }          
+        } break;
+        case 6 :
+        case 7 :
+        case 8 : {
+          for (let i = 3; i < 6; i++){
+            square.push(board[i][6])
+            square.push(board[i][7])
+            square.push(board[i][8])
+          }          
+        } break;        
+      } break;
+    case 6 : // -------------------------------------- вот здесь третья треть
+    case 7 :
+    case 8 :
+      switch (y){
+        case 0 :
+        case 1 :
+        case 2 : {
+          for (let i = 6; i < 9; i++){
+            square.push(board[i][0])
+            square.push(board[i][1])
+            square.push(board[i][2])
+          }          
+        } break;
+        case 3 :
+        case 4 :
+        case 5 : {
+          for (let i = 6; i < 9; i++){
+            square.push(board[i][3])
+            square.push(board[i][4])
+            square.push(board[i][5])
+          }          
+        } break;
+        case 6 :
+        case 7 :
+        case 8 : {
+          for (let i = 6; i < 9; i++){
+            square.push(board[i][6])
+            square.push(board[i][7])
+            square.push(board[i][8])
+          }          
+        } break;        
+      } break;
+    }
+
+    // теперь отбираем нужное
+    result = []
+    for (let i = 0; i < candidates.length; i++) {
+      if(!square.includes(candidates[i])){
+        result.push(candidates[i])
+      }
+    }
+    return result
+  }
+
+
+
+
 
 module.exports = {
   makeBoard,
