abf38289575b5ddf0b4e7645cffae8e77e1a1b21
Ilya <westandsouth2@gmail.com>
2021-11-22 16:53:38 +0300

    sudokuParse

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ const sudokuPuzzles = fs.readFileSync('./sudoku-puzzles.txt', 'utf-8');
 
 function sudokuParse(content, puzzleNumber = 0) {
   let puzzle = content.split('\n')[puzzleNumber];
-  console.log(puzzle);
-  return puzzle;
+  let arr = [];
+  for(let i = 0; i < 81; i += 9) {
+    arr.push(puzzle.slice(i, i + 9).split(''));
+    console.log(puzzle.slice(i, i + 9));
+  }
+  console.log(puzzle.length);
+  return arr;
 }
 
+console.log(sudokuParse(sudokuPuzzles));
 
-sudokuParse(sudokuPuzzles);
-sudokuParse(sudokuPuzzles, 1);
-sudokuParse(sudokuPuzzles, 2);
-sudokuParse(sudokuPuzzles, 3);
-sudokuParse(sudokuPuzzles, 4);
