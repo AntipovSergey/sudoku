2604c1fc0087119b6444a10c1978a05890e0086b
VVonderfu11 <kabaka093@gmail.com>
2021-11-23 16:35:07 +0500

    SudokuReshatel 1-5

--- sudoku.js
+++ sudoku.js
@@ -1,4 +1,5 @@
-let text = "1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89---";
+// let text = "1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89---";
+let text = '---6891--8------2915------84-3----5-2----5----9-24-8-1-847--91-5------6--6-41----'
 let arrSudoky = text
   .match(/(.{9})/g)
   .map(el => el.split(''))
@@ -23,6 +24,8 @@ function findStick(arrEdit) {
   return newArr
 }
 
+let sticks = findStick(arrSudoky)
+
 function rowSearch(findStick, board) {
   const digit = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   for (let i = 0; i < board[findStick[0]].length; i++) {
@@ -33,19 +36,20 @@ function rowSearch(findStick, board) {
   return digit
 }
 
-//Поис по вертикали судоку
-function columnSearch(arrSudoky, findStick, rowSearch) {
-    let x = findStick[0] // 
-    let y = findStick[1] // 
+
+function columnSearch(arrSudoky, findStick, futureVal) {
+    let x = findStick[0]
+    let y = findStick[1]
     arr = [];
     for (let i = 0; i < arrSudoky.length; i++){
         if (arrSudoky[i][y] !== null) {
                 arr.push(arrSudoky[i][y])
         }
     }
-    result = futureVal[x].filter(el => !~arr.indexOf(el));
+    result = futureVal.filter(el => !~arr.indexOf(el));
     return result
 }
+
 
 function searchInSquare (elemArr, sudArr, resultsArr) {
     const topOfSquare = [];
@@ -55,13 +59,38 @@ function searchInSquare (elemArr, sudArr, resultsArr) {
         for (let j = topOfSquare[0]; j < (topOfSquare[0] + 3); j++) {
             if (resultsArr.includes(sudArr[j][i])) {
                 resultsArr.splice(resultsArr.indexOf(sudArr[j][i]), 1) 
-            }
-            
-        }
-        
+            }   
+        }   
     }
-    if (resultsArr.length === 1) {
-            sudArr[elemArr[0]][elemArr[1]] = resultsArr[0];
-        }
-    return sudArr
+    return resultsArr
 }
+
+
+function sudokuReshatel (arrSudoky, sticks){
+  let temp = []
+  for (let i = 0; i < arrSudoky.length; i++) {
+    if(!arrSudoky[i].includes(null)){
+      temp.push(true)
+    } else {
+      temp.push(false)
+    }
+  }
+  if (!temp.includes(false)){
+    return arrSudoky
+  }
+
+
+  for(let i = 0; i < sticks.length; i++){
+    let resultsOfRow = rowSearch(sticks[i], arrSudoky)
+    let resultOfcolumn = columnSearch(arrSudoky, sticks[i], resultsOfRow)
+    let results = searchInSquare(sticks[i], arrSudoky, resultOfcolumn)
+    if (results.length === 1) {
+            arrSudoky[sticks[i][0]][sticks[i][1]] = results[0];
+            sticks.splice(i, 1);
+         }
+  }
+  console.table(arrSudoky)
+  return sudokuReshatel(arrSudoky, sticks)
+}
+// console.table(arrSudoky);
+console.log(sudokuReshatel(arrSudoky, sticks));
