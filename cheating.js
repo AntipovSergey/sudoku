fc1d8a0397db4616d1e02d47cc09d52b3b005761
G.Podakov <podakov@gmail.com>
2021-11-23 13:01:40 +0300

    cheating update

--- cheating.js
+++ cheating.js
@@ -4,13 +4,16 @@ const solve = require('@mattflow/sudoku-solver');
 const sudokuPuzzles = fs.readFileSync('./sudoku-puzzles.txt', 'utf-8');
 
 function sudokuSolver2_1(content, puzzleNumber = 0) {
-  const puzzle = content.split('\n')[puzzleNumber].replace(/-/gm, '0');
-  const solved = solve(puzzle);
+  const puzzle = content.split('\n')[puzzleNumber];
+  const solved = solve(puzzle, {
+    maxIterations: 9999999, emptyValue: '-', hintCheck: false,
+  }); //Аргументы в объекте. Он не решал последние судоку из-за того, что было мало изначально заполненных значений. 
+  // По правилам судоку их должно быть 17. Ошибка решается добавлением hintCheck: false в объект аргумента
   const solvedArr = [];
   for (let i = 0; i < 81; i += 9) {
-    solvedArr.push(solved.slice(i, i + 9).split(''));
+   solvedArr.push(solved.slice(i, i + 9).split(''));
   }
   return solvedArr;
 }
 
-console.table(sudokuSolver2_1(sudokuPuzzles));
+console.table(sudokuSolver2_1(sudokuPuzzles, 14));
