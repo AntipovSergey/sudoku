ab7da2418dbb12c22a6f5a5b72615b38c6e4fb45
Nagornaya <o.nagornaya7@gmail.com>
2021-12-03 15:57:55 +0300

    pretty 2

--- spec/sudoku.spec.js
+++ spec/sudoku.spec.js
@@ -1,2 +1,12 @@
-const sudoku = require('../sudoku.js')
+const {prettyBoard} = require('../sudoku.js')
 
+let board = '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--';
+
+describe('prettyBoard must return', () => {
+  it('should return', () => {
+    expect(prettyBoard(board)[0]).toEqual(['1',  '.', '5', '8', '.', '2', '.', '.', '.']);
+  });
+  it('should return', () => {
+    expect(prettyBoard(board)[1]).toStrictEqual( ['.', '9',  '.', '.', '7',  '6', '4',  '.', '5']);
+  });
+})
--- sudoku.js
+++ sudoku.js
@@ -18,7 +18,77 @@ function solve(boardString) {}
 // или нет решена предоставленная плата.
 // Плата ввода будет в любом
 // возврат из формы `resolve`.
-function isSolved(board) {}
+
+// let board = [
+//   [2,3,8,9,6,5,7,1,4],
+//   [7,5,9,4,1,3,6,8,2],
+//   [4,1,6,2,7,8,9,5,3],
+//   [9,4,5,1,3,6,2,7,8],
+//   [6,8,7,5,2,4,1,3,9],
+//   [3,2,1,8,9,7,4,6,5],
+//   [1,6,2,3,5,9,8,4,7],
+//   [5,7,4,6,8,2,3,9,1],
+//   [8,9,3,7,4,1,5,2,6],
+// ];
+
+// //Функция для нахождения чисел от 1 до 9 во входящем массиве
+// //Возвращает false, если не найдено какое либо число 
+// function isSolved (input) {
+//   for (let i=1; i<=9; i++) {
+//       if (input.indexOf(i) == -1) {
+//           return false;
+//       }
+//   }
+  
+//   return true;
+// }
+
+// //перевернуть массив для проверки строк
+// function reversed(arr) {
+//   for (x = 0; x < 9; x++)
+//       for (y = x + 1; y < 9; y++) {
+//           let temp = arr[x][y];
+//           arr[x][y] = arr[y][x];
+//           arr[y][x] = temp;
+//       }
+//   return arr;
+// };
+
+// //создание массива из квадрата 3x3 
+// // X и Y координаты верхнего левого угла квадрата
+// function box(Y, X, arr) {
+//   let output = [];
+// for (let i=0; i<3; i++) {
+//   for (let j=0; j<3; j++) {
+//           output.push(arr[Y+i][X+j]);
+//   };
+//   };
+//   return output;
+// };
+
+// var transposedBoard = reversed(board);
+
+// //проверка массива board
+// for (let i=0; i<9; i++) {
+//   //сначала проверяем правильность строк и столбцов
+//   if (!isSolved(board[i]) || !isSolved(transposedBoard[i])) {
+//       console.log("некорректно");
+//       return false;
+//   }
+
+//   //проверка квадратов 3x3
+// let y=Math.floor(i/3)*3;
+//   let x=(i%3)*3;
+//   if (!isSolved(box(y,x,board))) {
+//       console.log("некорректно");
+//       return false; 
+//   }
+// };
+
+
+// console.log("корректно");
+// return true;
+
 
 // Takes in a board in some form and
 // returns a String that's well formatted
@@ -50,12 +120,12 @@ function prettyBoard(board) {
   }
   return arr;
 }
-console.log(prettyBoard(board));
+// console.log(prettyBoard(board));
 
-Exports all the functions to use them in another file.
-Экспортирует все функции, чтобы использовать их в другом файле.
+// Exports all the functions to use them in another file.
+// Экспортирует все функции, чтобы использовать их в другом файле.
 module.exports = {
-	solve: solve,
-	isSolved: isSolved,
+	// solve: solve,
+	// isSolved: isSolved,
 	prettyBoard: prettyBoard
 }
