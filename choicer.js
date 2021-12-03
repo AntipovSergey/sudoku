9c14e8a9bb12c47dc130bd34b378eb128e274f14
Alexei.G <Bigud007@mail.ru>
2021-12-03 15:24:35 +0300

    finder+

---
+++ choicer.js
@@ -0,0 +1,25 @@
+const solve = () => {
+  const currPos = findEmpty(board);
+
+  if (currPos === null) {
+      return true;
+  }
+  // console.log('------------------------------');
+  for (let i = 1; i < size + 1; i++) {
+      const currNum = i.toString();
+      const isValid = validate(currNum, currPos, board);
+      // console.log('currPos ', currPos, 'currNum ',currNum, 'isValid ',isValid);
+      if (isValid) {
+          const [x,y] = currPos;
+          board[x][y] = currNum;
+
+          if(solve()) {
+              return true;
+          }
+
+          board[x][y] = '.';
+      }
+  }
+
+  return false;
+}
