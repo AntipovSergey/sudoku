a0aa1eaf63db3ee3e94ead1d4cbacf401346138e
Alex <piratkaignat@gmail.com>
2021-12-03 13:20:57 +0200

    podmasteria9

--- matrixboard.js
+++ matrixboard.js
@@ -8,7 +8,7 @@ function getBoard (puzzle) {
     board.push(arr.splice(0, 9))
   }
   return board
-}
+}bhg
 
 console.table( getBoard ('1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--')
 );
