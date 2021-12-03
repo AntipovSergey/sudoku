1eecdc870d27ad5d7f8799d3230c0c49fe53f5cf
Nagornaya <o.nagornaya7@gmail.com>
2021-12-03 13:21:09 +0300

    prettyboard function release

--- sudoku.js
+++ sudoku.js
@@ -8,10 +8,7 @@
 // что-то, представляющее доску после
 // ваш решатель пытался решить эту проблему.
 // Как вы представляете свою доску, зависит от вас!
-function solve(boardString) {
-
-}
-
+function solve(boardString) {}
 
 // Returns a boolean indicating whether
 // or not the provided board is solved.
@@ -21,10 +18,7 @@ function solve(boardString) {
 // или нет решена предоставленная плата.
 // Плата ввода будет в любом
 // возврат из формы `resolve`.
-function isSolved(board) {
-
-}
-
+function isSolved(board) {}
 
 // Takes in a board in some form and
 // returns a String that's well formatted
@@ -36,12 +30,30 @@ function isSolved(board) {
 // для вывода на экран.
 // Плата ввода будет в любом
 // возврат из формы `resolve`.
+
+let board =
+  '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--';
+
 function prettyBoard(board) {
-
+  let arr = new Array(9);
+  let char = 0;
+  for (let index = 0; index < arr.length; index++) {
+    arr[index] = [];
+    for (let i = 0; i < 9; i++) {
+      if (board[char] != '-') {
+        arr[index].push(board[char]);
+      } else {
+        arr[index].push('.');
+      }
+      char++;
+    }
+  }
+  return arr;
 }
+console.log(prettyBoard(board));
 
-// Exports all the functions to use them in another file.
-// Экспортирует все функции, чтобы использовать их в другом файле.
+Exports all the functions to use them in another file.
+Экспортирует все функции, чтобы использовать их в другом файле.
 module.exports = {
 	solve: solve,
 	isSolved: isSolved,
