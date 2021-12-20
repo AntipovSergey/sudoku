3626a4458170079bf6b32c0ca072005d5601c2b2
Valeria <valeria1992.08@bk.ru>
2021-12-20 17:24:32 +0300

    доска,до проверки

--- sudoku.js
+++ sudoku.js
@@ -3,6 +3,7 @@
 // something representing a board after
 // your solver has tried to solve it.
 // How you represent your board is up to you!
+
 function solve(boardString) {
   const board = [];
   for (let i = 0; i < 9; i++) {
@@ -16,8 +17,41 @@ function solve(boardString) {
         }),
     );
   }
-
+  const size = 9;
+  const boxSize = 3;
+  
+  function findEmpty(board)  {
+  for (let r = 0; r < size; r++) {
+    for ( let c = 0; c < size; c++) {
+      if ( board[r][c] === 0) {
+        return [r,c];
+      }
+    }
+  }
+  return null;
 }
+
+
+function solveOne (board) {
+  const currPos = findEmpty(board)
+  if (currPos === null) {
+    return true;
+  }
+  
+for ( let i = 0; i < size ; i++) {
+  const currNum = i;
+  const isvalid = validate( currNum,currPos,board);
+} return false;
+}
+ function validate(num,pos,board) {
+const [r,c] = pos;
+for ( let i = 0; i < size; i++) {
+  
+}
+ }
+}
+
+
 // solve(boardString);
 // Returns a boolean indicating whether
 // or not the provided board is solved.
@@ -25,7 +59,9 @@ function solve(boardString) {
 // form `solve` returns.
 function isSolved(board) {
 
+
 }
+
 
 // Takes in a board in some form and
 // returns a String that's well formatted
