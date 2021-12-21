f152ec1c792b29401c66930221fcd047a99c6024
Andrey Semenets <andreysemenets@MacBook-Air-Dara.local>
2021-12-21 11:45:13 +0300

    Почти чуть-чуть...

--- app.js
+++ app.js
@@ -1,72 +1,89 @@
 const fs = require('fs')
 const data = fs.readFileSync('./sudoku-puzzles.txt',
-'utf-8')
+	'utf-8')
 console.log('получаем всё\n', data)
 
+// Получаем доску
 
 function getBoard(data) {
-  let sudokuBoard = [];
-  for (let i = 0; i < 81; i += 9) {
-    sudokuBoard.push(data.slice(i, i + 9).split(''))
-  }
-  return sudokuBoard
+	let sudokuBoard = [];
+	for (let i = 0; i < 81; i += 9) {
+		sudokuBoard.push(data.slice(i, i + 9).split(''))
+	}
+	return sudokuBoard
 }
-console.log('отрисовываем доску')
-console.table(getBoard(data))
+// console.log('отрисовываем доску')
+// console.table(getBoard(data))
 
 const board = getBoard(data);
 
+// Получаем пустую ячейку
+
 function findEmpty() {
-  for (let horizon = 0; horizon < 9; horizon++) {
-    for (let vertical = 0; vertical < 9; vertical++) {
-      if (board[horizon][vertical] === '-') {
-        return [horizon, vertical];
-      }
-    }
-  }
-  return 'done'
+	for (let horizon = 0; horizon < 9; horizon++) {
+		for (let vertical = 0; vertical < 9; vertical++) {
+			if (board[horizon][vertical] === '-') {
+				return [horizon, vertical];
+			}
+		}
+	}
+	return 'done'
 }
-console.log('получаем пустое значение')
-console.log(findEmpty(board))
-// след функция берет это значение и проверяет, что можно подставить под него
+// console.log('получаем пустое значение')
+// console.log(findEmpty(board))
 
-// а для этого ей нужно сравнить все строки и столбцы, и квадраты на цифры   [1, 2, 3, 4, 5, 6, 7, 8, 9]
-// затем подставляет 
+let emptyValue = findEmpty(board);
 
-// переходит к следующему и пока пустые значения не закончатся
-// если не находит значение то откатывается назад и проверяет предыдущие значения 
-function solve (){
+function recurse(board, emptyValue) {
 
+	if (emptyValue === 'done') {
+		return board;
+	}
 
-  return getBoard;
+	for (let i = 1; i <= 9; i++) {
+		if (check(emptyValue, board, i) && checkBox(i, emptyValue, board)) { // Проверяем подобранные циклом числа на возможность проставить горизнталь/вертикаль
+			const [h, v] = emptyValue;
+			board[h][v] = i;
+			if (recurse(board, emptyValue)) {
+				return board;
+			}
+		}
+		board[h][v] = '-';
+	}
+	return false;
+}
+recurse(board, emptyValue);
+
+function check(emptyValue, board, num) {
+	const horizon = emptyValue[0];
+	const vertical = emptyValue[1];
+
+	//идем по строке
+
+	const arrVer = [];
+	for (let i = 0; i < 9; i++) {
+		arrVer.push(board[i][vertical]);
+	}
+
+	const arrHor = board[horizon];
+
+	if (!arrVer.includes(num) && !arrHor.includes(num)) {
+		return true;
+	}
+	return false;
 }
 
-let emptyValue = findEmpty(board)
-// console.log('ddd', emptyValue); 
-function check(emptyValue, board) {
-  const horizon = emptyValue[0];
-  const vertical = emptyValue[1];
-  const digits = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  //идем по строке
-  for (i = 0; i <= 9; i++) {
-    for(j = 0; j < digits.length; j++){
-      if(board[horizon][i] === digits[j])
-    }
-  }
+function checkBox(currentNum, emptyValue, board) {
+	const boxRow = Math.floor(emptyValue[0] / 3) * 3;
+	const boxCol = Math.floor(emptyValue[1] / 3) * 3;
 
+	for (let i = 1; i < boxRow + 3; i++) {
+		for (let j = 1; i < boxCol + 3; j++) {
+			if (board[i][j] === currentNum && i !== emptyValue[0] && j !== emptyValue[1]) {
+				return false; //!== currentRow && j !== currentCol исключает проверку текущего числа
+			}
+		}
+	}
+	return true;
 }
-// Returns a boolean indicating whether
-// or not the provided board is solved.
-// The input board will be in whatever
-// form `solve` returns.
-function isSolved(board) {
 
-}
-// Takes in a board in some form and
-// returns a String that's well formatted
-// for output to the screen.
-// The input board will be in whatever
-// form `solve` returns.
-function prettyBoard(board) {
-
-}
