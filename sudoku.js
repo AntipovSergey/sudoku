e013f5cea6985ffa0cc48532536474ad050cda49
Nikita <nik.bolshakov95@mail.ru>
2021-12-24 15:50:11 +0300

    random

--- sudoku.js
+++ sudoku.js
@@ -3,14 +3,19 @@
 // something representing a board after
 // your solver has tried to solve it.
 // How you represent your board is up to you!
+const { strictEqual } = require('assert');
 const fs = require('fs');
 const text = fs.readFileSync('./project-sudoku/sudoku-puzzles.txt', 'utf-8')
 console.log(text)
-function solve(boardString) {
+// function solve(boardString) {
+// 	const Array = [];
+// 	for(let i = 0;i < 0; i+=1 )	({
+// 		// Array.push(string.slice(i,i+8).split(''));
+// 	}
+// 	// console.table(Array);
+// 	return Array
 
-
-
-}
+// }
 
 
 // Returns a boolean indicating whether
@@ -30,6 +35,17 @@ function isSolved(board) {
 function prettyBoard(board) {
 
 }
+
+function randomNum (str){
+	let num = 0;
+	for ( let i=0;i<str.length; i+=1 ){
+		if (str[i] === '-'){
+			 return Math.floor(Math.random()*9)
+			}			
+		}
+	}
+	console.log (randomNum('1-58-2----9-'))
+
 
 // Exports all the functions to use them in another file.
 module.exports = {
