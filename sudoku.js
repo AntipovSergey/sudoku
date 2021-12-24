f604e6e1dd7b605be637ba06934032b5dcb658f0
AlexG <g_engineer@mail.ru>
2021-12-24 15:25:44 +0300

    f4 function

--- sudoku.js
+++ sudoku.js
@@ -1,17 +1,17 @@
-// Takes a board as a string in the format
-// you see in the puzzle file. Returns
-// something representing a board after
-// your solver has tried to solve it.
-// How you represent your board is up to you!
+// Принимает строку в формате поля
+// (вы видите заполненное поле головоломки). Возврат
+// что-то, представляющее доску после
+// ваш решатель попытался решить эту проблему.
+// Как вы представляете свою доску, зависит от вас!
 function solve(boardString) {
 
 }
 
 
-// Returns a boolean indicating whether
-// or not the provided board is solved.
-// The input board will be in whatever
-// form `solve` returns.
+// Возвращает логическое значение, указывающее,
+// или нет решена предоставленная плата.
+// Плата ввода будет в любом
+// возврат из формы `resolve`.
 function isSolved(board) {
 
 }
