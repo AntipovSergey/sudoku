387cabfddbc7144ac0055fff252431c260d95296
Ilya <solovyevilya96@gmail.com>
2021-12-24 15:19:01 +0300

    f2

--- sudoku.js
+++ sudoku.js
@@ -1,12 +1,24 @@
+function f2(i, j, arr0) {
+  for (let k = 0; k < 9; ++k) {
+    arr0[i].push(arr0[k][j])
+  }
+  let arr1 = [];
+  for (let x = 1; x < 10; ++x) {
+    let number = x.toString();
+    if (!arr0[i].includes(number)) {
+      arr1.push(number);
+    }
+  }
+}
+
 // Takes a board as a string in the format
 // you see in the puzzle file. Returns
 // something representing a board after
 // your solver has tried to solve it.
 // How you represent your board is up to you!
 function solve(boardString) {
-
+  
 }
-
 
 // Returns a boolean indicating whether
 // or not the provided board is solved.
@@ -30,5 +42,7 @@ function prettyBoard(board) {
 module.exports = {
 	solve: solve,
 	isSolved: isSolved,
-	prettyBoard: prettyBoard
+	prettyBoard: prettyBoard,
+  f2,
+  f3,
 }
