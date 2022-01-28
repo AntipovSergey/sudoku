dca07ef05b8e178a8ee07671873b8f02a56b07e7
Dmitriy Rudakov <cska2004@gmail.com>
2022-01-28 15:57:05 +0300

    Minor changes

--- linear-sudoku.js
+++ linear-sudoku.js
@@ -1,21 +1,26 @@
-const {
-  eugenFunc: checkNum,
-} = require('check-numbers');
+//const sudoku = ;
 
 function solveSudokuLinear(sudoku) {
   let isSolveChanged = true;
+  let numbersCount = 0;
+  let startI = 0;
+  let startj = 0;
 
   while (isSolveChanged) {
-    loop: for (let i = 0; i < sudoku.length; i++) {
-      for (let j = 0; j < sudoku.length; i++) {
+    loop: for (let i = startI; i < sudoku.length; i++) {
+      for (let j = startJ; j < sudoku.length; i++) {
         if (sudoku[i][j] === 0) {
-          let allowedNumbers = eugenFunc(i, j, sudoku);
+          let allowedNumbers = eugenFunc(i, j);
           if (allowedNumbers.length === 1) {
             sudoku[i][j] = allowedNumbers[0];
-            isSolveChanged = (i == sudoku.length - 1 && j == sudoku.length - 1) ? false : true;
+            startI = (j < 9) ? i : i + 1;
+            startJ = (j < 9) ? j + 1 : 0;
+            isSolveChanged = (startI == sudoku.length) ? false : true;
+            numbersCount++;
           }
           break loop;
         }
+        numbersCount++;
         if (i == sudoku.length - 1 && j == sudoku.length - 1) {
           isSolveChanged = false;
         }
@@ -23,9 +28,9 @@ function solveSudokuLinear(sudoku) {
     }
   }
 
-  return sudoku;
+  return [sudoku, numbersCount === 81];
 }
 
-module.exports = {
-  solveSudokuLinear,
-}
+// module.exports = {
+//   eugenFunc,
+// }
