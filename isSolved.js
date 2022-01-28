112f70786c7fda1c0361039001dcb0c410116dad
Bond <sasha.bond1995@gmail.com>
2022-01-28 13:59:19 +0300

    Проверка по колонне

--- isSolved.js
+++ isSolved.js
@@ -1,41 +1,23 @@
 const invalidBoard = [
-  [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ],
-  [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ],
-  [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ],
-  [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ],
-  [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ],
-  [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ],
-  [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ],
-  [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ],
-  [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ],
+  [ 1, 1, 1, 1, 1, 1, 1, 1, 1 ],
+  [ 2, 2, 2, 2, 2, 2, 2, 2, 2 ],
+  [ 3, 3, 3, 3, 3, 3, 3, 3, 3 ],
+  [ 4, 4, 4, 4, 4, 4, 4, 4, 4 ],
+  [ 5, 5, 5, 5, 5, 5, 5, 5, 5 ],
+  [ 6, 6, 6, 6, 6, 6, 6, 6, 6 ],
+  [ 7, 7, 7, 7, 7, 7, 7, 7, 7 ],
+  [ 8, 8, 8, 8, 8, 8, 8, 8, 8 ],
+  [ 9, 9, 9, 9, 9, 9, 9, 9, 9 ],
 ];
 
-function isSolvedRows(arr) {
-  let result = 45;
-  for(let i = 0; i < 9; i++){
-    const solvedRow = arr[i]
-    const reducer = (previousValue, currentValue) => previousValue + currentValue;
-    if (solvedRow.reduce(reducer) === result){
-      continue
-    } else {
-      return false;
+function isSolvedColumns(arr, cor, num){
+  for(i = 0 ; i < arr.length; i++){
+    console.log(arr[i][cor[1]], num);
+    if( arr[i][cor[1]] === num){
+      return false
     }
   }
   return true;
 }
 
-console.log(isSolvedRows(invalidBoard));
-
-
-
-// const validBoard = [
-//   [ 5, 3, 4, 6, 7, 8, 9, 1, 2 ],
-//   [ 6, 7, 2, 1, 9, 5, 3, 4, 8 ],
-//   [ 1, 9, 8, 3, 4, 2, 5, 6, 7 ],
-//   [ 8, 5, 9, 7, 6, 1, 4, 2, 3 ],
-//   [ 4, 2, 6, 8, 5, 3, 7, 9, 1 ],
-//   [ 7, 1, 3, 9, 2, 4, 8, 5, 6 ],
-//   [ 9, 6, 1, 5, 3, 7, 2, 8, 4 ],
-//   [ 2, 8, 7, 4, 1, 9, 6, 3, 5 ],
-//   [ 3, 4, 5, 2, 8, 6, 1, 7, 9 ],
-// ];
+console.log(isSolvedColumns(invalidBoard, [4,4], 4))
