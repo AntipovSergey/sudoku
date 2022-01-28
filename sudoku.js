01fb1812449701fffcc460b4373ed13b0801e9cd
Viktor-Vrazumitel <Viktor_bogatyrev@mail.ru>
2022-01-28 16:42:29 +0300

    Release1

--- sudoku.js
+++ sudoku.js
@@ -1,34 +1,130 @@
-// Takes a board as a string in the format
-// you see in the puzzle file. Returns
-// something representing a board after
-// your solver has tried to solve it.
-// How you represent your board is up to you!
-function solve(boardString) {
 
-}
+const { table } = require('console');
+const fs = require('fs')
 
 
-// Returns a boolean indicating whether
-// or not the provided board is solved.
-// The input board will be in whatever
-// form `solve` returns.
-function isSolved(board) {
+let input = fs.readFileSync('./sudoku-puzzles.txt', 'utf8').slice(0, 81).split('').map(el => el).reduce((acc, cur, i) => {
+	let arr = Math.floor(i / 9);
+	if (!Array.isArray(acc[arr])) {
+		acc[arr] = [cur];
+	} else {
+		acc[arr].push(cur)
+	}
+	return acc
+}, []);
 
-}
+//console.log(input)
+
+let solveSudoku = function(board) {
+    const size = 9;
+    const boxSize = 3;
+
+    const findEmpty = (board) => {
+        for (let r = 0; r < size; r++) {
+            for (let c = 0; c < size; c++) {
+                if(board[r][c] === '-') {
+                    return [r,c];
+                }
+            }
+        }
+        return null;
+    }
+
+    const validate = (num, pos, board) => {
+        const [r,c] = pos;
+
+        //Проверка строк
+        for (let i = 0; i < size; i++) {
+            if (board[i][c] === num && i !== r) {
+                return false;
+            }
+        }
+
+        //Проверка колон
+        for (let i = 0; i < size; i++) {
+            if (board[r][i] === num && i !== c) {
+                return false;
+            }
+        }
 
 
-// Takes in a board in some form and
-// returns a String that's well formatted
-// for output to the screen.
-// The input board will be in whatever
-// form `solve` returns.
-function prettyBoard(board) {
+        //Проверка квадратов
+        const boxRow = Math.floor( r/boxSize ) * boxSize;
+        const boxCol = Math.floor( c/boxSize ) * boxSize;
 
-}
+        for (let i = boxRow; i < boxRow + boxSize; i++) {
+            for (let j = boxCol; j < boxCol + boxSize; j++) {
+                if (board[i][j] === num && i !== r && j !== c) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    const solve = () => {
+        const currPos = findEmpty(board);
+
+        if (currPos === null) {
+            return true;
+        }
+        
+        for (let i = 1; i < size + 1; i++) {
+            const currNum = i.toString();
+            const isValid = validate(currNum, currPos, board);
+            
+            if (isValid) {
+                const [x,y] = currPos;
+                board[x][y] = currNum;
+
+                if(solve()) {
+                    return true;
+                }
+
+                board[x][y] = '-';
+            }
+        }
+
+        return false;
+    }
+
+    solve();
+    return board.join('\n');
+};
+
+console.table(input);
+console.table(solveSudoku(input));
+
+
+
+
+
+
+
+
+
+// // Returns a boolean indicating whether
+// // or not the provided board is solved.
+// // The input board will be in whatever
+// // form `solve` returns.
+// function isSolved(board) {
+
+// }
+
+
+// // Takes in a board in some form and
+// // returns a String that's well formatted
+// // for output to the screen.
+// // The input board will be in whatever
+// // form `solve` returns.
+// function prettyBoard(board) {
+
+// }
 
 // Exports all the functions to use them in another file.
 module.exports = {
-	solve: solve,
-	isSolved: isSolved,
-	prettyBoard: prettyBoard
+	solveSudoku: solveSudoku,
+	//isSolved: isSolved,
+	//prettyBoard: prettyBoard
 }
