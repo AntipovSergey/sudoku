dea9438edbf3c7673158e3acc955751bfbd77a34
Dmitriy Rudakov <cska2004@gmail.com>
2022-01-28 16:10:50 +0300

    Fixed

--- linear-sudoku.js
+++ linear-sudoku.js
@@ -1,44 +1,31 @@
-<<<<<<< HEAD
-//const sudoku = ;
-
+const {
+  checkNum,
+} = require('./check-numbers');
 function solveSudokuLinear(sudoku) {
-  let isSolveChanged = true;
-  let numbersCount = 0;
-  let startI = 0;
-  let startj = 0;
-
-  while (isSolveChanged) {
-    loop: for (let i = startI; i < sudoku.length; i++) {
-      for (let j = startJ; j < sudoku.length; i++) {
-        if (sudoku[i][j] === 0) {
-          let allowedNumbers = eugenFunc(i, j);
-          if (allowedNumbers.length === 1) {
-            sudoku[i][j] = allowedNumbers[0];
-            startI = (j < 9) ? i : i + 1;
-            startJ = (j < 9) ? j + 1 : 0;
-            isSolveChanged = (startI == sudoku.length) ? false : true;
-            numbersCount++;
-          }
-          break loop;
-        }
-        numbersCount++;
-        if (i == sudoku.length - 1 && j == sudoku.length - 1) {
-          isSolveChanged = false;
+  let start = 0;
+  let count = 0;
+  const go = () => {
+    console.log(count);
+    for (let i = start; i < 81; i++) {
+      count++;
+      let x = Math.floor(i / 9);
+      let y = i % 9;
+      if (sudoku[x][y] === 0) {
+        let allowedNumbers = checkNum(x, y, sudoku);
+        if (allowedNumbers.length === 1) {
+          sudoku[x][y] = allowedNumbers[0];
+          return true;
+        } else {
+          start = i + 1;
+          return false;
         }
       }
     }
-  } 
-
+  }
   while (count !== 81) {
     if (go()) {
       start = 0;
       count = 0;
     }
   }
-
-  return [sudoku, numbersCount === 81];
-}
-
-// module.exports = {
-//   eugenFunc,
-// }
+  return sudoku;}module.exports = {  solveSudokuLinear,}
