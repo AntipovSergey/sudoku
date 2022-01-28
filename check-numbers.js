0a09d89c0c5294a34dff908090804f7646ab6540
Evgenii <v2454802@gmail.com>
2022-01-28 12:30:32 +0300

    release 0

---
+++ check-numbers.js
@@ -0,0 +1,20 @@
+let puzzle = [
+  ["a", "k", "f", "o", "x", "e", "s"],
+  ["s", "o", "a", "w", "a", "h", "p"],
+  ["i", "t", "c", "k", "e", "t", "n"],
+  ["o", "t", "s", "d", "h", "o", "h"],
+  ["s", "e", "x", "g", "s", "t", "a"],
+  ["u", "r", "p", "i", "w", "e", "u"],
+  ["z", "s", "b", "n", "u", "i", "r"]
+]
+
+function (x, y, array) {
+  let entryNum = [1, 2, 3, 4, 5, 6, 7, 8, 9,]
+  for (let i = 0; i < array.length; i++) {
+      arr.push(array[x][i])
+      arr.push(array[i][y])
+  }
+
+
+}
+
