27f5a3360ae66e981df16dffd0671cbcedbaa5b9
RomanPendyur <51956203+RomanPendyur@users.noreply.github.com>
2022-02-07 14:11:12 +0200

    func isSolve finish

--- sudoku.js
+++ sudoku.js
@@ -4,14 +4,37 @@
 // your solver has tried to solve it.
 // How you represent your board is up to you!
 function solve(boardString) {
-  console.log('first changes');
+    console.log('first changes');
 }
 
 // Returns a boolean indicating whether
 // or not the provided board is solved.
 // The input board will be in whatever
 // form `solve` returns.
-function isSolved(board) {
+function isSolved(number, position, board) {
+    let goriz = position[0];
+    let vertical = position[1]
+    let line = Math.floor(goriz / 3) * 3;
+    let colum = Math.floor(vertical / 3) * 3;
+    for (let elem = 0; elem < 9; elem++) {
+        if (board[elem][vertical] === number && elem !== goriz) {
+            return false
+        }
+    }
+    for (let el = 0; el < 9; el++) {
+        if (board[goriz][el] === number && el !== vertical) {
+            return false
+        }
+    }
+    for (let el = line; el < line + 3; el++) {
+        for (let ele = colum; ele < colum + 3; ele++) {
+            if (board[el][ele] === number && el !== goriz && ele !== vertical) {
+                return false
+            }
+        }
+
+    }
+    return true
 
 }
 
@@ -26,7 +49,7 @@ function prettyBoard(board) {
 
 // Exports all the functions to use them in another file.
 module.exports = {
-  solve,
-  isSolved,
-  prettyBoard,
+    solve,
+    isSolved,
+    prettyBoard,
 };
