fd78c3ffdf7ccc4c775c648c7c833bbab9ca4363
Merdzhen <merdzhen.k@gmail.com>
2022-02-07 17:51:00 +0300

    test isSolved - blocks, empty cells

--- sudoku.js
+++ sudoku.js
@@ -1,7 +1,7 @@
 const {
   isSolvedBlocks,
-  isSolvedEmptyCells
-} = require("./is-solved-blocks-empty.js");
+  isSolvedEmptyCells,
+} = require('./is-solved-blocks-empty.js');
 
 // Takes a board as a string in the format
 // you see in the puzzle file. Returns
@@ -9,22 +9,21 @@ const {
 // your solver has tried to solve it.
 // How you represent your board is up to you!
 
-//const fs = require('fs');
+// const fs = require('fs');
 
-//const sudoku = fs.readFileSync('./sudoku-puzzles.txt', 'utf8')
-//console.log(sudoku);
+// const sudoku = fs.readFileSync('./sudoku-puzzles.txt', 'utf8')
+// console.log(sudoku);
 
-const sud1 = "1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89---"
+const sud1 = '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89---';
 function solve(boardString) {
-  let board = [];
-  //console.log('--------', board);
+  const board = [];
+  // console.log('--------', board);
   for (let i = 0; i < 81; i += 9) {
-    board.push(boardString.slice(i, i + 9).split(''))
+    board.push(boardString.slice(i, i + 9).split(''));
   }
-  return board
+  return board;
 }
 // console.table(solve(sud1));
-
 
 // Returns a boolean indicating whether
 // or not the provided board is solved.
@@ -32,14 +31,10 @@ function solve(boardString) {
 // form `solve` returns.
 
 function isSolved(board) {
-  let result = []
+  const result = [];
   result.push(isSolvedBlocks(board), isSolvedEmptyCells(board));
-  return result.includes(false) ? false : true
+  return !result.includes(false);
 }
-// console.log(isSolved('123456789456789123789123456214365897365897214897214365531642978642978531978531642'))
-// console.log(isSolved('1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--'))
-// console.log(isSolved('122456789456789123789123456214365897365897214897214365531642978642978531978531642'))
-  
 
 // Takes in a board in some form and
 // returns a String that's well formatted
@@ -47,13 +42,11 @@ function isSolved(board) {
 // The input board will be in whatever
 // form `solve` returns.
 function prettyBoard(board) {
-  const rows = new Array(9).fill('')
-  const table = rows.map(row => row = new Array(9).fill(''))
-  //console.table(table);
-  //console.log(rows);
-
+  const rows = new Array(9).fill('');
+  const table = rows.map((row) => row = new Array(9).fill(''));
+  // console.table(table);
+  // console.log(rows);
 }
-
 
 // Exports all the functions to use them in another file.
 module.exports = {
