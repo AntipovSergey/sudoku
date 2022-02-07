df31d1f56693df4317a5b8c2f0269ae4bb7a2478
RomanPendyur <51956203+RomanPendyur@users.noreply.github.com>
2022-02-07 16:02:21 +0200

    func isSolve two finish

--- sudoku.js
+++ sudoku.js
@@ -4,17 +4,13 @@
 // your solver has tried to solve it.
 // How you represent your board is up to you!
 function solve(boardString) {
-<<<<<<< HEAD
-    console.log('first changes');
-=======
-  let board =[]
-  let arrBoardString = boardString.split('')
-  for (let i = 0; i < 9; i++) {
-    board.push(arrBoardString.splice(0,9))
-  }
+    let board = []
+    let arrBoardString = boardString.split('')
+    for (let i = 0; i < 9; i++) {
+        board.push(arrBoardString.splice(0, 9))
+    }
 
-  return board
->>>>>>> 253c31a540c95845b015b5dd2fe1aef8580937c8
+    return board
 }
 
 // Returns a boolean indicating whether
@@ -42,10 +38,19 @@ function isSolved(number, position, board) {
                 return false
             }
         }
-
     }
     return true
+}
 
+function findElem(board) {
+    for (let line = 0; line < 9; line++) {
+        for (let colum = 0; colum < 9; colum++) {
+            if (board[line][colum] === '-') {
+                return [line, colum];
+            }
+        }
+    }
+    return null;
 }
 
 
@@ -55,27 +60,15 @@ function isSolved(number, position, board) {
 // The input board will be in whatever
 // form `solve` returns.
 
-let input = [
-  ["5", "3", "-", "-", "7", "-", "-", "-", "-"],
-  ["6", "-", "-", "1", "9", "5", "-", "-", "-"],
-  ["-", "9", "8", "-", "-", "-", "-", "6", "-"],
-  ["8", "-", "-", "-", "6", "-", "-", "-", "3"],
-  ["4", "-", "-", "8", "-", "3", "-", "-", "1"],
-  ["7", "-", "-", "-", "2", "-", "-", "-", "6"],
-  ["-", "6", "-", "-", "-", "-", "2", "8", "-"],
-  ["-", "-", "-", "4", "1", "9", "-", "-", "5"],
-  ["-", "-", "-", "-", "8", "-", "-", "7", "9"]
-];
+
 
 function prettyBoard(board) {
-  let result = [];
-  for (let i = 0; i < board.length; i++){
-    result.push(board[i].join(' ') + '\n')
-  }
-  return result.join('')
+    let result = [];
+    for (let i = 0; i < board.length; i++) {
+        result.push(board[i].join(' ') + '\n')
+    }
+    return result.join('')
 }
-
-console.log(prettyBoard(input))
 
 // Exports all the functions to use them in another file.
 module.exports = {
