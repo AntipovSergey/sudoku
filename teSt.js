8649045de01ac4390638c9956e98ee9baae11c8b
RomanPendyur <51956203+RomanPendyur@users.noreply.github.com>
2022-02-08 13:38:27 +0200

    ranomaizer finish

--- teSt.js
+++ teSt.js
@@ -1,17 +1,29 @@
+const fs = require('fs');
+let data = fs.readFileSync('./sudoku-puzzles.txt', 'utf-8');
+let parseData = sudokuParse(data)
 
-function sudokuParse(content, puzzleNumber = 0) {
-    let puzzle = content.split('\n')[puzzleNumber];
-    console.log(puzzle);
-    console.log('parser');
+function sudokuParse(content) {
+    let puzzle = content.split('\n');
     return puzzle;
 }
 
-// Takes a board as a string in the format
-// you see in the puzzle file. Returns
-// something representing a board after
-// your solver has tried to solve it.
-// How you represent your board is up to you!x
-let str2 = '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--'
+function getRandomArray(data) {
+    let randomArrNum = 0;
+    let result
+    for (let el = 0; el < data.length; el++) {
+        randomArrNum = Math.floor(Math.random() * el);
+        result = data[randomArrNum]
+    }
+    return result
+}
+
+// console.log(getRandomArray(parseData));
+
+let str2 = getRandomArray(parseData)
+
+
+let boardStart = solve(str2)
+let board = solve(str2)
 
 function solve(boardString) {
     let arr = []
@@ -21,21 +33,14 @@ function solve(boardString) {
     }
     return arr
 }
-let boardStart = solve(str2)
-let board = solve(str2)
 
-
-// Returns a boolean indicating whether
-// or not the provided board is solved.
-// The input board will be in whatever
-// form `solve` returns.
-function isSolved( board) {
+function isSolved(board) {
 
     const findEmpty = (board) => {
         for (let r = 0; r < 9; r++) {
             for (let c = 0; c < 9; c++) {
-                if(board[r][c] === '-') {
-                    return [r,c];
+                if (board[r][c] === '-') {
+                    return [r, c];
                 }
             }
         }
@@ -43,26 +48,19 @@ function isSolved( board) {
     }
 
     const validate = (num, pos, board) => {
-        const [r,c] = pos;
-
-        //Check rows
+        const [r, c] = pos;
         for (let i = 0; i < 9; i++) {
             if (board[i][c] === num && i !== r) {
                 return false;
             }
         }
-
-        //Check cols
         for (let i = 0; i < 9; i++) {
             if (board[r][i] === num && i !== c) {
                 return false;
             }
         }
-
-
-        //Check box
-        const boxRow = Math.floor( r/3 ) * 3;
-        const boxCol = Math.floor( c/3 ) * 3;
+        const boxRow = Math.floor(r / 3) * 3;
+        const boxCol = Math.floor(c / 3) * 3;
 
         for (let i = boxRow; i < boxRow + 3; i++) {
             for (let j = boxCol; j < boxCol + 3; j++) {
@@ -71,7 +69,6 @@ function isSolved( board) {
                 }
             }
         }
-
         return true;
     }
 
@@ -81,52 +78,37 @@ function isSolved( board) {
         if (currPos === null) {
             return true;
         }
-        //console.log('------------------------------');
         for (let i = 1; i < 9 + 1; i++) {
             const currNum = i.toString();
             const isValid = validate(currNum, currPos, board);
-            //console.log('currPos ', currPos, 'currNum ',currNum, 'isValid ',isValid);
             if (isValid) {
-                const [x,y] = currPos;
+                const [x, y] = currPos;
                 board[x][y] = currNum;
 
-                if(solution()) {
+                if (solution()) {
                     return true;
                 }
 
                 board[x][y] = '-';
             }
         }
-
         return false;
     }
-
     solution();
     return board;
 };
 
+
 console.log(isSolved(board))
-
-
-// Takes in a board in some form and
-// returns a String that's well formatted
-// for output to the screen.
-// The input board will be in whatever
-// form `solve` returns.
-
-
-
-
-// Формирование сетки без чисел
 
 const root = document.querySelector('#root');
 
 function get_table(row_count, column_count) {
     const outer_table = document.createElement('table');
-    for (let i = 0 ; i < row_count; i++) {
+    for (let i = 0; i < row_count; i++) {
         const row = document.createElement('tr')
         outer_table.appendChild(row);
-        for (let i = 0 ; i < column_count; i++) {
+        for (let i = 0; i < column_count; i++) {
             const column = document.createElement('td')
             row.appendChild(column);
         }
@@ -134,30 +116,20 @@ function get_table(row_count, column_count) {
     return outer_table
 }
 
-const mainTable = get_table(9,9);
+const mainTable = get_table(9, 9);
 
 root.appendChild(mainTable)
 
-
-
-// Формирование сетки START
-
-function fillSudoku(lst){
+function fillSudoku(lst) {
     const line_lst = [].concat(...lst);
     mainTable.querySelectorAll('td')
-        .forEach((td, i) => td.innerText=(line_lst[i] === '-') ? '' : line_lst[i]);
+        .forEach((td, i) => td.innerText = (line_lst[i] === '-') ? '' : line_lst[i]);
 }
 
 fillSudoku(boardStart)
 
-
-// Формирование сетки SOLUTION
-
 document.querySelector('#solution')
     .addEventListener('click', () => fillSudoku(board));
-
-
-// Формирование сетки BACK
 
 document.querySelector('#reset')
     .addEventListener('click', () => fillSudoku(boardStart));
