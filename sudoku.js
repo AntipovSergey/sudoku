4261150335dfe815941c10a67f73f6474ecf817d
Ertine <ertine.rb@gmail.com>
2022-02-18 16:43:39 +0300

    search

--- sudoku.js
+++ sudoku.js
@@ -12,7 +12,14 @@ function solve(boardString) {
 // The input board will be in whatever
 // form `solve` returns.
 function isSolved(board) {
-
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  for (let i = 0; i < arr.length; i += 1) {
+    for (let j = 0; j < arr.length; j += 1) {
+      if (arr[i][j] === '-') {
+        return [i, j];
+      }
+    }
+  }
 }
 
 // Takes in a board in some form and
