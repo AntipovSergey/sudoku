5b47b5e2f08e836108d40ee6cdc64b51d3557bc2
Anton <anton_st_01_02@mail.ru>
2022-02-18 18:09:03 +0500

    adds solver

--- runner.js
+++ runner.js
@@ -26,5 +26,31 @@ function validator(sudokuBoard, row, column, valueFromTile) {
   }
   return true;
 }
-
+function solver(data) {
+  for (let i = 0; i < 9; i++) {
+    // первым циклом проходим по горизонтальной строке и ищем индекс где находится "-"
+    for (let j = 0; j < 9; j++) {
+      // вторым циклом ищем индекс где находится "-" в вертикальмом столбце
+      if (data[i][j] == '-') {
+        // условие - если находит координаты где "-" запускается цикл, который подбирает цифру на это место 
+        for (let k = 1; k <= 9; k++) {
+        // цикл подбора цифры
+          if (validator(data, i, j, k)) {
+            // если функция "validator" дает true, подставляем цифру
+            data[i][j] = `${k}`;
+            if (solver(data)) {
+              // 
+              return true;
+            }
+            data[i][j] = '-';
+            // если функция validator дает false то подставляется обратно "-"
+          }
+        }
+        return false;
+      }
+    }
+  }
+  return true;
+}
+console.table(solver())
 module.exports = { validator, solver };
