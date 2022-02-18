765df9a03c40322ddb9029851a1edc483a3832b7
Hitand <andrey2795@mail.ru>
2022-02-18 13:23:51 +0300

    сделали массив из строки чисел

--- sudoku.js
+++ sudoku.js
@@ -3,9 +3,17 @@
 // something representing a board after
 // your solver has tried to solve it.
 // How you represent your board is up to you!
+
+let str = '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--'
 function solve(boardString) {
-  console.log('first changes');
+  let board = [];
+  // цикл преобразует строку в массив 9х9
+  for (let i = 0; i < boardString.length; i += 9) {
+    let row = boardString.slice(i, i + 9).split('');
+    board.push(row);
+  }
 }
+console.log(solve(str))
 
 // Returns a boolean indicating whether
 // or not the provided board is solved.
