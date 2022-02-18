1b5eb4019cee0387eb30f317df96d69c340dfd63
Maxim Mukhanov <98232811+MaximMukhanov@users.noreply.github.com>
2022-02-18 15:35:17 +0300

    Revert "Adds Validator"

--- runner.js
+++ runner.js
@@ -1,28 +1,9 @@
-const { modelData } = require("./model.js");
-// Переменные;
-// sudokuBoard - данные нашего поля;
-// row - данные ряда;
-// column - данные колоки;
-// valueFromTile - данные из solver;
+// Переменные
+// sudokuBoard - данные нашего поля
+// row -
 //
-// Validator - пропускает значения из функции solver для проверки
-// в соответствии с правилами игры и возвращает boolean значение
-// в зависимости от результата;
-function validator(sudokuBoard, row, column, valueFromTile) {
-  for (let i = 0; i < 9; i += 1) {
-    // Ось X
-    const checkX = 3 * Math.trunc(row / 3) + Math.trunc(i / 3);
-    // Ось Y
-    const checkY = 3 * Math.trunc(column / 3) + (i % 3);
-    if (
-      sudokuBoard[row][i] == valueFromTile ||
-      sudokuBoard[i][column] == valueFromTile ||
-      sudokuBoard[checkX][checkY] == valueFromTile
-    ) {
-      return false;
-    }
-  }
-  return true;
-}
+//
+//
+function validator(SudokuBoard, row, column, numVariant) {}
 
-module.exports = { validator, solver };
+module.exports = {};
