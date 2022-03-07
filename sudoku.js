62659da90412f5147f55afb9321306537b25002e
Asan <amemetoff@yandex.ru>
2022-03-07 21:00:13 +0500

    сделали функц возвращающую строку в массиве

--- sudoku.js
+++ sudoku.js
@@ -2,27 +2,74 @@ const fs = require('fs');
 
 const boardStringVar = fs.readFileSync('./sudoku-puzzles.txt', 'utf-8');
 const boardString = boardStringVar.split('\n').map((el) => [...el]);
-console.table(boardString[0]);
-//console.log(boardString[0]);
-function abc(strArr){
-  let arr = [];
-  for(let i = 0;i < strArr.length; i+= 1){
-    if(i < 9) {
-      arr.push(strArr[i]);
-    }
-  }
-  return arr;
-}
+//console.table(boardString[0]);
+//console.log(boardString[0],'boardString[0]');
 
- console.log(abc(boardString[0]));
+const sudocuFild = (strArr) => {
+let arrayIndex = -1;
+  return strArr.reduce((final, curr, i) => {
+    if (i % 9 === 0) {
+      final.push([curr]);
+      arrayIndex++;
+    } else {
+      final[arrayIndex].push(curr);
+    }
+    return final;
+  }, []);
+}
+console.log(sudocuFild(boardString[13]))
+console.log(boardString[14]);
+
+
+
 // Принимает доску как строку в формате
 // вы видите в файле головоломки. Возвращает
 // что-то, представляющее доску после
 // ваш решатель пытался ее решить.
 // То, как вы представляете свою доску, зависит от вас!
 
-function solve(boardString) {
-  console.log('first changes');
+function solve(sudocuFild) {
+  const size = 9;
+  const boxSize = 3;
+
+  const findEmpty = (sudocuFild) => {
+    for(let r = 0; r < size; r += 1){
+      for(let c = 0; c < size; c += 1){
+        if(sudocuFild[r][c] === '-'){
+          return [r,c];
+        }
+      }
+    }
+    return null;
+  }
+  const validate = (num, pos, sudocuFild) => {
+  const [r,c] = pos;
+
+
+  // проверка строк
+  for(let i = 0; i < size; i += 1){
+    if(sudocuFild[r][c] === num){
+
+    }
+  }
+}
+
+
+  const solveSud = () => {
+   const currPos = findEmpty(sudocuFild);
+   if(currPos === null){
+     return true;
+   }
+
+   for(let i = 1; i < size + 1; i += 1){
+     const currNum = i.toString();
+     const isValid = validate(currNum, currPos, sudocuFild);
+   }
+
+   return false;
+  }
+ solveSud();
+  return sudocuFild;
 }
 
 // Returns a boolean indicating whether
