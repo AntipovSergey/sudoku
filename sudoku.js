24fb4306caeda95afc1c2b63569cf8e247dcde8b
yxngbenji <bogdanshutko4job@gmail.com>
2022-03-11 11:57:01 +0300

    created validator of empty places

--- sudoku.js
+++ sudoku.js
@@ -4,9 +4,25 @@
 // your solver has tried to solve it.
 // How you represent your board is up to you!
 function solve(boardString) {
-  console.log('first changes');
+  const size = 9;
+  const boxsize = 3;
+  const arrBoard = [...boardString.split('\n')].map((e) => e.split(''));
+  console.table(arrBoard);
+  const findEmptyPosition = (arrBoard) => {
+    for (let i = 0; i < arrBoard.length; i += 1) {
+      for (let j = 0; j < arrBoard.length; j += 1) {
+        if (arr[i][j] === '-') {
+          return [i, j];
+        }
+      }
+    }
+    return null;
+  };
+
+  // return arrBoard
 }
 
+solve(prettyBoard('1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--'));
 // Returns a boolean indicating whether
 // or not the provided board is solved.
 // The input board will be in whatever
@@ -21,7 +37,13 @@ function isSolved(board) {
 // The input board will be in whatever
 // form `solve` returns.
 function prettyBoard(board) {
-
+  let result = '';
+  for (let i = 0; i <= board.length; i += 1) {
+    if (i % 9 === 0) {
+      result += `${board.slice(i - 9, i)}\n`;
+    }
+  }
+  return result.trim();
 }
 
 // Exports all the functions to use them in another file.
