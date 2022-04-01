a4122339a02abfb41043f95a3a72d1ec0da741f6
Aleksandr <viking26@mail.ru>
2022-04-01 14:51:37 +0300

    add function strToArr

--- strToArr.js
+++ strToArr.js
@@ -1,1 +1,15 @@
-lol
+// const sudokuParse = require('./runner')
+// const text = sudokuParse.sudokuParse()
+
+function strToArr(text) {
+  const strTwo = text.split('')
+  const arr = []
+  for (let i = 0; i < 81; i += 9) {
+    arr.push(strTwo.slice(i,i+9))
+  }
+  return arr
+}
+
+module.exports = { strToArr }
+
+
