f585422a4722054f5f34425e56153c91eaedd73c
Yami <Yamikoretsy@gmail.com>
2022-04-01 11:40:04 +0300

    createNewBranch

--- sudoku.js
+++ sudoku.js
@@ -1,9 +1,15 @@
+
+const fs = require('fs');
+const sudoku = require('./sudoku');
+
+const zero = function 
 // Takes a board as a string in the format
 // you see in the puzzle file. Returns
 // something representing a board after
 // your solver has tried to solve it.
 // How you represent your board is up to you!
 function solve(boardString) {
+
   console.log('first changes');
 }
 
