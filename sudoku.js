ed6195af4f5e68f39655745bbb6355b580c25b75
khabibjon Kuchkarboev <97886241+khxabib@users.noreply.github.com>
2022-04-01 14:29:16 +0300

    star

--- sudoku.js
+++ sudoku.js
@@ -5,6 +5,7 @@
 // How you represent your board is up to you!
 function solve(boardString) {
   console.log('first changes');
+  
 }
 
 // Returns a boolean indicating whether
@@ -33,19 +34,7 @@ function validate () {
 }
 
 const start = function () {  // Мы ZZZ
-  while (checkResult(initialBoard)) {
-    for (let i = 0; i < initialBoard.length; i++) {
-      for (let j = 0; j < initialBoard.length; j++) {
-        if (tempBoard[i][j] === ‘.’) {
-          tempBoard[i][j] = [];
-          for (let k = 1; k <= 9; k++) {
-            if (checkLine(tempBoard, [i, j], k) && checkColumn(tempBoard, [i, j], k) && checkBox(tempBoard, [i, j], k)) {
-              tempBoard[i][j].push(k);
-            }
-          }
-        }
-      }
-    }
+  
     for (let i = 0; i < initialBoard.length; i++) {
       for (let j = 0; j < initialBoard.length; j++) {
         if (Array.isArray(tempBoard[i][j]) && tempBoard[i][j].length === 1) {
@@ -53,9 +42,9 @@ const start = function () {  // Мы ZZZ
         }
       }
     }
-
+    /// /////////////////
     tempBoard = JSON.parse(JSON.stringify(initialBoard));
-  }
+  
 };
 
 // Exports all the functions to use them in another file.
