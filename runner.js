93e3647ca06b804e68e1bae456b807e8eab6c6ab
EkaterinaErokhina <erokhinaeka9@gmail.com>
2022-04-01 12:28:39 +0300

    измение

--- runner.js
+++ runner.js
@@ -1,15 +1,7 @@
-// Use filesystem.
 const fs = require('fs');
-// Use functions from sudoku.js file.
+
 const sudoku = require('./sudoku');
 
-// The sudoku puzzles that your program will solve can be found
-// in the sudoku-puzzles.txt file.
-//
-// Remember, the file has newline characters at the end of each line,
-// so you should remove them.
-
-// Gets one puzzle from the text file.
 function sudokuParse(content, puzzleNumber = 0) {
   let puzzle = content.split('\n')[puzzleNumber];
   console.log(puzzle);
@@ -33,7 +25,7 @@ function readAndSolve(err, data) {
   }
 }
 
-// Reads file and sends data from it to the readAndSolve function.
+
 fs.readFile(
   './sudoku-puzzles.txt',
   'utf-8',
--- sudoku.js
+++ sudoku.js
@@ -1,31 +1,22 @@
-// Takes a board as a string in the format
-// you see in the puzzle file. Returns
-// something representing a board after
-// your solver has tried to solve it.
-// How you represent your board is up to you!
+const sudokuParse = require('./ranner');
+
+const initialBoard = sudokuParse(process.argv[2])
+
 function solve(boardString) {
   console.log('first changes');
 }
 
-// Returns a boolean indicating whether
-// or not the provided board is solved.
-// The input board will be in whatever
-// form `solve` returns.
 function isSolved(board) {
 
 }
 
-// Takes in a board in some form and
-// returns a String that's well formatted
-// for output to the screen.
-// The input board will be in whatever
-// form `solve` returns.
+
 function prettyBoard(board) {
-  console.log(Привет);
+  
 
 }
 
-// Exports all the functions to use them in another file.
+
 module.exports = {
   solve,
   isSolved,
