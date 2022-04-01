e6e9cad0cab92216955172736711de96bee477cd
EkaterinaErokhina <erokhinaeka9@gmail.com>
2022-04-01 12:39:39 +0300

    Check res

--- sudoku.js
+++ sudoku.js
@@ -1,6 +1,7 @@
 const sudokuParse = require('./ranner');
 
-const initialBoard = sudokuParse(process.argv[2])
+const initialBoard = sudokuParse(process.argv[2]);
+let tempBoard = JSON.parse(JSON.stringify(initialBoard));
 
 function solve(boardString) {
   console.log('first changes');
@@ -16,6 +17,14 @@ function prettyBoard(board) {
 
 }
 
+const checkResult = function (board) {
+  for (let i = 0; i < initialBoard.length; i++) {
+    for (let j = 0; j < initialBoard.length; j++) {
+      if (board[i][j] === '-') return true;
+    }
+  }
+  return false;
+}
 
 module.exports = {
   solve,
