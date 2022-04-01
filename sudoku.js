107b579d9398379b8699a6a4d12f29a203ee0063
David <101105159+amDavo@users.noreply.github.com>
2022-04-01 14:18:35 +0300

    Revert "Solve function"

--- sudoku.js
+++ sudoku.js
@@ -3,58 +3,8 @@
 // something representing a board after
 // your solver has tried to solve it.
 // How you represent your board is up to you!
-
-let boardString = [
-  ["5", "3", ".", ".", "7", ".", ".", ".", "."],
-  ["6", ".", ".", "1", "9", "5", ".", ".", "."],
-  [".", "9", "8", ".", ".", ".", ".", "6", "."],
-  ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
-  ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
-  ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
-  [".", "6", ".", ".", ".", ".", "2", "8", "."],
-  [".", ".", ".", "4", "1", "9", ".", ".", "5"],
-  [".", ".", ".", ".", "8", ".", ".", "7", "9"],
-];
-
 function solve(boardString) {
-  //get sizes of Sudoku
-  const size = 9;
-  const boxSize = 3;
-
-  /* finding empty cell in our board
-findEmptyCell is required function
- variable 'r' is row
- variable 'c' is column
-*/
-
-  /* checking whether number is valid in current cell
-  function isNumAcceptableInCell (num, pos, board)*/
-
-  // in validate we check row, column and boxes
-
-  // check row
-  // index 'i' is for row
-  for (let i = 0; i < size; i += 1) {
-    if (board[i][c] === num && i !== r) {
-      return false;
-    }
-  }
-
-  //check columns
-  // index 'i' is for columns
-
-
-    //check boxes
-  // index 'i' is for columns
-
-
-
-// recursive method for sudoku
-// variable 'x' is for rows
-// variable 'y' is for columns
-
-
-
+  console.log('first changes');
 }
 
 // Returns a boolean indicating whether
@@ -62,7 +12,7 @@ findEmptyCell is required function
 // The input board will be in whatever
 // form `solve` returns.
 function isSolved(board) {
-  initial;
+initial
 }
 
 // Takes in a board in some form and
@@ -70,7 +20,9 @@ function isSolved(board) {
 // for output to the screen.
 // The input board will be in whatever
 // form `solve` returns.
-function prettyBoard(board) {}
+function prettyBoard(board) {
+
+}
 
 // Exports all the functions to use them in another file.
 module.exports = {
