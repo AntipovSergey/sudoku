5822c2f55b35009af32e11e4d396b11102b6f4e6
Ilyas <ilkhay@mail.ru>
2022-04-01 14:09:03 +0300

    Удалил файл checkBox, сделал функцию sudokuParse

--- runner.js
+++ runner.js
@@ -10,12 +10,16 @@ const sudoku = require('./sudoku');
 // so you should remove them.
 
 // Gets one puzzle from the text file.
-function sudokuParse(content, puzzleNumber = 0) {
-  let puzzle = content.split('\n')[puzzleNumber];
-  console.log(puzzle);
-  console.log('parser');
-  return puzzle;
+function sudokuParse(content, puzzleNumber = 1) {
+  const newArr = [];
+  const puzzle = content.split('\n')[puzzleNumber];
+  for (let i = 0; i < puzzle.length; i += 9) {
+    newArr.push([puzzle.slice(i, i + 9)][0].split(''));
+    // console.log(newArr);
+  }
+  return newArr;
 }
+// console.log(sudokuParse());
 
 function readAndSolve(err, data) {
   if (err) {
