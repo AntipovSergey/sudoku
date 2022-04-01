af9ae2d7a09dec0da8cf4251602dc4ce636534f5
David <chobanyandav@gmail.com>
2022-04-01 15:46:28 +0300

    we did it

--- sudoku.js
+++ sudoku.js
@@ -5,22 +5,32 @@
 // How you represent your board is up to you!
 
 
+// let board = [
+//   ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+//   ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+//   [".", "9", "8", ".", ".", ".", ".", "6", "."],
+//   ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+//   ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+//   ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+//   [".", "6", ".", ".", ".", ".", "2", "8", "."],
+//   [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+//   [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+// ];
+
 let board = [
-  ["5", "3", ".", ".", "7", ".", ".", ".", "."],
-  ["6", ".", ".", "1", "9", "5", ".", ".", "."],
-  [".", "9", "8", ".", ".", ".", ".", "6", "."],
-  ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
-  ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
-  ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
-  [".", "6", ".", ".", ".", ".", "2", "8", "."],
-  [".", ".", ".", "4", "1", "9", ".", ".", "5"],
-  [".", ".", ".", ".", "8", ".", ".", "7", "9"]
-];
+  [".", ".", ".", ".", ".", ".", ".", ".", "."],
+  [".", ".", ".", ".", ".", ".", ".", ".", "."],
+  [".", ".", ".", ".", ".", ".", ".", ".", "."],
+  [".", ".", ".", ".", ".", ".", ".", ".", "."],
+  [".", ".", ".", ".", ".", ".", ".", ".", "."],
+  [".", ".", ".", ".", ".", ".", ".", ".", "."],
+  [".", ".", ".", ".", ".", ".", ".", ".", "."],
+  [".", ".", ".", ".", ".", ".", ".", ".", "."],
+  [".", ".", ".", ".", ".", ".", ".", ".", "."],
+]
 
 
-
-
-function solve(boardString) {
+function solve(board) {
   //get sizes of Sudoku
   const size = 9;
   const boxSize = 3;
@@ -50,33 +60,35 @@ findEmptyCell is required function
     
 
 
-  // check rows
-  for (let i = 0; i < size; i++) {
-    if (board[i][c] === num && i !== r) {
+    // check rows
+    for (let i = 0; i < size; i++) {
+      if (board[i][c] === num && i !== r) {
         return false;
-    }
-}
-
-  //check columns
-  // index 'i' is for columns
-  for (let i = 0; i < size; i++) {
-    if (board[r][i] === num && i !== c) {
-        return false;
-    }
-}
-
-  //check boxes
-  // index 'i' is for columns
-  const boxRow = Math.floor( r/boxSize ) * boxSize;
-      const boxCol = Math.floor( c/boxSize ) * boxSize;
-
-      for (let i = boxRow; i < boxRow + boxSize; i++) {
-          for (let j = boxCol; j < boxCol + boxSize; j++) {
-              if (board[i][j] === num && i !== r && j !== c) {
-                  return false;
-              }
-          }
       }
+    }
+
+    //check columns
+    // index 'i' is for columns
+    for (let i = 0; i < size; i++) {
+      if (board[r][i] === num && i !== c) {
+        return false;
+      }
+    }
+
+    //check boxes
+    // index 'i' is for columns
+    const boxRow = Math.floor(r / boxSize) * boxSize;
+    const boxCol = Math.floor(c / boxSize) * boxSize;
+
+    for (let i = boxRow; i < boxRow + boxSize; i++) {
+      for (let j = boxCol; j < boxCol + boxSize; j++) {
+        if (board[i][j] === num && i !== r && j !== c) {
+          return false;
+        }
+      }
+    }
+    return true
+  }
 
   // recursive method for sudoku
   // variable 'x' is for rows
@@ -110,7 +122,7 @@ findEmptyCell is required function
 };
 
 console.table(board);
-  console.table(solveSudoku(input));
+ console.table(solve(board));
   
 // Returns a boolean indicating whether
 // or not the provided board is solved.
@@ -131,3 +143,5 @@ module.exports = {
   isSolved,
   prettyBoard,
 };
+
+
