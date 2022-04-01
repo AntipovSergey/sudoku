886283b405678fc5137facec71d30373712761a3
Hilib <hilib2137@mail.ru>
2022-04-01 12:19:49 +0300

    Aaaa

--- sudoku.js
+++ sudoku.js
@@ -27,6 +27,32 @@ function prettyBoard(board) {
 
 }
 
+const start = function () {  // Мы ZZZ
+  while (checkResult(initialBoard)) {
+    for (let i = 0; i < initialBoard.length; i++) {
+      for (let j = 0; j < initialBoard.length; j++) {
+        if (tempBoard[i][j] === ‘.’) {
+          tempBoard[i][j] = [];
+          for (let k = 1; k <= 9; k++) {
+            if (checkLine(tempBoard, [i, j], k) && checkColumn(tempBoard, [i, j], k) && checkBox(tempBoard, [i, j], k)) {
+              tempBoard[i][j].push(k);
+            }
+          }
+        }
+      }
+    }
+    for (let i = 0; i < initialBoard.length; i++) {
+      for (let j = 0; j < initialBoard.length; j++) {
+        if (Array.isArray(tempBoard[i][j]) && tempBoard[i][j].length === 1) {
+          initialBoard[i][j] = `${tempBoard[i][j][0]}`;
+        }
+      }
+    }
+
+    tempBoard = JSON.parse(JSON.stringify(initialBoard));
+  }
+};
+
 // Exports all the functions to use them in another file.
 module.exports = {
   solve,
