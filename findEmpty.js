b9b8301c169a94c3c7b9ed601ba23adc3f79902c
Natasha <3686091@gmail.com>
2022-04-01 11:17:42 +0300

    addFunctionFindEmpty

---
+++ findEmpty.js
@@ -0,0 +1,9 @@
+const findEmpty = (board) => {
+  for (let r = 0; r < size; r++) {
+    for (let c = 0; c <size; c++) {
+      if(board[r][c] === '-') {
+        return [r, c];
+      }
+    }
+  }
+};
