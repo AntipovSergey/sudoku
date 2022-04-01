5e56384cacb2591cb175c8da61026220685a79f4
David <chobanyandav@gmail.com>
2022-04-01 13:58:47 +0300

    Add function flow for our team

--- sudoku.js
+++ sudoku.js
@@ -3,8 +3,53 @@
 // something representing a board after
 // your solver has tried to solve it.
 // How you represent your board is up to you!
+
+let boardString = [
+  ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+  ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+  [".", "9", "8", ".", ".", ".", ".", "6", "."],
+  ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+  ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+  ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+  [".", "6", ".", ".", ".", ".", "2", "8", "."],
+  [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+  [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+];
+
 function solve(boardString) {
-  console.log('first changes');
+  //get sizes of Sudoku
+  const size = 9;
+  const boxSize = 3;
+
+  /* finding empty cell in our board
+findEmptyCell is required function
+ variable 'r' is row
+ variable 'c' is column
+*/
+
+  /* checking whether number is valid in current cell
+  function isNumAcceptableInCell (num, pos, board)*/
+
+  // in validate we check row, column and boxes
+
+  // check row
+  // index 'i' is for row
+
+  //check columns
+  // index 'i' is for columns
+
+
+    //check boxes
+  // index 'i' is for columns
+
+
+
+// recursive method for sudoku
+// variable 'x' is for rows
+// variable 'y' is for columns
+
+
+
 }
 
 // Returns a boolean indicating whether
@@ -12,7 +57,7 @@ function solve(boardString) {
 // The input board will be in whatever
 // form `solve` returns.
 function isSolved(board) {
-initial
+  initial;
 }
 
 // Takes in a board in some form and
@@ -20,9 +65,7 @@ initial
 // for output to the screen.
 // The input board will be in whatever
 // form `solve` returns.
-function prettyBoard(board) {
-
-}
+function prettyBoard(board) {}
 
 // Exports all the functions to use them in another file.
 module.exports = {
