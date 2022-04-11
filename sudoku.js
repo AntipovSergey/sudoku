d2fd6123785b1d5cee618fb290409f331ce2dbec
a7heis7 <a7295@yandex.ru>
2022-04-11 17:19:33 +0300

    Release 0.2

--- sudoku.js
+++ sudoku.js
@@ -3,9 +3,84 @@
 // something representing a board after
 // your solver has tried to solve it.
 // How you represent your board is up to you!
-function solve(boardString) {
-  console.log('first changes');
+function solve() {
+  const size = 9;
+  const boxSize = 3;
+
+  const findEmpty = (board) => {
+    for (let r = 0; r < size; r++) {
+      for (let c = 0; c < size; c++) {
+        if (board[r][c] === '-') {
+          return [r, c];
+        }
+      }
+    }
+    return null;
+  };
+
+  const validate = (num, pos, board) => {
+    const [r, c] = pos;
+
+    // Check rows
+    for (let i = 0; i < size; i++) {
+      if (board[i][c] === num && i !== r) {
+        return false;
+      }
+    }
+
+    // Check cols
+    for (let i = 0; i < size; i++) {
+      if (board[r][i] === num && i !== c) {
+        return false;
+      }
+    }
+
+    // Check box
+    const boxRow = Math.floor(r / boxSize) * boxSize;
+    const boxCol = Math.floor(c / boxSize) * boxSize;
+
+    for (let i = boxRow; i < boxRow + boxSize; i++) {
+      for (let j = boxCol; j < boxCol + boxSize; j++) {
+        if (board[i][j] === num && i !== r && j !== c) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  };
+
+  const solveProgress = (board) => {
+    const currPos = findEmpty(board);
+
+    if (currPos === null) {
+      return true;
+    }
+    console.log('------------------------------');
+    for (let i = 1; i < size + 1; i++) {
+      const currNum = i.toString();
+      const isValid = validate(currNum, currPos, board);
+      console.log('currPos ', currPos, 'currNum ', currNum, 'isValid ', isValid);
+      if (isValid) {
+        const [x, y] = currPos;
+        board[x][y] = currNum;
+
+        if (solveProgress()) {
+          return true;
+        }
+
+        board[x][y] = '-';
+      }
+    }
+
+    return false;
+  };
+
+  solveProgress();
+  return board;
 }
+const puzzle = '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--';
+console.table(solve(prettyBoard(puzzle)));
 
 // Returns a boolean indicating whether
 // or not the provided board is solved.
