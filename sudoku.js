f14d93be1ac5e2ec29beaa957eab231b010ed8cd
Anna Madyakina <annachuvatkina1906@gmail.com>
2022-04-11 19:52:20 +0500

    Anna1

--- sudoku.js
+++ sudoku.js
@@ -5,11 +5,22 @@
 // How you represent your board is up to you!
 //преобразует строку в массив 
 let str = '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--'
+let board = [['1', '-', '5', '8', '-', '2', '-', '-', '-']
+['-', '9', '-', '-', '7', '6', '4', '-', '5']
+['2', '-', '-', '4', '-', '-', '8', '1', '9']
+['-', '1', '9', '-', '-', '7', '3', '-', '6']
+['7', '6', '2', '-', '8', '3', '-', '9', '-']
+['-', '-', '-', '-', '6', '1', '-', '5', '-']
+['-', '-', '7', '6', '-', '-', '-', '3', '-']
+['4', '3', '-', '-', '2', '-', '5', '-', '1']
+['6', '-', '-', '3', '-', '8', '9', '-', '-']]
+
 function solve(boardString) {
   let arr = []
   for(let i = 0; i < boardString.length; i +=9) {
     arr.push(boardString.slice(i, i+9).split(''))
   }
+
   return arr
   console.log('first changes');
 }
@@ -19,6 +30,17 @@ function solve(boardString) {
 // The input board will be in whatever
 // form `solve` returns.
 //проверяет решилась или нет 
+const cellCheck = (board) => {
+for(let i = 0; i < board.length; i++) {
+  for(let j = 0; j < board[i].length; j++) {
+  if( board[i][j] == '-')
+  return [i, j]
+  }
+ }
+ 
+}
+
+
 function isSolved(board) {
 
 }
