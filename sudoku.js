c91af6c6baea1e751b2bcb886fb4a6e030dd2620
a7heis7 <a7295@yandex.ru>
2022-04-11 17:51:27 +0300

    Release 0.3

--- sudoku.js
+++ sudoku.js
@@ -81,6 +81,7 @@ function solve() {
 }
 const puzzle = '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--';
 console.table(solve(prettyBoard(puzzle)));
+// console.table(solve()))
 
 // Returns a boolean indicating whether
 // or not the provided board is solved.
