ba6e5c3070297169adb17bc79ef7a2e68cb60721
Lisa <lisaliskin@gmail.com>
2022-04-22 15:26:22 +0300

    group check v.1

--- sudoku.js
+++ sudoku.js
@@ -20,30 +20,23 @@ const board = [
   [1, 3, 0, 2, 4, 0, 6, 9, 0],
 ];
 
-function groupCheck(board, x, y, arr) {
-  const row = Math.floor(x / 3) * 3; // проверка по ряду
+function groupCheck(board, indexRow, indexColumn, options) {
+  const row = Math.floor(indexRow / 3) * 3; // старт строки
   console.log(row);
-  const colomn = Math.floor(y / 3) * 3; // проверка по столбцу
+  const colomn = Math.floor(indexColumn / 3) * 3; // старт столбца
   console.log(colomn);
-  let first; // первый ряд группы
-  let second; // второй ряд группы
-  let third; // третий ряд группы
-  for (let yy = 0; yy < 3; yy += 1) { // цикл проверки первого ряда в группе
-    for (let xx = 0; xx < 3; xx += 1) {
-      arr.forEach((elem) => {
-        if (elem === board[yy][xx]) arr.pop(elem)
-      })
+  const endRow = row + 3; // старт строки
+  console.log(endRow);
+  const endColomn = colomn + 3; // старт столбца
+  console.log(endColomn);
+  for (let yy = colomn; yy < endColomn; yy += 1) { // цикл проверки первого ряда в группе
+    for (let xx = row; xx < endRow; xx += 1) {
+      if (options.indexOf(board[xx][yy]) !== -1) options.splice(options.indexOf(board[xx][yy]), 1);
+      console.log(options)
     }
   }
-  // if (board[first][second] === row[third]) return false;
-  // if (board[first][second] !== row[third]) return true;
-  // for (let y = 3; y < 6; y += 1) { // цикл проверки второго ряда в группе
-  //   for (let z = 6; z < 9; z += 1) { // цикл проверки третьего ряда в группе
-
-  //   }
-  // }
 }
-console.log(groupCheck(board, 2, 7, [2, 3]));
+console.log(groupCheck(board, 2, 7, [1, 3]));
 
 // Returns a boolean indicating whether
 // or not the provided board is solved.
