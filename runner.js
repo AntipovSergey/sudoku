ac978631f998f63e6d81a0cbf31b7774ecae6b5c
ghuser1984 <dmefr.mail@gmail.com>
2022-04-22 18:33:23 +0300

    First commit attempt

--- runner.js
+++ runner.js
@@ -10,25 +10,41 @@ const sudoku = require('./sudoku');
 // so you should remove them.
 
 // Gets one puzzle from the text file.
+const fileIn = fs.readFileSync(
+  './sudoku-puzzles.txt',
+  'utf-8',
+);
+// console.log(fileIn);
+
 function sudokuParse(content, puzzleNumber = 0) {
-  let puzzle = content.split('\n')[puzzleNumber];
+  const puzzle = content.split('\n')[puzzleNumber];
+  // console.log('parser');
   console.log(puzzle);
-  console.log('parser');
-  return puzzle;
+  // console.log(puzzle.length);
+  const re = /.{9}/gi;
+  // console.log(puzzle.match(re));
+  const intArray = puzzle.match(re);
+  const outArray = intArray.map((el) => el.split(''));
+  // console.log(outArray.length);
+  // console.log(outArray);
+  return outArray;
 }
+
+const arrTest = sudokuParse(fileIn, 0);
+console.log(arrTest);
 
 function readAndSolve(err, data) {
   if (err) {
     throw err;
   }
-  let puzzle = sudokuParse(data);
 
-  let solvedPuzzle = sudoku.solve(puzzle);
+  const puzzle = sudokuParse(data);
+
+  const solvedPuzzle = sudoku.solve(puzzle);
   if (sudoku.isSolved(solvedPuzzle)) {
-    console.log("The board was solved!");
+    console.log('The board was solved!');
     console.log(sudoku.prettyBoard(solvedPuzzle));
-  }
-  else {
+  } else {
     console.log("The board wasn't solved :(");
   }
 }
@@ -37,6 +53,5 @@ function readAndSolve(err, data) {
 fs.readFile(
   './sudoku-puzzles.txt',
   'utf-8',
-  readAndSolve
+  readAndSolve,
 );
-
