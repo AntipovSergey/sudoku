abfde84801290549057b87c485eaa346e45c575a
mac <mac@MacBook-Pro-mac.local>
2022-04-23 13:24:03 +0300

    my release

--- sudoku.js
+++ sudoku.js
@@ -1,8 +1,29 @@
+const fs = require('fs');
 // Takes a board as a string in the format
 // you see in the puzzle file. Returns
 // something representing a board after
 // your solver has tried to solve it.
 // How you represent your board is up to you!
+let text = fs.readFileSync('./sudoku-puzzles.txt', 'utf-8')
+let arr = text.split('\n')
+let arr1 = []
+for (let i = 0; i < 15; i ++) {
+arr1.push(arr[i].split(''))
+}
+//console.log(arr1)
+let r = ''
+for(let i = 0; i < arr1[0].length; i ++) {
+  if(i % 9 === 0) {
+  r = r + '\n' + arr[0][i]
+  } else { r = r + arr[0][i] }
+//console.log(arr[0][i])
+}
+let re = r.split('\n').slice(1)
+console.log(r.split('\n').slice(1))
+let syd = re.map((a) => a.split(''))
+console.log(syd)
+
+
 function solve(boardString) {
   console.log('first changes');
 }
