9e73e952643ab98f4b77879c6e9275d20d3e367a
Vasily <89163091443@mail.ru>
2022-04-24 15:03:15 +0300

    relise - 0.2

---
+++ proverka.js
@@ -0,0 +1,100 @@
+function proverka(array){
+  
+  let result = [];
+
+  for(let j=1; j<=9;j++){
+    let count = 0;
+    for(let i=0; i < array.length; i++){
+      if(j==array[i]){
+       count++
+      }
+    }
+    
+    if(count==0)
+     result.push(j);
+  }
+  
+  if(result.length==1){
+    return result[0]
+  } 
+
+   return 0;
+   
+}
+
+function getNumbers(i=0,j=0,array) {
+  let expectedArray = [];
+  // let counter = 0;
+  for (let k = 0; k < array.length; k += 1) {
+    
+      if ( typeof (array[i][k]) === 'number' ) expectedArray.push(array[i][k]);
+    
+  }
+  for (let g = 0; g<array.length; g++){
+    if ( typeof (array[g][j]) === 'number' ) expectedArray.push(array[g][j]);
+  }
+  return expectedArray;
+}
+
+const fs = require('fs');
+const sudoku = fs.readFileSync('./sudoku-puzzles.txt','utf-8');
+
+
+function getArray( index = 0,  string = '' ){
+  let puzzle = string.split('\n')[index];
+  const sliceArr = [];
+  const copy = [...puzzle];
+  for(let i = 0; i < 9; i++){
+    sliceArr.push(copy.splice(0,9))
+   if(string.length === 0) return null;
+  }
+  return sliceArr.map(line=> {
+    return line.map(el=> {
+      if(el === '-'){
+        return el
+      }else {
+        return Number(el) 
+      }
+    })
+  });
+}
+​
+
+console.log(getArray(sudoku));
+
+function addIscl(i, g, arr, arrB) {
+  let startI;
+  let endI;
+  switch (i) {
+    case 0: case 1: case 2: startI = 0; endI = 2; break;
+    case 3: case 4: case 5: startI = 3; endI = 5; break;
+    case 6: case 7: case 8: startI = 6; endI = 8;
+    default: break;
+  }
+  let startG;
+  let endG;
+  switch (g) {
+    case 0: case 1: case 2: startG = 0; endG = 2; break;
+    case 3: case 4: case 5: startG = 3; endG = 5; break;
+    case 6: case 7: case 8: startG = 6; endG = 8;
+    default: break;
+  }
+​
+  console.log('координаты', startI, endI, startG, endG)
+​
+  for (; startI <= endI; startI += 1) {
+    console.log('------', startI)
+    for (let G = startG; G <= endG; G++) {
+      console.log('ходы', 'строчка ход i', startI, 'столбец ход g', G)
+      if (typeof (arrB[startI][G]) === 'number') {
+        arr.push(arrB[startI][G])
+      }
+    }
+  }
+  return arr
+}
+
+  
+  module.exports = {proverka,getNumbers,getArray,addIscl}
+
+
