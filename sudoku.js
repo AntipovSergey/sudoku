5b922130acf53c68ab91cc3e9e386b4dbfd53f63
Raketa86 <80678776+Raketa86@users.noreply.github.com>
2022-04-25 12:21:26 +0300

    added isValid

--- sudoku.js
+++ sudoku.js
@@ -4,8 +4,30 @@
 // your solver has tried to solve it.
 // How you represent your board is up to you!
 
-function solve(boardString) {
-  console.log('first changes');
+function solve(text) {
+  function valid(n, cord, sudoku) {
+    const [a, b] = cord;
+    for (let i = 0; i < sudoku.length; i += 1) {
+      if (sudoku[i][b] === n && i !== a) {
+        return false;
+      }
+    }
+    for (let i = 0; i < sudoku.length; i += 1) {
+      if (sudoku[a][i] === n && i !== b) {
+        return false;
+      }
+    }
+    const cubeA = Math.floor(a / sudoku.length) * sudoku.length;
+    const cubeB = Math.floor(b / sudoku.length) * sudoku.length;
+    for (let i = cubeA; i < cubeA + 9; i += 1) {
+      for (let g = cubeB; g < cubeB + 9; g += 1) {
+        if (sudoku[i][g] === n && i !== a && g !== b) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
 }
 
 // Returns a boolean indicating whether
