f7554eaa0693aeeb30d87d1cde658dc0837841c6
IvanNovokshanov <vanotik1@gmail.com>
2022-05-09 17:36:55 +0500

    release 0 creatBoard

--- sudoku.js
+++ sudoku.js
@@ -1,19 +1,28 @@
+function createArrays(boardString) {
+	const result = [];
+	const boardArr = boardString.split('');
+	while (boardArr.length > 0) {
+		result.push(boardArr.splice(0, 9));
+	}
+	console.log(result);
+	return result;
+}
+const test =
+	'1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--';
 // Takes a board as a string in the format
 // you see in the puzzle file. Returns
 // something representing a board after
 // your solver has tried to solve it.
 // How you represent your board is up to you!
 function solve(boardString) {
-  console.log('first changes');
+	console.log('first changes');
 }
 
 // Returns a boolean indicating whether
 // or not the provided board is solved.
 // The input board will be in whatever
 // form `solve` returns.
-function isSolved(board) {
-
-}
+function isSolved(board) {}
 
 // Takes in a board in some form and
 // returns a String that's well formatted
@@ -21,12 +30,13 @@ function isSolved(board) {
 // The input board will be in whatever
 // form `solve` returns.
 function prettyBoard(board) {
-
+	return board.join('\n');
 }
+console.log(prettyBoard(createArrays(test)));
 
 // Exports all the functions to use them in another file.
 module.exports = {
-  solve,
-  isSolved,
-  prettyBoard,
+	solve,
+	isSolved,
+	prettyBoard,
 };
