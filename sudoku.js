34cc903641feae60210cf62d8233df826e19a497
EkaterinaRechkina <ekaterinarechkinav@yandex.ru>
2022-05-10 10:44:18 +0300

    Release0

--- sudoku.js
+++ sudoku.js
@@ -1,24 +1,13 @@
-function findEmpty(arr) {
-  let arrN = []
-  for (let i = 0; i < arr.length; i++) {
-    for (let g = 0; g < arr[i].length; g++) {
-      if (arr[i][[g]] === "-") {
-        arrN.push([i,g])
-      }
-    }
-  }
-  return arrN
-
 //num - подставляемое число
 
-function checkSquare(arr, pos, num) {
+function checkSquare(num, pos, board) {
   const [r, c] = pos; //Деструктуризация получаем данные о позиции
   const boxRow = Math.floor(r / 3) * 3;
   const boxCol = Math.floor(c / 3) * 3;
 
   for (let i = boxRow; i < boxRow + 3; i++) {
     for (let j = boxCol; j < boxCol + 3; j++) {
-      if (arr[i][j] === num) {
+      if (board[i][j] === num) {
         return false;
       }
     }
@@ -43,7 +32,8 @@ function createArrays(boardString) {
   console.log(result);
   return result;
 }
-
+const test =
+  "1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--";
 
 function checkRow(arr, pos, num) {
   for (let i = 0; i < arr.length; i += 1) {
@@ -63,53 +53,20 @@ function checkColumn(arr, pos, num) {
   return true;
 }
 
-
-function checkNum (arr, pos, num) {
-  return checkColumn(arr, pos, num) && checkRow(arr, pos, num) && checkSquare(arr, pos, num)
-}
-
 // Takes a board as a string in the format
 // you see in the puzzle file. Returns
 // something representing a board after
 // your solver has tried to solve it.
 // How you represent your board is up to you!
-
 function solve(boardString) {
-  const boardArr = createArrays(boardString)
-  const emptyPosArr = findEmpty(boardArr)
-  const numArr = []
-  for (let i = 0; i < emptyPosArr.length; i++) {
-    for (var j = 1; j < 10; j++) {
-      if (checkNum (boardArr, emptyPosArr[i], j)) {
-        numArr.push(j)
-      }
-    }
-    if (numArr.length > 1) {
-      console.log(numArr);
-    } else {
-      boardArr[emptyPosArr[i][0]][emptyPosArr[i][1]] = numArr[0]
-    }
-  }
-
-
-
-
   console.log("first changes");
 }
-
-
 
 // Returns a boolean indicating whether
 // or not the provided board is solved.
 // The input board will be in whatever
 // form `solve` returns.
-function isSolved(board) {
-  let arr = solve(board)
-  if (findEmpty(arr).length !== 0) {
-    return false
-  }
-  return true
-}
+function isSolved(board) {}
 
 // Takes in a board in some form and
 // returns a String that's well formatted
@@ -127,4 +84,4 @@ module.exports = {
   solve,
   isSolved,
   prettyBoard,
-}
+};
