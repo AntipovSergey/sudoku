16011cff908d207efdcde7bae20315a4d825338f
KuvaldinDmitry <kuvaldin2012@gmail.com>
2022-05-10 11:44:25 +0400

    release 1

--- runner.js
+++ runner.js
@@ -1,3 +1,6 @@
+require
+
+
 // Use filesystem.
 const fs = require("fs");
 // Use functions from sudoku.js file.
--- sudoku.js
+++ sudoku.js
@@ -8,6 +8,7 @@ function findEmpty(arr) {
     }
   }
   return arrN
+}
 
 //num - подставляемое число
 
