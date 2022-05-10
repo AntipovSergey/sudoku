bba992de0a1896023e8e1da77948d25697df0af8
Maksim <maximka5291@gmail.com>
2022-05-10 11:19:10 +0400

    test

--- sudoku.js
+++ sudoku.js
@@ -11,64 +11,76 @@ function solve(boardString) {
 
   const arr = [];
   let coordinate;
-  for(let i = 0; i < board.length; i = i + 9){
-  
-      arr.push(board.slice(i, i + 9).split(''));
+  for (let i = 0; i < board.length; i = i + 10) {
+
+    arr.push(board.slice(i, i + 9).split(''));
+  }
+  console.log(arr);
+  // return arr;
+  for (let r = 0; r < board.length; r++) {
+    for (let c = 0; c < board[j].length; c++) {
+      coordinate = arr[r][c]; ///работает определили координату
+      if (coordinate === '-') {
+        return [r, c]
       }
-     console.log(arr);
-      // return arr;
-  for(let r = 0; r < board.length;r++){
-    for(let c = 0; c < board[j].length; c++){
-     coordinate = arr[r][c]; ///работает определили координату
-      if(coordinate === '-'){ 
-       return true;
-       }
-     
+
       console.log(arr[r][c]);
     }
   }
-  
+
   console.log('first changes');
- }
-console.log(solve(board));
-//   console.log("first changes");
-//   const board = [];
-//   for (let i = 0; i < 9; i++) {
-//     board.push(resultBoard.slice(i * 9, i * 9 + 9).split(""));
-//   }
-//   return board;
-// }
-
-// Returns a boolean indicating whether
-// or not the provided board is solved.
-// The input board will be in whatever
-// form `solve` returns.
-function isSolved(board) {}
-
-// Takes in a board in some form and
-// returns a String that's well formatted
-// for output to the screen.
-// The input board will be in whatever
-// form `solve` returns.
-
-//////красивая доска(работает в виде массива массивов)
-function prettyBoard() {
- 
-//   for(let i = 0; i < board.length; i = i + 9){
-  
-//   arr.push(board.slice(i, i + 9).split(''));
-//   }
-// // console.log(arr);
-//   return arr;
 }
-console.log(prettyBoard(board))
+console.log(solve(board));
 
-// function prettyBoard(board) {}
+const emptySudoku = solvet(resultBoard);
+// поиск значений с точкой-пустых элементов
+const findEmpty = (emptySudoku) => {
+    for (let r = 0; r < size; r++) {
+      for (let c = 0; c < size; c++) {
+        if (emptySudoku[r][c] === ".") {
+          return [r, c];
+        }
+      }
 
 
-// Exports all the functions to use them in another file.
-module.exports = {
-  solve,
-  isSolved,
-  prettyBoard,
-};
+      //   console.log("first changes");
+      //   const board = [];
+      //   for (let i = 0; i < 9; i++) {
+      //     board.push(resultBoard.slice(i * 9, i * 9 + 9).split(""));
+      //   }
+      //   return board;
+      // }
+
+      // Returns a boolean indicating whether
+      // or not the provided board is solved.
+      // The input board will be in whatever
+      // form `solve` returns.
+      function isSolved(board) {}
+
+      // Takes in a board in some form and
+      // returns a String that's well formatted
+      // for output to the screen.
+      // The input board will be in whatever
+      // form `solve` returns.
+
+      //////красивая доска(работает в виде массива массивов)
+      function prettyBoard() {
+
+        //   for(let i = 0; i < board.length; i = i + 9){
+
+        //   arr.push(board.slice(i, i + 9).split(''));
+        //   }
+        // // console.log(arr);
+        //   return arr;
+      }
+      console.log(prettyBoard(board))
+
+      // function prettyBoard(board) {}
+
+
+      // Exports all the functions to use them in another file.
+      module.exports = {
+        solve,
+        isSolved,
+        prettyBoard,
+      };
