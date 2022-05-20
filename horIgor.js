3f5771ae22fb450d50aa587eadecdf7f4d943b0e
IgorGap <ig.gaponenko@gmail.com>
2022-05-20 13:59:07 +0300

    Releases

---
+++ horIgor.js
@@ -0,0 +1,21 @@
+// uuyu
+let puzzle = [
+  [1, 0, 5, 8, 0, 2, 0, 45, 0],
+  [1, 0, 5, 8, 0, 2, 0, 0, 0],
+  [1, 0, 5, 8, 0, 2, 0, 0, 0],
+]
+let num = 45
+function searchStraightLineInclude(puzzle, num) {
+
+  for (let i = 0; i < puzzle.length; i++) {
+    const word = [];
+    for (let j = 0; j < puzzle[i].length; j++) {
+      word.push(puzzle[j][i])
+    }
+    if (word.join('').includes(num)) return true;
+  }
+  return false;
+}
+
+
+console.log(searchStraightLineInclude(puzzle, num))
