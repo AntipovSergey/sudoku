f763d29ff1da2e04672f388b939b047d14188b1f
Ev-genius <tabunov370@mail.ru>
2022-05-20 16:42:11 +0300

    Release 2

---
+++ MySudoku.js
@@ -0,0 +1,79 @@
+let board = [
+  [".", ".", ".", ".", ".", ".", ".", ".", "."],
+  [".", ".", ".", ".", ".", ".", ".", ".", "."],
+  [".", ".", ".", ".", ".", ".", ".", ".", "."],
+  [".", ".", ".", ".", ".", ".", ".", ".", "."],
+  [".", ".", ".", ".", ".", ".", ".", ".", "."],
+  [".", ".", ".", ".", ".", ".", ".", ".", "."],
+  [".", ".", ".", ".", ".", ".", ".", ".", "."],
+  [".", ".", ".", ".", ".", ".", ".", ".", "."],
+  [".", ".", ".", ".", ".", ".", ".", ".", "."],
+];
+
+let solveSudoku = function (board) {
+  const size = 9;
+  const boxSize = 3;
+
+  const findEmpty = (board) => {
+    for (let r = 0; r < size; r += 1) {
+      for (let c = 0; c < size; c += 1) {
+        if (board[r][c] === ".") {
+          return [r, c];
+        }
+      }
+    }
+    return null;
+  };
+  const validate = (num, pos, board) => {
+    const [r, c] = pos;
+    //Check rows
+    for (let i = 0; i < size; i += 1) {
+      if (board[i][c] === num && i !== r) {
+        return false;
+      }
+    }
+    //Check cols
+    for (let i = 0; i < size; i += 1) {
+      if (board[r][i] === num && i !== r) {
+        return false;
+      }
+    }
+    //Check box
+    const boxRow = Math.floor(r / boxSize) * boxSize;
+    const boxCol = Math.floor(c / boxSize) * boxSize;
+    for (let i = boxRow; i < boxRow + board; i += 1) {
+      for (let j = boxCol; j < boxCol + boxSize; i += 1) {
+        if (board[r][i] === num && i !== r && j !== c) {
+          return false;
+        }
+      }
+    }
+    return true;
+  };
+  const solve = () => {
+    const currPos = findEmpty(board);
+    if (currPos == null) {
+      return true;
+    }
+    for (let i = 1; i < size + 1; i += 1) {
+      const currNum = i.toString();
+      const isValid = validate(currNum, currPos, board);
+
+      if (isValid) {
+        const [x, y] = currPos;
+        board[x][y] = currNum;
+
+        if (solve()) {
+          return true;
+        }
+        board[x][y] = ".";
+      }
+    }
+
+    return false;
+  };
+  solve();
+  return board;
+};
+console.table(board);
+console.table(solveSudoku(board));
--- funkHorizontal.js
+++ funkHorizontal.js
@@ -3,29 +3,29 @@
 
 // [1,0,5,8,0,2,0,0,0]
 let arr = [
-  [1,2,3,4,5,6,7,8,9,],
-  [1,2,3,4,5,6,7,8,9,],
-  [1,2,3,4,5,6,7,8,9,],
-  [1,2,3,4,5,6,7,8,9,],
-  [1,2,3,4,5,6,7,8,9,],
-  [1,2,3,4,5,6,7,8,9,], 
-  [1,2,3,4,5,6,7,8,9,],
-  [1,2,3,4,5,6,7,8,9,],
-  [1,2,3,4,5,6,7,8,9,],
+  [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  [1, 2, 3, 4, 5, 6, 7, 8, 9],
 ];
 
 let num = 45;
 let resSum = 0;
-function lineSearch(arr, num){
-  for(let i = 0; i < arr.length; i += 1){
+function lineSearch(arr, num) {
+  for (let i = 0; i < arr.length; i += 1) {
     let res = [];
-    for(let j = 0; j < arr[i].length; j += 1){
-    resSum += arr[i][j]
-    res.push(arr[i][j])
+    for (let j = 0; j < arr[i].length; j += 1) {
+      resSum += arr[i][j];
+      res.push(arr[i][j]);
+    }
+
+    if (resSum == 45) return true;
   }
- 
-  if(resSum == 45) return true;
-}
-return false
+  return false;
 }
 console.log(lineSearch(arr, num));
