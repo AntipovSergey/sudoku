430996495920c228907ff48843727446f54181b3
Denis Gutsulak <d.99113@gmail.com>
2022-05-20 11:53:02 +0300

    newFun

---
+++ funGenSudoku.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+function funGenSudoku(params) {
+
+}
+
+
+
+
+
+
+module.exports = {funGenSudoku};
--- sudoku.js
+++ sudoku.js
@@ -1,8 +1,8 @@
-// Takes a board as a string in the format
-// you see in the puzzle file. Returns
-// something representing a board after
-// your solver has tried to solve it.
-// How you represent your board is up to you!
+Takes a board as a string in the format
+you see in the puzzle file. Returns
+something representing a board after
+your solver has tried to solve it.
+How you represent your board is up to you!
 function solve(boardString) {
   console.log('first changes');
 }
@@ -30,10 +30,3 @@ module.exports = {
   isSolved,
   prettyBoard,
 };
-//start denis
-//start matiy
-//start banu_comit 2
-//git merge new mari
-//start banu
-//new str merge
-//new str merge banu
