888b99d8c9497fa6ed5ef8ef44efcb0e1a6f3aa3
Ev-genius <tabunov370@mail.ru>
2022-05-20 14:49:33 +0300

    Release 1

--- funkHorizontal.js
+++ funkHorizontal.js
@@ -1,8 +1,8 @@
 // const fs = require("fs");
 // const arr = fs.readFileSync("./sudoku-puzzles.txt", "utf-8").trim().split("\n");
 
-let arr =
-[
+// [1,0,5,8,0,2,0,0,0]
+let arr = [
   [1,2,3,4,5,6,7,8,9,],
   [1,2,3,4,5,6,7,8,9,],
   [1,2,3,4,5,6,7,8,9,],
@@ -13,18 +13,19 @@ let arr =
   [1,2,3,4,5,6,7,8,9,],
   [1,2,3,4,5,6,7,8,9,],
 ];
-//[1,0,5,8,0,2,0,0,0]
 
-let num = 8;
-function lineSearch(arr, num) {
-  for (let i = 0; i < arr.length; i += 1) {
+let num = 45;
+let resSum = 0;
+function lineSearch(arr, num){
+  for(let i = 0; i < arr.length; i += 1){
     let res = [];
-    for (let j = 0; j < arr[i]; j += 1) {
-      res.push(arr[i][j]);
-      console.log(res);
-    }
-    if (res.join('').includes(num)) return true;
+    for(let j = 0; j < arr[i].length; j += 1){
+    resSum += arr[i][j]
+    res.push(arr[i][j])
   }
-  return false;
+ 
+  if(resSum == 45) return true;
+}
+return false
 }
 console.log(lineSearch(arr, num));
