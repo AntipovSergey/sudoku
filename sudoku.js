36e56eadd32134649a40419d3afe5248fd079813
NekrasovDv <nekrasovdv91@gmail.com>
2022-06-06 19:51:26 +0400

    change solve

--- sudoku.js
+++ sudoku.js
@@ -1,45 +1,74 @@
+//Решение задачи
+// вызываем формирование доски
+boardString =
+  "1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--";
+let board = stringToTable(boardString);
+console.log(typeof board);
 
-let boardString = '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--'
+function solve(board) {
+  // console.log("first changes");
 
-function getArrayToString(puzzle) {
-  const board = []
-  for (let i = 0; i < 81; i += 9) {
-    board.push(puzzle.split('').slice(i, i + 9))
+  // поиск позиции
+  let position = searchPos(board);
+
+  //переприсваиваем входные данные
+  let row = position[1];
+  let col = position[0];
+
+  //  Выход Условие
+  if (searchPos(board) === null) {
+    return board;
   }
-  return board
+
+  //
+  // function recrution
+  for (let i = 1; i < 10; i++) {
+    let rowsColumns = isValid(board, row, col, i);
+    let miniBoard = miniBoards(board, position, i);
+    if (rowsColumns && miniBoard) {
+      board[position[0]][position[1]] = `${i}`;
+      // console.log(board);
+      solve(board);
+    }
+  }
 }
-console.table(getArrayToString(boardString))
+console.log(solve(board));
 
+//формирование  доски
+function getArrayToString(puzzle) {
+  const board = [];
+  for (let i = 0; i < 81; i += 9) {
+    board.push(puzzle.split("").slice(i, i + 9));
+  }
+  return board;
+}
 
-const value = getArrayToString(boardString);
-
-
+//Проверка строка столбец отдельно переприсвоить данные
 function isValid(board, row, col, k) {
   for (let i = 0; i < 9; i += 1) {
-    if (board[row][i] === k || board[i][col] === k) {
-      return false
-    }   
-  }
-  return true
-}
-console.log(isValid(value))
-function stringToTable(puzzle) {
-  const arr = puzzle.split('')
-  let board = []
-    for (let i = 0; i < arr.length; i += 9) {
-      let row = arr.slice(i, i + 9)
-      board.push(row)
+    if (board[row][i] == k || board[i][col] == k) {
+      return false;
     }
-   return board 
-} 
-
-
-function solve(boardString) {
-
-  console.log('first changes');
-
+  }
+  return true;
 }
+
+//формирование  доски
+function stringToTable(puzzle) {
+  const arr = puzzle.split("");
+  let board = [];
+  for (let i = 0; i < arr.length; i += 9) {
+    let row = arr.slice(i, i + 9);
+    board.push(row);
+  }
+  return board;
+}
+
+//проверка миниборда
 function miniBoards(board, position, number) {
+  let numbers = "";
+  numbers += number;
+
   let x = -1,
     y = -1;
   const miniBoard = [];
@@ -66,26 +95,24 @@ function miniBoards(board, position, number) {
       miniBoard.push(board[i][j]);
     }
   }
-  console.log(miniBoard);
-  if (miniBoard.includes(number)) {
+  if (miniBoard.includes(numbers)) {
     return false;
+  } else {
+    return true;
   }
-  return true;
 }
-console.log(miniBoards(board, position, number));
 
-function searchPos (board) {
+//первый пропуск
+function searchPos(board) {
   for (let s = 0; s < 9; s++) {
     for (let c = 0; c < 9; c++) {
-        if(board[s][c] == '-') {
-            return [s,c];
-        }
+      if (board[s][c] == "-") {
+        return [s, c];
+      }
     }
+  }
+  return null;
 }
-return null;
-}
-
-
 
 // Returns a boolean indicating whether
 // or not the provided board is solved.
@@ -98,12 +125,12 @@ function isSolved(board) {}
 // for output to the screen.
 // The input board will be in whatever
 // form `solve` returns.
-function prettyBoard(board) {} 
+function prettyBoard(board) {}
 
 // Exports all the functions to use them in another file.
 module.exports = {
   solve,
   isSolved,
   prettyBoard,
-  stringToTable
+  stringToTable,
 };
