6d18fd376cdca4ae90c08767dc55e5c219c8c5b0
IvanM <mahankovage@gmail.com>
2022-06-06 16:25:44 +0300

    isSolved done

--- sudoku.js
+++ sudoku.js
@@ -12,7 +12,14 @@ function solve(boardString) {
 // The input board will be in whatever
 // form `solve` returns.
 function isSolved(board) {
-
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      if (board[i][j] === '-') {
+        return false;
+      }
+    }
+  }
+  return true;
 }
 
 // Takes in a board in some form and
