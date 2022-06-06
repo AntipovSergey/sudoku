0ed704aec378d741cefcb3cb152e8d7c70bcec12
Sadan <daniel.sadkov12@gmail.com>
2022-06-06 16:07:22 +0300

    isValid

--- sudoku.js
+++ sudoku.js
@@ -1,25 +1,43 @@
-// Takes a board as a string in the format
-// you see in the puzzle file. Returns
-// something representing a board after
-// your solver has tried to solve it.
-// How you represent your board is up to you!
+let boardString = '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--'
+
+function getArrayToString(puzzle) {
+  const board = []
+  for (let i = 0; i < 81; i += 9) {
+    board.push(puzzle.split('').slice(i, i + 9))
+  }
+  return board
+}
+console.table(getArrayToString(boardString))
+
+
+const value = getArrayToString(boardString);
+
+
+function isValid(board, row, col, k) {
+  for (let i = 0; i < 9; i += 1) {
+    if (board[row][i] === k || board[i][col] === k) {
+      return false
+    }   
+  }
+  return true
+}
+console.log(isValid(value))
+
 function solve(boardString) {
   console.log('first changes');
 }
 
-// Returns a boolean indicating whether
-// or not the provided board is solved.
-// The input board will be in whatever
-// form `solve` returns.
+
+
+
+
+
+
+
 function isSolved(board) {
 
 }
 
-// Takes in a board in some form and
-// returns a String that's well formatted
-// for output to the screen.
-// The input board will be in whatever
-// form `solve` returns.
 function prettyBoard(board) {
 
 }
