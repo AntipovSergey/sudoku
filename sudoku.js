cdead6b3db448eea6dead26d0ef7b64868212cac
Lelikk <Lelikkon@gmail.com>
2022-06-07 15:05:59 +0300

    isSolved end prettyBoard

--- sudoku.js
+++ sudoku.js
@@ -1,32 +1,28 @@
-// Takes a board as a string in the format
-// you see in the puzzle file. Returns
-// something representing a board after
-// your solver has tried to solve it.
-// How you represent your board is up to you!
+
 function solve(boardString) {
-  console.log('first changes');
+console.log('first changes');
+
 }
 
-// Returns a boolean indicating whether
-// or not the provided board is solved.
-// The input board will be in whatever
-// form `solve` returns.
 function isSolved(board) {
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      if (board[i][j] === 0) {
+        return false;
+      }
+    }
+  }
 
+  return true;
 }
 
-// Takes in a board in some form and
-// returns a String that's well formatted
-// for output to the screen.
-// The input board will be in whatever
-// form `solve` returns.
 function prettyBoard(board) {
-
+  return board.join('\n').split(',').join(' ');
 }
 
-// Exports all the functions to use them in another file.
 module.exports = {
   solve,
   isSolved,
   prettyBoard,
 };
+
