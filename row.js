441ccca22c8d6123d1ece7b42738d39e5c6f9c3f
U10 <uliaten10@gmail.com>
2022-06-10 16:02:56 +0300

    gor merge

--- row.js
+++ row.js
@@ -1,13 +1,13 @@
-const createBoard = require('./simBoard')
+const createBoard = require('./simBoard');
 
-function row(boord, num, i, j){
-        for (let k = 0; k < boord.length; k++){
-            if (+boord[i][k] == num && j !== k){
-                return false
-            }
-        }
-        return true
+function row(boord, num, i, j) {
+  for (let k = 0; k < boord.length; k++) {
+    if (+boord[i][k] == num && j !== k) {
+      return false;
+    }
+  }
+  return true;
 }
 // console.log(row(createBoard(), 4, 0, 1))
 
-module.exports = row
+module.exports = row;
--- spec/sudoku.spec.js
+++ spec/sudoku.spec.js
@@ -1,5 +1,8 @@
 const sudoku = require('../sudoku.js');
 const createBoard = require('../simBoard');
+const runner = require('../runner');
+const row = require('../row');
+const column = require('../column')
 
 describe('Проверка доски', () => {
   it('Доска - массив', () => {
@@ -7,8 +10,25 @@ describe('Проверка доски', () => {
   });
 });
 
+let board;
 beforeEach(() => {
-  const board = createBoard();
+  board = createBoard();
 });
 
-describe('Проверка по строке');
+describe('Проверка по горизонталь', () => {
+  it('Можно ли поставить 8? должен вернуть false', () => {
+    expect(row(board, 2, 0, 1)).toBe(false);
+  });
+  it('Можно ли поставить 2? должен вернуть false', () => {
+    expect(row(board, 2, 8, 8)).toBe(true);
+  });
+});
+
+describe('Проверка по вертикали', () => {
+    it('Можно ли поставить 2? должен вернуть false', () => {
+      expect(column(board, 2, 0, 1)).toBe(true);
+    });
+    it('Можно ли поставить 2? должен вернуть false', () => {
+      expect(column(board, 2, 2, 5)).toBe(false);
+    });
+  });
