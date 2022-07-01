2315553d5c1895af67aec8189a626f7662b74190
Elizaveta <veta.gubina@yandex.ru>
2022-07-01 13:35:47 +0300

    Validate

---
+++ validate.js
@@ -0,0 +1,30 @@
+function validate(num, pos, board) {
+  const [r, c] = pos;
+
+  for (let i = 0; i < size; i++) {
+    if (board[i][c] === num && i !== r) {
+      return false;
+    }
+  }
+
+  for (let i = 0; i < size; i++) {
+    if (board[r][i] === num && i !== c) {
+      return false;
+    }
+  }
+
+  const boxRow = Math.floor(r / boxSize) * boxSize;
+  const boxCol = Math.floor(c / boxSize) * boxSize;
+
+  for (let i = boxRow; i < boxRow + boxSize; i++) {
+    for (let j = boxCol; j < boxCol + boxSize; j++) {
+      if (board[i][j] === num && i !== r && j !== c) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
+
+module.exports = validate;
