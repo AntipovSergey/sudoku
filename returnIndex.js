0e0f5c1dc29f327b05913b16b57c1f23f0b7e9f7
Edouard <professionnel.baillot@gmail.com>
2022-07-11 14:17:04 +0200

    function returnIndex

---
+++ returnIndex.js
@@ -0,0 +1,12 @@
+const defolt = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+function returnIndex(arr) {
+	const newArr = []
+	for (let row = 0; row < arr.length; row++) {
+		for (let column = 0; column < arr[row].length; column++) {
+			if (arr[row][column] == '-') {
+				newArr.push(row, column)
+			}
+		}
+	}
+	return newArr
+}
