613d364e86fbc66cf5096c0e2355f57912fcfa8e
Ruslan <rrrruuus@yandex.ru>
2022-07-11 18:01:00 +0300

    1

--- sudoku.js
+++ sudoku.js
@@ -5,44 +5,49 @@
  */
 const fs = require('fs');
 
-function makeArr() {
-  const str = fs.readFileSync('./puzzles.txt', 'utf-8');
-  const puzzles = str
-  .split('\n')
-  .filter((line) => line !== '');
-  return puzzles
-}
-
-const sudStr = makeArr()
-console.log(sudStr)
-
-function randomNumber() {
-  if (process.argv[2]) {
-    return process.argv[2];
+function err(fileData) {
+  const str = fs.readFileSync(fileData, 'utf-8');
+  if (str.match(/[a-zA-Z]/g)) {
+    return 'error';
   }
-  return Math.floor(Math.random() * 4);
+  return str
 }
 
-const ranNum = randomNumber()
+console.log(err('./ERRpuzzles.txt'))
 
-function arr(str) {
-  return str.replace(/(.{9})/g, (match, n) => `${n}\n`)
-            .split('\n')
-            .filter((line) => line !== '')
-            .map((el) => el.split(''));
+function make() {
+  
+  const sudStr = makeArr();
+  function makeArr() {
+    const str = fs.readFileSync('./puzzles.txt', 'utf-8');
+    const puzzles = str
+    .split('\n')
+    .filter((line) => line !== '');
+    return puzzles
+  }
+
+  const ranNum = randomNumber();
+  function randomNumber() {
+    if (process.argv[2]) {
+      return process.argv[2];
+    }
+    return Math.floor(Math.random() * 4);
+  }
+
+  function arr(str) {
+    return str.replace(/(.{9})/g, (match, n) => `${n}\n`)
+              .split('\n')
+              .filter((line) => line !== '')
+              .map((el) => el.split(''));
+  
+  }
+  return arr(sudStr[ranNum])
 }
 
-const bString = arr(sudStr[randomNumber()])
 
 function solve(boardString) {
-  return boardString
+ 
 }
-
-console.log(solve(bString))
-
-console('hello')
-
-
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
@@ -68,28 +73,82 @@ function prettyBoard(board) {
   prettyBoard,
 };*/
 
-const arr1 = [1, 2, '-', '-', 5, '-', 6, 9, '-']
+let solveSudoku = function(board) {
+  const size = 9;
+  const boxSize = 3;
 
-
-function sud(arr) {
-  const res = [];  
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] !== '-') {
-      res.push(Number(arr[i]))
-    } else {
-       let ran = Math.ceil(Math.random() * (9 - 1) + 1)
-       if (arr.includes(ran)) {
-    return sud(res)
-  } else {
-    res.push(ran)
-  }      
-    }
+  const findEmpty = (board) => {
+      for (let r = 0; r < size; r++) {
+          for (let c = 0; c < size; c++) {
+              if(board[r][c] === '-') {
+                  return [r,c];
+              }
+          }
+      }
+      return null;
   }
-  return res
-}
+
+  const validate = (num, pos, board) => {
+      const [r,c] = pos;
+
+      //Check rows
+      for (let i = 0; i < size; i++) {
+          if (board[i][c] === num && i !== r) {
+              return false;
+          }
+      }
+
+      //Check cols
+      for (let i = 0; i < size; i++) {
+          if (board[r][i] === num && i !== c) {
+              return false;
+          }
+      }
 
 
-console.log(sud(arr1))
+      //Check box
+      const boxRow = Math.floor( r/boxSize ) * boxSize;
+      const boxCol = Math.floor( c/boxSize ) * boxSize;
 
+      for (let i = boxRow; i < boxRow + boxSize; i++) {
+          for (let j = boxCol; j < boxCol + boxSize; j++) {
+              if (board[i][j] === num && i !== r && j !== c) {
+                  return false;
+              }
+          }
+      }
 
+      return true;
+  }
 
+  const solve = () => {
+      const currPos = findEmpty(board);
+
+      if (currPos === null) {
+          return true;
+      }
+      //console.log('------------------------------');
+      for (let i = 1; i < size + 1; i++) {
+          const currNum = i.toString();
+          const isValid = validate(currNum, currPos, board);
+          //console.log('currPos ', currPos, 'currNum ',currNum, 'isValid ',isValid);
+          if (isValid) {
+              const [x,y] = currPos;
+              board[x][y] = currNum;
+
+              if(solve()) {
+                  return true;
+              }
+
+              board[x][y] = '-';
+          }
+      }
+
+      return false;
+  }
+
+  solve();
+  return board;
+};
+
+console.table(solveSudoku(make()))
