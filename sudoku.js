97b290e917935ede7d5b4801c9fcc17d21dd4830
Daria <deimoses04@gmal.com>
2022-07-11 15:08:29 +0300

    getArrayFromString

--- sudoku.js
+++ sudoku.js
@@ -1,3 +1,37 @@
+const fs = require('fs');
+ 
+function getPuzzle (){
+const puzzles = fs.readFileSync('./puzzles.txt', 'utf-8')
+   .split('\n')
+   .filter((line) => line !== '');
+ 
+ // Получить номер судоку из process.argv, либо взять 1-й судоку по умолчанию.
+ let puzzleNumber = Number(process.argv[2]) || 1;
+ if (puzzleNumber > puzzles.length) {
+   puzzleNumber = puzzles.length;
+ }
+ 
+ // Получить желаемый судоку по индексу и вывести его в консоль.
+ const puzzle = puzzles[puzzleNumber - 1];
+ 
+ console.log(`Решаем судоку №${puzzleNumber}:`);
+ //console.log(puzzle, '\n');
+ return puzzle
+}
+ 
+function getArrayFromString(string) {
+   let arrayFromString = string.split('');
+   let size = 9;
+    let subarray = [];
+    for (let i = 0; i < arrayFromString.length / size; i++) {
+     subarray[i] = arrayFromString.slice(i * size, i * size + size);
+   }
+  
+   return subarray;
+ }
+ 
+ console.log( getArrayFromString(getPuzzle()));
+
 /**
  * Принимает игровое поле в формате строки — как в файле sudoku-puzzles.txt.
  * Возвращает игровое поле после попытки его решить.
