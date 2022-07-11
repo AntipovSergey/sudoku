ba82d7c229fdeb8f3e69548c65e470eecdf2de32
Artem <artem.galchenko.88@gmail.com>
2022-07-11 14:47:45 +0300

    reliase0

--- sudoku.js
+++ sudoku.js
@@ -3,8 +3,16 @@
  * Возвращает игровое поле после попытки его решить.
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
-function solve(boardString) {
+ const sudokuStr= '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--'
+function solve(sudokuStr) {
+   const sudoku = sudokuStr.split('\n');
+
+  console.log(sudoku)
 }
+solve(sudokuStr)
+
+
+
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
@@ -22,6 +30,11 @@ function isSolved(board) {
 function prettyBoard(board) {
 
 }
+
+
+
+
+
 
 // Экспортировать функции для использования в другом файле (например, readAndSolve.js).
 module.exports = {
