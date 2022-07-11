c3e41c2b81eafcf34971258bb2f62807dda01637
Julia <sizova.julia.1990@gmail.com>
2022-07-11 11:58:14 +0300

    release 0,5

--- sudoku.js
+++ sudoku.js
@@ -3,7 +3,30 @@
  * Возвращает игровое поле после попытки его решить.
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
+
+const findEmpty = (boardStringToArray) => {
+  const size = 9;
+  for (let r = 0; r < size; r++) {
+    for (let c = 0; c < size; c++) {
+      if (boardStringToArray[r][c] === '-') {
+        console.log(c, r)
+        return [c, r];
+        
+      }
+    }
+  }
+  return null;
+};
+console.log(findEmpty('1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--'));
 function solve(boardString) {
+  const boardStringToArray = (boardString) => {
+    const board = boardString.split('');
+    const boardArray = [];
+    for (let i = 0; i < board.length; i += 9) {
+      boardArray.push(board.slice(i, i + 9));
+    }
+    return boardArray;
+  };
 }
 
 /**
@@ -11,7 +34,10 @@ function solve(boardString) {
  * Возвращает булевое значение — решено это игровое поле или нет.
  */
 function isSolved(board) {
-
+  const size = 9;
+  const boxSize = 3;
+  const boxRow = Math.floor( r/boxSize ) * boxSize;
+  const boxCol = Math.floor( c/boxSize ) * boxSize;
 }
 
 /**
