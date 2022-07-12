03bc5301f7829c60ba1185dc8c9731496ccb8013
IuriiSer <yusergeevi4@gmail.com>
2022-07-12 16:41:56 +0300

    done

--- button.js
+++ button.js
@@ -2,6 +2,8 @@
 const parentEl1 = document.querySelector('.sudocu__wrapper');
 const changeBtn = document.querySelector('.btn__sudocu_change');
 const solveBtn = document.querySelector('.btn__sudocu_solve');
+const stepBtn = document.querySelector('.btn__sudocu_step');
+const animBtn = document.querySelector('.btn__sudocu_anim');
 
 function createEl() {
   const str = '---------------------------------------------------------------------------------';
@@ -28,22 +30,49 @@ function updateEl(str) {
 }
 
 const addStr = document.querySelector('.sudocu__item');
-let textBordBefore;
+let textBordBefore = '---------------------------------------------------------------------------------';
+let textBordAfter = solve(textBordBefore);
 changeBtn.addEventListener('click', () => {
-  // const fileData = fs.readFileSync(
-  //     './puzzles.txt',
-  //     'utf-8'
-  // );
-
   const puzzles = text.split('\n').filter((line) => line !== '');
   const puzzleNumberRandom = Math.floor(Math.random() * 14);
-  // Получить желаемый судоку по индексу и вывести его в консоль.
   const puzzle = puzzles[puzzleNumberRandom];
   textBordBefore = puzzle;
-  console.log('~ textBordBefore', textBordBefore);
+  textBordAfter = solve(textBordBefore);
   updateEl(puzzle);
 });
 
 solveBtn.addEventListener('click', () => {
-  updateEl(solve(textBordBefore));
+  updateEl(textBordAfter);
+});
+
+stepBtn.addEventListener('click', () => {
+  const itemEl = document.getElementsByClassName('sudocu__item');
+  let finded = false;
+  let count = 1;
+  while (!finded && count < 300) {
+    const randmInd = Math.floor(Math.random() * 80);
+    if (itemEl[randmInd].innerText === '-') {
+      itemEl[randmInd].innerHTML = textBordAfter[randmInd];
+      finded = true;
+    }
+    count += 1;
+  }
+});
+animBtn.addEventListener('click', () => {
+  const itemEl = document.getElementsByClassName('sudocu__item');
+  for (let countInd = 0; countInd < 81; countInd += 1) {
+    // eslint-disable-next-line no-loop-func
+    setTimeout(() => {
+      let count = 1;
+      let finded = false;
+      while (!finded && count < 300) {
+        const randmInd = Math.floor(Math.random() * 80);
+        if (itemEl[randmInd].innerText === '-') {
+          itemEl[randmInd].innerHTML = textBordAfter[randmInd];
+          finded = true;
+        }
+        count += 1;
+      }
+    }, countInd * 200);
+  }
 });
--- sudoku.js
+++ sudoku.js
@@ -7,7 +7,6 @@
  */
 
 function solve(boardString) {
-  console.log('~ boardString', boardString);
   // eslint-disable-next-line max-len
   const cellMap = Array.from({ length: 9 }, (_, rowInd) => boardString.substring(rowInd * 9, (rowInd + 1) * 9));
   cellMap.forEach((row, rowInd) => {
@@ -16,13 +15,11 @@ function solve(boardString) {
   solvePuzzle(cellMap);
   return cellMap.flat().join('');
 }
-const test = '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--';
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает булевое значение — решено это игровое поле или нет.
  */
-
 function isSolved(board) {
   return !board.includes('-');
 }
@@ -32,9 +29,6 @@ function isSolved(board) {
  * Возвращает строку с игровым полем для последующего вывода в консоль.
  * Подумай, как симпатичнее сформировать эту строку.
  */
-
-// let str = '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--';
-
 function prettyBoard(board) {
   let newStr;
   for (let i = 0; i < 9; i++) {
