24e02b78a0c3884ea674c94cb32eb776304aa063
Edouard <professionnel.baillot@gmail.com>
2022-07-12 11:37:00 +0200

    Function of table

--- scriptWeb.js
+++ scriptWeb.js
@@ -1,16 +1,13 @@
+const buttonSubmit = document.querySelector('.btn-input');
+let sudokuValue = document.querySelector('.input');
 
+const initialArr = getArrayFromString(getPuzzle());
+// const arr = solve(getArrayFromString(getPuzzle()));
 
-console.log(solve(getArrayFromString(getPuzzle())));
-
-// const buttonSubmit = document.querySelector('.btn-input');
-// let sudokuValue = document.querySelector('.input');
-
-const arr = solve(getArrayFromString(getPuzzle()));
-
-/* buttonSubmit.addEventListener('click', function () {
+buttonSubmit.addEventListener('click', function () {
 	let tbl = document.querySelector('table');
 	for (let i = 0; i < tbl.rows.length; i++) {
 		for (let j = 0; j < tbl.rows[i].cells.length; j++)
-			tbl.rows[i].cells[j].innerText = arr[i][j];
+			tbl.rows[i].cells[j].innerText = initialArr[i][j];
 	}
-}) */
+})
