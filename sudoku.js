0ce1b7fb180daaa19ff9035dcb7609b00f1a7dc0
Kirill <kirill28102002@icloud.com>
2022-07-22 12:47:15 +0300

    Add sudoku

--- sudoku.js
+++ sudoku.js
@@ -19,7 +19,8 @@ function isSolved(board) {
  * Возвращает строку с игровым полем для последующего вывода в консоль.
  * Подумай, как симпатичнее сформировать эту строку.
  */
-function prettyBoard(board) {
+function prettyBoard(sudoku) {
+
 
 }
 
---
+++ test.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+let str = fs.readFileSync('./puzzles.txt', 'utf8');
+let str1 = ''
+let arr =[]
+let sudoku = []
+for (let i = 0; i < 81; i ++) {
+    str1 += str[i]
+}
+str = str1.split('')
+for (let j = 1; j <= 81; j += 1) {
+    arr.push(str[j - 1])
+    if((j % 9 === 0) && (j !== 0)) {
+        sudoku.push(arr)
+        arr = []
+    }
+}
+console.log(str);
+console.log(sudoku);
