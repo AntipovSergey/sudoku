c98d36cd43ff3dd574097891a5a6e53c53826c9a
Dmitri <dmitri.kosunovski@gmail.com>
2022-07-22 13:04:44 +0300

    more cheker in solved

--- sudoku.js
+++ sudoku.js
@@ -4,27 +4,6 @@
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
 function solve(boardString) {
-    const currPos = findEmpty(board);
-
-    if (currPos === null) {
-        return true;
-    }
-    for (let i = 1; i < size + 1; i++) {
-        const currNum = i.toString();
-        const isValid = validate(currNum, currPos, board);
-        if (isValid) {
-            const [x,y] = currPos;
-            board[x][y] = currNum;
-
-            if(solve()) {
-                return true;
-            }
-
-            board[x][y] = '.';
-        }
-    }
-
-    return false;
 }
 
 /**
@@ -32,7 +11,58 @@ function solve(boardString) {
  * Возвращает булевое значение — решено это игровое поле или нет.
  */
 function isSolved(board) {
+  const size = 9;
+  const boardSize = 3;
 
+  const findEmpty = (board) => {
+    for (let i = 0; i < size; i++) {
+      for (let j = 0; j < size; j++) {
+        if ([i][j] === '.') {
+          return [i][j]
+        }
+      }
+    }
+  }
+
+  const validate = (num, pos, board) => {
+    const [r,c] = pos;
+
+    for (let i = 0; i < size; i++) {
+        if (board[i][c] === num && i !== r) {
+            return false;
+        }
+    }
+    for (let i = 0; i < size; i++) {
+        if (board[r][i] === num && i !== c) {
+            return false;
+        }
+    }
+    
+    const boxRow = Math.floor( r/boxSize ) * boxSize;
+    const boxCol = Math.floor( c/boxSize ) * boxSize;
+
+    for (let i = boxRow; i < boxRow + boxSize; i++) {
+        for (let j = boxCol; j < boxCol + boxSize; j++) {
+            if (board[i][j] === num && i !== r && j !== c) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+  const solve = () => {
+    const currenPos = findEmpty(board)
+    if (currenPos === null) {
+      return true;
+    }
+    for(let k = 1; k < size + 1; k++){
+      const currenNum = k.toString()
+    }
+  }
+  solve()
+  return board;
 }
 
 /**
