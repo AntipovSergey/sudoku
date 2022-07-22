7a988f40eb6c4d75229d9e512ab3f492b48bbbe6
AynurMv <aynurmotigulov@gmail.com>
2022-07-22 13:08:54 +0300

    1

--- sudoku.js
+++ sudoku.js
@@ -4,7 +4,48 @@
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
 function solve(boardString) {
+  let splittedBoardString = boardString.split('')
+  let rows = [];
+  let counter = 0;
+  for (let i = 1; i <= 9; i += 1) {
+    let row = [];
+    for (let j = 1; j <= 9; j += 1) {
+      row.push(splittedBoardString[counter])
+      counter++;
+    }
+    rows.push(row);
+  }
+
+  let columns = [];
+  let tableArr = [];
+
+  for (let j = 0; j < rows.length; j += 1) {
+      let column = [];
+      for (let i = 0; i < rows[j].length; i += 1) {
+          column.push(rows[i][j])
+      }
+      columns.push(column);
+  }
+
+  let boxes = [];
+
+  let box = [];
+  for (let i = 0; i < 3; i += 1) {
+    let BoxRow = [];
+    let BoxRowCount = 0;
+    for (let j = 0; j < 3; j += 1) {
+      BoxRow.push(rows[BoxRowCount*3 + j][i*3 + i])
+    }
+    box.push(BoxRow)
+    BoxRowCount = BoxRowCount + 1;
+  }
+  console.log(boxes)
+
 }
+
+
+solve('1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--')
+
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
