99cc2ea77acae266866f73f1f13e61ebd6b4856e
Kirill <kirill28102002@icloud.com>
2022-07-22 14:45:34 +0300

    function examination created

--- test-Kirill.js
+++ test-Kirill.js
@@ -1,7 +1,20 @@
 function examination(sudoku) {
-    for (let x = 0; x < puzzle.length; x++){
-        for (let y = 0; y < puzzle.length; y++){
-            
+    for (let x = 0; x < sudoku.length; x++){
+        for (let y = 0; y < sudoku.length; y++){
+            if(sudoku[i][j] === 0) {
+                let gridNums = артем(x, y)
+                let crossVals = максим(x, y)  //let crossValues = (x, y) => {return getX(x).concat(getY(y)).filter(a => a > 0).sort((a, b) => { return a - b })}
+                let potentialNumbers = максим(gridNums, crossVals); //подбор цифр
+                if (potentialNumbers === 1){
+                    sudoku[i][j] = potentialNumbers[0]
+                }
+            }
         }
     }
+    for (let index = 0; index < sudoku.length; index++) {
+        if (sudoku[index].includes(0) == true) {
+            return examination(sudoku)
+        }  
+    }=
+    // return puzzle; в конце основной функции
 }
