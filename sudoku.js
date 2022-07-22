8d57eed27c5beb4818a416e0668a7a34d483bfde
Filipp <philipp3gs@gmail.com>
2022-07-22 14:25:49 +0300

    Insert sudoku.js

--- sudoku.js
+++ sudoku.js
@@ -1,9 +1,92 @@
+let solveSudoku = function(board) {
+  const size = 9;
+  const boxSize = 3;
+
+  const findEmpty = (board) => {
+      for (let r = 0; r < size; r++) {
+          for (let c = 0; c < size; c++) {
+              if(board[r][c] === '.') {
+                  return [r,c];
+              }
+          }
+      }
+      return null;
+  }
+
+  const validate = (num, pos, board) => {
+      const [r,c] = pos;
+
+      //Check rows
+      for (let i = 0; i < size; i++) {
+          if (board[i][c] === num && i !== r) {
+              return false;
+          }
+      }
+
+      //Check cols
+      for (let i = 0; i < size; i++) {
+          if (board[r][i] === num && i !== c) {
+              return false;
+          }
+      }
+
+
+      //Check box
+      const boxRow = Math.floor( r/boxSize ) * boxSize;
+      const boxCol = Math.floor( c/boxSize ) * boxSize;
+
+      for (let i = boxRow; i < boxRow + boxSize; i++) {
+          for (let j = boxCol; j < boxCol + boxSize; j++) {
+              if (board[i][j] === num && i !== r && j !== c) {
+                  return false;
+              }
+          }
+      }
+
+      return true;
+  }
+
+  const solve = () => {
+      const currPos = findEmpty(board);
+
+      if (currPos === null) {
+          return true;
+      }
+      //console.log('------------------------------');
+      for (let i = 1; i < size + 1; i++) {
+          const currNum = i.toString();
+          const isValid = validate(currNum, currPos, board);
+          //console.log('currPos ', currPos, 'currNum ',currNum, 'isValid ',isValid);
+          if (isValid) {
+              const [x,y] = currPos;
+              board[x][y] = currNum;
+
+              if(solve()) {
+                  return true;
+              }
+
+              board[x][y] = '.';
+          }
+      }
+
+      return false;
+  }
+
+  solve();
+  return board;
+};
+
+console.table(input);
+console.table(solveSudoku(input));
+
+
+
 /**
  * Принимает игровое поле в формате строки — как в файле sudoku-puzzles.txt.
  * Возвращает игровое поле после попытки его решить.
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
-function solve(boardString) {
+ function solve(boardString) {
 }
 
 /**
@@ -29,3 +112,4 @@ module.exports = {
   isSolved,
   prettyBoard,
 };
+
