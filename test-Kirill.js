d7bd0cc0716b070f22c4122458b4bfe884eb286e
Kirill <kirill28102002@icloud.com>
2022-07-22 13:00:07 +0300

    Add sudoku upgrade

---
+++ test-Kirill.js
@@ -0,0 +1,7 @@
+function examination(sudoku) {
+    for (let x = 0; x < puzzle.length; x++){
+        for (let y = 0; y < puzzle.length; y++){
+            
+        }
+    }
+}
--- test.js
+++ test.js
@@ -6,9 +6,10 @@ let sudoku = []
 for (let i = 0; i < 81; i ++) {
     str1 += str[i]
 }
-str = str1.split('')
+
+str = str1.replaceAll('-', 0).split('')
 for (let j = 1; j <= 81; j += 1) {
-    arr.push(str[j - 1])
+    arr.push(Number(str[j - 1]))
     if((j % 9 === 0) && (j !== 0)) {
         sudoku.push(arr)
         arr = []
