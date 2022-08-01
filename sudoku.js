60c9e3444a4ba1d03aaed914527392a75cccd4ec
Andrew <yau7237@gmail.com>
2022-08-01 18:05:06 +0500

    solve func

--- sudoku.js
+++ sudoku.js
@@ -3,25 +3,31 @@
  * Возвращает игровое поле после попытки его решить.
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
+
 function solve(boardString) {
+  let strArr = boardString.split('');
+  let size = 9;
+  let resSud = [];
+  for (let i = 0; i < strArr.length; i += size) {
+    resSud.push(strArr.slice(i, i + size));
+  }
+  return resSud;
 }
+
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает булевое значение — решено это игровое поле или нет.
  */
-function isSolved(board) {
-
-}
+function isSolved(board) {}
+// console.log(isSolved())
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает строку с игровым полем для последующего вывода в консоль.
  * Подумай, как симпатичнее сформировать эту строку.
  */
-function prettyBoard(board) {
-
-}
+function prettyBoard(board) {}
 
 // Экспортировать функции для использования в другом файле (например, readAndSolve.js).
 module.exports = {
