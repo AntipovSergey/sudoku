e8f1892585127cd81f43118dd433ed348ad24e45
Emilia <ursus.m@icloud.com>
2022-08-02 09:04:15 +0300

    0

--- sudoku.js
+++ sudoku.js
@@ -3,25 +3,52 @@
  * Возвращает игровое поле после попытки его решить.
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
-function solve(boardString) {
+function reStr (boardString){
   let strArr = boardString.split('');
   let size = 9;
-  let resSud =[];
+  let BoardArr =[];
   for(let i=0;i < strArr.length; i+=size){
-    resSud.push(strArr.slice(i,i+size))
+    BoardArr.push(strArr.slice(i,i+size))
   }
-  console.log(resSud);
-  
-  return resSud;
-
+  return BoardArr;
 }
+function solve(BoardArr) {
+  
+  console.log(BoardArr);
+  for (let i=0; i< 9; i++){
+    for(let j=0; j< 9; j++){
+      if (BoardArr[i][j]=== '-'){
+        for( let a =1; a<=9; a++){
+          if(isSolved(BoardArr,i,j,a)){
+            BoardArr[i][j] = a;
+            if(solve(BoardArr)){
+              return true;
+            }else{
+              BoardArr[i][j] = '-';
+            }
+          }
+        }
+return false;
+      }
+    }
+  }
+  return true;
+}
+
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает булевое значение — решено это игровое поле или нет.
  */
-function isSolved(board) {
-
+function isSolved(board,ro,co,a) {
+for (let i=0;i < 9;i++){
+let x = 3* Math.floor(ro / 3) + Math.floor(i / 3);
+let y = 3 * Math.floor(co / 3) + i % 3;
+if (board[ro][i] == a || board[i][co] == a || board[x][y] == a){
+  return false;
+}
+}
+return true;
 }
 
 /**
@@ -35,6 +62,7 @@ function prettyBoard(board) {
 
 // Экспортировать функции для использования в другом файле (например, readAndSolve.js).
 module.exports = {
+  reStr,
   solve,
   isSolved,
   prettyBoard,
