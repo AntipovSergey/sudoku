9fce1b44f45478500b992464b1be556ebdbe9f2e
Anastasia Melnikova <nastasia.savina@gmail.com>
2022-08-02 08:36:05 +0300

    findEmptySpot --> done

--- sudoku.js
+++ sudoku.js
@@ -4,13 +4,44 @@
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
 function solve(boardString) {
-}
+  let board = [];
+  let arr = boardString.split('');
+   for (let i = 0; i < arr.length; i++){
+    board.push(arr.splice(0,9))
+    }
+    console.log('=====>',board)
+    //findEmpty//
+    const size = 9;
+    // const boxSize = 3;
+     for (let r = 0; r < size; r += 1) {
+      for (let c = 0; c < size; c += 1) {
+        
+        if (board[r][c] === '-') {
+          console.log('=======>', [r,c])  
+          return [r,c]; 
+        }
+        
+      }
+      return null;
+    }
+    
+    let [r,c] = pos;
+    //check rows
 
+    //  return board
+  }
+  
+  
+
+ 
+
+  
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает булевое значение — решено это игровое поле или нет.
  */
 function isSolved(board) {
+  
 
 }
 
