aa9672ac30ec7fcff03d9afc508bcb902b8d86a7
Kirgon <zinzo131@gmail.com>
2022-08-12 14:56:06 +0300

    change run

--- run.js
+++ run.js
@@ -1,34 +1,32 @@
-// const {
-//   rowColumnParser,
-// } = require('./rowColumnParser');
+const {
+  rowColumnParser,
+} = require('./rowColumnParser');
 
-// const {
-//   squareParser,
-// } = require('./squareParser');
+const {
+  squareParser,
+} = require('./squareParser');
 
 function runSudoku(massSudoku) {
-  let isEnd = true;
-  while (isEnd) {
+  const start = new Date().getTime();
+  while (1) {
     let massEmpties = [];
     for (let i = 0, k = 0; i < massSudoku.length; i++) {
       for (let j = 0; j < massSudoku[i].length; j++) {
-        if (!(massSudoku[i][j] >= '0' && massSudoku[i][j] <= '9')) {
+        if (massSudoku[i][j] === '-') {
           massEmpties[k] = [i, j];
-          k++;
+          k += 1;
         }
       }
     }
-    if (massEmpties.length < 1) {
-      isEnd = false;
-      continue;
-    }
-    break;
-    // for (let i = 0; i < massEmpties.length; i++) {
-    //   massSudoku = rowColumnParser(massEmpties[i], massSudoku);
-    //   massSudoku = squareParser(massEmpties[i], massSudoku);
-    // }
+    if (massEmpties.length < 1)
+      break;
+    for (let i = 0; i < massEmpties.length; i++)
+      massSudoku = squareParser(massEmpties[i], massSudoku, rowColumnParser(massEmpties[i], massSudoku));
+    const end = new Date().getTime(); 
+    if (end - start >= 15)
+      return { mass: massSudoku, fl: 0 };
   }
-  return ({mass:massSudoku, fl:1});
+  return { mass: massSudoku, fl: 1 };
 }
 
 module.exports = { runSudoku };
