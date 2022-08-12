fb3c482e583f2549fff8842d4f23ebdbc2c75ba5
keruha <kirpan1994@mail.ru>
2022-08-12 16:25:16 +0300

    probably fixed

--- rowColumnParser.js
+++ rowColumnParser.js
@@ -1,17 +1,13 @@
 function rowColumnParser(index, puzzle) {      // index[0] - i , index[1] - j
     const candidates = ['1','2','3','4','5','6','7','8','9']
-    for (let i = index[0]; i < puzzle.length; i++) {
-        if (puzzle[i].includes(candidates[i])) {
-            candidates.splice(puzzle[i].indexOf(candidates[i]), 1)
-            puzzle[i].splice(puzzle[i], 1, candidates[i])
-         }
+    for (let i = index[0]; i < 9; i++) {
+      if (candidates.indexOf(puzzle[index[0]][i]) !== -1)
+      candidates.splice(candidates.indexOf(puzzle[index[0]][i]), 1)
         }
 
-    for (let j = index[1]; j < puzzle.length; j++) {
-        if (puzzle[j][index[1]].includes(candidates[j])) {
-            candidates.splice(puzzle[j].indexOf(candidates[j]), 1)
-            puzzle[j][index[1]].splice(puzzle[j], 1, candidates[j])
-         }
+    for (let j = index[1]; j < 9; j++) {
+      if (candidates.indexOf(puzzle[j][index[1]]) !== -1)
+      candidates.splice(candidates.indexOf(puzzle[j][index[1]]), 1)
     }
 
     if (candidates.length > 1) { 
