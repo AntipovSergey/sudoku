9786d7d4057fb78d3ca5acee9b0165f3ad2e97bc
Yaroslava <yaroslavamakedonskaya@yandex.ru>
2022-08-12 15:30:17 +0300

    smth fixed

--- rowCol.js
+++ rowCol.js
@@ -1,24 +1,16 @@
-
-// строки и колонки 
+// строки и колонки
 function valide(number, position, board) {
-    let i = 0;
-    while (i < size) {
-        if (board[i][column] === number && i !== row) {
-            return false
-        }
-        i++
+  let i = 0;
+  while (i < size) {
+    if (board[i][column] === number && i !== row) {
+      return false;
     }
-    
-    for (let i = 0; i < size; i++) { 
-        if (board[row][i] === num && i !== row) {
-            return false
-        }
+    i++;
+  }
+
+  for (let i = 0; i < size; i++) {
+    if (board[row][i] === num && i !== row) {
+      return false;
     }
+  }
 }
-
-
-
-
-
-
-
