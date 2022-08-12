ca808b626cb1c208d4eca7a8bb7c87324be14f9f
Kirgon <zinzo131@gmail.com>
2022-08-12 12:34:28 +0300

    table for all sudoku

--- reader.js
+++ reader.js
@@ -7,8 +7,11 @@ const puzzles = fs.readFileSync('./puzzles.txt','utf-8')
 
 function reader(puzzles){
     let arr = puzzles.split('\n')
-    let bord = arr[0].split('')
-    const res = bord.reduce((acc, val, i) => {
+    // let bord = arr[0].split('')
+    let res
+    for (let i = 0; i < 15; i++){
+        let bord = arr[i].split('')
+        res = bord.reduce((acc, val, i) => {
         const tmp = Math.floor(i/9)
         if (!Array.isArray(acc[tmp])){
             acc[tmp] = [val]
@@ -16,11 +19,12 @@ function reader(puzzles){
             acc[tmp].push(val)
         }
         return acc
-    }, [])
-        
-        
-    
+        }, [])
+        console.table(res)
+    }
+   
     return res
 }
 
-console.table (reader(puzzles));
+// console.table (reader(puzzles));
+reader(puzzles)
