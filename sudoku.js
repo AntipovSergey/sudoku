67f1587559d0a1bce15f2ce0c70b2b5ffb478990
DanilaTru <dan.trurk@gmail.com>
2022-08-29 17:11:24 +0300

    delete all

--- sudoku.js
+++ sudoku.js
@@ -1,3 +1,11 @@
+// в solve(board) -
+// 1. найти пустую ячейку 
+// 2. подставить число и провести проверку
+// 3. вернуть решеный борд
+
+//isSolved(board, который вернулся из ф-ции solve)
+
+
 /**
  * Принимает игровое поле в формате строки — как в файле sudoku-puzzles.txt.
  * Возвращает игровое поле после попытки его решить.
@@ -8,29 +16,10 @@ function solve(boardString) {
   let sudokuBaza = fs.readFileSync('./puzzles.txt','utf-8');
   sudokuBaza = sudokuBaza.split('\n')
   const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-  
-  sudokuBaza.forEach(sudoku => {
-    let strings = []
-    let columns = []
-    for (let index = 0; index < sudoku.length; index += 9) {
-      let myColumns = ''
-      let myString = strings.push(sudoku.slice(index, index +9));
-      for (let i = 0; i < sudoku.length; i += 9) {
-        myColumns += sudoku[i]
-      }
-      columns.push(myColumns)
-    }
-    console.log(columns);
-    
-    numbers.forEach(num => {
-      // console.log(num);
-    });
-    
-  });
-  
-  
+
+
+  // console.log(solve());
 }
-console.log(solve());
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
@@ -46,8 +35,16 @@ function isSolved(board) {
  * Подумай, как симпатичнее сформировать эту строку.
  */
 function prettyBoard(board) {
-
+  let sudokuBoard = [];
+  let i = 0;
+  while(i < board.length) {
+    const row = board.substring(i, i + 9);
+    sudokuBoard.push(Array.from(row));
+    i += 9;
+  }
+  return sudokuBoard;
 }
+// [[0,1,2,3,4,5,6,7,8], [9,10,11,12,13,14,15,16,17]]
 
 // Экспортировать функции для использования в другом файле (например, readAndSolve.js).
 module.exports = {
