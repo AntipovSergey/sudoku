0e601b9f16dd827609ade3eef3c9a8aa0e6ef7d6
LeonidVE <ermachkovleonid@gmail.com>
2022-09-02 12:48:51 +0300

    isSovedPlusPretty

--- sudoku.js
+++ sudoku.js
@@ -4,15 +4,17 @@
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
 function solve(boardString) {
-  
+
 }
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает булевое значение — решено это игровое поле или нет.
  */
-function isSolved(board) {
 
+function isSolved(board) {
+  const puzzle = JSON.stringify(board);
+  if (puzzle.includes('-')) return false;
 }
 
 /**
@@ -21,7 +23,13 @@ function isSolved(board) {
  * Подумай, как симпатичнее сформировать эту строку.
  */
 function prettyBoard(board) {
-
+  const f = [];
+  for (let n = 0; n < board.length; n += 1) {
+    f.push(board[n].join(' '));
+  }
+  const newBoard = f.join('\n');
+  const trimmedNewBoard = newBoard.trim();
+  return trimmedNewBoard;
 }
 
 // Экспортировать функции для использования в другом файле (например, readAndSolve.js).
