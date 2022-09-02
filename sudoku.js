c786f6be1db8c1076c74aec3efc06f3e0e0ae026
Edward <musicalmadness@bk.ru>
2022-09-02 16:31:08 +0300

    prettyBoard

--- sudoku.js
+++ sudoku.js
@@ -4,14 +4,21 @@
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
 function solve(boardString) {
+   const arrBoardString = boardString.split('');
+   const puzzle = [];
+   for (let i = 0; i < boardString.length; i += 9) {
+    puzzle.push(arrBoardString.slice(i, i + 9));
+  }
+  return puzzle;
 }
+
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает булевое значение — решено это игровое поле или нет.
  */
 function isSolved(board) {
-
+  
 }
 
 /**
@@ -19,8 +26,14 @@ function isSolved(board) {
  * Возвращает строку с игровым полем для последующего вывода в консоль.
  * Подумай, как симпатичнее сформировать эту строку.
  */
-function prettyBoard(board) {
-
+ function prettyBoard(board) {
+  const stringReplace = board.toString().replace(/,/g, ' ');
+    const regex = /\d{1}\s\d{1}\s\d{1}\s\d{1}\s\d{1}\s\d{1}\s\d{1}\s\d{1}\s\d{1}/g;
+    const matchStr = stringReplace.match(regex);
+    const res = matchStr.map((el) => (el.slice(0, 6) + ' ' + el.slice(6, 12) +  ' ' + el.slice(12)));
+    res.splice(3, 0, ' '.repeat(19));
+    res.splice(7, 0, ' '.repeat(19));
+    return res.join('\n');
 }
 
 // Экспортировать функции для использования в другом файле (например, readAndSolve.js).
