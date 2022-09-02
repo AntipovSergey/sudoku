c430370b5803207062386641c8303203fd2a9ac2
Kaitag7 <tashbekoff@mail.ru>
2022-09-02 16:10:48 +0300

    release

--- sudoku.js
+++ sudoku.js
@@ -5,12 +5,12 @@
  */
 const board = '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--';
 
-function checkRow(arr) {
+function checkRow(arr, position1, position2) {
   let num = 0;
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = position1; i < arr.length; i++) {
     for (let j = 1; j <= 9; j++) {
       if (!arr[i].includes(j.toString())) {
-        for (let k = 0; k < arr.length; k += 1) {
+        for (let k = position2; k < arr.length; k += 1) {
           const array = [];
           for (let n = 0; n < arr[k].length; n += 1) {
             array.push(arr[n][k]);
@@ -25,13 +25,13 @@ function checkRow(arr) {
   return num;
 }
 
-function solve(boardString) {
+function solve(board) {
   const arr = [];
   let count = 0;
   for (let i = 0; i < 9; i++) {
     const arr2 = [];
     for (let j = 0; j < 9; j++) {
-      arr2.push(boardString[count]);
+      arr2.push(board[count]);
       count += 1;
     }
     arr.push(arr2);
@@ -40,7 +40,9 @@ function solve(boardString) {
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr[i].length; j++) {
       if (arr[i][j] === '-') {
-        arr[i][j] = checkRow(arr).toString();
+        const position1 = i;
+        const position2 = j;
+        arr[i][j] = checkRow(arr, position1, position2).toString();
       }
     }
   }
