ccd3174e585a359b98709a79140146d40d5f05a3
Daniil Pylenok <danila.pwork@mail.ru>
2022-09-03 22:57:24 +0300

    Mickro revor function solve

--- sudoku.js
+++ sudoku.js
@@ -1,60 +1,42 @@
 const fs = require('fs');
-/**
- * Принимает игровое поле в формате строки — как в файле sudoku-puzzles.txt.
- * Возвращает игровое поле после попытки его решить.
- * Договорись со своей командой, в каком формате возвращать этот результат.
- */
 
-function solve(boardString) {
-
+// eslint-disable-next-line no-use-before-define
+function solve(board) {
+  const cols = board.length;
+  const boarded = board;
+  // поиск пустой ячейки
+  // eslint-disable-next-line no-use-before-define
+  findEmpty(0, 0);
+  function findEmpty(r, c) {
+    for (let row = r; r < cols; row += 1) {
+      for (let col = c; c < cols; col += 1) {
+        if (board[row][col] === '-') {
+          for (let k = 1; k < 10; k += 1) {
+            // eslint-disable-next-line no-use-before-define
+            if (checkNum(row, col, k, boarded) === true) {
+              findEmpty(row, col);
+            }
+            // eslint-disable-next-line no-use-before-define
+            if (row === 9 && col === 9) boarded[row][col] = k.toString();
+          }
+        }
+      }
+    }
+    return false;
+  }
 }
 
-/**
- * Принимает игровое поле в том формате, в котором его вернули из функции solve.
- * Возвращает булевое значение — решено это игровое поле или нет.
- */
-// function isSolved(board) {
-//   const size = 9;
-//   const blockSize = 3;
-
-//   // поиск первой пустой клетки
-
-//   const emptyPosition (board) => {
-//     for (let x = 0; x < size; x++) {
-//       for (let y = 0; y < size; y++) {
-//         if (board[y][x] !== '-') {
-//           return [y, x]
-//         }
-//         else return null
-//       }
-//     }
-//   }
-
-//   // проверка строк и столбцов
-//   const isValid (number, x, y) => {
-//     for (let i = 0; i < size; i++) {
-//       board[i][x] == number && board[i][x] !== x
-//       return false
-//     }
-//     for (let i = 0; i < size; i++) {
-//       board[y][i] == number && board[y][i] !== y
-//       return false
-//     }
-//   }
-// }
-
-/**
- * Принимает игровое поле в том формате, в котором его вернули из функции solve.
- * Возвращает строку с игровым полем для последующего вывода в консоль.
- * Подумай, как симпатичнее сформировать эту строку.
- */
-
-// Экспортировать функции для использования в другом файле (например, readAndSolve.js).
-// module.exports = {
-//   solve,
-//   isSolved,
-//   prettyBoard,
-// };
+// Сравнение переданной цифры в колонке строке и блоке
+function checkNum(r, c, k, board) {
+  const row = Math.floor((r / 3) * 3);
+  const col = Math.floor((c / 3) * 3) - 1;
+  for (let a = 0; a <= 3; a += 1) {
+    for (let b = 0; b <= 3; b += 1) {
+      if (board[row + a][col + b] === k.toString()) return false;
+    }
+  }
+  return true;
+}
 
 function conversed() {
   const arr = fs.readFileSync(`${__dirname}/puzzles.txt`, 'utf-8').split('\n');
@@ -62,7 +44,6 @@ function conversed() {
     // eslint-disable-next-line no-use-before-define
     arr[i] = reversed(arr[i]);
   }
-  arr.map((el) => console.table(el));
   return arr;
   function reversed(str) {
     const arrN = [];
@@ -75,4 +56,15 @@ function conversed() {
   }
 }
 
-conversed();
+const borderArray = conversed();
+console.log(borderArray.map((el) => solve(el)));
+
+// function prettyBoard(board) {
+//   const stringReplace = board.toString().replace(/,/g, ' ');
+//   const regex = /\d{1}\s\d{1}\s\d{1}\s\d{1}\s\d{1}\s\d{1}\s\d{1}\s\d{1}\s\d{1}/g;
+//   const matchStr = stringReplace.match(regex);
+//   const res = matchStr.map((el) => (`${el.slice(0, 6)} ${el.slice(6, 12)} ${el.slice(12)}`));
+//   res.splice(3, 0, ' '.repeat(19));
+//   res.splice(7, 0, ' '.repeat(19));
+//   return res.join('\n');
+// }
