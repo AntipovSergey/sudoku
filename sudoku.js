896a2973046e0777bddcca1d14f92cad1e65af65
Simon Mishchenko <symon261731@gmail.com>
2022-09-23 11:19:51 +0300

    function solve done

--- sudoku.js
+++ sudoku.js
@@ -1,9 +1,21 @@
+const fs = require('fs');
+
+const sudoku = fs.readFile('./puzzles.txt', 'utf-8').split('\n');
 /**
  * Принимает игровое поле в формате строки — как в файле sudoku-puzzles.txt.
  * Возвращает игровое поле после попытки его решить.
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
 function solve(boardString) {
+  const res = [];
+  for (let i = 0; i < boardString.length; i += 9) {
+    const elem = [];
+    for (let k = i; k < i + 9; k += 1) {
+      elem.push(boardString[k]);
+    }
+    res.push(elem);
+  }
+  return res;
 }
 
 /**
