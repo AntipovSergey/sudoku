cb2c3e7133d64be35e8dd1ebf71596a330bd350a
Alexandra <lubnina.alexandra@gmail.com>
2022-09-23 12:48:18 +0300

    Fix exersices

--- sudoku.js
+++ sudoku.js
@@ -6,7 +6,6 @@ const prettyBoardFromNewFile = require('./NewFiles/prettyBoard');
  * Принимает игровое поле в формате строки — как в файле sudoku-puzzles.txt.
  * Возвращает игровое поле после попытки его решить.
  * Договорись со своей командой, в каком формате возвращать этот результат.
- *
  */
 function solve(solveFromNewFile) {
   return solveFromNewFile;
