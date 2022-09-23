004814cbbd4e834b5cba06989985307c2d9b1fa5
Sarkis <sarkis_strelok@mail.ru>
2022-09-23 14:44:16 +0300

    create getting board for start

---
+++ testing.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+const str = fs.readFileSync('puzzles.txt', 'utf-8');
+
+console.log(str);
+function letBoard(n) {
+  const newstr = str.split('\n')[n].split('');
+  const count = 9;
+  const newArr = [];
+  for (let i = 0; i < newstr.length; i += 9) {
+    newArr.push(newstr.slice(i, i + count));
+  }
+  return newArr;
+}
+console.table(letBoard(3));
