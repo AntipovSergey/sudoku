eb5dc5f882b6f6a4e6f5aa91a4da4556c18035e6
Ko.Kko <ko.kko@mrm.ru>
2022-09-23 12:50:16 +0300

    readFile + transform 50%

--- readAndSolve.js
+++ readAndSolve.js
@@ -1,46 +1,80 @@
 // Подключить функции из файла sudoku.js.
+const fs = require('fs');
 const sudoku = require('./sudoku');
 
 function readAndSolve(error, fileData) {
   // Если чтение файла не удалось, выбросить ошибку с описанием проблемы и
   // завершить работу функции.
-  if (error) {
-    throw error;
-  }
+  // fs.readFile('./puzzles.txt', 'utf8', (err, data) => {
+  //   if (err) throw err;
+  //   data.split('\n').filter((line) => line !== '');
+  //   const box = data.split('\n').
+  //   map((el)=> el.split('').map((el)=> ({
+  //     val: el == '-'? 0: Number(el),
 
-  // Разбить содержимое файла построчно и отфильтровать все пустые строки.
-  const puzzles = fileData
-    .split('\n')
-    .filter((line) => line !== '');
+  //   })))
+  //   console.log(box[0]);
+  fs.readFile('./puzzles.txt', 'utf8', (err, data) => {
+    if (err) throw err;
+    data.split('\n')
+      .filter((line) => line !== '');
 
-  // Получить номер судоку из process.argv, либо взять 1-й судоку по умолчанию.
-  let puzzleNumber = Number(process.argv[2]) || 1;
+    const box = data.split('\n')
+      .map((el) => el.split('')
+        .map((el) => (el == '-' ? 0 : Number(el))));
+    console.log(box[5]);
 
-  // Ограничить номер судоку максимальным числом массива с паззлами.
-  if (puzzleNumber > puzzles.length) {
-    puzzleNumber = puzzles.length;
-  }
+    box.map((el) =>{
+     if(el === 0){
+      
+     }
+    }
+    )
 
-  // Получить желаемый судоку по индексу и вывести его в консоль.
-  const puzzle = puzzles[puzzleNumber - 1];
-  console.log(`Решаем судоку №${puzzleNumber}:`);
-  console.log(puzzle, '\n');
+    // const sudoky = box.map((el) => el.split('\n'))
 
-  // Использовать функцию solve из файла sudoku.js для решения судоку.
-  const solvedPuzzle = sudoku.solve(puzzle);
+    // console.log(sudoky);
 
-  // Использовать функцию isSolved из файла sudoku.js для проверки решения судоку.
-  if (!sudoku.isSolved(solvedPuzzle)) {
-    console.log(`Не смогли решить судоку №${puzzleNumber} :(`, '\n');
-    return; // Если судоку не решён, завершить работу этой функции.
-  }
+    // sudoky.forEach(el => {
+    // console.log('---', el.map(string => +string))
+    // el.map(string => +string)
 
-  // Код ниже сработает, только если проверка решения судоку прошла успешно.
-  console.log(`Судоку №${puzzleNumber} решён успешно!`);
+    // });
+  });
 
-  // Использовать функцию prettyBoard из файла sudoku.js для форматирования
-  // игрового поля в строку в желаемом формате.
-  console.log(sudoku.prettyBoard(solvedPuzzle), '\n');
-}
+  // // Разбить содержимое файла построчно и отфильтровать все пустые строки.
+  // const puzzles = fileData
+  //   .split('\n')
+  //   .filter((line) => line !== '');
 
+  // // Получить номер судоку из process.argv, либо взять 1-й судоку по умолчанию.
+  // let puzzleNumber = Number(process.argv[2]) || 1;
+
+  // // Ограничить номер судоку максимальным числом массива с паззлами.
+  // if (puzzleNumber > puzzles.length) {
+  //   puzzleNumber = puzzles.length;
+  // }
+
+  // // Получить желаемый судоку по индексу и вывести его в консоль.
+  // const puzzle = puzzles[puzzleNumber - 1];
+  // console.log(`Решаем судоку №${puzzleNumber}:`);
+  // console.log(puzzle, '\n');
+
+  // // Использовать функцию solve из файла sudoku.js для решения судоку.
+  // const solvedPuzzle = sudoku.solve(puzzle);
+
+  // // Использовать функцию isSolved из файла sudoku.js для проверки решения судоку.
+  // if (!sudoku.isSolved(solvedPuzzle)) {
+  //   console.log(`Не смогли решить судоку №${puzzleNumber} :(`, '\n');
+  //   return; // Если судоку не решён, завершить работу этой функции.
+  // }
+
+  // // Код ниже сработает, только если проверка решения судоку прошла успешно.
+  // console.log(`Судоку №${puzzleNumber} решён успешно!`);
+
+  // // Использовать функцию prettyBoard из файла sudoku.js для форматирования
+  // // игрового поля в строку в желаемом формате.
+  // console.log(sudoku.prettyBoard(solvedPuzzle), '\n');
+}// закрытие функции
+readAndSolve();
 module.exports = readAndSolve;
