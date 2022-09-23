d6090b0604b939b5fb868b2d01850fd0ef75117e
LizaSP <elizavetasolodovnik12@mail.ru>
2022-09-23 10:14:33 +0300

    initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,16 @@
 module.exports = {
   env: {
+    browser: true,
     commonjs: true,
     es2021: true,
     node: true,
   },
-  extends: [
-    'airbnb-base',
+  extends: 'airbnb-base',
+  overrides: [
   ],
   parserOptions: {
     ecmaVersion: 'latest',
   },
   rules: {
-    'no-console': 0,
   },
 };
