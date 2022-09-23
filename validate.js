1a9b0ae7d7187032ad8e7d75dcad62a9001845f9
Evgenii K <e.kaminskii1990@gmail.com>
2022-09-23 13:06:18 +0300

    update validate

--- validate.js
+++ validate.js
@@ -1,27 +1,88 @@
+let pos = [8, 0]
+
+
+let board = [
+  [
+    '1', '-', '5',
+    '8', '-', '2',
+    '-', '-', '-'
+  ],
+  [
+    '-', '9', '-',
+    '-', '7', '6',
+    '4', '-', '5'
+  ],
+  [
+    '2', '-', '-',
+    '4', '-', '-',
+    '8', '1', '9'
+  ],
+  [
+    '-', '1', '9',
+    '-', '-', '7',
+    '3', '-', '6'
+  ],
+  [
+    '7', '6', '2',
+    '-', '8', '3',
+    '-', '9', '-'
+  ],
+  [
+    '-', '-', '-',
+    '-', '6', '1',
+    '-', '5', '-'
+  ],
+  [
+    '-', '-', '7',
+    '6', '-', '-',
+    '-', '3', '-'
+  ],
+  [
+    '4', '3', '-',
+    '-', '2', '-',
+    '5', '-', '1'
+  ],
+  [
+    '6', '-', '-',
+    '3', '-', '8',
+    '9', '-', '-'
+  ]
+]
+
+let num = 7;
+
+
 function validate (num, pos, board) {
   let [r,c] = pos
+  let boxSize = 3
+
 
       // проверка по горизонтали
-      const checkHorizontal = !(board[r].join('').includes(String(num))) 
+      let checkHorizontal = board[r].join('').includes(String(num)) 
+      if (!(checkHorizontal) === false) return false
+
 
       // проверка по вертикали
       let lineVertical = ''
       for(let t = 0; t < 9; t++){
-        lineVertical += board[t][c]
+        lineVertical += board[r][t]
       }
-      const checkVertical = !(lineVertical.includes(String(num)))
+      let checkVertical = lineVertical.includes(String(num))
+      if (!(checkVertical) === false) return false
 
       //проверка в квадрате
       let lineBox = ''
-      const boxRow = Math.floor(r/3) * 3
-      const boxCol = Math.floor(c/3) * 3
-      for(let j = boxRow; j < boxRow + 3; j++) {
-        for (let t = boxCol; t < boxCol + 3; t++) {         
+      const boxRow = Math.floor(r/boxSize) * boxSize
+      const boxCol = Math.floor(c/boxSize) * boxSize
+      for(let j = boxRow; j < boxRow + boxSize; j++) {
+        for (let t = boxCol; t < boxCol + boxSize; t++) {         
           lineBox += board[j][t]
         }
       }
-      const checkBox = !(lineBox.includes(String(num)))
+      let checkBox = lineBox.includes(String(num))
+      if (!(checkBox) === false) return false
 
-  if (checkHorizontal || checkVertical || checkBox) return false
-  if (checkHorizontal && checkVertical && checkBox) return true
+      return true
 }
+
+console.log(validate(num, pos, board))
