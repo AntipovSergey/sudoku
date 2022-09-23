27a0ae9a23927a59c735a4f96daaa6bc85b5f437
Nikolay <nikolaysnow77@gmail.com>
2022-09-23 18:36:47 +0300

    read sudoku. ver 2

--- read-sudoku.js
+++ read-sudoku.js
@@ -1,35 +1,28 @@
 const fs = require('fs');
 
-function readMap (){
-  let box;
-  fs.readFile('./puzzles.txt', 'utf8', (err, data) => {
-    if (err) throw err;
-    data.split('\n').filter((line) => line !== '');
-    box = data.split('\n')
-    // console.log(box);
-    // console.log(typeof box[0]);//
-     return box;
-    
-  }); return box
+function readMap() {
+  const file = fs.readFileSync('./puzzles.txt', 'utf8');
+  const tr = file.split('\n').filter((line) => line !== '');
+  // console.log(tr);
+  return tr;
 }
-  console.log(readMap());
-//   const arrayString = readMap();
-  
-//     function numSud (){
-//       let box = arrayString;
-//       console.log(box);
-//       let num = [];
-//       for (let j =0; j < box.length; j++){
-//       for (let i = 0; i <box[j].length; i++) {
-//         if (parceint(box[j][i]) >=1 && parceint(box[j][i]) <=9){
-// num.push[box[j][i]]
-//         }
-//         else {
-//           num.push[0]
-//         }
-//         return num
-//       }
-//     }
-//   }
-//   console.log(numSud(readMap()))
-// // console.log(numSud(readMap()))
+// console.log(readMap());
+// const read = readMap();
+function numSud(arr) {
+  const num = [];
+  console.log(arr);
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      if (arr[i][j] === '-') {
+        num.push(0);
+      } else if (Number(arr[i][j]) >= 1 && Number(arr[i][j]) <= 9) {
+        num.push(arr[i][j]);
+      }
+    }
+  }
+  console.log(num);
+  return num;
+}
+
+console.log(numSud(readMap()));
+// console.log(numSud(readMap()))
