d87500e7679ed3f370e1d8c7c695a843288cab96
Vladislav <vlad.devidson.harle@gmail.com>
2022-10-03 11:24:45 +0300

    1

--- sudoku.js
+++ sudoku.js
@@ -3,8 +3,18 @@
  * Возвращает игровое поле после попытки его решить.
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
+let boardString = '-8--2-----4-5--32--2-3-9-466---9---4---64-5-1134-5-7--36---4--24-723-6-----7--45-'
+
 function solve(boardString) {
+  let result = []
+  let resSplit = boardString.split('')
+   for (let i = 0; i < resSplit.length; i = i + 9) {
+      result.push(resSplit.slice(i, i+9))
+    
+   }
+return result
 }
+console.log(solve(boardString))
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
