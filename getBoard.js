34afc621df80f9335378abde2695afd1f3a39020
Nikita Kardopolov <reverdj@yandex.ru>
2022-10-14 13:46:57 +0300

    Mini Game Menu created

--- getBoard.js
+++ getBoard.js
@@ -1,15 +1,21 @@
 const { log } = require('console');
 const fs = require('fs');
 
+function gameMenu() {
+  console.log('**********************************');
+  const warn1 = console.warn('Please, choose a board.\nEnter number (from "1" to "14")')
+  return warn1;
+}
+
 function getBoards(sudokuNumber) {
   const readTxt = fs.readFileSync('./puzzles.txt', 'utf-8');
   const strings = readTxt.split('\n');
   strings.pop();
   const getArrays = strings.map((el) => el.split(''));
-  
-
-
+  if (process.argv[2] > 14) {
+    gameMenu();
+    return '**********************************';
+  }
   return getArrays[sudokuNumber];
-  
 }
-console.log(getBoards(process.argv[2]));
+console.log(getBoards(process.argv[2] || 0));
