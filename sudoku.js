73e9b1005239d986084f911e183335f4c83228ab
Aleksandr Proshkov <all9100@mail.ru>
2022-10-14 13:23:48 +0300

    func checker

--- sudoku.js
+++ sudoku.js
@@ -4,21 +4,27 @@ const fs = () => {
 };
 
 // Функция прохода горизонталь и вертикаль (Саша)
-const checkerRowColumn = () => {
-
+const checkerRowColumn = (arr, func) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; i < arr[i].length; j++) {
+      for (let g = 0; g < arr[i][j].length; g++) {
+        if (arr[i][j] === '-') arr[i][j].replace(func);
+      }
+    }
+  }
 };
 
 // Функция прохода коробки 3х3
 
 const checkerBox = () => {
 
-}
+};
 
 // Функция, которая проверяет можно ли точно подставить числа
 
 const currentNums = () => {
 
-}
+};
 
 module.exports = {
 };
