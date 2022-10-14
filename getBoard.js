5b005f8142042d23bfe35aecd5a09e387af1290e
Nikita Kardopolov <reverdj@yandex.ru>
2022-10-14 11:23:39 +0300

    Create First Game Board

--- getBoard.js
+++ getBoard.js
@@ -2,9 +2,16 @@ const { log } = require('console');
 const fs = require('fs');
 
 function getBoards() {
-  const readtxt = fs.readFileSync('./puzzles.txt', 'utf-8');
-  return readtxt;
+  const readTxt = fs.readFileSync('./puzzles.txt', 'utf-8');
+  const firstBoard = readTxt.slice(0, 81).split('');
+  // To Get Main Board?
+  const sizeOfArray = 9;
+  const readyFirstBoard = [];
+  for (let i = 0; i < Math.ceil(firstBoard.length/sizeOfArray); i++) {
+    readyFirstBoard[i] = firstBoard.slice((i * sizeOfArray), (i * sizeOfArray) + sizeOfArray);
+  }
+  return readyFirstBoard;
 }
 
-console.log(getBoards());
+console.table(getBoards());
 
