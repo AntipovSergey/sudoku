e2298cdf0c3f5f961dbc1604021fbd48dd29a31c
Kirill <kirill@192.168.0.4>
2022-10-24 16:15:26 +0300

    function strok

--- sudoku.js
+++ sudoku.js
@@ -4,29 +4,26 @@
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
 function solve(boardString) {
-  const perm = '1582159212746';
-  const base = '123456789';
-  const uniqe = '3';
+  const perm = "1582159212746";
+  const base = "123456789";
+  const uniqe = "3";
   const result = [];
   for (let i = 0; i < boardString.length; i += 9) {
-    result.push(boardString.slice(i, i + 9).split(''));
+    result.push(boardString.slice(i, i + 9).split(""));
   }
-
- 
 }
 
+function table(board) {}
 
-function table(board) {
-
+function stroke(board, height) {
+  let result = "";
+  for (let i = 0; i < 9; i++) {
+    result += board[height][i];
+  }
+  return result;
 }
 
-function stroke(board) {
-
-}
-
-function column(board) {
-
-} 
+function column(board) {}
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает булевое значение — решено это игровое поле или нет.
@@ -34,11 +31,12 @@ function column(board) {
 function isSolved(board) {
   const result = [];
   for (let i = 0; i < board.length; i += 9) {
-    result.push(board.slice(i, i + 9).split(''));
+    result.push(board.slice(i, i + 9).split(""));
   }
   for (let i = 0; i < result.length; i++) {
-    if (result[i].indexOf('-') !== -1) return false;
-  } return true;
+    if (result[i].indexOf("-") !== -1) return false;
+  }
+  return true;
 }
 
 /**
@@ -46,11 +44,13 @@ function isSolved(board) {
  * Возвращает строку с игровым полем для последующего вывода в консоль.
  * Подумай, как симпатичнее сформировать эту строку.
  */
-function prettyBoard(board) {
+function prettyBoard(board) {}
 
-}
-
-console.table(isSolved('1258726789922764152024118192191273267625834912145611512-7612331431221511612318912'));
+console.log(
+  solve(
+    "1258726789922764152024118192191273267625834912145611512-7612331431221511612318912"
+  )
+);
 
 // Экспортировать функции для использования в другом файле (например, readAndSolve.js).
 module.exports = {
