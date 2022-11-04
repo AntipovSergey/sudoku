58162b50407339e164ae53a2807ad0ae4afd6614
ZakharovaYana <z.kharova.ya@gmail.com>
2022-11-04 14:02:04 +0300

    SearchSquareIsDone

--- square.js
+++ square.js
@@ -1,41 +1,10 @@
-const array = [
-  [
-    '1', '-', '5', '8', '-', '2', '-', '-', '-',
-  ],
-  [
-    '-', '9', '-', '-', '7', '6', '4', '-', '5',
-  ],
-  [
-    '2', '-', '-', '4', '-', '-', '8', '1', '9',
-  ],
-  [
-    '-', '1', '9', '-', '-', '7', '3', '-', '6',
-  ],
-  [
-    '7', '6', '2', '-', '8', '3', '-', '9', '-',
-  ],
-  [
-    '-', '-', '-', '-', '6', '1', '-', '5', '-',
-  ],
-  [
-    '-', '-', '7', '6', '-', '-', '-', '3', '-',
-  ],
-  [
-    '4', '3', '-', '-', '2', '-', '5', '-', '1',
-  ],
-  [
-    '6', '-', '-', '3', '-', '8', '9', '-', '-',
-  ],
-];
-console.table(array);
-
-function squareValidation(board, row, col, element) {
+function squareSolution(board, row, col, num) {
   for (let i = 0; i < 9; i++) {
     const squareRow = 3 * Math.floor(row / 3) + Math.floor(i / 3);
     const squareColumn = 3 * Math.floor(col / 3) + (i % 3);
-    if (board[squareRow][squareColumn] === element) return false;
+    if (board[squareRow][squareColumn] === num) return false;
   }
   return true;
 }
 
-console.log(squareValidation(array, 1, 1, '3'));
+module.exports = { squareSolution };
