d5eafabc96a2c07846bc157048a26b0debb21a7e
thorenDK <alexelkin17@yandex.ru>
2022-11-04 12:50:29 +0300

    firstone

--- sudoku.js
+++ sudoku.js
@@ -1,9 +1,30 @@
+const fs = require('fs');
 /**
  * Принимает игровое поле в формате строки — как в файле sudoku-puzzles.txt.
  * Возвращает игровое поле после попытки его решить.
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
+const read = fs.readFileSync('./puzzles.txt', 'utf-8');
+const slice =read.slice(0,81);
+// function boardString() {
+//   return read.slice(0, 82)
+// }
 function solve(boardString) {
+  const table = [];
+  const split = boardString.split('');
+  for (let i = 9; i < split.length; i+=10) {
+    split.splice(i, 0, ',');
+  }
+  return split
+  // for (let i = 0; i < boardString.length; i++) {
+  //   const row = [];
+  //   for (let j = 0; j < 9; j+=10) {
+  //     row.push(boardString[j]);
+  //   }
+  //   table.push(row);
+  //   boardString.slice()
+  // }
+  // return table
 }
 
 /**
