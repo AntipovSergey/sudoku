f85ca3b2db336b51b96bc070fae1f56be3186b9f
Ivan Kolpakov <vanjaklp95@gmail.com>
2022-11-21 18:10:57 +0300

    sudoku solve part

--- sudoku.js
+++ sudoku.js
@@ -4,24 +4,36 @@
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
 
-let sudokuLine = '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--';
 
-function solver() {
-  let res = [];
-  for (let i = 0; i < sudokuLine.length; i += 9) {
-    res.push(sudokuLine.slice(i, i + 9).split(''));
-  }
-  return res;
-}
-console.log(solver());
-console.log(solver()[0][2]);
+    
 
-// console.log(solve(boardString) )
+  const solve = () => {
+    const currPos = findEmpty(board);
+    if (currPos === null) {
+      return true;
+    }
+    for (let i = 1; i < boxLength + 1; i++) {
+      const currNum = i.toString();
+      const isValid = validate(currNum, currPos, board);
+      console.log('currPos=', currPos, 'currNum=', currNum, 'isValid=', isValid);
+      if (isValid) {
+        const [x, y] = currPos;
+        board[x][y] = currNum;
 
- //  ТЕСТ!!!!!
-  
-  
+        if (solve()) {
+          return true;
+        }
+        board[x][y] = '-';
+      }
+    }
+    return false;
+  };
+  solve();
+  return board;
+};
 
+console.table(solver());
+console.table(sudokuSolver(solver()));
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
