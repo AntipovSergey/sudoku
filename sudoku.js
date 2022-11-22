3dc535ccb63a69490d5118b03bbd46da6304d294
Ivan Kolpakov <vanjaklp95@gmail.com>
2022-11-22 16:32:52 +0300

    release validator done

--- sudoku.js
+++ sudoku.js
@@ -4,14 +4,12 @@
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
 
-
 function sudokuToArr(line) {
   const res = [];
   for (let i = 0; i < line.length; i += 9) {
     res.push(line.slice(i, i + 9).split(''));
-
   }
-  console.table(res)
+  console.table(res);
   return res;
 }
 function solve(str) {
@@ -23,7 +21,7 @@ function solve(str) {
     for (let row = 0; row < 9; row++) {
       for (let column = 0; column < 9; column++) {
         if (board[row][column] === '-') {
-          console.log([row, column]);
+          console.log('position',[row, column]);
           return [row, column];
         }
       }
@@ -82,7 +80,6 @@ function solve(str) {
   };
   numsFinder();
 
-  
   return sudokuChois;
 }
 
@@ -95,14 +92,156 @@ function solve(str) {
  */
 
 function isSolved(board) {
-
+  // Проверка строк
   for (let i = 0; i < board.length; i++) {
-    if (board[i].indexOf('-') === -1) return true;
+    for (let j = 0; j < board[i].length - 1; j++) {
+      for (let k = j + 1; k < board[i].length; k++) {
+        if (board[i][j] === board[i][k] || Number(board[i][j]) > 9 || Number(board[i][j]) < 1) {
+          return false;
+        }
+      }
+    }
   }
-  return false;
+  // Проверка столбцов
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length - 1; j++) {
+      for (let k = j + 1; k < board[i].length; k++) {
+        if (board[j][i] === board[k][i]) {
+          return false;
+        }
+      }
+    }
+  }
+  // Проверка квадратов
 
+  const arr1 = [];
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      arr1.push(board[i][j]);
+    }
+  }
+  for (let i = 0; i < arr1.length - 1; i++) {
+    for (let j = i + 1; j < arr1.length; j++) {
+      if (arr1[i] === arr1[j]) {
+        return false;
+      }
+    }
+  }
+  const arr2 = [];
+  for (let i = 3; i < 6; i++) {
+    for (let j = 0; j < 3; j++) {
+      arr2.push(board[i][j]);
+    }
+  }
+  for (let i = 0; i < arr2.length - 1; i++) {
+    for (let j = i + 1; j < arr2.length; j++) {
+      if (arr2[i] === arr2[j]) {
+        return false;
+      }
+    }
+  }
+  const arr3 = [];
+  for (let i = 6; i < 9; i++) {
+    for (let j = 0; j < 3; j++) {
+      arr3.push(board[i][j]);
+    }
+  }
+  for (let i = 0; i < arr3.length - 1; i++) {
+    for (let j = i + 1; j < arr3.length; j++) {
+      if (arr3[i] === arr3[j]) {
+        return false;
+      }
+    }
+  }
+  const arr4 = [];
+  for (let i = 0; i < 3; i++) {
+    for (let j = 3; j < 6; j++) {
+      arr4.push(board[i][j]);
+    }
+  }
+  for (let i = 0; i < arr4.length - 1; i++) {
+    for (let j = i + 1; j < arr4.length; j++) {
+      if (arr4[i] === arr4[j]) {
+        return false;
+      }
+    }
+  }
+  const arr5 = [];
+  for (let i = 3; i < 6; i++) {
+    for (let j = 3; j < 6; j++) {
+      arr5.push(board[i][j]);
+    }
+  }
+  for (let i = 0; i < arr5.length - 1; i++) {
+    for (let j = i + 1; j < arr5.length; j++) {
+      if (arr5[i] === arr5[j]) {
+        return false;
+      }
+    }
+  }
+  const arr6 = [];
+  for (let i = 6; i < 9; i++) {
+    for (let j = 3; j < 6; j++) {
+      arr6.push(board[i][j]);
+    }
+  }
+  for (let i = 0; i < arr6.length - 1; i++) {
+    for (let j = i + 1; j < arr6.length; j++) {
+      if (arr6[i] === arr6[j]) {
+        return false;
+      }
+    }
+  }
+  const arr7 = [];
+  for (let i = 0; i < 3; i++) {
+    for (let j = 6; j < 9; j++) {
+      arr7.push(board[i][j]);
+    }
+  }
+  for (let i = 0; i < arr7.length - 1; i++) {
+    for (let j = i + 1; j < arr7.length; j++) {
+      if (arr7[i] === arr7[j]) {
+        return false;
+      }
+    }
+  }
+  const arr8 = [];
+  for (let i = 3; i < 6; i++) {
+    for (let j = 6; j < 9; j++) {
+      arr8.push(board[i][j]);
+    }
+  }
+  for (let i = 0; i < arr8.length - 1; i++) {
+    for (let j = i + 1; j < arr8.length; j++) {
+      if (arr8[i] === arr8[j]) {
+        return false;
+      }
+    }
+  }
+  const arr9 = [];
+  for (let i = 6; i < 9; i++) {
+    for (let j = 6; j < 9; j++) {
+      arr9.push(board[i][j]);
+    }
+  }
+  for (let i = 0; i < arr9.length - 1; i++) {
+    for (let j = i + 1; j < arr9.length; j++) {
+      if (arr9[i] === arr9[j]) {
+        return false;
+      }
+    }
+  }
+  for (let i = 0; i < board.length; i++) {
+    if (board[i].indexOf('-') !== -1) return false;
+  }
+  for (let i = 0; i < board.length; i++) {
+    if (board[i] < 0 && board[i] > 9) {
+      return false;
+    }
+  }
+
+  return true;
 }
- 
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
@@ -110,16 +249,14 @@ function isSolved(board) {
  * Подумай, как симпатичнее сформировать эту строку.
  */
 
-
 function prettyBoard(board) {
   let stringPretty = '';
-  for(let i = 0; i < board.length; i++){
-  stringPretty = stringPretty + board[i].join(' ') + '\n';
+  for (let i = 0; i < board.length; i++) {
+    stringPretty = `${stringPretty + board[i].join(' ')}\n`;
   }
-  return '\n' + stringPretty
-
+  return `\n${stringPretty}`;
 }
-  
+
 // Экспортировать функции для использования в другом файле (например, readAndSolve.js).
 module.exports = {
   solve,
