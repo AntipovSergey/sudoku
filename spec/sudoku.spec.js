b61af013a32d8a701977c8d3868d7e82677ce432
Zarina <zromanovva@gmail.com>
2022-11-25 16:17:11 +0300

    sss

--- spec/sudoku.spec.js
+++ spec/sudoku.spec.js
@@ -1,2 +1,8 @@
-const sudoku = require('../sudoku.js')
+/* eslint-disable no-undef */
+const sudoku = require("../sudoku.js");
 
+describe('Basic tests', () => {
+  it('Does the function return an array?', () => {
+    expect(typeof solve(board)).toEqual('array');
+  });
+});
--- sudoku.js
+++ sudoku.js
@@ -7,106 +7,111 @@ const fs = require('fs');
 
 const txt = fs.readFileSync('./puzzles.txt', 'utf8');
 
-
-const boardString = '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--'
-
 function solve(boardString) {
-  let newBoard = boardString.split('')
-  let board = [];
-  for(let i = 0; i < boardString.length; i += 9) {
-    board.push(newBoard.slice(i, i + 9))
+  const newBoard = boardString.split('');
+  const board = [];
+  for (let i = 0; i < boardString.length; i += 9) {
+    board.push(newBoard.slice(i, i + 9));
   }
 
-const findEmpty = (board) => {
-  for (let r = 0; r < 9; r++) {
+  // for (let i = 0; i < board.length; i++) {
+  //   for (let j = 0; j <= 9 + 3; j++) {
+  //     let num = j + 1;
+  //     board[i][j].includes(num.toString()) ? board[i][j] = num + 1 : j;
+  //   }
+  // }
+
+  const findEmpty = (board) => {
+    for (let r = 0; r < 9; r++) {
       for (let c = 0; c < 9; c++) {
-          if(board[r][c] === '-') {
-              return [r,c];
-          }
+        if (board[r][c] === '-') {
+          return [r, c];
+        }
       }
-  }
-  return null;
-}
+    }
+    return null;
+  };
 
-const validate = (num, position, board) => {
-  const [r,c] = position;
+  const validate = (num, position, board) => {
+    const [r, c] = position;
 
-  for (let i = 0; i < 9; i++) {
+    for (let i = 0; i < 9; i++) {
       if (board[i][c] === num && i !== r) {
-          return false;
+        return false;
       }
-  }
+    }
 
-  //Check cols
-  for (let i = 0; i < 9; i++) {
+    // Check cols
+    for (let i = 0; i < 9; i++) {
       if (board[r][i] === num && i !== c) {
-          return false;
+        return false;
       }
-  }
+    }
 
+    // Check box
+    const boxRow = Math.floor(r / 3) * 3;
+    const boxCol = Math.floor(c / 3) * 3;
 
-  //Check box
-  const boxRow = Math.floor( r/3 ) * 3;
-  const boxCol = Math.floor( c/3 ) * 3;
-
-  for (let i = boxRow; i < boxRow + 3; i++) {
+    for (let i = boxRow; i < boxRow + 3; i++) {
       for (let j = boxCol; j < boxCol + 3; j++) {
-          if (board[i][j] === num && i !== r && j !== c) {
-              return false;
-          }
+        if (board[i][j] === num && i !== r && j !== c) {
+          return false;
+        }
       }
-  }
+    }
 
-  return true;
-}
+    return true;
+  };
 
-const solve = () => {
-  const currPos = findEmpty(board);
+  const solveSudo = () => {
+    const currPos = findEmpty(board);
 
-  if (currPos === null) {
+    if (currPos === null) {
       return true;
-  }
+    }
 
-  for (let i = 1; i < 9 + 1; i++) {
+    for (let i = 1; i < 9 + 1; i++) {
       const currNum = i.toString();
       const isValid = validate(currNum, currPos, board);
 
       if (isValid) {
-          const [x,y] = currPos;
-          board[x][y] = currNum;
+        const [x, y] = currPos;
+        board[x][y] = currNum;
 
-          if(solve()) {
-              return true;
-          }
+        if (solveSudo()) {
+          return true;
+        }
 
-          board[x][y] = '-';
+        board[x][y] = '-';
       }
-  }
+    }
 
-  return false;
+    return false;
+  };
+
+  solveSudo();
+  return console.table(board);
 }
-
-solve();
-return console.table(board);
-};
-
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает булевое значение — решено это игровое поле или нет.
  */
-function isSolved(board) {
 
-}
+function isSolved(board) {}
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает строку с игровым полем для последующего вывода в консоль.
  * Подумай, как симпатичнее сформировать эту строку.
  */
+
+
 function prettyBoard(board) {
-    return board.map((el)=>el).join('\n')
+  return board.map((el) => el.join(' ')).join('\n');
 }
+
+// console.log(prettyBoard(board))
 
 // Экспортировать функции для использования в другом файле (например, readAndSolve.js).
 module.exports = {
