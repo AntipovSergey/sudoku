4b45358609a4d9e375a51dffd4fb66efcd6158e4
Zarina <zromanovva@gmail.com>
2022-11-25 14:53:18 +0300

    Solve

--- sudoku.js
+++ sudoku.js
@@ -7,6 +7,7 @@ const fs = require('fs');
 
 const txt = fs.readFileSync('./puzzles.txt', 'utf8');
 
+
 const boardString = '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--'
 
 function solve(boardString) {
@@ -16,46 +17,78 @@ function solve(boardString) {
     board.push(newBoard.slice(i, i + 9))
   }
 
-
-
-  for(let i = 0; i < board.length; i++) {
-    for (let j = 0; j <= 9; j ++) {
-      if(board[i][j] === '-') {
-        let num = i.toString()
-        board[i] .includes(num) ? num = i + 1 : i
-            board[i][j] = num
-        }
-    }
+const findEmpty = (board) => {
+  for (let r = 0; r < 9; r++) {
+      for (let c = 0; c < 9; c++) {
+          if(board[r][c] === '-') {
+              return [r,c];
+          }
+      }
   }
-return board
+  return null;
 }
 
-console.table(solve(boardString))
+const validate = (num, position, board) => {
+  const [r,c] = position;
+
+  for (let i = 0; i < 9; i++) {
+      if (board[i][c] === num && i !== r) {
+          return false;
+      }
+  }
+
+  //Check cols
+  for (let i = 0; i < 9; i++) {
+      if (board[r][i] === num && i !== c) {
+          return false;
+      }
+  }
 
 
+  //Check box
+  const boxRow = Math.floor( r/3 ) * 3;
+  const boxCol = Math.floor( c/3 ) * 3;
 
+  for (let i = boxRow; i < boxRow + 3; i++) {
+      for (let j = boxCol; j < boxCol + 3; j++) {
+          if (board[i][j] === num && i !== r && j !== c) {
+              return false;
+          }
+      }
+  }
 
+  return true;
+}
 
+const solve = () => {
+  const currPos = findEmpty(board);
 
+  if (currPos === null) {
+      return true;
+  }
 
+  for (let i = 1; i < 9 + 1; i++) {
+      const currNum = i.toString();
+      const isValid = validate(currNum, currPos, board);
 
+      if (isValid) {
+          const [x,y] = currPos;
+          board[x][y] = currNum;
 
+          if(solve()) {
+              return true;
+          }
 
+          board[x][y] = '-';
+      }
+  }
 
+  return false;
+}
 
-// [
-//   [[135] [842] [---]] [[135] [842] [---]] [[135] [842] [---]]
-
-// [
-// [39--764-5]
-// [2--4--819]
-// -19--73-6
-// 762-83-9-
-// ----61-5-
-// --76---3- 
-// 43--2-5-1
-// 6--3-89--]
-// ]
+solve();
+return console.table(board);
+};
 
 
 /**
