ae3ccb85b4754c9aa2c7e359d99a3c90e34b9e63
boonany <slavpolichev@gmail.com>
2023-01-09 05:56:08 -0800

    tests created

--- spec/sudoku.spec.js
+++ spec/sudoku.spec.js
@@ -1,2 +1,21 @@
 const sudoku = require('../sudoku.js')
+const fs = require('fs')
 
+describe(' Судоку ', () => {
+  let rightSudoku;
+  let line;
+  let row;
+
+  beforEach(() => {
+    rightSudoku = getColumns();
+    
+  });
+
+  describe(' Проверка строки ', () => {
+    expect(rightSudoku[0].length === [...new Set(rightSudoku[0])].length).toBe(true);
+  });
+
+  describe(' Проверка столбцов', () => {
+
+  })
+})
