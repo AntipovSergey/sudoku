8e3fc72211e5421198e32e376476cd906553316d
Aleksei <alexius.xz@gmail.com>
2023-01-09 16:52:58 +0300

    squares

--- sudoku.js
+++ sudoku.js
@@ -3,6 +3,28 @@
  * Возвращает игровое поле после попытки его решить.
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
+const fs = require('fs');
+
+const box = fs.readFileSync('./puzzles.txt', 'utf8').split('\n');
+
+function getSquares(str1) {
+  const arr = [[], [], [], [], [], [], [], [], []];
+  if (process.argv[2] === undefined) {
+    const str1 = box[0].match(/.{9}/g).map((el) => el.split(''));
+  } else {
+    const str1 = box[process.argv[2]].match(/.{9}/g).map((el) => el.split(''));
+  }
+  for (let i = 0; i < 9; i += 3) {
+    arr[i].push(str1[i][0], str1[i][1], str1[i][2], str1[i + 1][0], str1[i + 1][1], str1[i + 1][2], str1[i + 2][0], str1[i + 2][1], str1[i + 2][2]);
+    arr[i + 1].push(str1[i][3], str1[i][4], str1[i][5], str1[i + 1][3], str1[i + 1][4], str1[i + 1][5], str1[i + 2][3], str1[i + 2][4], str1[i + 2][5]);
+    arr[i + 2].push(str1[i][6], str1[i][7], str1[i][8], str1[i + 1][6], str1[i + 1][7], str1[i + 1][8], str1[i + 2][6], str1[i + 2][7], str1[i + 2][8]);
+  }
+  // const obj = {};
+  // for (let i = 0; i < arr.length; i += 1) {
+  //   obj[i + 1] = arr[i];
+  // }
+  return arr;
+}
 function solve(boardString) {
 
 }
