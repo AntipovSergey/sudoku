e37b731b4b2eebd635216676f84b76b6d7fe9186
Semochka <semochkaelse@gmail.com>
2023-01-09 17:21:45 +0300

    getColumns

--- sudoku.js
+++ sudoku.js
@@ -4,6 +4,32 @@ const box = fs.readFileSync('puzzles.txt', 'utf-8').split('\n');
 let board;
 
 
+function getColumns(board) {
+  const col = [];
+  let mincol = [];
+  const columns = {};
+  if (process.argv[2] === undefined) {
+    board = box[0].match(/.{9}/g).map((el) => el.split(''));
+  } else {
+    board = box[process.argv[2]].match(/.{9}/g).map((el) => el.split(''));
+  }
+  for (let i = 0; i < board.length; i++) {
+    mincol = [];
+    for (let j = 0; j < board[i].length; j++) {
+      mincol.push(board[j][i]);
+    }
+    col.push(mincol);
+  }
+  for (let i = 1; i <= 9; i++) {
+    columns[i] = col[i - 1];
+  }
+  return columns;
+}
+
+
+
+console.log(getColumns());
+
 /*********************************************************************************
  * Принимает игровое поле в формате строки — как в файле sudoku-puzzles.txt.
  * Возвращает игровое поле после попытки его решить.
@@ -18,7 +44,7 @@ function solve(boardString) {
 
 }
 
-console.log(solve());
+
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
