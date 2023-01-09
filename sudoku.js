7c22f1ae1b49bedf324c910ff5ce6def959a3874
Semochka <semochkaelse@gmail.com>
2023-01-09 14:53:14 +0300

    Reading file

--- sudoku.js
+++ sudoku.js
@@ -1,10 +1,24 @@
+const fs = require('fs');
+const process = require('process');
+const box = fs.readFileSync('puzzles.txt', 'utf-8').split('\n');
+let board;
+
+
 /*********************************************************************************
  * Принимает игровое поле в формате строки — как в файле sudoku-puzzles.txt.
  * Возвращает игровое поле после попытки его решить.
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
 function solve(boardString) {
+  if (process.argv[2] === undefined) {
+    boardString = box[0].match(/.{9}/g).map((el) => el.split(''));
+  } else {
+    boardString = box[process.argv[2]].match(/.{9}/g).map((el) => el.split(''));
+  }
+
 }
+
+console.log(solve());
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
