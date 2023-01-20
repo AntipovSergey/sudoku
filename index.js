ee218f79901708a7969bf45122dcb2be56a233f8
Starikoff00 <starikoff00@gmail.com>
2023-01-20 16:53:24 +0300

    update data

---
+++ index.js
@@ -0,0 +1,32 @@
+const checkRow = (num, pos, board) => {
+  const [r,c] = pos;
+
+  //Check rows
+  for (let i = 0; i < size; i++) {
+      if (board[i][c] === num && i !== r) {
+          return false;
+      }
+  }
+
+  //Check cols
+  for (let i = 0; i < size; i++) {
+      if (board[r][i] === num && i !== c) {
+          return false;
+      }
+  }
+
+
+  //Check box
+  const boxRow = Math.floor( r/boxSize ) * boxSize;
+  const boxCol = Math.floor( c/boxSize ) * boxSize;
+
+  for (let i = boxRow; i < boxRow + boxSize; i++) {
+      for (let j = boxCol; j < boxCol + boxSize; j++) {
+          if (board[i][j] === num && i !== r && j !== c) {
+              return false;
+          }
+      }
+  }
+
+  return true;
+}
