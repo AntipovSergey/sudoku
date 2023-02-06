0398b3ff2207e4bfadf4a8d83c13fbd773cdf62b
Daniel <odindlrus@gmail.com>
2023-02-06 17:05:22 +0400

    fixBags

--- sudoku.js
+++ sudoku.js
@@ -1,13 +1,13 @@
 function solve(boardString) {
-  boardString =
-    "1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--";
-  const bordArray = boardString.split("");
-  let size = 9;
-  let subarray = [];
-  for (let i = 0; i < Math.ceil(bordArray.length / size); i++) {
+  const bordArray = boardString.split('');
+  const size = 9;
+  const subarray = [];
+  for (let i = 0; i < Math.ceil(bordArray.length / size); i += +1) {
     subarray[i] = bordArray.slice(i * size, i * size + size);
   }
+  return subarray;
 }
+
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает булевое значение — решено это игровое поле или нет.
