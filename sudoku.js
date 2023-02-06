371e327e5c3ab3143d76cddb95bf43a016edb18f
Yaroslav <philyaren@yandex.ru>
2023-02-07 01:42:22 +0400

    рекурсия ван лан

--- sudoku.js
+++ sudoku.js
@@ -27,125 +27,106 @@ function predef(board) {
   return str;
 }
 
-function generateOptions(board, predefined) {
-  const arr = [];
-  for (let i = 0; i < board.length; i++) {
-    if (predefined[i] === "1") {
-      arr.push(true);
-    } else {
-      arr.push([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-    }
-  }
-  return arr;
-}
-
-function checkRow(i, options, board, position) {
-  let opt = [...options];
-  for (let j = 0; j < 9; j++) {
-    const index = rc2i(position.row, j);
-    if (position == i2rc(i)) {
+function checkOption(index, board) {
+  let rc = i2rc(index);
+  let options = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  for (let r = 0; r < 9; r++) {
+    if (rc.row === r) {
       continue;
     }
-    if (board[index] !== "-") {
-      opt = opt.filter((val) => val != board[index]);
+    const i = rc2i(r, rc.column);
+    if (options.includes(board[i])) {
+      options = options.filter((val) => val !== board[i]);
     }
   }
-  return opt;
-}
-
-function checkColumn(i, options, board, position) {
-  let opt = [...options];
-  for (let j = 0; j < 9; j++) {
-    const index = rc2i(j, position.column);
-    if (position == i2rc(i)) {
+  for (let c = 0; c < 9; c++) {
+    if (rc.column === c) {
       continue;
     }
-    if (board[index] !== "-") {
-      opt = opt.filter((val) => val != board[index]);
+    const i = rc2i(rc.row, c);
+    if (options.includes(board[i])) {
+      options = options.filter((val) => val !== board[i]);
     }
   }
-  return opt;
-}
-
-function checkSquare(options, board, position, squarePos) {
-  let opt = [...options];
-  for (let r = squarePos.row; r < squarePos.row + 3; r++) {
-    for (let c = squarePos.column; c < squarePos.column + 3; c++) {
-      const index = rc2i(r, c);
-      if (position == index) {
+  const checkedSquare = square(rc.row, rc.column);
+  for (let r = checkedSquare.row; r < checkedSquare.row + 3; r++) {
+    for (let c = checkedSquare.column; c < checkedSquare.column + 3; c++) {
+      const i = rc2i(r, c);
+      if (rc2i(rc.row, rc.column) === i) {
         continue;
       }
-      if (board[index] !== "-") {
-        opt = opt.filter((val) => val != board[index]);
+
+      if (options.includes(board[i])) {
+        options = options.filter((val) => val !== board[i]);
       }
     }
   }
-  return opt;
+  return options;
 }
 
-// function checkIfOnlyOneOption(options, position, squarePos) {
-//   let checkedOptions = options[rc2i[position]];
-//   for (let i = 0; checkedOptions.length; i++) {
-//     const checkedValue = options[i];
-//     for (let r = squarePos.row; r < squarePos.row + 3; r++) {
-//       for (let c = squarePos.column; c < squarePos.column + 3; c++) {
-//         const index = rc2i(r, c);
-//         if (position == i2rc(index)) {
-//           continue;
-//         }
-//         if (
-//           options[rc2i(r, c)] !== true &&
-//           options[rc2i(r, c)].includes(checkedValue)
-//         ) {
-//           continue;
-//         } else 
-//       }
+// function solve(boardString, index = 0) {
+//   let newStr = [...boardString].join("");
+//   for (let i = index; i < boardString.length; i++) {
+//     if (boardString[i] !== "-") continue;
+
+//     const options = checkOption(i, newStr);
+//     if (!options.length) {
+//       return false;
 //     }
+//     if (i === 80) {
+//       newStr = newStr.substring(0, i) + options[0] + newStr.substring(i + 1);
+//       return newStr;
+//     }
+
+//     for (let val = 0; val < options.length; val++) {
+//       newStr = newStr.substring(0, i) + options[val] + newStr.substring(i + 1);
+//       if (i === 79) {
+//         newStr = solve(newStr, i + 1);
+//       }
+//       if (i === 80) {
+//         return solve(newStr, i + 1)
+//       }
+//       solve(newStr, i + 1);
+//     }
+//     if (!newStr.includes('-')) {
+//       return newStr
+//     }
+//     return newStr;
 //   }
+//   return newStr;
 // }
 
-const firstSudoku =
-  "---6891--8------2915------84-3----5-2----5----9-24-8-1-847--91-5------6--6-41----";
-
-function solve(boardString) {
-  let board = boardString;
-  const options = generateOptions(boardString, predef(boardString));
-  for (let i = 0; i < board.length; i++) {
-    if (options[i] === true) {
-      continue;
+function solve(boardString, index = 0) {
+  let newStr = [...boardString].join("");
+  while (boardString[index] !== "-" && index < 81) {
+    index++;
+  }
+  if (!newStr.includes("-")) {
+    return newStr;
+  }
+  const options = checkOption(index, boardString);
+  if (!options.length) return false;
+  if (index === 80) {
+    newStr =
+      newStr.substring(0, index) + options[0] + newStr.substring(index + 1);
+    return newStr;
+  }
+  for (let val = 0; val < options.length; val++) {
+    newStr =
+      newStr.substring(0, index) + options[val] + newStr.substring(index + 1);
+    solve(newStr, index + 1);
+    if (solve && !solve(newStr, index + 1).includes('-')) {
+      newStr = solve(newStr, index + 1)
     }
-    const position = i2rc(i);
-    // Проверка по ряду
-    options[i] = checkRow(i, options[i], board, position);
-    // Проверка по колонке
-    options[i] = checkColumn(i, options[i], board, position);
-    // Проверка по квадрату
-    const squarePos = square(position.row, position.column);
-    options[i] = checkSquare(options[i], board, position, squarePos);
   }
-  let iteration = 0;
-  const arrBoard = [...board];
-  while (board.includes("-") && iteration < 1_000_000) {
-    for (let i = 0; i < board.length; i++) {
-      if (options[i] === true) {
-        continue;
-      }
-      if (options[i].length === 1) {
-        arrBoard[i] = String(options[i][0]);
-      }
-      const position = i2rc(i);
-      // проверка по строке
-      options[i] = checkRow(i, options[i], arrBoard, position);
-      // Проверка по колонке
-      options[i] = checkColumn(i, options[i], arrBoard, position);
-      // Проверка по квадрату
-      const squarePos = square(position.row, position.column);
-      options[i] = checkSquare(options[i], arrBoard, position, squarePos);
-    } 
-    iteration++;
-  }
-  return [options, arrBoard.join("")];
+  return newStr;
 }
+
+const firstSudoku =
+  "----754----------8-8-19----3----1-6--------34----6817-2-4---6-39------2-53-2-----";
+
+const fnResult =
+  "14589267389317642527643581951924738676258319438496175295761423843872956162135894-";
 
 function converter(str) {
   const reg = /(.{3})(.{3})(.{3})(.{3})(.{3})(.{3})(.{3})(.{3})(.{3})+?/g;
@@ -153,22 +134,8 @@ function converter(str) {
     return `${g1}    ${g2}    ${g3}\n${g4}    ${g5}    ${g6}\n${g7}    ${g8}    ${g9}\n\n`;
   });
 }
-
-function arrayConverter(arr) {
-  let str = "";
-  for (let i = 0; i < 9; i++) {
-    for (let j = 0; j < 9; j++) {
-      str += `${i}.${j}| ${arr[rc2i(i, j)]}  |`;
-    }
-    str += "\n";
-  }
-  return str;
-}
-
-const [options, board] = solve(firstSudoku);
-// console.log(converter(firstSudoku));
-console.log(arrayConverter(options));
-console.log(converter(board));
+// console.log(converter(fnResult));
+console.log(solve(firstSudoku));
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
