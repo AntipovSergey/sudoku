54fd52aeed72fee578dda270fd853e6a8274cdda
Денис Лазарев <luchsound@gmail.com>
2023-02-06 15:15:01 +0300

    function addNumber, findEmptyCell

---
+++ findEmptyCell.js
@@ -0,0 +1,14 @@
+function findEmptyCell (arr){
+
+    for(let i = 0; i < arr.length; i++){
+        for(let j = 0; j < arr[i].length; j++){
+            if(arr[i][j] === '-'){
+                return [i, j]
+            }
+        }
+    }
+    return -1 
+    }
+    
+    module.exports = findEmptyCell 
+    
