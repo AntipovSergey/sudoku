35526b0a556621eb8bc55800dfd3c1878e16f658
Daniel <odindlrus@gmail.com>
2023-02-07 14:47:27 +0400

    исправил визуальное отображение

--- sudoku.js
+++ sudoku.js
@@ -1,20 +1,20 @@
-const fs = require("fs");
+const fs = require('fs');
 
-const { prettyBoard } = require("./prettyboard");
+const { prettyBoard } = require('./prettyboard');
 
-const inputStrings = fs.readFileSync("./puzzles.txt", "utf-8");
+const inputStrings = fs.readFileSync('./puzzles.txt', 'utf-8');
 
-const arrOfBoards = inputStrings.split("\n");
+const arrOfBoards = inputStrings.split('\n');
 
 function prettyB(board) {
-  const newBordNum = board.map((el) => {
-    el.split("");
+  const newBordNum = board.map(el => {
+    el.split('');
     const size = 9;
     const subarray = [];
     for (let i = 0; i < Math.ceil(el.length / size); i += +1) {
       subarray[i] = el.slice(i * size, i * size + size);
     }
-    return subarray.map((elem) => elem.split(""));
+    return subarray.map(elem => elem.split(''));
   });
 
   return newBordNum;
@@ -23,7 +23,7 @@ function prettyB(board) {
 function isSolved(board) {
   for (let i = 0; i < 9; i += 1) {
     for (let j = 0; j < 9; j += 1) {
-      if (board[i][j] === "-") {
+      if (board[i][j] === '-') {
         return [i, j];
       }
     }
@@ -79,7 +79,7 @@ function solve(boardString) {
           return true;
         }
 
-        boardString[x][y] = "-";
+        boardString[x][y] = '-';
       }
     }
     return false;
@@ -90,7 +90,8 @@ function solve(boardString) {
 
 const sudokuBoardStr = prettyB(arrOfBoards);
 console.log(prettyBoard(sudokuBoardStr[Number(process.argv[2])]));
-console.log(prettyBoard(solve(sudokuBoard[Number(process.argv[2])])));
+console.log('\n');
+console.log(prettyBoard(solve(sudokuBoardStr[Number(process.argv[2])])));
 
 module.exports = {
   solve,
