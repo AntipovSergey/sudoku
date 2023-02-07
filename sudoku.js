64f68f75830b8b2067863c9f39935c7b8230e647
Pavel <goncharov_p@bk.ru>
2023-02-07 11:46:11 +0300

    obnulenie solve

--- sudoku.js
+++ sudoku.js
@@ -8,85 +8,7 @@
 let boardString =
   "1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--";
 
-function solve(boardString) {
-  const arr = boardString.split("");
-  const board = [];
-  const sqwrs = [];
-
-  for (let i = 0; i < 9; i++) {
-    board.push(arr.slice(0, 9));
-    arr.splice(0, 9);
-  }
-
-  let a = 0;
-  let b = 3;
-  for (let g = 0; g < 3; g++) {
-    let x = 0;
-    let y = 3;
-    for (let h = 0; h < 3; h++) {
-      let nmbrs = [];
-      for (let i = a; i < b; i++) {
-        for (let j = x; j < y; j++) {
-          nmbrs.push(board[i][j]);
-        }
-      }
-      sqwrs.push(nmbrs);
-      x += 3;
-      y += 3;
-    }
-    a += 3;
-    b += 3;
-  }
-
-  function compliteSudocu(field, sqwr) {
-    const numberPool = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-    let startField = field;
-
-    for (let i = 0; i < startField.length; i += 1) {
-      startField[i].forEach((num, index) => {
-        if (num === "-") {
-          // console.log(`${num} === "-"`); // сработает
-          // eslint-disable-next-line no-restricted-syntax
-          slovo: for (let j = 0; j < numberPool.length; j += 1) {
-            // console.log(`${j} пошел`);
-
-            if (
-              !startField[i].includes(numberPool[j]) &&
-              startField[0][index] !== numberPool[j] &&
-              startField[1][index] !== numberPool[j] &&
-              startField[2][index] !== numberPool[j]
-            ) {
-              for (let h = 0; h < sqwr.length; h++) {
-                if (sqwr[h].includes(num)) {
-                  console.log(
-                    `${startField[i].indexOf(num)} nahidtsya v kvadrat ${h}`
-                  );
-                  if (!sqwr[h].includes(numberPool[j])) {
-                    startField[i][index] = numberPool[j];
-                    break slovo;
-                  }
-                }
-              }
-              // console.log("if includes сработал");
-              // startField[i][index] = numberPool[j];
-            }
-          }
-        }
-      });
-    }
-
-    for (element of field) {
-      if (!element.includes("-")) return startField;
-      return startField;
-    }
-  }
-
-  compliteSudocu(board, sqwrs);
-  return board;
-}
-
-console.log(solve(boardString));
-// console.log(isSolved(solve(boardString)));
+function solve(boardString) {}
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
@@ -102,7 +24,6 @@ function isSolved(board) {
   return true;
 }
 
-console.log(isSolved(solve(boardString)));
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает строку с игровым полем для последующего вывода в консоль.
@@ -126,21 +47,6 @@ function prettyBoard(board) {
 
   return result.join(" ");
 }
-console.log(prettyBoard(solve(boardString)));
-// const field = [
-//   [1, 2, 3, 4, 5, 6, 7, 8, 9],
-//   [1, 2, 3, 4, 5, 6, 7, 8, 9],
-//   [1, 2, 3, 4, 5, 6, 7, 8, 9],
-//   [1, 2, 3, 4, 5, 6, 7, 8, 9],
-//   [1, 2, 3, 4, 5, 6, 7, 8, 9],
-//   [1, 2, 3, 4, 5, 6, 7, 8, 9],
-//   [1, 2, 3, 4, 5, 6, 7, 8, 9],
-//   [1, 2, 3, 4, 5, 6, 7, 8, 9],
-//   [1, 2, 3, 4, 5, 6, 7, 8, 9],
-// ];
-
-// console.log(isSolved(solve(boardString)));
-// console.log(prettyBoard(solve(boardString)));
 
 // Экспортировать функции для использования в другом файле (например, readAndSolve.js).
 module.exports = {
