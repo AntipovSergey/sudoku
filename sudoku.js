ec9787ad472c9872929f85bf1e3fc4547c28acdd
Sanal Karuev <sanalkaruev11@yandex.ru>
2023-02-07 14:43:34 +0400

    logica

--- sudoku.js
+++ sudoku.js
@@ -34,15 +34,51 @@ function solve(boardString) {
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает булевое значение — решено это игровое поле или нет.
  */
-function isSolved(board) {
-  for (let i = 1; i <= 9; i++) {
-    if (board.indexOf(i) === -1) {
-      return false;
-    }
+// function isSolved(board) {
+//   for (let i = 0; i < 9; i++) {
+//     for (let j = 0; j < 3; j++) {
+//       if (board[i][j] === "-") {
+//         return false;
+//       }
+//     }
+//   }
+
+//   return true;
+// }
+const validate = (num, pos, board) => {
+  const [r,c] = pos;
+
+  //Check rows
+  for (let i = 0; i < size; i++) {
+      if (board[i][c] === num && i !== r) {
+          return false;
+      }
+  }
+
+  //Check cols
+  for (let i = 0; i < size; i++) {
+      if (board[r][i] === num && i !== c) {
+          return false;
+      }
+  }
+
+
+  //Check box
+  const boxRow = Math.floor( r/boxSize ) * boxSize;
+  const boxCol = Math.floor( c/boxSize ) * boxSize;
+
+  for (let i = boxRow; i < boxRow + boxSize; i++) {
+      for (let j = boxCol; j < boxCol + boxSize; j++) {
+        if (board[i][j] === num && i !== r && j !== c) {
+          return false;
+        }
+      }
   }
 
   return true;
 }
+
+
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
