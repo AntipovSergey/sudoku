847d2f4679805746c2a1acd15fb16406b9f194f2
vtaeke <vtaeke@gmail.com>
2023-02-10 13:18:53 +0300

    myCorrect

--- readFile.js
+++ readFile.js
@@ -1,9 +1,9 @@
-const sudoku = require("./sudoku");
-const fs = require("fs");
+const fs = require('fs');
+const sudoku = require('./sudoku');
 
 function readFile() {
-  const readFileSudoku = fs.readFileSync("./puzzles.txt", "utf-8").split("\n");
-  const firstSudoku = readFileSudoku[0].split("");
+  const readFileSudoku = fs.readFileSync('./puzzles.txt', 'utf-8').split('\n');
+  const firstSudoku = readFileSudoku[0].split('').map((el) => (el !== '-' ? Number(el) : el));
   const res = [];
   for (let i = 0; i < firstSudoku.length; i += 9) {
     const chunk = firstSudoku.slice(i, i + 9);
