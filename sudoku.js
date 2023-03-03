cf81086e583c7973bf0e33c638dfdd26494878db
Aina <vi.vi.1998@list.ru>
2023-03-03 15:06:08 +0300

    aina

--- sudoku.js
+++ sudoku.js
@@ -2,18 +2,40 @@
  * Принимает игровое поле в формате строки — как в файле sudoku-puzzles.txt.
  * Возвращает игровое поле после попытки его решить.
  * Договорись со своей командой, в каком формате возвращать этот результат.
- */
-function solve(boardString) {
+//  */
+ const fs = require('fs');
+ const string = fs.readFileSync('./puzzles.txt', 'utf8');
+
+function solve() {
+  const arr = string.split('\n');
+  const arr2 = arr[0].split('').map((el) => (el !== '-' ? Number(el) : el));
+  const res = [];
+  for (let i = 0; i < arr2.length; i += 9) {
+    const chunk = arr2.slice(i, i+9);
+    res.push(chunk);
+  }
+  return res;
 }
+
+
+console.log(solve());
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает булевое значение — решено это игровое поле или нет.
  */
-function isSolved(board) {
-
+function isSolved(board = '-') {
+  const arrayIsArr = solve();
+  for (let i = 0; i < arrayIsArr.length; i++) {
+    for (let j = 0; j < arrayIsArr.length; j++) {
+        if (arrayIsArr[i][j] === board) {
+          return "решай сам";
+      }
+    }
+  }
 }
 
+console.log(isSolved())
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает строку с игровым полем для последующего вывода в консоль.
