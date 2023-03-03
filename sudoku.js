e58dffe6efd4b41c1db83b6c2c8ffd0e5935e2a4
Anna <a_karvatskaya@mail.ru>
2023-03-03 16:11:41 +0300

    My code

--- sudoku.js
+++ sudoku.js
@@ -3,51 +3,49 @@
  * Возвращает игровое поле после попытки его решить.
  * Договорись со своей командой, в каком формате возвращать этот результат.
 //  */
- const fs = require('fs');
- const string = fs.readFileSync('./puzzles.txt', 'utf8');
+const fs = require('fs');
+const string = fs.readFileSync('./puzzles.txt', 'utf8');
 
 function solve() {
   const arr = string.split('\n');
-  const arr2 = arr[0].split('').map((el) => (el !== '-' ? Number(el) : el));
+  const arr2 = arr[0]
+    .split('')
+    .map((el) => (el !== '-' ? Number(el) : Math.floor(Math.random() * 9 + 1)));
   const res = [];
   for (let i = 0; i < arr2.length; i += 9) {
-    const chunk = arr2.slice(i, i+9);
+    const chunk = arr2.slice(i, i + 9);
     res.push(chunk);
   }
   return res;
 }
 
+console.table(solve());
 
-console.log(solve());
-
-/**
- * Принимает игровое поле в том формате, в котором его вернули из функции solve.
- * Возвращает булевое значение — решено это игровое поле или нет.
- */
+// /**
+//  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
+//  * Возвращает булевое значение — решено это игровое поле или нет.
+//  */
 function isSolved(board = '-') {
   const arrayIsArr = solve();
   for (let i = 0; i < arrayIsArr.length; i++) {
     for (let j = 0; j < arrayIsArr.length; j++) {
-        if (arrayIsArr[i][j] === board) {
-          return "решай сам";
+      if (arrayIsArr[i][j] === board) {
+        return 'решай сам';
       }
     }
   }
 }
+console.log(isSolved(3));
+// /**
+//  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
+//  * Возвращает строку с игровым полем для последующего вывода в консоль.
+//  * Подумай, как симпатичнее сформировать эту строку.
+//  */
+// function prettyBoard(board) {}
 
-console.log(isSolved())
-/**
- * Принимает игровое поле в том формате, в котором его вернули из функции solve.
- * Возвращает строку с игровым полем для последующего вывода в консоль.
- * Подумай, как симпатичнее сформировать эту строку.
- */
-function prettyBoard(board) {
-
-}
-
-// Экспортировать функции для использования в другом файле (например, readAndSolve.js).
+// // Экспортировать функции для использования в другом файле (например, readAndSolve.js).
 module.exports = {
   solve,
   isSolved,
-  prettyBoard,
+  // prettyBoard,
 };
