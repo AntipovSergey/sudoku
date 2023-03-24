aebb0899150bd6b6a389817bd88de1834a191a9c
FooBuTT <foobu@mail.ru>
2023-03-24 11:28:20 +0300

    getArray

--- sudoku.js
+++ sudoku.js
@@ -4,24 +4,38 @@
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
 function solve(boardString) {
+  const sudokuArr = [];
+  let index = 0;
+  for (let i = 0; i < 9; i++) {
+    const row = [];
+    for (let j = 0; j < 9; j++) {
+      const char = boardString.charAt(index);
+      if (char === "-") {
+        row.push(0);
+      } else {
+        row.push(parseInt(char));
+      }
+      index++;
+    }
+    sudokuArr.push(row);
+  }
+  return console.table(sudokuArr);
 }
-
+solve(
+  "1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--"
+);
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает булевое значение — решено это игровое поле или нет.
  */
-function isSolved(board) {
-
-}
+function isSolved(board) {}
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает строку с игровым полем для последующего вывода в консоль.
  * Подумай, как симпатичнее сформировать эту строку.
  */
-function prettyBoard(board) {
-
-}
+function prettyBoard(board) {}
 
 // Экспортировать функции для использования в другом файле (например, readAndSolve.js).
 module.exports = {
