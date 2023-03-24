82c7a9ef138e8095ee7380c3da133256a87e62eb
DjoDik <djodi046@gmail.com>
2023-03-24 14:55:16 +0300

    function solve

--- sudoku.js
+++ sudoku.js
@@ -4,24 +4,55 @@
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
 function solve(boardString) {
+  const size = 9;
+
+  function stringToArray(string) {
+    const sudokuArr = [];
+    for (let i = 0; i < sudokuString.length; i += size) {
+      const subString = sudokuString.slice(i, i + size).split('');
+      sudokuArr.push(subString);
+      return sudokuArr;
+    }
+  }
+
+
+  function solve() {
+    const currPos = findEmpty(board);
+
+    if (currPos == null) {
+      return true;
+    }
+    for (let i = 0; i <= size; i++) {
+      const currNum = i.toString();
+      const isValid = validate(currNum, currPos, board);
+
+      if (isValid) {
+        const [x, y] = currPos;
+        board[x][y] = currNum;
+
+        if (solve()) {
+          return true;
+        }
+        board[x][y] = '-';
+      }
+    }
+
+    return false;
+  }
 }
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает булевое значение — решено это игровое поле или нет.
  */
-function isSolved(board) {
-
-}
+function isSolved(board) {}
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает строку с игровым полем для последующего вывода в консоль.
  * Подумай, как симпатичнее сформировать эту строку.
  */
-function prettyBoard(board) {
-
-}
+function prettyBoard(board) {}
 
 // Экспортировать функции для использования в другом файле (например, readAndSolve.js).
 module.exports = {
