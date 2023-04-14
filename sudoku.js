85f3cbaaa05f1d9bda79a72796c2f82d688bf0e0
Vyacheslav <slava_badlaev1@mail.ru>
2023-04-14 16:19:23 +0300

    Поиск индексов -

--- sudoku.js
+++ sudoku.js
@@ -1,27 +1,46 @@
+const fs = require('fs');
+const readFile = fs
+  .readFileSync('./puzzles.txt', 'utf-8')
+  .split('\n')
+  .slice(0, 1);
+
 /**
  * Принимает игровое поле в формате строки — как в файле sudoku-puzzles.txt.
  * Возвращает игровое поле после попытки его решить.
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
+
 function solve(boardString) {
+  const split = boardString.join('').split('');
+  const board = [];
+  for (let i = 0; i < split.length; i += 9) {
+    board.push(split.slice(i, i + 9));
+  }
+}
+
+function findSymbol(board) {
+  for (let r = 0; r < size; r += 1) {
+    for (let c = 0; c < size; c += 1) {
+      if (board[r][c] === '-') {
+        return [r, c];
+      }
+    }
+  }
+  return null;
 }
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает булевое значение — решено это игровое поле или нет.
  */
-function isSolved(board) {
-
-}
+function isSolved(board) {}
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает строку с игровым полем для последующего вывода в консоль.
  * Подумай, как симпатичнее сформировать эту строку.
  */
-function prettyBoard(board) {
-
-}
+function prettyBoard(board) {}
 
 // Экспортировать функции для использования в другом файле (например, readAndSolve.js).
 module.exports = {
