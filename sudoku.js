584cd73b5d60397582cfe31ff5d518ac492175e6
Vyacheslav <slava_badlaev1@mail.ru>
2023-04-14 16:20:45 +0300

    Поиск индексов - (удалил лишние функции)

--- sudoku.js
+++ sudoku.js
@@ -1,22 +1,3 @@
-const fs = require('fs');
-const readFile = fs
-  .readFileSync('./puzzles.txt', 'utf-8')
-  .split('\n')
-  .slice(0, 1);
-
-/**
- * Принимает игровое поле в формате строки — как в файле sudoku-puzzles.txt.
- * Возвращает игровое поле после попытки его решить.
- * Договорись со своей командой, в каком формате возвращать этот результат.
- */
-
-function solve(boardString) {
-  const split = boardString.join('').split('');
-  const board = [];
-  for (let i = 0; i < split.length; i += 9) {
-    board.push(split.slice(i, i + 9));
-  }
-}
 
 function findSymbol(board) {
   for (let r = 0; r < size; r += 1) {
@@ -28,23 +9,3 @@ function findSymbol(board) {
   }
   return null;
 }
-
-/**
- * Принимает игровое поле в том формате, в котором его вернули из функции solve.
- * Возвращает булевое значение — решено это игровое поле или нет.
- */
-function isSolved(board) {}
-
-/**
- * Принимает игровое поле в том формате, в котором его вернули из функции solve.
- * Возвращает строку с игровым полем для последующего вывода в консоль.
- * Подумай, как симпатичнее сформировать эту строку.
- */
-function prettyBoard(board) {}
-
-// Экспортировать функции для использования в другом файле (например, readAndSolve.js).
-module.exports = {
-  solve,
-  isSolved,
-  prettyBoard,
-};
