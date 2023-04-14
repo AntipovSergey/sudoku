f82226266712a19473b53a82b72b3dc580a39765
Екатерина6761240 <egm6761240@gmail.com>
2023-04-14 14:09:44 +0300

    Получили пустые ячейки

--- sudoku.js
+++ sudoku.js
@@ -13,7 +13,10 @@ function solve(boardString) {
 
   return arrBoard;
 }
-console.log(solve("1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--"))
+
+const str ='1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--'
+const ArrArr = solve(str);
+
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает булевое значение — решено это игровое поле или нет.
@@ -33,3 +36,18 @@ module.exports = {
   isSolved,
   prettyBoard,
 };
+
+function search(ArrArr) {
+  const newArr = [];
+
+  for (let y = 0; y < ArrArr.length; y += 1) {
+    for (let x = 0; x < ArrArr.length; x += 1) {
+      if (ArrArr[y][x] === '-') {
+        newArr.push([y, x]);
+      }
+    }
+  }
+
+  return newArr;
+}
+console.log(search(ArrArr));
