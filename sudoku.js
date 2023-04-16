35a00602800ee7a73ea7c898788b93b18f8e561c
Ilia <iliya.britvin@gmail.com>
2023-04-16 19:27:39 +0300

    improved naming

--- sudoku.js
+++ sudoku.js
@@ -1,108 +1,181 @@
-const fs = require('fs');
-let readFile = fs.readFileSync('./puzzles.txt', 'utf-8');
-readFile = readFile.split('\n').slice(0, 10);
-// console.log(readFile);
+const SUDOKU_SIZE = 9;
+const SQUARE_SIZE = 3;
+const EMPTY_CELL_VALUE = '-';
 
-// return array.map((subarray) =>
-// subarray.map((cell) => (cell === '-' ? parseInt(cell) : '-' ))
-// );
+const parseBoard = (boardString) => {
+  const board = [];
+  const cells = boardString.split('');
+
+  for (let i = 0; i < cells.length; i += SUDOKU_SIZE) {
+    board.push(cells.slice(i, i + SUDOKU_SIZE));
+  }
+
+  return board;
+};
+
+function validateRow({ board, emptyCellX, potentialNumber }) {
+  for (let col = 0; col < SUDOKU_SIZE; col++) {
+    if (board[emptyCellX][col] === potentialNumber) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function validateColumn({ board, emptyCellY, potentialNumber }) {
+  for (let row = 0; row < SUDOKU_SIZE; row++) {
+    if (board[row][emptyCellY] === potentialNumber) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function validateSubgrid({ board, emptyCellX, emptyCellY, potentialNumber }) {
+  const subgridRow = Math.floor(emptyCellX / SQUARE_SIZE) * SQUARE_SIZE;
+  const subgridCol = Math.floor(emptyCellY / SQUARE_SIZE) * SQUARE_SIZE;
+  for (let row = subgridRow; row < subgridRow + SQUARE_SIZE; row++) {
+    for (let col = subgridCol; col < subgridCol + SQUARE_SIZE; col++) {
+      if (board[row][col] === potentialNumber) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+function validatePotentialPlacement({
+  board,
+  emptyCellX,
+  emptyCellY,
+  potentialNumber,
+}) {
+  return (
+    validateRow({ board, emptyCellX, potentialNumber }) &&
+    validateColumn({ board, emptyCellY, potentialNumber }) &&
+    validateSubgrid({ board, emptyCellX, emptyCellY, potentialNumber })
+  );
+}
+
+function findEmptyCell(board) {
+  for (let rowIdx = 0; rowIdx < board.length; rowIdx++) {
+    for (let colIdx = 0; colIdx < board[rowIdx].length; colIdx++) {
+      if (board[rowIdx][colIdx] === EMPTY_CELL_VALUE) {
+        return [rowIdx, colIdx];
+      }
+    }
+  }
+  return null;
+}
+
+function solveBoard(board) {
+  const emptyCell = findEmptyCell(board);
+
+  // If not empty cell is found it means that we have solved the board
+  if (!emptyCell) {
+    return true;
+  }
+
+  const [emptyCellX, emptyCellY] = emptyCell;
+
+  // Try to place the number in the empty cell
+  for (
+    let rawPotentialNumber = 1;
+    rawPotentialNumber <= 9;
+    rawPotentialNumber++
+  ) {
+    const potentialNumber = String(rawPotentialNumber);
+
+    const isValidPotentialPlacement = validatePotentialPlacement({
+      board,
+      emptyCellX,
+      emptyCellY,
+      potentialNumber,
+    });
+
+    if (isValidPotentialPlacement) {
+      board[emptyCellX][emptyCellY] = potentialNumber;
+      if (solveBoard(board)) {
+        return true;
+      }
+      // If can't solve with this number undo the placement
+      board[emptyCellX][emptyCellY] = EMPTY_CELL_VALUE;
+    }
+  }
+  // Couldn't find a valid placement thus the board can't be solved with this number
+  return false;
+}
+
+/**
+ * Принимает игровое поле в формате строки — как в файле sudoku-puzzles.txt.
+ * Возвращает игровое поле после попытки его решить.
+ * Договорись со своей командой, в каком формате возвращать этот результат.
+ */
+function solve(boardString) {
+  const board = parseBoard(boardString);
+  solveBoard(board);
+  return board;
+}
+
+function validateSudokuBoard(board) {
+  // Check rows
+  for (let i = 0; i < SUDOKU_SIZE; i++) {
+    const row = new Set(board[i]);
+    if (row.size !== SUDOKU_SIZE) {
+      return false;
+    }
+  }
+
+  // Check columns
+  for (let i = 0; i < SUDOKU_SIZE; i++) {
+    const column = new Set();
+    for (let j = 0; j < SUDOKU_SIZE; j++) {
+      column.add(board[j][i]);
+    }
+    if (column.size !== SUDOKU_SIZE) {
+      return false;
+    }
+  }
+
+  // Iterate over squares
+  for (let squareX = 0; squareX < SUDOKU_SIZE; squareX += 3) {
+    for (let squareY = 0; squareY < SUDOKU_SIZE; squareY += 3) {
+      const square = new Set();
+      // Iterate over square cells
+      for (let row = squareX; row < squareX + 3; row++) {
+        for (let col = squareY; col < squareY + 3; col++) {
+          square.add(board[row][col]);
+        }
+      }
+      if (square.size !== SUDOKU_SIZE) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
+
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает булевое значение — решено это игровое поле или нет.
  */
 function isSolved(board) {
-  const outerSquareSize = 9;
-  const innerSquareSize = 3;
-
-  function findSymbol(board) {
-    for (let r = 0; r < outerSquareSize; r += 1) {
-      for (let c = 0; c < outerSquareSize; c += 1) {
-        if (board[r][c] === '-') {
-          return [r, c];
-        }
-      }
-    }
-    return null;
-  }
-
-  const validate = (number, position, board) => {
-    const [r, c] = position;
-
-    //check row
-    for (let i = 0; i < outerSquareSize; i += 1) {
-      if (board[i][c] === number && i !== r) {
-        return false;
-      }
-    }
-
-    //check colums
-    for (let i = 0; i < outerSquareSize; i += 1) {
-      if (board[r][i] === number && i !== c) {
-        return false;
-      }
-    }
-
-    //check section
-    const boxRow = Math.floor(r / innerSquareSize) * innerSquareSize;
-    const boxColumn = Math.floor(c / innerSquareSize) * innerSquareSize;
-
-    for (let i = boxRow; i < innerSquareSize + boxRow; i += 1) {
-      for (let j = boxColumn; j < boxColumn + innerSquareSize; j += 1) {
-        if (board[i][j] === number && i !== r && j !== c) {
-          return false;
-        }
-      }
-    }
-    return true;
-  };
-
-  const solvePuzzle = () => {
-    const currentPosition = findSymbol(board);
-    if (currentPosition === null) {
-      return true;
-    }
-
-    for (let i = 1; i < outerSquareSize + 1; i += 1) {
-      const currentNumber = i.toString();
-      const isValid = validate(currentNumber, currentPosition, board);
-
-      if (isValid) {
-        const [x, y] = currentPosition;
-        board[x][y] = currentNumber;
-
-        if (solvePuzzle()) {
-          // throw board;
-          return true;
-        }
-        board[x][y] = '-';
-      }
-    }
-    return false;
-  };
-  solvePuzzle();
-  return board.slice(0, 9);
+  return validateSudokuBoard(board);
 }
 
-// try {
-//   isSolved(solve(readFile));
-// } catch (board) {
-//   console.log('Done', board);
-// }
-console.table(isSolved(solve(readFile)));
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает строку с игровым полем для последующего вывода в консоль.
  * Подумай, как симпатичнее сформировать эту строку.
  */
-function prettyBoard(board) {}
-// function prettyBoard(board) {
-//   return isSolved(solve(board))
-// }
+function prettyBoard(board) {
+  console.table(board);
+}
 
 // Экспортировать функции для использования в другом файле (например, readAndSolve.js).
 module.exports = {
   solve,
   isSolved,
   prettyBoard,
-  // prettyBoard,
 };
