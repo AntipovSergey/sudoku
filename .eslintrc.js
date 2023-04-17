46f1de7283abd7fce900d29835bc4ab870384392
Ilia <iliya.britvin@gmail.com>
2023-04-17 08:47:34 +0300

    better eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,13 +4,15 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base'],
   parserOptions: {
     ecmaVersion: 'latest',
   },
   rules: {
     'no-console': 0,
+    'operator-linebreak': 0,
+    'no-plusplus': 0,
+    'no-param-reassign': 0,
+    'object-curly-newline': 0,
   },
 };
