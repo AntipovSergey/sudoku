4533e9d5466a50f4daf5d6048a5c3fd985389914
kotekkarry2 <karinakarina1246@gmail.com>
2023-05-05 11:57:48 +0300

    readfile

---
+++ readFile.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+
+const readFile = () => {
+  const readFileText = fs
+    .readFileSync("puzzles.txt", "utf8")
+    .split("\r\n")
+    .slice(0, 5)
+    .join("")
+    .match(/.{1,9}/g)
+    .map((el) => el.split(""));
+
+  return readFileText;
+};
+module.exports = readFile;
