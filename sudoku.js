09ec6a6a30a5ec853f32e37256bd259fbeac53a8
Alena <aa_bow@mail.ru>
2023-05-05 13:03:05 +0300

    Реализация функции solve

--- sudoku.js
+++ sudoku.js
@@ -4,8 +4,17 @@
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
 function solve(boardString) {
-}
+  const size = 9;
+  const boardArre = [];
+  const newArre = boardString.split('')
+  for (let i = 0; i < newArre.length; i += size){
+    boardArre.push(newArre.slice(i,i+size))
+    }
+    return boardArre
+  }
 
+
+console.log(solve('1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--'))
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает булевое значение — решено это игровое поле или нет.
