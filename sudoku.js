960810d0d780cc0707f980179903716fd312c40c
Kenlamss <kenlams@icloud.com>
2023-06-02 13:30:33 +0300

    solve

--- sudoku.js
+++ sudoku.js
@@ -4,6 +4,70 @@
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
 function solve(boardString) {
+  
+
+
+  const box = 3;
+  const size = 9;
+  const findEmpty = function (board) {
+    for (let r = 0; r < size; r++) {
+      for (let c = 0; c < size; c++) {
+        if (board[r][c] === '-') return [r, c];
+      }
+    }
+    return undefined;
+  };
+
+  const checkPos = function (num, pos, board) {
+    const [r, c] = pos;
+
+    for (let i = 0; i < size; i++) {
+      if (board[i][c] === num && i !== r) {
+        return false;
+      }
+    }
+
+    for (let i = 0; i < size; i++) {
+      if (board[r][i] === num && i !== c) {
+        return false;
+      }
+    }
+
+    const boxRow = Math.floor(r / box) * box;
+    const boxCol = Math.floor(c / box) * box;
+
+    for (let i = boxRow; i < boxRow + box; i++) {
+      for (let j = boxCol; j < boxCol + box; j++) {
+        if (board[i][j] === num && i !== r && j !== c) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  };
+
+  const master = () => {
+    const currPos = findEmpty(board);
+    if (currPos === undefined) {
+      return true;
+    }
+    for (let i = 1; i < size + 1; i++) {
+      const currNum = i.toString();
+      const isValid = checkPos(currNum, currPos, board);
+      if (isValid) {
+        const [x, y] = currPos;
+        board[x][y] = currNum;
+
+        if (master()) return true;
+        board[x][y] = '-';
+      }
+    }
+    return false;
+  };
+
+  master();
+  return board;
 }
 
 /**
@@ -23,7 +87,6 @@ function prettyBoard(board) {
 
 }
 
-// Экспортировать функции для использования в другом файле (например, readAndSolve.js).
 module.exports = {
   solve,
   isSolved,
