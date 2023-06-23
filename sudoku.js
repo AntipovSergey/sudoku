443114468cb7f793f1f89fe63b753fb7c4b05710
AEvgeniyB <aeb1986@yandex.ru>
2023-06-23 16:25:43 +0300

    функция проверки строк и столбцов

--- sudoku.js
+++ sudoku.js
@@ -3,7 +3,6 @@
  */
 function convertString(str) {
   const arrayWithElements = str.split('');
-  console.log(arrayWithElements);
   const chunk = 9;
   let board = [];
   for (let i = 0; i < arrayWithElements.length; i += chunk) {
@@ -20,12 +19,33 @@ function convertString(str) {
  */
 function solve(boardString) {}
 
-function solveSimple(boardString) {}
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает булевое значение — решено это игровое поле или нет.
  */
-function isSolved(board) {}
+function isSolved(board) {
+  let statementrow;
+  let statementcol;
+  for (row = 0; row < board.length; row++) {
+    const check = arr[i];
+    for (let i in check) {
+      if (check.indexOf(i) === check.lastIndexOf(i)) {
+        statementrow = true;
+      }
+    }
+  }
+  for (col = 0; col < board.length; col++) {
+    for (row = 0; row < board.length; row++) {
+      const arr = [];
+      arr.push(board[row][col]);
+      for (let i in arr) {
+        if (check.indexOf(i) === check.lastIndexOf(i)) {
+          statementcol = true;
+        }
+      }
+    }
+  }
+}
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
@@ -40,3 +60,8 @@ module.exports = {
   isSolved,
   prettyBoard,
 };
+// console.log(
+//   convertString(
+//     '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--'
+//   )
+// );
