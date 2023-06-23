a38e18d185163be9483d14158a5ace1eff43c80e
AEvgeniyB <aeb1986@yandex.ru>
2023-06-23 13:59:13 +0300

    реализованна функция создания игрового поля

--- sudoku.js
+++ sudoku.js
@@ -1,16 +1,26 @@
 /**
+ * Принимает строку с данными и конвертирует её в двумерный массив
+ */
+function convertString(str) {
+  const arrayWithElements = str.split('');
+  console.log(arrayWithElements);
+  const chunk = 9;
+  let board = [];
+  for (let i = 0; i < arrayWithElements.length; i += chunk) {
+    board.push(arrayWithElements.slice(i, i + chunk));
+  }
+
+  return board;
+}
+
+/**
  * Принимает игровое поле в формате строки — как в файле sudoku-puzzles.txt.
  * Возвращает игровое поле после попытки его решить.
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
-function solve(boardString) {
-  const board = boardString.split('');
-  return board;
-}
+function solve(boardString) {}
 
-function solveSimple(boardString) {
-  
-}
+function solveSimple(boardString) {}
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает булевое значение — решено это игровое поле или нет.
