e1508c6c5236c1063ab57b465d44619eb41063f1
AEvgeniyB <aeb1986@yandex.ru>
2023-06-23 17:02:34 +0300

    проверка строк и столбцов вроде работает

--- sudoku.js
+++ sudoku.js
@@ -68,10 +68,8 @@ function solve(boardString) {
   return board;
 }
 
-console.log(
-  solve(
-    '-6-584917547219368891-63245278-451391593274866-4891572785132694416958723923476851'
-  )
+const boards = solve(
+  '-6-584917547219368891-63245278-451391593274866-4891572785132694416958723923476851'
 );
 
 /**
@@ -82,10 +80,13 @@ function isSolved(board) {
   let statementrow;
   let statementcol;
   for (row = 0; row < board.length; row++) {
-    const check = arr[i];
+    const check = board[row];
     for (let i in check) {
       if (check.indexOf(i) === check.lastIndexOf(i)) {
         statementrow = true;
+      } else {
+        statementrow = false;
+        return statementcol;
       }
     }
   }
@@ -94,14 +95,20 @@ function isSolved(board) {
       const arr = [];
       arr.push(board[row][col]);
       for (let i in arr) {
-        if (check.indexOf(i) === check.lastIndexOf(i)) {
+        if (arr.indexOf(i) === arr.lastIndexOf(i)) {
           statementcol = true;
+        } else {
+          statementrow = false;
+          return statementrow;
         }
       }
     }
   }
+  if (statementcol === true && statementrow === true) {
+    return true;
+  }
 }
-
+console.log(isSolved(boards));
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает строку с игровым полем для последующего вывода в консоль.
