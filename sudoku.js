ff8fd7c785e94f5c4d3c6761aefc920d0a17229b
Demon5611 <d555601@ya.ru>
2023-06-23 15:55:41 +0300

    66

--- sudoku.js
+++ sudoku.js
@@ -31,7 +31,7 @@ function solve(boardString)
     for (let i = 0; i < board.length; i += 1) {
   for (let j = 0; j < arrOfNumbers.length; i += 1) {
     if (!board.includes(arrOfNumbers[j])){ 
-      let index = board[i].findIndex(0)
+      let index = board[i].findIndex('-')
       board[i][index] = arrOfNumbers[j]
     } fullBoard (board)
   }
