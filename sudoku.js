6688bfcec4dbd26b90c5539d9c839a58c2a099ba
NailyaGayfutdinova <ngayfutdinova@gmail.com>
2023-06-23 12:50:20 +0300

    add empty function

--- sudoku.js
+++ sudoku.js
@@ -8,6 +8,9 @@ function solve(boardString) {
   return board;
 }
 
+function solveSimple(boardString) {
+  
+}
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает булевое значение — решено это игровое поле или нет.
