ba5905db89beb7fb4cc4cb32acb698b3da23e866
Demon5611 <d555601@ya.ru>
2023-06-23 10:27:29 +0300

    1

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,15 @@
 module.exports = {
-  env: {
-    commonjs: true,
-    es2021: true,
-    node: true,
-  },
-  extends: [
-    'airbnb-base',
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-  },
-  rules: {
-    'no-console': 0,
-  },
-};
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+}
