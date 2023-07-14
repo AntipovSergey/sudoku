a2636a62fcbbb5e9fc8bb5fb852cd4ab8c178bac
Oldnesss <viv251985@gmail.com>
2023-07-14 16:40:53 +0300

    release 0

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,13 +4,17 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base'],
   parserOptions: {
     ecmaVersion: 'latest',
   },
   rules: {
     'no-console': 0,
+    'linebreak-style': 0,
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+    'operator-assignment': ['error', 'never'],
+    'prefer-template': 'off',
+    quotes: 'off',
+    'no-param-reassign': ['error', { props: false }],
   },
 };
