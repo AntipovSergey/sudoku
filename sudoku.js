9e437470b91e87dc321ee4d80f728bdaf809137c
Faultless0506 <azazellodaroncgsense@gmail.com>
2023-07-14 12:11:58 +0300

    transformated puzzles done

--- sudoku.js
+++ sudoku.js
@@ -3,25 +3,32 @@
  * Возвращает игровое поле после попытки его решить.
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
-function solve(boardString) {
-}
+const fs = require("fs");
+const puzzles = fs.readFileSync("./puzzles.txt", "utf-8");
+
+const transformatedPuzzles = (num) => {
+  const transformated = puzzles.split("\n")[num - 1];
+  const tempArr = []
+  for (let i = 0; i < 9; i += 1) {
+    tempArr[i] = transformated.slice(0 + i*9, 9 + i*9).split('')
+  }
+  return tempArr;
+};
+console.log(transformatedPuzzles(1))
+function solve(boardString) {}
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает булевое значение — решено это игровое поле или нет.
  */
-function isSolved(board) {
-
-}
+function isSolved(board) {}
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает строку с игровым полем для последующего вывода в консоль.
  * Подумай, как симпатичнее сформировать эту строку.
  */
-function prettyBoard(board) {
-
-}
+function prettyBoard(board) {}
 
 // Экспортировать функции для использования в другом файле (например, readAndSolve.js).
 module.exports = {
