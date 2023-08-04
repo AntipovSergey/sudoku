1fa31e0c43eb8e43c8591287f43a39715d221f19
SatiMik <sati62638@gmail.com>
2023-08-04 14:40:27 +0300

    fun fillS

--- sudoku.js
+++ sudoku.js
@@ -24,31 +24,45 @@ function prettyBoard(board) {
 }
 
 const sudokuArr = [
-  [1, 0, 5, 8, 0, 2, 0, 0, 0]
-    [0, 9, 0, 0, 7, 6, 4, 0, 5]
-    [2, 0, 0, 4, 0, 0, 8, 1, 9]
-    [0, 1, 9, 0, 0, 7, 3, 0, 0]
-    [6, 7, 6, 2, 0, 8, 3, 0, 9]
-    [0, 0, 0, 0, 0, 0, 0, 6, 1]
-    [0, 5, 0, 0, 0, 7, 6, 0, 0]
-    [0, 0, 3, 0, 4, 3, 0, 2, 0]
-    [5, 0, 1, 6, 0, 0, 3, 0, 8],
+  ['1', '-', '5', '8', '-', '2', '-', '-', '-'],
+  ['-', '9', '-', '-', '7', '6', '4', '-', '5'],
+  ['2', '-', '-', '4', '-', '-', '8', '1', '9'],
+  ['-', '1', '9', '-', '-', '7', '3', '-', '-'],
+  ['6', '7', '6', '2', '-', '8', '3', '-', '9'],
+  ['-', '-', '-', '-', '-', '-', '-', '6', '1'],
+  ['-', '5', '-', '-', '-', '7', '6', '-', '-'],
+  ['-', '-', '3', '-', '4', '3', '-', '2', '-'],
+  ['5', '-', '1', '6', '-', '-', '3', '-', '8'],
 ];
-function fillSudoku(i, j, sudokuArr) {
+
+const nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+function fillSudoku(sudokuArr) {
   for (let i = 0; i < 9; i++) {
     for (let j = 0; j < 9; j++) {
       if (sudokuArr[i][j] !== sudokuArr[i][j + 1]) {
         // board = takeSudoku(), задать переменную и заменить на нее
+        console.log(sudokuArr[i][j]);
         return true;
       }
     }
     return false;
   }
+  // for (let b = 0; b < sudokuArr.length; b++) {
+  //   for (let b1 = 0; b1 < sudokuArr.length; b1++) {
+  //     if (sudokuArr[b][b1] !== sudokuArr[b1][b]) {
+  //       // board = takeSudoku(), задать переменную и заменить на нее
+  //       console.log(sudokuArr[b][b1]);
+  //       return true;
+  //     }
+  //   }
+  //   return false;
 }
+
+console.log(fillSudoku(sudokuArr));
 // Экспортировать функции для использования в другом файле (например, readAndSolve.js).
 module.exports = {
   solve,
   isSolved,
   prettyBoard,
-  checkVertical,
+  fillSudoku,
 };
