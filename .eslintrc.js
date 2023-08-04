8d7ef789167f528ace2438111e7171098ec58f1c
SantOg163e <ssanturan79@gmail.com>
2023-08-04 10:14:17 +0300

    initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,13 +4,23 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: [
-    'airbnb-base',
+  extends: 'airbnb-base',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
   ],
   parserOptions: {
     ecmaVersion: 'latest',
   },
   rules: {
-    'no-console': 0,
   },
 };
