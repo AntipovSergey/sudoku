d1ffeeee00474168db227246add8c48e9a4ae0bf
darya <atomicleda@mail.ru>
2023-08-04 16:20:07 +0300

    table 9*9

--- runner.js
+++ runner.js
@@ -7,6 +7,12 @@ const fs = require('fs');
 // Все судоку для решения доступны в файле puzzles.txt.
 // Прочесть файл puzzles.txt в кодировке 'utf-8' и передать его содержимое в функцию readAndSolve.
 
-const fileData = fs.readFileSync('./puzzles.txt', 'utf-8').split('\n').slice(0, 1);
+const fileData = fs.readFileSync('./puzzles.txt', 'utf-8').slice(0, 81);
 
-console.log(fileData);
+const sudokuArray = [];
+
+for (let i = 0; i < fileData.length; i += 9) {
+  sudokuArray.push(fileData.slice(i, i + 9).split(''));
+}
+
+console.table(sudokuArray);
