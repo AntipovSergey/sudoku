03b66f112006ad50704fecdcffcd9636922f205d
Korrusx <o2e3s483@gmail.com>
2023-08-04 12:32:19 +0300

    сделал функции, вычисляющие недостающие циры в колонке

--- sudoku.js
+++ sudoku.js
@@ -3,6 +3,62 @@
  * Возвращает игровое поле после попытки его решить.
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
+const sudoky = [
+  ['1 - 5 8 - 2 - - -'],
+  ['- 9 - - 7 6 4 - 5'],
+  ['2 - - 4 - - 8 1 9'],
+  ['- 1 9 - - 7 3 - 6'],
+  ['7 6 2 - 8 3 - 9 -'],
+  ['- - - - 6 1 - 5 -'],
+  ['- - 7 6 - - - 3 -'],
+  ['4 3 - - 2 - 5 - 1'],
+  ['6 - - 3 - 8 9 - -'],
+];
+
+function parseSudoku(sudokuArray) {
+  return sudokuArray.map((row) => row[0]
+    .split(' ')
+    .map((cell) => (cell === '-' ? 0 : parseInt(cell))));
+}
+const parsedSudoku = parseSudoku(sudoky);
+
+function checkColums(sudokuBoard) {
+  const maybeArr = [];
+  for (let col = 0; col < 9; col++) {
+    const colValues = [];
+    for (let row = 0; row < 9; row++) {
+      colValues.push(sudokuBoard[row][col]);
+    }
+    const missNum = missingNumbers(colValues);
+    maybeArr.push(missNum);
+  }
+  return maybeArr;
+}
+
+// function checkColRow(col, row) {
+// const result = [];
+// for
+// }
+
+function checkSquare(sudokuBoard) {
+
+  const 
+}
+
+function missingNumbers(arr) {
+  const findmissingNumbers = [];
+  for (let num = 1; num <= 9; num++) {
+    if (!arr.includes(num)) {
+      findmissingNumbers.push(num);
+    }
+  }
+  return findmissingNumbers;
+}
+
+const sudokuCheckColums = checkColums(parsedSudoku);
+
+console.log(sudokuCheckColums);
+
 function solve(boardString) {
 }
 
