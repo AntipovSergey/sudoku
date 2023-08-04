06cda261a2d47ec92818212a02ed3263f13fd228
powerckbestest <ivanchikoff-danila@yandex.ru>
2023-08-04 14:31:45 +0300

    getSuggestedNumbers function done

--- sudoku.js
+++ sudoku.js
@@ -23,9 +23,46 @@ function prettyBoard(board) {
 
 }
 
+const sudoku = [
+  [5, 3, 0, 0, 7, 0, 0, 0, 0],
+  [6, 0, 0, 1, 9, 5, 0, 0, 0],
+  [0, 9, 8, 0, 0, 0, 0, 6, 0],
+  [8, 0, 0, 0, 6, 0, 0, 0, 3],
+  [4, 0, 0, 8, 0, 3, 0, 0, 1],
+  [7, 0, 0, 0, 2, 0, 0, 0, 6],
+  [0, 6, 0, 0, 0, 0, 2, 8, 0],
+  [0, 0, 0, 4, 1, 9, 0, 0, 5],
+  [0, 0, 0, 0, 8, 0, 0, 7, 9],
+];
+
+function getSuggestedNumbers(sudoku, row, col) {
+  if (sudoku[row][col] !== 0) {
+    return [];
+  }
+  const suggestedNumbers = new Set();
+  const size = sudoku.length;
+  for (let i = 0; i < size; i++) {
+    suggestedNumbers.add(sudoku[row][i]);
+  }
+  for (let i = 0; i < size; i++) {
+    suggestedNumbers.add(sudoku[i][col]);
+  }
+  const startRow = Math.floor(row / 3) * 3;
+  const startCol = Math.floor(col / 3) * 3;
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      suggestedNumbers.add(sudoku[startRow + i][startCol + j]);
+    }
+  }
+  suggestedNumbers.delete(0);
+  return Array.from(suggestedNumbers);
+}
+
+console.log(getSuggestedNumbers(sudoku, 0, 2));
 // Экспортировать функции для использования в другом файле (например, readAndSolve.js).
 module.exports = {
   solve,
   isSolved,
   prettyBoard,
+  getSuggestedNumbers,
 };
