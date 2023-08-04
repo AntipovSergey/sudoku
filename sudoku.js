f328a0a2fb66f04e20e88e22d7f938d84083f454
deson <sakhlike@gmail.com>
2023-08-04 13:21:32 +0300

    Горизонталь

--- sudoku.js
+++ sudoku.js
@@ -1,3 +1,16 @@
+// const board = [
+//   ['1 - 5 8 - 2 - - -']
+//   ['- 9 - - 7 6 4 - 5']
+//   ['2 - - 4 - - 8 1 9']
+//   ['- 1 9 - - 7 3 - 6']
+//   ['7 6 2 - 8 3 - 9 -']
+//   ['- - - - 6 1 - 5 -']
+//   ['- - 7 6 - - - 3 -']
+//   ['4 3 - - 2 - 5 - 1']
+//   ['6 - - 3 - 8 9 - -']
+// ];
+
+
 /**
  * Принимает игровое поле в формате строки — как в файле sudoku-puzzles.txt.
  * Возвращает игровое поле после попытки его решить.
@@ -10,9 +23,10 @@ function solve(boardString) {
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает булевое значение — решено это игровое поле или нет.
  */
-function isSolved(board) {
+function isSolved(board, num) {
 
 }
+
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
@@ -23,6 +37,41 @@ function prettyBoard(board) {
 
 }
 
+/* 
+Проверка по горизонтали
+*/
+// function CheckHorizontal(h) {
+//   for(let i = 0; i < 9; i++) {
+//     if(h == board[row][i])
+//     return false
+//   }
+// }
+
+function CheckHorizontal(sudokuBoard) {
+  const horizontal = []
+  for(let row = 0; row < 9; row++) {
+    const rowValues = []
+    for( let col= 0; col < 9; col++) {
+    rowValues.push(board[row][col])
+    }
+    const missNum = missingNumbers(rowValues)
+  }
+}
+function missingNumbers(arr) {
+  const findmissNumbers = []
+  for( let i =0 ; i < 9; i++) {
+    if(!arr.includes[i]) {
+      findmissNumbers.push[i]
+    }
+  }
+  return findmissNumbers
+}
+
+
+
+// function CheckSquare(x, y) {
+// for(let i = 0; i < x.length; i++)
+// }
 // Экспортировать функции для использования в другом файле (например, readAndSolve.js).
 module.exports = {
   solve,
