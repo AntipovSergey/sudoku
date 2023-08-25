20074cdffdd386753ca8f7c2b7feec05bc89c04f
ZarinaAvazbekKyzy <zarinaavazbekkyzy1@gmail.com>
2023-08-25 16:46:12 +0300

    function solve

--- sudoku.js
+++ sudoku.js
@@ -3,23 +3,91 @@ function read() {
    * Прочесть файл puzzles.txt в кодировке 'utf-8' и вернуть эти данные из функции
    */
 }
-let puzzle =
-  "219543678543876912876219345432765891765198234198432567321564789654987123987321456";
-function checkSqr(arr) {
-  let array = arr.match(/.{1,9}/g);
-  const puzzle = [];
+
+let solveSudoku = function (arr) {
+  let array = arr.replace(/-/g, ".").match(/.{1,9}/g);
+  const board = [];
   for (let i = 0; i < array.length; i++) {
-    let oneLine = array[i].split("").map(Number);
-    puzzle.push(oneLine);
+    let oneLine = array[i].split("");
+    board.push(oneLine);
   }
-}
-checkSqr(puzzle);
-function solve() {
-  /**
-   * Принимает игровое поле в том формате, в котором его вернули из функции read.
-   * Возвращает игровое поле после попытки его решить.
-   */
-}
+
+  const size = 9;
+  const boxSize = 3;
+
+  const findEmpty = (board) => {
+    for (let r = 0; r < size; r++) {
+      for (let c = 0; c < size; c++) {
+        if (board[r][c] === ".") {
+          return [r, c];
+        }
+      }
+    }
+    return null;
+  };
+
+  const validate = (num, pos, board) => {
+    const [r, c] = pos;
+
+    //Check rows
+    for (let i = 0; i < size; i++) {
+      if (board[i][c] === num && i !== r) {
+        return false;
+      }
+    }
+
+    //Check cols
+    for (let i = 0; i < size; i++) {
+      if (board[r][i] === num && i !== c) {
+        return false;
+      }
+    }
+
+    //Check box
+    const boxRow = Math.floor(r / boxSize) * boxSize;
+    const boxCol = Math.floor(c / boxSize) * boxSize;
+
+    for (let i = boxRow; i < boxRow + boxSize; i++) {
+      for (let j = boxCol; j < boxCol + boxSize; j++) {
+        if (board[i][j] === num && i !== r && j !== c) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  };
+
+  const solve = () => {
+    const currPos = findEmpty(board);
+
+    if (currPos === null) {
+      return true;
+    }
+    //console.log('------------------------------');
+    for (let i = 1; i < size + 1; i++) {
+      const currNum = i.toString();
+      const isValid = validate(currNum, currPos, board);
+      //console.log('currPos ', currPos, 'currNum ',currNum, 'isValid ',isValid);
+      if (isValid) {
+        const [x, y] = currPos;
+        board[x][y] = currNum;
+
+        if (solve()) {
+          return true;
+        }
+
+        board[x][y] = ".";
+      }
+    }
+
+    return false;
+  };
+
+  solve();
+  return board;
+};
+console.log(solveSudoku(puzzle));
 
 function isSolved() {
   /**
