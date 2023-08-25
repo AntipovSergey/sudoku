1485df1a8bde92973c522b67bec873897ed99401
E.Apakina <e.apakina@gmail.com>
2023-08-25 15:33:29 +0300

    release solve

--- sudoku.js
+++ sudoku.js
@@ -55,25 +55,19 @@ function solve(arr) {
       arr[rowPosition][colomnPosition] = number;
       const result = solve(arr);
       if (result) {
-        console.log('я тут');
         return result;
       }
       arr[rowPosition][colomnPosition] = 0;
     }
-    console.log('я здесь');
   }
   return false;
-
-  if (findFirstZero(arr)[0] !== -1) { arr[rowPosition][colomnPosition] = 123; }
-  console.log('ttttt');
-  return arr;
 }
 const array1 = [[1, 0, 5, 8, 0, 2, 0, 0, 0], [0, 9, 0, 0, 7, 6, 4, 0, 5],
   [2, 0, 0, 4, 0, 0, 8, 1, 9],
   [0, 1, 9, 0, 0, 7, 3, 0, 6], [7, 6, 2, 0, 8, 3, 0, 9, 0], [0, 0, 0, 0, 6, 1, 0, 5, 0], [
     0, 0, 7, 6, 0, 0, 0, 3, 0], [4, 3, 0, 0, 2, 0, 5, 0, 1], [6, 0, 0, 3, 0, 8, 9, 0, 0]];
-
-console.table(solve(array1));
+console.table(array1);
+solve(array1);
 console.table(array1);
 
 function isSolved() {
