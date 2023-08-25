0191306349ec09774dd3816ec61679b297e1c38b
E.Apakina <e.apakina@gmail.com>
2023-08-25 14:01:34 +0300

    add function

--- sudoku.js
+++ sudoku.js
@@ -32,7 +32,7 @@ function checkSquare(arr, row, colomn, number) {
   const squareColomn = Math.floor(colomn / 3) * 3;
   for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 3; j++) {
-      if (arr[squareRow + i][squareColomn + colomn] === number) { return false; }
+      if (arr[squareRow + i][squareColomn + j] === number) { return false; }
     }
   }
   return true;
@@ -44,21 +44,28 @@ function checkAll(arr, row, colomn, number) {
 }
 function solve(arr) {
   const position = findFirstZero(arr);
+  console.log(position);
   const rowPosition = position[0];
   const colomnPosition = position[1];
   if (rowPosition === -1) { return arr; } // больше нет пустых позиций
   for (let number = 1; number < 10; number++) {
     if (checkAll(arr, rowPosition, colomnPosition, number)) {
       arr[rowPosition][colomnPosition] = number;
-      solve(arr);
+      const result=solve(arr);
+      if (result) return result;
+      arr[rowPosition][colomnPosition] = 0;
     }
   }
 
   if (findFirstZero(arr)[0] !== -1) { arr[rowPosition][colomnPosition] = 0; }
   return arr;
 }
-let array1=[1,0,5,8,0,2,0,0,0],[0,9,0,0,7,6,4,0,5],[2,0,0,4,0,0,8,1,9],[0,9,0,0,7,3,0,6,7],[6,2,0,8,3,0,9,0,0],[0,0,0,6,1,0,5,0,0],[0,7,6,0,0,0,3,0,4],[3,0,0,2,0,5,0,1,6],[0,0,3,0,8,9,0,0
+const array1 = [[1, 0, 5, 8, 0, 2, 0, 0, 0], [0, 9, 0, 0, 7, 6, 4, 0, 5],
+  [2, 0, 0, 4, 0, 0, 8, 1, 9],
+  [0, 1, 9, 0, 0, 7, 3, 0, 6], [7, 6, 2, 0, 8, 3, 0, 9, 0], [0, 0, 0, 0, 6, 1, 0, 5, 0], [
+    0, 0, 7, 6, 0, 0, 0, 3, 0], [4, 3, 0, 0, 2, 0, 5, 0, 1], [6, 0, 0, 3, 0, 8, 9, 0, 0]];
 
+console.log(solve(array1));
 
 function isSolved() {
   /**
