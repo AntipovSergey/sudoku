22acc2d64dceeb7ed817d007acbd86668a295b88
irinazar <iramiami@yandex.ru>
2023-08-25 16:10:10 +0300

    final read

--- sudoku.js
+++ sudoku.js
@@ -1,21 +1,26 @@
 const fs = require('fs');
 
 function read() {
-  const str = (fs.readFileSync('puzzles.txt', 'utf8')).split('\n')[0];
+  const a = process.argv[2];
+  console.log(a);
+
+  const str = (fs.readFileSync('puzzles.txt', 'utf8')).split('\n')[a - 1];
+
+  console.log(str);
 
   const newStr = [...str.replace(/-/g, 0)];
-
   const arrPazzle = [];
-  const smallArr = [];
-  for (let i = 0; i < newStr.length; i++) {
+  let smallArr = [];
+
+  for (let i = 0; i <= newStr.length; i++) {
     if (smallArr.length < 9) {
       smallArr.push(newStr[i]);
+    } else {
       arrPazzle.push(smallArr);
+      smallArr = [newStr[i]];
     }
   }
-
-
-  console.log(arrPazzle)
+  console.log(arrPazzle);
   return arrPazzle;
 }
 
