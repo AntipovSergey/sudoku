deca0719cde2ffc98e456faebed03743ddc1012f
ytka98 <rickda1ton@yandex.ru>
2023-09-15 12:31:01 +0300

    read

--- sudoku.js
+++ sudoku.js
@@ -1,10 +1,21 @@
-function read() {
-  /**
-   * Прочесть файл puzzles.txt в кодировке 'utf-8' и вернуть эти данные из функции
-   */
-}
+const {arr} = require('./getboard.js')
 
-function solve() {
+function read(arr) {
+  let num = 0;
+  let emptySpaces = []
+  for (let x = 0; x < arr.length; x++) {
+    for (let y = 0; y < arr.length; y++) {
+      if(arr[x][y] === 0){
+        emptySpaces.push([x,y])
+      }
+    }
+  }
+  console.log(emptySpaces)
+  return emptySpaces
+}
+read(arr)
+
+function solve(arr) {
   /**
    * Принимает игровое поле в том формате, в котором его вернули из функции read.
    * Возвращает игровое поле после попытки его решить.
@@ -25,3 +36,4 @@ function prettyBoard() {
    * Подумай, как симпатичнее его вывести.
    */
 }
+module.exports = {read}
