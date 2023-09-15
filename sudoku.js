2e706081c316f5be306a0255fad2ba1550f27407
maximbabich10 <babich10-maxim@mail.ru>
2023-09-15 12:22:47 +0300

    readFunc

--- sudoku.js
+++ sudoku.js
@@ -1,8 +1,22 @@
-function read() {
-  /**
-   * Прочесть файл puzzles.txt в кодировке 'utf-8' и вернуть эти данные из функции
-   */
+const fs = require('fs')
+function read(num = 0) {
+
+  const fullArr = fs.readFileSync('./puzzles.txt', 'utf-8').trim().split('\n')
+  const newArr = fullArr[num].split('')
+  const board = []
+  for (let i = 0; i < 9; i++) {
+    board.push(newArr.splice(0, 9))
+  }
+  return board
+
 }
+
+/**
+ * Прочесть файл puzzles.txt в кодировке 'utf-8' и вернуть эти данные из функции
+ */
+
+
+read()
 
 function solve() {
   /**
