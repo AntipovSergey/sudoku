5bd126d868b748460a759f6ff070c22cd9a93779
avsavenkov <orgtaktik@yandex.ru>
2023-09-15 15:15:01 +0300

    Release. Reading UPD, Find Empty

--- sudoku.js
+++ sudoku.js
@@ -1,19 +1,57 @@
 const fs = require("fs");
-function read() {
-  const file = fs.readFileSync("./puzzles.txt", "utf-8").replaceAll("\n", "");
-  const sud = [];
-  for (let i = 0; i < 15; i++) {
-    for (let i = 0; i < file.length; i += 9) {
-      sud.push(file.slice(i, i + 9));
-    }
+function read(num) {
+  const file = fs.readFileSync("./puzzles.txt", "utf-8").split("\n")
+  .map((el) => el.split(''))
+  .map((arr) => arr.map((el) => el === '-'? 0 : Number(el) ))
+  // const sud = [];
+  // for (let i = 0; i < 15; i++) {
+  //   for (let i = 0; i < file.length; i += 9) {
+  //     sud.push(file.slice(i, i + 9));
+  //   }
+  // }
+  // const board = [];
+  // for (let i = 0; i < 15; i += 1) {
+  //   board.push(sud.splice(0, 9));
+  // }
+  // return board.map((sudoku) => sudoku.map((row) => row.split("")));
+const newArr = []
+  for(let i = 0; i < 9; i++) {
+    newArr.push(file[num].splice(0,9))
   }
-  const board = [];
-  for (let i = 0; i < 15; i += 1) {
-    board.push(sud.splice(0, 9));
-  }
-  return board.map((sudoku) => sudoku.map((row) => row.split("")));
+  return newArr;
+
 }
-console.log(read());
+const board = read(0)
+console.table(board);
+
+
+
+function findEmpty(board) {
+  // const line = 9;
+  // const side = 3;
+  // const gameField = read()
+
+      for (let r = 0; r < 9; r++) {
+        for (let c = 0; c < 3; c++) {
+          if (board[r][c] === 0) {
+            return [r,c]
+          }
+        }
+      }
+      return null;
+
+    }
+
+    const empty = findEmpty(board)  
+    if(!empty){
+      return true
+    }
+
+    const [row, col] = empty;
+
+    
+
+    console.log(empty);
 
 function solve() {
   /**
