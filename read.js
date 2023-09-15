912f6b6bcb1e6fb0097afe6500396c950d81a739
Artur Agafontsev <dingodevelop@gmail.com>
2023-09-15 12:20:56 +0300

    readyFunc

--- read.js
+++ read.js
@@ -3,7 +3,6 @@ const fs = require('fs');
 function read() {
   const readingFile = fs.readFileSync('./puzzles.txt', 'utf8').trim().split('\n');
   const sudoku = readingFile.map((el) => Array.from(el));
-  // console.log(sudoku);
 }
 read();
 
