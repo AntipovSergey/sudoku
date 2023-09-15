9621ae318b69e82a377eb9017f2ce0895fe3493f
FirsBroSman <firsbrosman@gmail.com>
2023-09-15 13:46:59 +0300

    Check_Step

--- sudoku.js
+++ sudoku.js
@@ -25,7 +25,24 @@ function solve() {
    */
 }
 
-function isSolved() {
+function isSolved(num, board, colum, row) {
+  num = num.toString()
+  for(let i = 0; i < board.length; i++) {
+    if(board[row][i] === num || board[i][colum] === num) {
+      return false
+    }
+    const startRow = Math.floor(startRow / 3)*3
+    const startColum = Math.floor((startColum / 3)*3)
+    for(let j = startRow; j < startRow + 3; j++) {
+      for(let k = startColum; k < startColum + 3; k++) {
+        if(board[j][k] === num) {
+          return false
+        }
+      }
+
+    } 
+  }
+
   /**
    * Принимает игровое поле в том формате, в котором его вернули из функции solve.
    * Возвращает булевое значение — решено это игровое поле или нет.
