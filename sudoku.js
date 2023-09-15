4c5e0d0ae5b0637bf123743cc39219956c49c2c8
Oldnesss <viv251985@gmail.com>
2023-09-15 16:23:46 +0300

    viev

--- sudoku.js
+++ sudoku.js
@@ -1,8 +1,10 @@
-const fs = require("fs");
+const fs = require('fs');
 function read(num) {
-  const file = fs.readFileSync("./puzzles.txt", "utf-8").split("\n")
-  .map((el) => el.split(''))
-  .map((arr) => arr.map((el) => el === '-'? 0 : Number(el) ))
+  const file = fs
+    .readFileSync('./puzzles.txt', 'utf-8')
+    .split('\n')
+    .map((el) => el.split(''))
+    .map((arr) => arr.map((el) => (el === '-' ? 0 : Number(el))));
   // const sud = [];
   // for (let i = 0; i < 15; i++) {
   //   for (let i = 0; i < file.length; i += 9) {
@@ -14,44 +16,38 @@ function read(num) {
   //   board.push(sud.splice(0, 9));
   // }
   // return board.map((sudoku) => sudoku.map((row) => row.split("")));
-const newArr = []
-  for(let i = 0; i < 9; i++) {
-    newArr.push(file[num].splice(0,9))
+  const newArr = [];
+  for (let i = 0; i < 9; i++) {
+    newArr.push(file[num].splice(0, 9));
   }
   return newArr;
-
 }
-const board = read(0)
+const board = read(0);
 console.table(board);
-
-
 
 function findEmpty(board) {
   // const line = 9;
   // const side = 3;
   // const gameField = read()
 
-      for (let r = 0; r < 9; r++) {
-        for (let c = 0; c < 3; c++) {
-          if (board[r][c] === 0) {
-            return [r,c]
-          }
-        }
+  for (let r = 0; r < 9; r++) {
+    for (let c = 0; c < 3; c++) {
+      if (board[r][c] === 0) {
+        return [r, c];
       }
-      return null;
-
     }
+  }
+  return null;
+}
 
-    const empty = findEmpty(board)  
-    if(!empty){
-      return true
-    }
+const empty = findEmpty(board);
+if (!empty) {
+  return true;
+}
 
-    const [row, col] = empty;
+const [row, col] = empty;
 
-    
-
-    console.log(empty);
+console.log(empty);
 
 function solve() {
   /**
@@ -67,10 +63,18 @@ function isSolved() {
    */
 }
 
-function prettyBoard() {
-  /**
-   * Принимает игровое поле в том формате, в котором его вернули из функции solve.
-   * Выводит в консоль/терминал судоку.
-   * Подумай, как симпатичнее его вывести.
-   */
+function prettyBoard(board) {
+  for (let row = 0; row < 9; row++) {
+    if (row % 3 === 0 && row !== 0) {
+      console.log("-".repeat(21)); // Горизонтальная линия для разделения блоков
+    }
+    for (let col = 0; col < 9; col++) {
+      if (col % 3 === 0 && col !== 0) {
+        process.stdout.write("| "); // Вертикальная черта для разделения блоков
+      }
+      process.stdout.write(board[row][col] + " ");
+    }
+
+  }
 }
+
