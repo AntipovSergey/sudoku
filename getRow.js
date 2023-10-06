c32bbc6377f8d38453c5be4c40d57e66a2075827
Валентин Мысякин <mysyakinq@gmail.com>
2023-10-06 15:25:32 +0300

    fix exports

--- getRow.js
+++ getRow.js
@@ -2,8 +2,6 @@ function getRow(sud, index) {
   const ind = Math.floor(index / 9);
   const start = ind * 9;
   const finish = start + 9;
-  console.log(start);
-  console.log(finish);
   return sud.slice(start, finish);
 }
 module.exports = { getRow };
--- runner.js
+++ runner.js
@@ -1,4 +1,6 @@
 // Используйте для решения судоку необходимые функции из файла sudoku.js
-const {read, solve, isSolved, prettyBoard} = require('./sudoku');
+const {
+  read, solve, isSolved, prettyBoard,
+} = require('./sudoku');
 
 const puzzles = read('/puzzles.txt');
