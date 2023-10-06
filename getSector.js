baa190d5bddca6625ae311f1ddd1ceec82923308
KirillMaslov <kirill.maslov777@gmail.com>
2023-10-06 15:39:38 +0300

    add getSector

---
+++ getSector.js
@@ -0,0 +1,40 @@
+function getSector(sud, ind) {
+  const ind0 = [0, 1, 2, 9, 10, 11, 18, 19, 20];
+  const ind1 = [3, 4, 5, 12, 13, 14, 21, 22, 23];
+  const ind2 = [6, 7, 8, 15, 16, 17, 24, 25, 26];
+  const ind3 = [27, 28, 29, 35, 37, 38, 45, 46, 47];
+  const ind4 = [30, 31, 32, 39, 40, 41, 48, 49, 50];
+  const ind5 = [33, 34, 35, 42, 43, 44, 51, 52, 53];
+  const ind6 = [54, 55, 56, 63, 64, 65, 72, 73, 74];
+  const ind7 = [57, 58, 59, 66, 67, 68, 75, 76, 77];
+  const ind8 = [60, 61, 62, 69, 70, 71, 78, 79, 80];
+  const obj = {
+    0: ind0,
+    1: ind1,
+    2: ind2,
+    3: ind3,
+    4: ind4,
+    5: ind5,
+    6: ind6,
+    7: ind7,
+    8: ind8,
+  };
+  const result = [];
+  const row = Math.floor(ind / 9);
+  const col = Math.floor(ind % 9);
+  const sector = Math.floor(row / 3) * 3 + Math.floor(col / 3);
+  const indq = obj[sector];
+  console.log(indq);
+  //   for (let i = 0; i < 3; i++) {
+  //     for (let j = 0; j < 3; j++) {
+  //       const ind = sud[i * sector + col + (i % 3) + Math.floor(i / 3) * 9];
+  //       console.log(
+  //         Math.floor(row / 3) * 27 + Math.floor(col / 3) * 3 + i * 9 + j
+  //       );
+  //       result.push(sud[ind]);
+  //     }
+  //   }
+  return result;
+}
+
+module.exports = { getSector };
--- runner.js
+++ runner.js
@@ -1,4 +1,7 @@
 // Используйте для решения судоку необходимые функции из файла sudoku.js
-const {read, solve, isSolved, prettyBoard} = require('./sudoku');
+const { read, solve, isSolved, prettyBoard } = require("./sudoku");
+const { getSector } = require("./getSector");
 
-const puzzles = read('/puzzles.txt');
+const puzzles = read("/puzzles.txt");
+
+console.log(getSector(puzzles[0], 8));
