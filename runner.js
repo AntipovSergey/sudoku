7510646e404bff59423731496889beb5a347a0a6
Валентин Мысякин <mysyakinq@gmail.com>
2023-10-06 16:57:09 +0300

    final releasew

--- runner.js
+++ runner.js
@@ -1,10 +1,15 @@
 // Используйте для решения судоку необходимые функции из файла sudoku.js
 const { read, solve, isSolved, prettyBoard } = require("./sudoku");
 const { getSector } = require("./getSector");
-
+const {possibleValues} = require("./possibleValues")
 
 const puzzles = read("/puzzles.txt");
 
 // solve(puzzles[0]);
 
-console.log(getSector(puzzles[0], 56))
+// console.log(getSector(puzzles[0], 56))
+let solution = [];
+solution = possibleValues(puzzles[0]);
+console.log(solution)
+solution = possibleValues(solution);
+console.log(solution)
