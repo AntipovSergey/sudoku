056d1aaf3f995260514fe0adead5b7ba26d4eb57
Eldiyar88 <eldiyareltaev@gmail.com>
2023-10-06 14:10:03 +0300

    working profress commit

--- sudoku.js
+++ sudoku.js
@@ -1,10 +1,16 @@
-const arrTest = ["1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--"]
+const strTest = "1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--"
 
 
 function read() {
-  /**
-   * Прочесть файл puzzles.txt в кодировке 'utf-8' и вернуть эти данные из функции
-   */
+  let arr = [[], [], [], [], [], [], [], [], []]
+  for (let i = 0; i < strTest.length - 1; i += 8) {
+    let counter = 0
+    if (arr[counter].length < 9) {
+      arr[counter].push(strTest[i])
+    } else {
+      counter++
+    }
+  }
 }
 
 function solve() {
