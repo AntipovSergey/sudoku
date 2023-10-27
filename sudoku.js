5d8dcfb8e742a6c8bfd4b234ec8d99aa5664e918
Avupa <ruslan.davletshin@mail.ru>
2023-10-27 14:07:02 +0300

    rename arrs

--- sudoku.js
+++ sudoku.js
@@ -3,42 +3,38 @@ const fs = require('fs')
 
 function read(nameFile) {
   const file = fs.readFileSync(`${__dirname}/${nameFile}`, 'utf-8');
-  const allFilds = file.trim().split('\n')
-  // allFilds = allFilds.map((el) => el.split())
-  // const arr = [];
-  // for (let i = 0; i < allFilds.length; i += 9) {
-  //   arr.push()
-  // }
-  const arr = [];
-  for (let el of allFilds) {
+  const allFieldsStr = file.trim().split('\n')
+  const allRows = [];
+  for (let el of allFieldsStr) {
     for (let i = 0; i < el.length; i += 9) {
-      arr.push(el.slice(i, i+9))
+      allRows.push(el.slice(i, i+9))
     }
   }
 
-  const newArr = [];
-  for (let i = 0; i < arr.length; i += 9) {
-    newArr.push(arr.slice(i, i+9))
+  const allFields = [];
+  for (let i = 0; i < allRows.length; i += 9) {
+    allFields.push(allRows.slice(i, i+9))
   }
 
-  for (let i = 0; i < newArr.length; i++) {
-    for (let j = 0; j < newArr[i].length; j++) {
-      newArr[i][j] = newArr[i][j].split('')
+  for (let i = 0; i < allFields.length; i++) {
+    for (let j = 0; j < allFields[i].length; j++) {
+      allFields[i][j] = allFields[i][j].split('')
     }
   }
-  return newArr
+  return allFields
 }
-
-
-
-read('puzzles.txt')
 
 function solve(arr, numFild) {
   /**
    * Принимает игровое поле в том формате, в котором его вернули из функции read.
    * Возвращает игровое поле после попытки его решить.
    */
+
+  const Field = arr[numFild];
+  console.table(Field);
+
 }
+solve(read('puzzles.txt'),0)
 
 function isSolved() {
   /**
