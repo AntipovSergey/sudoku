096c99d3aa4dc857dd729059246266b16a38cb3b
Dmitriy <dim0on@mail.ru>
2023-10-27 14:57:04 +0300

    функция prettyBoard

--- sudoku.js
+++ sudoku.js
@@ -11,23 +11,29 @@ function read() {
 
   for (let i = 0; i < newStr.length; i += 9) {
     const newLine = newStr.slice(i, i + 9);
-    sudoku.push(Array.from(newLine));
+    const line = Array.from(newLine);
+    for (let element of line) {
+      element = parseInt(element);
+      console.log(typeof (element));
+    }
+    sudoku.push(Array.from(line));
   }
 
   return sudoku;
 }
 
-const arr = [
-  [1, '-', 5, 8, '-', 2, '-', '-', '-'],
-  ['-', 9, '-', '-', 7, 6, 4, '-', '5'],
-  [2, '-', '-', 4, '-', '-', 8, 1, 9],
-  ['-', 1, 9, '-', '-', 7, 3, '-', 6],
-  [7, 6, 2, '-', 8, 3, '-', 9, '-'],
-  ['-', '-', '-', '-', 6, 1, '-', 5, '-'],
-  ['-', '-', 7, 6, '-', '-', '-', 3, '-'],
-  [4, 3, '-', '-', 2, '-', 5, '-', 1],
-  [6, '-', '-', 3, '-', 8, 9, '-', '-'],
-];
+// [
+//   [1, '-', 5, 8, '-', 2, '-', '-', '-'],
+//   ['-', 9, '-', '-', 7, 6, 4, '-', '5'],
+//   [2, '-', '-', 4, '-', '-', 8, 1, 9],
+//   ['-', 1, 9, '-', '-', 7, 3, '-', 6],
+//   [7, 6, 2, '-', 8, 3, '-', 9, '-'],
+//   ['-', '-', '-', '-', 6, 1, '-', 5, '-'],
+//   ['-', '-', 7, 6, '-', '-', '-', 3, '-'],
+//   [4, 3, '-', '-', 2, '-', 5, '-', 1],
+//   [6, '-', '-', 3, '-', 8, 9, '-', '-'],
+// ];
+const arr = read();
 
 function solve(arr) {
   const possibleNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -45,7 +51,7 @@ function solve(arr) {
   }
   return arr;
 }
-console.log(solve(arr));
+console.log(read());
 
 function isSolved() {
   /**
@@ -55,5 +61,17 @@ function isSolved() {
 }
 
 function prettyBoard() {
+  const output = solve(arr);
 
+  output = `
+${arr[0]}\n
+${arr[1]}\n
+${arr[2]}\n
+${arr[3]}\n
+${arr[4]}\n
+${arr[5]}\n
+${arr[6]}\n
+${arr[7]}\n
+${arr[8]}\n`;
+console.log
 }
