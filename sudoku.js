2c2f884ac5c6f7653423da587b7054292f85126e
winnig181 <otiumpost@yandex.ru>
2023-10-27 14:22:03 +0300

    first on solve

--- sudoku.js
+++ sudoku.js
@@ -1,18 +1,40 @@
+const { Linter } = require('eslint');
 const fs = require('fs');
-function read() { 
-  /**
-   * Прочесть файл puzzles.txt в кодировке 'utf-8' и вернуть эти данные из функции
-   */
-  const data = fs.readFileSync('./puzzles.txt','utf-8');
-  console.log(data);
+function read(num) { 
+
 }
-read();
-function solve() {
+// console.log(read(0));
+
+const game = read(0);
+
+function solve(game) {
+ 
+//   console.table(game);
+//   let findings = [];
+//   game.forEach(line => {
+//     const sum = line.reduce((acc,cv)=>acc+cv,0);
+//     const missing = 45-sum;
+// let emptyFiguresIndices = []
+// line.forEach(fig,index => {
+//   fig === '-' ?  emptyFiguresIndices.push(index):console.log("Линия заполнена")
+//   console.log(emptyFiguresIndices)
+// });
+// if (missing <=9 && emptyFiguresIndices/length === 1){
+//   line[emptyFiguresIndices[0]] = missing;
+// }
+//     findings.push(missing);
+//   });
+//   return findings;  
+}
+// Проверки:
+
+
   /**
    * Принимает игровое поле в том формате, в котором его вернули из функции read.
    * Возвращает игровое поле после попытки его решить.
    */
-}
+
+solve(game);
 
 function isSolved() {
   /**
@@ -29,9 +51,9 @@ function prettyBoard() {
    */
 }
 
-module.exports = {
-  read,
-  solve,
-  isSolved,
-  prettyBoard
-}
+// module.exports = {
+//   read,
+//   solve,
+//   isSolved,
+//   prettyBoard
+// }
