1c7009a8bc151b9894b231c14b4c1173ff294d30
Tseren108 <tseren108@gmail.com>
2023-11-17 16:15:20 +0300

    function prettyBoard

--- sudoku.js
+++ sudoku.js
@@ -19,25 +19,20 @@ function read() {
   return sudoku;
 }
 
-function solve() {
-  const sudoku = read();
-  const solvedSudoku = solve(sudoku);
-  function solve(sudoku) {
-    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    for (let i = 0; i < sudoku.length; i++) {
-      for (let j = 0; j < sudoku[i].length; j++) {
-        if (sudoku[i][j] === 0) {
-          sudoku[i][j] = Math.floor(Math.random() * numbers.length);
-        }
+const sudoku = read();
+const solvedSudoku = solve(sudoku);
+function solve(sudoku) {
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  for (let i = 0; i < sudoku.length; i++) {
+    for (let j = 0; j < sudoku[i].length; j++) {
+      if (sudoku[i][j] === 0) {
+        sudoku[i][j] = Math.floor(Math.random() * numbers.length);
       }
     }
-    return sudoku;
   }
-  /**
-   * Принимает игровое поле в том формате, в котором его вернули из функции read.
-   * Возвращает игровое поле после попытки его решить.
-   */
+  return sudoku;
 }
+  
 
 function isSolved() {
   /**
@@ -46,10 +41,34 @@ function isSolved() {
    */
 }
 
-function prettyBoard() {
-  /**
-   * Принимает игровое поле в том формате, в котором его вернули из функции solve.
-   * Выводит в консоль/терминал судоку.
-   * Подумай, как симпатичнее его вывести.
-   */
+const output = solve(sudoku);
+function prettyBoard(output) {
+  for (const str of output) {
+    for (let i = 2; i < output.length; i += 3) {
+      str[i] = `${str[i]}|`;
+    }
+    for (let i = 0; i < output.length; i += 9) {
+      str[i] = `|${str[i]}`;
+    }
+  }
+
+  const result = `
+
+  - - - - - - - - - - - - - - -\n
+  ${output[0].join('  ')}\n
+  ${output[1].join('  ')}\n
+  ${output[2].join('  ')}\n
+  - - - - - - - - - - - - - - -\n
+  ${output[3].join('  ')}\n
+  ${output[4].join('  ')}\n
+  ${output[5].join('  ')}\n
+  - - - - - - - - - - - - - - -\n
+  ${output[6].join('  ')}\n
+  ${output[7].join('  ')}\n
+  ${output[8].join('  ')}\n
+  - - - - - - - - - - - - - - -\n`;
+
+  return result;
 }
+
+console.table(prettyBoard(output));
