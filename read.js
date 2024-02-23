e58092b2e5eb01a132c003e42e4162881df08702
Alexey-Plekhanov <plehanov234@gmail.com>
2024-02-23 14:45:17 +0300

    release 0

---
+++ read.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+const read = () => {
+    const num = process.argv[2];
+    const data = fs.readFileSync('./puzzles.txt', 'utf-8').trim().split('\n');
+    
+    const sudoku = data[num].split(''); 
+    
+    const arr = [];
+    for (let i = 0; i < 81; i += 9) {
+        arr.push(sudoku.slice(i, i + 9));
+    }
+    return arr;
+}
+
+// console.log(read())
+
+module.exports = read;
