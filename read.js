0e14ae29bf28461fba50a5a91496b4c0c4522c9e
sashaserov2 <sashaserov2@gmail.com>
2024-04-05 12:13:47 +0300

    final release read file

--- read.js
+++ read.js
@@ -5,6 +5,9 @@ const read = (num) => {
     const data = fs.readFileSync('puzzles.txt', 'utf-8')
     const lines = data.split('\n')
     const line = lines[num].split('')
-
+    for (let i = 0; i < line.length; i++){
+        arr.push(line.splice(0, 9).map((el) => Number(el.replaceAll('-', '0'))))
+    }
+    return arr
 }
 console.log(read(1))
