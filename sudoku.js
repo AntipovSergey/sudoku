7f4b8039952248637277b13e4583ad166ed09a7f
Batait <velichko.dmitrii97@gmail.com>
2024-04-05 13:27:08 +0300

    1

--- sudoku.js
+++ sudoku.js
@@ -4,12 +4,25 @@ function read() {
    */
 }
 
-function solve() {
-  /**
-   * Принимает игровое поле в том формате, в котором его вернули из функции read.
-   * Возвращает игровое поле после попытки его решить.
-   */
+const test1 = [
+  [1, 0, 3], [4, 5, 0], [7, 0, 9],
+  // [[2, 1, null], [7, null, 1], [null, 5, 4]],
+  // [[null, 9, 1], [6, null, 3], [7, 2, null]]
+];
+
+function solve(arr) {
+  let kor = []
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      const x = 
+      if (arr[i][j] === 0) {
+      }
+    }
+  }
+  return kor
 }
+
+console.table(solve(test1));
 
 function isSolved() {
   /**
