fd1a7b8abf95714ed63f1b1c1ac5e58abf5d7cc1
varyayaru <vgya00@mail.ru>
2024-04-05 12:32:03 +0300

    prettyBoard

---
+++ prettyBoard.js
@@ -0,0 +1,14 @@
+function prettyBoard(arr) {
+  const chertArr = new Array(9).fill('-');
+  const count = arr.length;
+  for (let i = 0; i < count; i++) {
+    const index = i * 2 + 1;
+    arr.splice(index, 0, chertArr);
+  }
+  arr.pop();
+  console.log(arr.join('\n').replaceAll(',', ' | '));
+}
+prettyBoard(arr);
+module.exports = {
+  prettyBoard,
+};
