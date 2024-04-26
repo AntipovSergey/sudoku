29a5b04756fd8c3dff3d7740f439466f40ec421e
mashak000 <mashakgood@gmail.com>
2024-04-26 12:04:09 +0300

    checkRow done

--- sudoku.js
+++ sudoku.js
@@ -28,8 +28,36 @@ function prettyBoard() {
 
 function findCellValues(sudoku, i, j) {
   const allPossibleValues = [];
-  allPossibleValues = allPossibleValues.concat(checkRow(sudoku, i, j));
-  allPossibleValues = allPossibleValues.concat(checkCol(sudoku, i, j));
-  allPossibleValues = allPossibleValues.concat(checkCube(sudoku, i, j));
+  const values = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  allPossibleValues = allPossibleValues.concat(checkRow(sudoku, i, values));
+  allPossibleValues = allPossibleValues.concat(checkCol(sudoku, i, j, values));
+  allPossibleValues = allPossibleValues.concat(checkCube(sudoku, i, j, values));
   return allPossibleValues;
 }
+
+// проверит все значения в строчке
+function checkRow(sudoku, i, values) {
+  const result = [...values];
+  for (let k = 0; k < sudoku[i].length; k += 1) {
+    if (sudoku[i][k] !== 0) {
+      if (result.includes(sudoku[i][k])) {
+        const index = result.indexOf(sudoku[i][k]);
+        result.splice(index, 1)
+      }
+    }
+  }
+  return result;
+}
+
+// const testPuzzle = [
+//   [0, 1, 2, 0, 0, 0, 4, 9, 7],
+//   [0, 0, 2, 0, 0, 0, 4, 9, 7],
+//   [3, 1, 2, 0, 0, 0, 4, 0, 7],
+//   [8, 0, 2, 0, 5, 0, 4, 9, 7],
+//   [6, 1, 2, 0, 0, 0, 4, 0, 7],
+//   [0, 0, 2, 0, 0, 0, 4, 9, 7],
+//   [0, 1, 2, 0, 0, 0, 4, 0, 7]
+// ];
+// const values = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// console.log(checkRow(testPuzzle, 0, values))
