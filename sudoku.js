5079c0f10d231d8686a476428f795854ebaf09cf
Idris <murtazaliev67@gmail.com>
2024-04-26 16:25:44 +0300

    Release 1

--- sudoku.js
+++ sudoku.js
@@ -1,8 +1,7 @@
-function read() {
-  return [
-    ['5', '3', '.', '.', '7', '.', '.', '.', '.'],
+const board = [
+    ['5', '3', '.', '.', '4', '.', '.', '.', '.'],
     ['6', '.', '.', '1', '9', '5', '.', '.', '.'],
-    ['.', '9', '8', '.', '.', '.', '.', '6', '.'],
+    ['.', '9', '4', '.', '.', '.', '.', '6', '.'],
     ['8', '.', '.', '.', '6', '.', '.', '.', '3'],
     ['4', '.', '.', '8', '.', '3', '.', '.', '1'],
     ['7', '.', '.', '.', '2', '.', '.', '.', '6'],
@@ -10,36 +9,35 @@ function read() {
     ['.', '.', '.', '4', '1', '9', '.', '.', '5'],
     ['.', '.', '.', '.', '8', '.', '.', '7', '9']
   ]
-}
 
-function solve() {
-  let board = read()
+function emptyEl(arr) {
 
-  let pos = []
   for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board[i].length; j++) {
       if (board[i][j] === '.') {
-        pos.push([i, j])
+        return [i, j]
       }
     }
   }
-  return null
 }
 
-console.log(solve())
+console.log(emptyEl(board));
+
 
 function isValid(row, colum, num, board) {
-  for (let i = 0; i < board.length; i++) {
-    if (board[row][i] === num && board[row][i] !== board[row][colum]) {
-      return false
-    }
-  }
+  num = num.toString()
 
-  for (let j = 0; j < board.length; j++) {
-    if (board[i][colum] === num && board[i][colum] !== board[row][colum]) {
+  for (let i = 0; i < board.length; i++) {
+    if (board[row][i] === num || board[i][colum] === num) {
       return false
     }
   }
+  // for (let j = 0; j < board.length; j++) {
+  //   if (board[j][colum] === num && board[j][colum] !== board[row][colum]) {
+  //     return false
+  //   }
+  //   console.log(board[j][2]);
+  // }
 
   let countRow = Math.floor(row / 3) * 3
   let countColum = Math.floor(colum / 3) * 3
@@ -49,43 +47,62 @@ function isValid(row, colum, num, board) {
       if (board[e][l] === num && board[e][l] !== board[row][colum]) {
         return false
       }
-      if (board[l][e] === num && board[l][e] !== board[colum][row]) {
-        return false
-      }
     }
   }
 
   return true
 }
 
-function countNumber(pos) {
-  if (pos.length === 0) {
-    return true
-  } else {
+
+
+
+
+function solve() {
     for (let num = 1; num < 10; num += 1) {
-      for (let position = 0; position < pos.length; position++) {
-        let [row, colum] = [position[i][0], position[i][1]]
-        if (isvalid(row, colum, num)) {
-          board[row][colum] = num
+      let pos = emptyEl(board)
+      let [row, colum] = [pos[0], pos[1]]
+
+  
+        if (isValid(row, colum, num, board)) {
+          board[row][colum] = num.toString()
+        } else {
+          board[row][colum] = '.'
         }
       }
+      return board
     }
-  }
-}
 
-console.log(countNumber(solve()))
+  
 
-function isSolved() {
-  /**
-   * Принимает игровое поле в том формате, в котором его вернули из функции solve.
-   * Возвращает булевое значение — решено это игровое поле или нет.
-   */
-}
+  
+console.table(solve(emptyEl()))
 
-function prettyBoard() {
-  /**
-   * Принимает игровое поле в том формате, в котором его вернули из функции solve.
-   * Выводит в консоль/терминал судоку.
-   * Подумай, как симпатичнее его вывести.
-   */
-}
+
+
+
+//   function isSolved(arr, obj) {
+//     const resss = [];
+//     for (let i = 0; i < arr.length; i++) {
+//       for (let j = 0; j < arr[i].length; j++) {
+//       }
+//     }
+//   }
+
+//   function prettyBoard(isSolved) {
+//     for (let i = 0; i < isSolved.length; i++) {
+//       if (i % 3 === 0 && i !== 0) {
+//         console.log('---- ---- ---- ----');
+//       }
+//       let newStr = '';
+//       for (let j = 0; j < isSolved[i].length; j++) {
+//         if (j % 3 === 0 && j !== 0) {
+//           newStr += '|';
+//         }
+//         newStr += `${isSolved[i][j]} `;
+//       }
+//       console.log(newStr);
+//     }
+//   }
+
+// console.log(prettyBoard(isSolved(solve)))
+
