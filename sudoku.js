f319be0c4c79a7df3d3166ae60198fa4e78a690c
Daniil <danchik890@mail.ru>
2024-04-26 15:20:44 +0300

    release new fanc

--- sudoku.js
+++ sudoku.js
@@ -23,14 +23,8 @@ function solve(puzleDone) {
   for (let row = 0; row < 9; row++) {
         for (let col = 0; col < 9; col++) {
             if (puzleDone[row][col] === 0) {
-                for (let num = 1; num <= 9; num++) {
-                    if (isValid(puzleDone, row, col, num)) {
-                        puzleDone[row][col] = num;
-                        if (solveSudoku(puzleDone)) {
-                            return true;
-                        }
-                        puzleDone[row][col] = 0; // Backtrack
-                    }
+              for (let num = 1; num <= 9; num++) {
+                  
                 }
                 return false; // Не найдено подходящего числа для этой ячейки
             }
@@ -39,7 +33,6 @@ function solve(puzleDone) {
     return true
 
 }
-//solve()
 function isSolved() {
   /**
    * Принимает игровое поле в том формате, в котором его вернули из функции solve.
