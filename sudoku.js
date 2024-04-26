0bc93c5756c79457e5e8fe75403d4a3f4f4a84ef
NikitaUsachev <nikitatekknik@yandex.ru>
2024-04-26 14:31:05 +0300

    added func_prettyBoard

--- sudoku.js
+++ sudoku.js
@@ -1,4 +1,6 @@
 function read() {
+
+
   /**
    * Прочесть файл puzzles.txt в кодировке 'utf-8' и вернуть эти данные из функции
    */
@@ -18,10 +20,19 @@ function isSolved() {
    */
 }
 
-function prettyBoard() {
-  /**
-   * Принимает игровое поле в том формате, в котором его вернули из функции solve.
-   * Выводит в консоль/терминал судоку.
-   * Подумай, как симпатичнее его вывести.
-   */
+function prettyBoard(arr) {
+  for (let i = 0; i < isSolved.length; i++) {
+    if (i % 3 === 0 && i !== 0) {
+      console.log('---- ---- ---- ----');
+    }
+    let newStr = '';
+    for (let j = 0; j < isSolved[i].length; j++) {
+      if (j % 3 === 0 && j !== 0) {
+        newStr += '|';
+      }
+      newStr += `${isSolved[i][j]} `;
+    }
+    console.log(newStr);
+  }
 }
+console.log(prettyBoard())
