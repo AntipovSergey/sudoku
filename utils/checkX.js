d9867ebe00d33d1a16fd072cbb9bea000720001a
csiop_(Mikhail_V) <mike0106vas@gmail.com>
2024-06-14 17:00:01 +0200

    add checkX & Y + change value

--- utils/checkX.js
+++ utils/checkX.js
@@ -9,62 +9,65 @@ let bord = [
   [4, 3, 0, 0, 2, 0, 5, 0, 1],
   [6, 0, 0, 3, 0, 8, 9, 0, 0],
 ];
-
-function solve(bord, x, num) {
-  let res1 = [];
-  let res2 = [];
-  for (let i = 0; i < bord.length; i++) {
-    res1.push(bord[x][i]);
-    res2.push(bord[i][x]);
-  }
-
-  for (let i = 0; i < res1.length; i++) {
-    if (res1[i] === 0 && res1.indexOf(1) < 0) {
-      res1[i] = 1;
-    } else if (res1[i] === 0 && res1.indexOf(2) < 0) {
-      res1[i] = 2;
-    } else if (res1[i] === 0 && res1.indexOf(3) < 0) {
-      res1[i] = 3;
-    } else if (res1[i] === 0 && res1.indexOf(4) < 0) {
-      res1[i] = 4;
-    } else if (res1[i] === 0 && res1.indexOf(5) < 0) {
-      res1[i] = 5;
-    } else if (res1[i] === 0 && res1.indexOf(6) < 0) {
-      res1[i] = 6;
-    } else if (res1[i] === 0 && res1.indexOf(7) < 0) {
-      res1[i] = 7;
-    } else if (res1[i] === 0 && res1.indexOf(8) < 0) {
-      res1[i] = 8;
-    } else if (res1[i] === 0 && res1.indexOf(9) < 0) {
-      res1[i] = 9;
+for (let i = 0; i < bord.length; i++) {
+  const solve = (bord, x, num) => {
+    let res1 = [];
+    let res2 = [];
+    for (let i = 0; i < bord.length; i++) {
+      res1.push(bord[x][i]);
+      res2.push(bord[i][x]);
     }
-  }
 
-  for (let i = 0; i < res2.length; i++) {
-    if (res2[i] === 0 && res2.indexOf(1) < 0) {
-      res2[i] = 1;
-    } else if (res2[i] === 0 && res2.indexOf(2) < 0) {
-      res2[i] = 2;
-    } else if (res2[i] === 0 && res2.indexOf(3) < 0) {
-      res2[i] = 3;
-    } else if (res2[i] === 0 && res2.indexOf(4) < 0) {
-      res2[i] = 4;
-    } else if (res2[i] === 0 && res2.indexOf(5) < 0) {
-      res2[i] = 5;
-    } else if (res2[i] === 0 && res2.indexOf(6) < 0) {
-      res2[i] = 6;
-    } else if (res2[i] === 0 && res2.indexOf(7) < 0) {
-      res2[i] = 7;
-    } else if (res2[i] === 0 && res2.indexOf(8) < 0) {
-      res2[i] = 8;
-    } else if (res2[i] === 0 && res2.indexOf(9) < 0) {
-      res2[i] = 9;
+    for (let i = 0; i < res1.length; i++) {
+      if (res1[i] === 0 && res1.indexOf(1) < 0) {
+        res1[i] = 1;
+      } else if (res1[i] === 0 && res1.indexOf(2) < 0) {
+        res1[i] = 2;
+      } else if (res1[i] === 0 && res1.indexOf(3) < 0) {
+        res1[i] = 3;
+      } else if (res1[i] === 0 && res1.indexOf(4) < 0) {
+        res1[i] = 4;
+      } else if (res1[i] === 0 && res1.indexOf(5) < 0) {
+        res1[i] = 5;
+      } else if (res1[i] === 0 && res1.indexOf(6) < 0) {
+        res1[i] = 6;
+      } else if (res1[i] === 0 && res1.indexOf(7) < 0) {
+        res1[i] = 7;
+      } else if (res1[i] === 0 && res1.indexOf(8) < 0) {
+        res1[i] = 8;
+      } else if (res1[i] === 0 && res1.indexOf(9) < 0) {
+        res1[i] = 9;
+      }
     }
-  }
 
-  for (let i = 0; i < bord.length; i++) {
-    bord[x][i] = res1[i];
-    bord[i][x] = res2[i];
-  }
+    for (let i = 0; i < res2.length; i++) {
+      if (res2[i] === 0 && res2.indexOf(1) < 0) {
+        res2[i] = 1;
+      } else if (res2[i] === 0 && res2.indexOf(2) < 0) {
+        res2[i] = 2;
+      } else if (res2[i] === 0 && res2.indexOf(3) < 0) {
+        res2[i] = 3;
+      } else if (res2[i] === 0 && res2.indexOf(4) < 0) {
+        res2[i] = 4;
+      } else if (res2[i] === 0 && res2.indexOf(5) < 0) {
+        res2[i] = 5;
+      } else if (res2[i] === 0 && res2.indexOf(6) < 0) {
+        res2[i] = 6;
+      } else if (res2[i] === 0 && res2.indexOf(7) < 0) {
+        res2[i] = 7;
+      } else if (res2[i] === 0 && res2.indexOf(8) < 0) {
+        res2[i] = 8;
+      } else if (res2[i] === 0 && res2.indexOf(9) < 0) {
+        res2[i] = 9;
+      }
+    }
+
+    for (let i = 0; i < bord.length; i++) {
+      bord[x][i] = res1[i];
+      bord[i][x] = res2[i];
+    }
+  };
+  solve(bord, i);
 }
-solve(bord, 1);
+
+console.log(bord);
