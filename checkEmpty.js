5a8340b32b145fac4d146248a90034e26000f9ce
q-zin <kuzin.esq@gmail.com>
2024-06-14 14:28:50 +0300

    checkEmpty-1.1

--- checkEmpty.js
+++ checkEmpty.js
@@ -1,11 +1,10 @@
-//
 const checkEmpty = (board) => {
   for (let x = 0; x < board.length; x += 1) {
     for (let y = 0; y < board[x].length; y += 1) {
-      if (board[x][y] === 0) {
+      if (board[x][y] === '-') {
         return [x, y];
       }
     }
   }
-  return true;
+  return false;
 };
