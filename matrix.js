ef28786769ce48822ce1601c23f51eb25887ace8
Mary38Z <3_mary@bk.ru>
2024-06-14 12:13:45 +0300

    F1

--- matrix.js
+++ matrix.js
@@ -1,9 +1,16 @@
-const fs = require('fs');
+const fs = require("fs");
 
 // функция считывания txt файла
 const readFile = () => {
-   const data = fs.readFileSync('./puzzles.txt', 'utf8');
-   return data
-  };
-console.log(readFile());
+  const data = fs.readFileSync("./puzzles.txt", "utf8");
+  return data;
+};
 
+// переводим в массив
+function readFileToArr() {
+  const data = readFile().trim();
+  const arr = data.split("\n");
+  // console.log(arr);
+}
+
+readFileToArr();
