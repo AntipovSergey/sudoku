ed1f6bc5d7b1d86c26c3825159b9f1149a4e1ab8
q-zin <kuzin.esq@gmail.com>
2024-06-14 14:36:40 +0300

    добавил экспорт функции

--- utils/checkEmpty.js
+++ utils/checkEmpty.js
@@ -1,10 +1,12 @@
 const checkEmpty = (board) => {
   for (let x = 0; x < board.length; x += 1) {
     for (let y = 0; y < board[x].length; y += 1) {
-      if (board[x][y] === '-') {
+      if (board[x][y] === "-") {
         return [x, y];
       }
     }
   }
   return false;
 };
+
+module.exports = { checkEmpty };
