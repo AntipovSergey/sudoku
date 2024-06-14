5b0dd4b317eee1bffbc3a84a32292c775be715b4
OlegUrasinov <UrasinovOB@gmail.com>
2024-06-14 14:18:09 +0300

    Get Sudoku

---
+++ utils/getSudoku.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+const sudokuIndex = process.argv[2];
+
+function getSudoku() {
+    return fs.readFileSync('../puzzles.txt', 'utf-8').trim().split('\n')[sudokuIndex];
+
+}
+
