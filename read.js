c14901ae8366df6b1dad3a87ccf1899bf88ed65c
Sergey Antipov <antipovarmy@gmail.com>
2024-07-26 12:23:38 +0300

    getSet

--- read.js
+++ read.js
@@ -1,37 +1,19 @@
-const fs = require('fs')
-
-// function createBoard (){
-//   let boards = fs.readFileSync('./puzzles.txt', 'utf-8').split('\n')
-//   console.table(boards)
-//   let firstBoard = boards[0].split('')
-//   console.table(firstBoard)
-//   let board = []
-//     for (let i = 1; i <= 81; i += 9) {
-//     board.push(firstBoard.slice(i, i + 9))}
-//     console.table(board)
-//   return board
-// }
-// createBoard()
+const fs = require("fs");
 
 function createAllBoards() {
-  let allBoard = fs
-    .readFileSync('./puzzles.txt', 'utf-8')
-    .trim()
-    .split('\n');
-    console.log(allBoard) // массив со значениями из txt 
+  const allBoard = fs.readFileSync("./puzzles.txt", "utf-8").trim().split("\n");
+  console.log(allBoard); // массив со значениями из txt
 
-  let allBoards = [];
+  const allBoards = [];
   for (let i = 0; i < allBoard.length; i += 1) {
-    let board = [];
+    const board = [];
     for (let j = 1; j < 81; j += 9) {
-      board.push(allBoard[i].split('').slice(j, j + 9));
+      board.push(allBoard[i].split("").slice(j, j + 9));
     }
     allBoards.push(board);
-    console.log(allBoards.length)
   }
   return allBoards;
 }
-createAllBoards()
+createAllBoards();
 
-module.exports = createAllBoards
-
+module.exports = createAllBoards;
